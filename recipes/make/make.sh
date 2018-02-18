a32a82ae719da63331af9eb0a1a0d962ec46fc18
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-18 08:31:38 +0000

    clean up extracted dir for make recipe

--- recipes/make/make.sh
+++ recipes/make/make.sh
@@ -6,8 +6,18 @@ rdir="${rfile//.tar.bz2/}"
 rsha256="d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
 
 eval "
+function cwclean_${rname}() {
+  pushd ${cwdl} >/dev/null 2>&1
+  rm -rf "${rdir}"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwinstall_${rname}() {
   cwfetchcheck "${rurl}" "${cwdl}/${rfile}" "${rsha256}"
+  cwclean_${rname}
   cwextract "${cwdl}/${rfile}" "${cwbuild}"
+  cwclean_${rname}
 }
 "
