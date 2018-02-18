74614bae8085aa2b8a49b2610aff3a1748280943
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-18 08:17:36 +0000

    cwinstall function for assembling and building a set of known recipes

--- recipes/make/make.sh
+++ recipes/make/make.sh
@@ -8,5 +8,6 @@ rsha256="d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
 eval "
 function cwinstall_${rname}() {
   cwfetchcheck "${rurl}" "${cwdl}/${rfile}" "${rsha256}"
+  cwextract "${cwdl}/${rfile}" "${cwbuild}"
 }
 "
