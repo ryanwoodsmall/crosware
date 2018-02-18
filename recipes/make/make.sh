0ef6716153338a9579c252db6ac1291e1e72d727
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-18 08:43:30 +0000

    source profile for installs; make recipe configure

--- recipes/make/make.sh
+++ recipes/make/make.sh
@@ -14,10 +14,25 @@ function cwclean_${rname}() {
 "
 
 eval "
-function cwinstall_${rname}() {
+function cwfetch_${rname}() {
   cwfetchcheck "${rurl}" "${cwdl}/${rfile}" "${rsha256}"
+}
+"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd ${cwbuild}/${rdir} >/dev/null 2>&1
+  ./configure --prefix="${cwsw}/${rname}/${rdir}" --disable-load
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwinstall_${rname}() {
+  cwfetch_${rname}
   cwclean_${rname}
   cwextract "${cwdl}/${rfile}" "${cwbuild}"
+  cwconfigure_${rname}
   cwclean_${rname}
 }
 "
