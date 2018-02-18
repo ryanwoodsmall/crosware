aa853fd3142f9e3f1c7d4d7169d0915b0f90860b
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-18 09:08:18 +0000

    start separting out common stuff

---
+++ recipes/common.sh
@@ -0,0 +1,49 @@
+eval "
+function cwclean_${rname}() {
+  pushd ${cwbuild} >/dev/null 2>&1
+  rm -rf "${rdir}"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwfetch_${rname}() {
+  cwfetchcheck "${rurl}" "${cwdl}/${rfile}" "${rsha256}"
+}
+"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd ${cwbuild}/${rdir} >/dev/null 2>&1
+  ./configure --prefix="${cwsw}/${rname}/${rdir}"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwbuild_${rname}() {
+  pushd ${cwbuild}/${rdir} >/dev/null 2>&1
+  make -j$(($(nproc)+1))
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd ${cwbuild}/${rdir} >/dev/null 2>&1
+  make install
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwinstall_${rname}() {
+  cwfetch_${rname}
+  cwclean_${rname}
+  cwextract "${cwdl}/${rfile}" "${cwbuild}"
+  cwconfigure_${rname}
+  cwbuild_${rname}
+  cwmakeinstall_${rname}
+  cwclean_${rname}
+}
+"
--- recipes/make/make.sh
+++ recipes/make/make.sh
@@ -5,19 +5,7 @@ rfile="$(basename ${rurl})"
 rdir="${rfile//.tar.bz2/}"
 rsha256="d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
 
-eval "
-function cwclean_${rname}() {
-  pushd ${cwbuild} >/dev/null 2>&1
-  rm -rf "${rdir}"
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
-function cwfetch_${rname}() {
-  cwfetchcheck "${rurl}" "${cwdl}/${rfile}" "${rsha256}"
-}
-"
+. "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
@@ -31,18 +19,14 @@ eval "
 function cwbuild_${rname}() {
   pushd ${cwbuild}/${rdir} >/dev/null 2>&1
   ./build.sh
-  file make
   popd >/dev/null 2>&1
 }
 "
 
 eval "
-function cwinstall_${rname}() {
-  cwfetch_${rname}
-  cwclean_${rname}
-  cwextract "${cwdl}/${rfile}" "${cwbuild}"
-  cwconfigure_${rname}
-  cwbuild_${rname}
-  cwclean_${rname}
+function cwmakeinstall_${rname}() {
+  pushd ${cwbuild}/${rdir} >/dev/null 2>&1
+  ./make install-binPROGRAMS
+  popd >/dev/null 2>&1
 }
 "
