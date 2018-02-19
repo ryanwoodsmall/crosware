58839cf15b1575e76400240ffcd10d2ed5043c42
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-19 07:49:32 +0000

    actually name toybox recipe

--- recipes/toybox/toybox.sh
+++ recipes/toybox/toybox.sh
@@ -1,3 +1,4 @@
 # make defconfig HOSTCC="$CC $CFLAGS"
 # make oldconfig HOSTCC="$CC $CFLAGS"
 # CC="$CC $CFLAGS" HOSTCC="$CC $CFLAGS" CFLAGS=$CFLAGS HOSTCFLAGS=$CFLAGS LDFLAGS="$LDFLAGS" make
+rname="toybox"
