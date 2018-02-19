a9e32ed1143734c25ead6614b9e08e4174198252
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-19 08:05:44 +0000

    busybox recipe working on x86_64, not yet on arm

--- recipes/busybox/busybox.sh
+++ recipes/busybox/busybox.sh
@@ -13,9 +13,9 @@ function cwconfigure_${rname}() {
   pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
   curl -kLsO https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh
   sed -i.ORIG 's/^make/#make/g;s/^test/#test/g' bb_config_script.sh
-  make defconfig HOSTCC="\${CC} \${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS}"
+  make defconfig HOSTCC="\${CC} \${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS} -lgcc -lc -lm"
   bash bb_config_script.sh -m -s
-  make oldconfig HOSTCC="\${CC} \${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS}"
+  make oldconfig HOSTCC="\${CC} \${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS} -lgcc -lc -lm"
   popd >/dev/null 2>&1
 }
 "
@@ -23,7 +23,7 @@ function cwconfigure_${rname}() {
 eval "
 function cwmake_${rname}() {
   pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
-  make -j$(($(nproc)+1)) CC="\${CC} \${CFLAGS}" HOSTCC="\${CC} \${CFLAGS}" CFLAGS="\${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS}"
+  make -j$(($(nproc)+1)) CC="\${CC} \${CFLAGS}" HOSTCC="\${CC} \${CFLAGS}" CFLAGS="\${CFLAGS}" HOSTCFLAGS="\${CFLAGS}" HOSTLDFLAGS="\${LDFLAGS} -lgcc -lc -lm"
   popd >/dev/null 2>&1
 }
 "
