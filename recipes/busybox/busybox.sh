6da181a5e203ed090a569675191c7c402d06b54a
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-19 06:38:55 +0000

    initial busybox recipe

---
+++ recipes/busybox/busybox.sh
@@ -0,0 +1,45 @@
+rname="busybox"
+rver="1.28.1"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.bz2"
+rurl="http://${rname}.net/downloads/${rfile}"
+rsha256="98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e"
+rprof="${cwetcprofd}/${rname}.sh"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  curl -kLsO https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh
+  bash bb_config_script.sh -m -s
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  make -j$(($(nproc)+1)) CC="\${CC}" HOSTCC="\${CC}" CFLAGS="\${CFLAGS}" HOSTCFLAGS="\${CFLAGS}"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  cwmkdir "${cwsw}/${rname}/${rdir}/bin"
+  rm -f "${cwsw}/${rname}/${rdir}/bin/${rname}"
+  cp -a "${rname}" "${cwsw}/${rname}/${rdir}/bin"
+  for a in \$(./${rname} --list) ; do
+    ln -sf ${rname} "${cwsw}/${rname}/${rdir}/bin/\${a}"
+  done
+  popd >/dev/null 2>&1
+}
+"
+
+eval "                                                                                                                                                                                                 
+function cwgenprofd_${rname}() {                                                                                                                                                                       
+  echo 'append_path "${cwsw}/${rname}/current/bin"' > ${rprof}
+}                                                                                                                                                                                                      
+"
