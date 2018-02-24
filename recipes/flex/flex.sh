f3df835e52e544a7c5f538cea96b9b523c63d5cb
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-24 08:43:26 +0000

    add m4 req to flex

--- recipes/flex/flex.sh
+++ recipes/flex/flex.sh
@@ -5,7 +5,7 @@ rfile="${rdir}.tar.gz"
 rurl="https://github.com/westes/flex/releases/download/v${rver}/${rfile}"
 rsha256="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
 rprof="${cwetcprofd}/${rname}.sh"
-rreqs="make"
+rreqs="make m4"
 
 . "${cwrecipe}/common.sh"
 
