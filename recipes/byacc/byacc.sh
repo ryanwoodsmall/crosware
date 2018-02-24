6303dd5605e4ae69d3f44ecfb8e590aaac556b5f
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-24 08:53:52 +0000

    byacc recipe

---
+++ recipes/byacc/byacc.sh
@@ -0,0 +1,16 @@
+rname="byacc"
+rver="20170709"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tgz"
+rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
+rsha256="27cf801985dc6082b8732522588a7b64377dd3df841d584ba6150bc86d78d9eb"
+rprof="${cwetcprofd}/${rname}.sh"
+rreqs="make m4"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+}
+"
