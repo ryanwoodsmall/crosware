1c346ffc305c8edcc2fa904d17734c2fd0e9473c
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-24 08:28:22 +0000

    initial flex and bison recipes

---
+++ recipes/bison/bison.sh
@@ -0,0 +1,16 @@
+rname="bison"
+rver="3.0.4"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
+rsha256="b67fd2daae7a64b5ba862c66c07c1addb9e6b1b05c5f2049392cfd8a2172952e"
+rprof="${cwetcprofd}/${rname}.sh"
+rreqs="make"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+}
+"
---
+++ recipes/flex/flex.sh
@@ -0,0 +1,16 @@
+rname="flex"
+rver="2.6.4"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://github.com/westes/flex/releases/download/v${rver}/${rfile}"
+rsha256="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+rprof="${cwetcprofd}/${rname}.sh"
+rreqs="make"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+}
+"
