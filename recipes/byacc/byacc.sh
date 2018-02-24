e60194aab69ca896deb8b21df2da34a1fa147fc4
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-24 08:56:58 +0000

    add flex prereq to byacc recipe

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -5,7 +5,7 @@ rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
 rsha256="27cf801985dc6082b8732522588a7b64377dd3df841d584ba6150bc86d78d9eb"
 rprof="${cwetcprofd}/${rname}.sh"
-rreqs="make m4"
+rreqs="make m4 flex"
 
 . "${cwrecipe}/common.sh"
 
