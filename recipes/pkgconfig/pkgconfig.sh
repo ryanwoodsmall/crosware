ed09a8127990f264cb8d76d59b7515a82632f043
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 04:23:20 +0000

    use new common setup for pkgconfig

--- recipes/pkgconfig/pkgconfig.sh
+++ recipes/pkgconfig/pkgconfig.sh
@@ -4,22 +4,21 @@ rdir="pkg-config-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://pkg-config.freedesktop.org/releases/${rfile}"
 rsha256="6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "                                                                                                                                                                                                 
 function cwconfigure_${rname}() { 
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1 
+  pushd "${rbdir}" >/dev/null 2>&1 
   # sed -i.ORIG 's/-Werror=format=. //g' glib/configure
-  ./configure --prefix="${cwsw}/${rname}/${rdir}" ${cwconfigurelibopts} --with-internal-glib
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-internal-glib
   popd >/dev/null 2>&1 
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
