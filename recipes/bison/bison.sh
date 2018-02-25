cf0dce3e51a6778de01c6afeef9bd13c69053249
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 03:47:34 +0000

    use new common setup for bison

--- recipes/bison/bison.sh
+++ recipes/bison/bison.sh
@@ -4,13 +4,12 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="b67fd2daae7a64b5ba862c66c07c1addb9e6b1b05c5f2049392cfd8a2172952e"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make m4 flex perl"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
