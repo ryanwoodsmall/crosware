a53d736fbcf4f0a5d67c37ed65afa3a7e6180102
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 09:37:24 +0000

    use openssl "make install_sw" to exclude docs

--- recipes/openssl/openssl.sh
+++ recipes/openssl/openssl.sh
@@ -17,6 +17,14 @@ function cwconfigure_${rname}() {
 "
 
 eval "
+function cwmakeinstall_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  make install_sw
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
