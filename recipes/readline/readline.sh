f84a7c159c59778403f171b22f735205c73ecb0b
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 07:32:15 +0000

    readline recipe

---
+++ recipes/readline/readline.sh
@@ -0,0 +1,26 @@
+rname="readline"
+rver="7.0"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
+rsha256="750d437185286f40a369e1e4f4764eda932b9459b5ec9a731628393dd3d32334"
+rreqs="make ncurses"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-curses
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include/${rname}\"' >> \"${rprof}\"
+}
+"
