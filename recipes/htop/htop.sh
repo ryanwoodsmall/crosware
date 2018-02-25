5fa1f7e5cd6c95c529d392683a4f6d3c4436440c
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 10:16:54 +0000

    htop needs python, try to use jython instead

--- recipes/htop/htop.sh
+++ recipes/htop/htop.sh
@@ -4,9 +4,27 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://hisham.hm/${rname}/releases/${rver}/${rfile}"
 rsha256="3260be990d26e25b6b49fc9d96dbc935ad46e61083c0b7f6df413e513bf80748"
-rreqs="make ncurses"
+rreqs="make ncurses jython"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} CC=\"\${CC} \${CFLAGS} \${LDFLAGS}\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  mkdir -p buildbin
+  ln -s \$(which jython) buildbin/python
+  env PATH=\"./buildbin:\${PATH}\" make
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
