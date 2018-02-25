a44f2a1342d2fffc85de61811f5d73ce890706e2
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 08:16:03 +0000

    zlib recipe

---
+++ recipes/zlib/zlib.sh
@@ -0,0 +1,25 @@
+rname="zlib"
+rver="1.2.11"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://zlib.net/${rfile}"
+rsha256="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+rreqs="make"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} --static
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' > \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+}
+"
