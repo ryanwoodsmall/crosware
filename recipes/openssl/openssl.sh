2efd20a0e8f2f4a95fd34e5977b507db436e169c
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 09:03:28 +0000

    add ldflags/cppflags/pkgconfig stuff for openssl

--- recipes/openssl/openssl.sh
+++ recipes/openssl/openssl.sh
@@ -19,5 +19,9 @@ function cwconfigure_${rname}() {
 eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include/${rname}\"' >> \"${rprof}\"
 }
 "
