3040d0eea4876210e435dc4dff30c73343848c57
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 04:03:59 +0000

    use new common setup for busybox

--- recipes/busybox/busybox.sh
+++ recipes/busybox/busybox.sh
@@ -4,14 +4,13 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="http://${rname}.net/downloads/${rfile}"
 rsha256="98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  pushd "${rbdir}" >/dev/null 2>&1
   curl -kLsO https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh
   sed -i.ORIG 's/^make/#make/g;s/^test/#test/g' bb_config_script.sh
   make defconfig HOSTCC=\"\${CC} -static\"
@@ -23,7 +22,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  pushd "${rbdir}" >/dev/null 2>&1
   make -j$(($(nproc)+1)) CC=\"\${CC}\" HOSTCC=\"\${CC} -static\" CFLAGS=\"\${CFLAGS}\" HOSTCFLAGS=\"\${CFLAGS}\" HOSTLDFLAGS=\"\${LDFLAGS}\"
   popd >/dev/null 2>&1
 }
@@ -31,12 +30,12 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
-  cwmkdir "${cwsw}/${rname}/${rdir}/bin"
-  rm -f "${cwsw}/${rname}/${rdir}/bin/${rname}"
-  cp -a "${rname}" "${cwsw}/${rname}/${rdir}/bin"
+  pushd "${rbdir}" >/dev/null 2>&1
+  cwmkdir "${rbdir}/bin"
+  rm -f "${rbdir}/bin/${rname}"
+  cp -a "${rname}" "${rbdir}/bin"
   for a in \$(./${rname} --list) ; do
-    ln -sf "${cwsw}/${rname}/${rdir}/bin/${rname}" "${cwsw}/${rname}/${rdir}/bin/\${a}"
+    ln -sf "${rbdir}/bin/${rname}" "${rbdir}/bin/\${a}"
   done
   popd >/dev/null 2>&1
 }
@@ -44,6 +43,6 @@ function cwmakeinstall_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
