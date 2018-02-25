6f37470ce9a79e5c80a20f4b9647cc0d86921ac6
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 12:32:42 +0000

    add abcl recipe

---
+++ recipes/abcl/abcl.sh
@@ -0,0 +1,30 @@
+rname="abcl"
+rver="1.5.0"
+rdir="${rname}-bin-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://common-lisp.net/project/armedbear/releases/${rver}/${rfile}"
+rsha256="f053608efb15beab0e85857900e3e44946cb9efe68329dc99d2397e8263a0690"
+rreqs="rlwrap"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current\"' > "${rprof}"
+}
+"
+
+eval "
+function cwinstall_${rname}() {
+  cwfetch_${rname}
+  cwsourceprofile
+  cwmkdir "${rtdir}"
+  cwextract "${cwdl}/${rfile}" "${rtdir}"
+  echo '#!/bin/sh' > "${ridir}/${rname}"
+  echo "rlwrap -C ${rname} java -jar ${ridir}/${rname}.jar \\\"\\\${@}\\\"" >> "${ridir}/${rname}"
+  cwchmod "755" "${ridir}/${rname}"
+  cwlinkdir "${rdir}" "${rtdir}"
+  cwgenprofd_${rname}
+  cwmarkinstall_${rname}
+}
+"
