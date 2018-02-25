faf7deb7fdaedf0309e37ee356c5773ffacfdf0c
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 03:49:07 +0000

    use new common setup for byacc

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -4,23 +4,22 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
 rsha256="27cf801985dc6082b8732522588a7b64377dd3df841d584ba6150bc86d78d9eb"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make m4 flex"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
+  pushd "${rbdir}" >/dev/null 2>&1
   make install
-  mv "${cwsw}/${rname}/current/bin/yacc" "${cwsw}/${rname}/current/bin/byacc"
-  ln -sf "${cwsw}/${rname}/current/bin/byacc" "${cwsw}/${rname}/current/bin/yacc"
+  mv "${rtdir}/current/bin/yacc" "${rtdir}/current/bin/byacc"
+  ln -sf "${rtdir}/current/bin/byacc" "${rtdir}/current/bin/yacc"
   popd >/dev/null 2>&1
 }
 "
