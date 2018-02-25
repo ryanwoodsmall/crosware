b741fcb060c5945595e23bde4dab7ccf8d346bcb
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 07:02:15 +0000

    pkgconfig req and path append for libevent

--- recipes/libevent/libevent.sh
+++ recipes/libevent/libevent.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/release-${rver}/${rfile}"
 rsha256="965cc5a8bb46ce4199a47e9b2c9e1cae3b137e8356ffdad6d94d3b9069b71dc2"
-rreqs="make"
+rreqs="make pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
@@ -20,6 +20,7 @@ eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
 }
 "
