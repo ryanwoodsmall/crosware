2d1d4756f93d4405c049be8f0937e5f42359ec41
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 03:57:16 +0000

    use new common setup for m4

--- recipes/m4/m4.sh
+++ recipes/m4/m4.sh
@@ -4,13 +4,12 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="6640d76b043bc658139c8903e293d5978309bf0f408107146505eca701e67cf6"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
