31abefceb0c49a5262cd9054a1f143b4d45f1944
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-25 04:10:18 +0000

    use new common setup for qemacs

--- recipes/qemacs/qemacs.sh
+++ recipes/qemacs/qemacs.sh
@@ -4,15 +4,14 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://bellard.org/${rname}/${rfile}"
 rsha256="2ffba66a44783849282199acfcc08707debc7169394a8fd0902626222f27df94"
-rprof="${cwetcprofd}/${rname}.sh"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
-  ./configure --prefix="${cwsw}/${rname}/${rdir}" \
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure --prefix="${ridir}" \
     --enable-tiny \
     --disable-x11 \
     --disable-xv \
@@ -31,9 +30,9 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${cwbuild}/${rdir}" >/dev/null 2>&1
-  cwmkdir "${cwsw}/${rname}/${rdir}/bin"
-  cwmkdir "${cwsw}/${rname}/${rdir}/man/man1"
+  pushd "${rbdir}" >/dev/null 2>&1
+  cwmkdir "${ridir}/bin"
+  cwmkdir "${ridir}/man/man1"
   make install
   popd >/dev/null 2>&1
 }
@@ -41,6 +40,6 @@ function cwmakeinstall_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path "${cwsw}/${rname}/current/bin"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
