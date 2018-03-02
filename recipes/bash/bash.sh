c7d71173b5bf3628ceb9d00d94274ac51f0e2fb4
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-02 01:42:16 +0000

    static bash 4.4.18 recipe

---
+++ recipes/bash/bash.sh
@@ -0,0 +1,52 @@
+rname="bash"
+rver="4.4.18"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
+rsha256="604d9eec5e4ed5fd2180ee44dd756ddca92e0b6aa4217bbab2b6227380317f23"
+rreqs="make bison flex"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} \
+    --disable-nls \
+    --disable-separate-helpfiles \
+    --enable-readline \
+    --enable-static-link \
+    --without-bash-malloc \
+    --without-curses \
+      LDFLAGS='-static' CPPFLAGS=''
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  # ganked from alpine
+  #  https://git.alpinelinux.org/cgit/aports/tree/main/bash/APKBUILD 
+  make y.tab.c
+  make -j$(($(nproc)+1)) builtins/libbuiltins.a
+  make -j$(($(nproc)+1))
+  make strip
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  cwmkdir "${ridir}/bin"
+  install "${rname}" \"${ridir}/bin/${rname}\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+}
+"
