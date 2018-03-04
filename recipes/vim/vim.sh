11cbbe35a95e854b677fdbad344a82387fea66a8
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-04 05:46:09 +0000

    vim update to 8.0.1563, enable lua interpreter

--- recipes/vim/vim.sh
+++ recipes/vim/vim.sh
@@ -1,17 +1,24 @@
 rname="vim"
-rver="8.0.1542"
+rver="8.0.1563"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="d0818df5c6da23db725aa68067c90c80f0779c2e3446811bd1389b1ac2c2df86"
-rreqs="make ncurses"
+rsha256="c5d795b4970b751bf751002f75bb1f7f9746d1b99144f86def5e30084dce75c5"
+rreqs="make ncurses lua"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} --with-tlib=ncurses --without-local-dir --with-features=huge --without-x --enable-gui=no
+  ./configure ${cwconfigureprefix} \
+    --with-tlib=ncurses \
+    --without-local-dir \
+    --with-features=huge \
+    --without-x \
+    --enable-gui=no \
+    --with-lua-prefix=${cwsw}/lua/current \
+    --enable-luainterp=yes
   popd >/dev/null 2>&1
 }
 "
