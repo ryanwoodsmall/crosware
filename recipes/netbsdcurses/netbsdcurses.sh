281dd1312204124913842db7172e5be298b880c8
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-06 06:02:05 +0000

    netbsdcurses (netbsd-curses v0.2.1) recipe

---
+++ recipes/netbsdcurses/netbsdcurses.sh
@@ -0,0 +1,47 @@
+rname="netbsdcurses"
+rver="0.2.1"
+rdir="netbsd-curses-${rver}"
+rfile="v${rver}.tar.gz"
+rurl="https://github.com/sabotage-linux/netbsd-curses/archive/${rfile}"
+rsha256="673d4f89a7ceddb77266052103ec366ebc90436b95d9e99af3e86cb1f7b9ed8c"
+rreqs="make"
+# we want this to come after ncurses
+rprof="${cwetcprofd}/zz_${rname}.sh"
+
+. "${cwrecipe}/common.sh"
+
+#eval "
+#function cwgenprofd_${rname}() {
+#  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+#  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> "${rprof}"
+#  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> "${rprof}"
+#  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> "${rprof}"
+#}
+#"
+
+eval "
+function cwconfigure_${rname}() {
+  cwscriptecho "${rname} configure noop"
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  cd nbperf
+  make nbperf CPPFLAGS='-I..' LDFLAGS='-static'
+  cd ..
+  make all-static PREFIX="${ridir}" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static'
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  # LN=echo will disable libncurses symlinks
+  make install-static PREFIX="${ridir}" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static' LN='echo'
+  rm -f ${ridir}/lib/pkgconfig/ncurses* ${ridir}/lib/pkgconfig/*w.pc
+  popd >/dev/null 2>&1
+}
+"
