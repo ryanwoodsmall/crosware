c8d2e3245c07ec054f8fb23e522e1499c88e4ca4
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-09 07:18:14 +0000

    add gnu ed as req for bc

--- recipes/bc/bc.sh
+++ recipes/bc/bc.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="62adfca89b0a1c0164c2cdca59ca210c1d44c3ffc46daf9931cf4942664cb02a"
-rreqs="flex make ncurses readline"
+rreqs="ed flex make ncurses readline"
 
 . "${cwrecipe}/common.sh"
 
