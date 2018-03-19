276afae43784f6bf2ca4d0b2d44ff72b7663561c
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-19 19:42:09 +0000

    libgcrypt recipe

---
+++ recipes/libgcrypt/libgcrypt.sh
@@ -0,0 +1,27 @@
+rname="libgcrypt"
+rver="1.8.2"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.bz2"
+rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
+rsha256="c8064cae7558144b13ef0eb87093412380efa16c4ee30ad12ecb54886a524c07"
+rreqs="make libgpgerror"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --disable-asm \
+    --with-libgpg-error-prefix=\${cwsw}/libgpgerror/current/
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+}
+"
