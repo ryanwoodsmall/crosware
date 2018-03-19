1491778844e3f14c9efcace7b549a6d0c3d3af76
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-19 19:31:32 +0000

    libgpg-error recipe (libgpgerror)

---
+++ recipes/libgpgerror/libgpgerror.sh
@@ -0,0 +1,25 @@
+rname="libgpgerror"
+rver="1.28"
+rdir="libgpg-error-${rver}"
+rfile="${rdir}.tar.bz2"
+rurl="https://gnupg.org/ftp/gcrypt/libgpg-error/${rfile}"
+rsha256="3edb957744905412f30de3e25da18682cbe509541e18cd3b8f9df695a075da49"
+rreqs="make"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+}
+"
