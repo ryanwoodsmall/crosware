e21efcd73ba9e81a4a33e0d28d72eaec42db5292
ryan woodsmall <rwoodsmall@gmail.com>
2018-03-29 00:09:12 +0000

    set LIBINTL=MUSL in gettexttiny recipe

--- recipes/gettexttiny/gettexttiny.sh
+++ recipes/gettexttiny/gettexttiny.sh
@@ -22,6 +22,22 @@ function cwconfigure_${rname}() {
 "
 
 eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  env LIBINTL=MUSL make -j$(($(nproc)+1))
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  env LIBINTL=MUSL make install
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
