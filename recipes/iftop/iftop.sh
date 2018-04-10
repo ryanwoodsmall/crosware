02a4357fc6b08ac18c61498eafbd8a690634f2e7
ryan woodsmall <rwoodsmall@gmail.com>
2018-04-10 04:27:33 +0000

    run autoreconf in iftop recipe

--- recipes/iftop/iftop.sh
+++ recipes/iftop/iftop.sh
@@ -4,9 +4,18 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://www.ex-parrot.com/~pdw/${rname}/download/${rfile}"
 rsha256="d032547c708307159ff5fd0df23ebd3cfa7799c31536fa0aea1820318a8e0eac"
-rreqs="make ncurses libpcap"
+rreqs="make ncurses libpcap autoconf automake libtool"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  autoreconf -fiv
+  ./configure ${cwconfigureprefix}
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
