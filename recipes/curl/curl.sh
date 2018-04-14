cade8eb192bb963fe211814cbb77749380a6eb19
ryan woodsmall <rwoodsmall@gmail.com>
2018-04-14 06:39:38 +0000

    enable libssh2 support in default openssl curl recipe

--- recipes/curl/curl.sh
+++ recipes/curl/curl.sh
@@ -12,14 +12,20 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://curl.haxx.se/download/${rfile}"
 rsha256="b5920ffd6a8c95585fb95070e0ced38322790cb335c39d0dab852d12e157b5a0"
-rreqs="make zlib openssl mbedtls wolfssl"
+rreqs="make zlib openssl mbedtls wolfssl libssh2"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-zlib --without-mbedtls --without-cyassl --with-ssl --with-default-ssl-backend=openssl
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-libssh2 \
+    --with-zlib \
+    --without-mbedtls \
+    --without-cyassl \
+    --with-ssl \
+    --with-default-ssl-backend=openssl
   popd >/dev/null 2>&1
 }
 "
@@ -44,7 +50,13 @@ eval "
 function cwmakeinstall_${rname}_mbedtls() {
   pushd "${rbdir}" >/dev/null 2>&1
   make distclean
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-zlib --without-ssl --without-cyassl --with-mbedtls --with-default-ssl-backend=mbedtls
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-zlib \
+    --without-libssh2 \
+    --without-ssl \
+    --without-cyassl \
+    --with-mbedtls \
+    --with-default-ssl-backend=mbedtls
   make -j${cwmakejobs}
   install -m 0755 src/curl ${ridir}/bin/curl-mbedtls
   popd >/dev/null 2>&1
@@ -55,7 +67,13 @@ eval "
 function cwmakeinstall_${rname}_wolfssl() {
   pushd "${rbdir}" >/dev/null 2>&1
   make distclean
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-zlib --without-ssl --without-mbedtls --with-cyassl --with-default-ssl-backend=cyassl
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-zlib \
+    --without-libssh2 \
+    --without-ssl \
+    --without-mbedtls \
+    --with-cyassl \
+    --with-default-ssl-backend=cyassl
   make -j${cwmakejobs}
   install -m 0755 src/curl ${ridir}/bin/curl-wolfssl
   popd >/dev/null 2>&1
