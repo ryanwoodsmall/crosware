60e0de6c133a4796a5e2c44e93b488f0f7594dea
ryan woodsmall <rwoodsmall@gmail.com>
2018-05-12 05:24:46 +0000

    groff requires gawk; configure groff to build without docs; build groff with single make job

--- recipes/groff/groff.sh
+++ recipes/groff/groff.sh
@@ -4,9 +4,29 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="3a48a9d6c97750bfbd535feeb5be0111db6406ddb7bb79fc680809cda6d828a5"
-rreqs="make perl"
+rreqs="gawk make perl"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} \
+    --without-doc \
+    --with-doc=no \
+    --with-awk=gawk \
+    --with-alt-awk=gawk
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  make
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
