d46369c3450c8e3143b44b1fc5a1f6e59a84c519
ryan woodsmall <rwoodsmall@gmail.com>
2018-05-12 06:15:48 +0000

    add config.guess/config.sub to rogue recipe

--- recipes/rogue/rogue.sh
+++ recipes/rogue/rogue.sh
@@ -8,9 +8,16 @@ rreqs="make ncurses"
 
 . "${cwrecipe}/common.sh"
 
+# XXX - config.* stuff needs to be in cwfetch_${rname}
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
+  mv config.guess{,.ORIG}
+  mv config.sub{,.ORIG}
+  cwfetch \"http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=3d5db9ebe860\" \"${rbdir}/config.guess\"
+  cwfetch \"http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=3d5db9ebe860\" \"${rbdir}/config.sub\"
+  cwchmod 755 \"${rbdir}/config.guess\"
+  cwchmod 755 \"${rbdir}/config.sub\"
   ./configure ${cwconfigureprefix} \
     --enable-wizardmode \
     --enable-scorefile=${rtdir}/rogue.scr \
