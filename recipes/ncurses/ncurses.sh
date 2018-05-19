2a06facff6eaccc417f42014a95be560c8912424
ryan woodsmall <rwoodsmall@gmail.com>
2018-05-19 09:01:45 +0000

    use cwlinkdir_ wrapper in ncurses recipe

--- recipes/ncurses/ncurses.sh
+++ recipes/ncurses/ncurses.sh
@@ -41,7 +41,7 @@ function cwinstall_${rname}() {
   cwclean_${rname}
   cwextract_${rname}
   cwbuild_${rname}
-  cwlinkdir "$(basename ${ridir})" "${rtdir}"
+  cwlinkdir_${rname}
   cwgenprofd_${rname}
   cwmarkinstall_${rname}
   cwclean_${rname}
