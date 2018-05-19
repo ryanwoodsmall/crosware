eac8421dba017c42641ddeb7a6e986fae7bf5999
ryan woodsmall <rwoodsmall@gmail.com>
2018-05-19 09:48:02 +0000

    update byacc recipe to 20180510

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -1,9 +1,9 @@
 rname="byacc"
-rver="20170709"
+rver="20180510"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
-rsha256="27cf801985dc6082b8732522588a7b64377dd3df841d584ba6150bc86d78d9eb"
+rsha256="d0940dbffbc7e9c9dd4985c25349c390beede84ae1d9fe86b71c0aa659a6d693"
 rreqs="make m4 flex"
 
 . "${cwrecipe}/common.sh"
