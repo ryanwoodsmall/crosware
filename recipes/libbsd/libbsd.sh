b369fa1fd59b63c8221fe351fd7872e9c5789263
ryan woodsmall <rwoodsmall@gmail.com>
2018-05-28 00:06:51 +0000

    update libbsd recipe to 0.9.1 and remove funky __BEGIN_DECLS workaround

--- recipes/libbsd/libbsd.sh
+++ recipes/libbsd/libbsd.sh
@@ -1,9 +1,9 @@
 rname="libbsd"
-rver="0.8.7"
+rver="0.9.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://libbsd.freedesktop.org/releases/${rfile}"
-rsha256="f548f10e5af5a08b1e22889ce84315b1ebe41505b015c9596bad03fd13a12b31"
+rsha256="56d835742327d69faccd16955a60b6dcf30684a8da518c4eca0ac713b9e0a7a4"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
@@ -11,14 +11,8 @@ rreqs="make"
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG '/__GLIBC_PREREQ/ s|^#if |#if 1 //|g' include/bsd/string.h include/bsd/stdlib.h
-  for h in \$(egrep -rl '__(BEGIN|END)_DECLS' include | grep -v '/cdefs\.h$') ; do
-    echo "attempting to fix up \${h}"
-    cat \${h} > \${h}.ORIG
-    sed '/^#ifndef __BEGIN_DECLS/,/^$/!d' include/bsd/sys/cdefs.h > \${h}
-    cat \${h}.ORIG >> \${h}
-  done
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS='-static' CPPFLAGS=''
+  echo '#include <fcntl.h>' >> config.h
   popd >/dev/null 2>&1
 }
 "
