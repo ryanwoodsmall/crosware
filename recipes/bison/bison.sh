3a235985cd7a84847c0cf34c6fc7acf8cdf387d7
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-16 02:29:58 +0000

    update bison recipe to 3.0.5

--- recipes/bison/bison.sh
+++ recipes/bison/bison.sh
@@ -1,9 +1,9 @@
 rname="bison"
-rver="3.0.4"
+rver="3.0.5"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.gz"
+rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="b67fd2daae7a64b5ba862c66c07c1addb9e6b1b05c5f2049392cfd8a2172952e"
+rsha256="075cef2e814642e30e10e8155e93022e4a91ca38a65aa1d5467d4e969f97f338"
 rreqs="make m4 flex perl"
 
 . "${cwrecipe}/common.sh"
