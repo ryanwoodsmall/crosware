7775751cc058ddcbf61ff245e3dc3e14e127b4db
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-21 04:10:26 +0000

    require and use gnu sed in cflow recipe

--- recipes/cflow/cflow.sh
+++ recipes/cflow/cflow.sh
@@ -4,9 +4,17 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="a33a89303b4cfbf7135589f15123f451ee79f69260f0c7f32d387b40e4967dfe"
-rreqs="make"
+rreqs="make sed"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  env PATH=\"${cwsw}/sed/current/bin:\${PATH}\" make -j${cwmakejobs}
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
