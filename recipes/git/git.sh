6d42cb4bbdf998fcccd81a4f651ca97a36177b0a
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-22 05:09:21 +0000

    update git recipe to 2.18.0

--- recipes/git/git.sh
+++ recipes/git/git.sh
@@ -1,9 +1,9 @@
 rname="git"
-rver="2.16.4"
+rver="2.18.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.kernel.org/pub/software/scm/${rname}/${rfile}"
-rsha256="6e69b0e9c487e5da52a14d4829f0b6a28b2c18a0bb6fb67c0dc8b5b5658bd532"
+rsha256="8b40be383a603147ae29337136c00d1c634bdfdc169a30924a024596a7e30e92"
 rreqs="make bzip2 zlib openssl curl expat pcre2 perl gettexttiny libssh2"
 
 . "${cwrecipe}/common.sh"
