f4705b46f12c4825ef3d49e2966d1dc8c3dd825b
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-23 04:22:09 +0000

    make gsed require toybox and prepend it during build since system sed (chrome os) might have default "sandboxing" - just wonderful

--- recipes/sed/sed.sh
+++ recipes/sed/sed.sh
@@ -4,14 +4,22 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="7aad73c8839c2bdadca9476f884d2953cdace9567ecd0d90f9959f229d146b40"
-rreqs="make"
+rreqs="make toybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} --program-prefix=g
+  env PATH=\"${cwsw}/toybox/current/bin:\${PATH}\" ./configure ${cwconfigureprefix} --program-prefix=g
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  env PATH=\"${cwsw}/toybox/current/bin:\${PATH}\" make -j${cwmakejobs}
   popd >/dev/null 2>&1
 }
 "
@@ -19,7 +27,7 @@ function cwconfigure_${rname}() {
 eval "
 function cwmakeinstall_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  make install
+  env PATH=\"${cwsw}/toybox/current/bin:\${PATH}\" make install
   ln -sf g${rname} ${ridir}/bin/${rname}
   popd >/dev/null 2>&1
 }
