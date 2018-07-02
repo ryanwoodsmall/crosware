3c1e3a3d9314fff0fd837577638e30a513df9279
ryan woodsmall <rwoodsmall@gmail.com>
2018-07-02 07:19:59 +0000

    cppcheck: include pcre support for rules, include config dir, set NDEBUG flag in CXXFLAGS

--- recipes/cppcheck/cppcheck.sh
+++ recipes/cppcheck/cppcheck.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/danmar/${rname}/archive/${rfile}"
 rsha256="aaa6293d91505fc6caa6982ca3cd2d949fa1aac603cabad072b705fdda017fc5"
-rreqs="make"
+rreqs="make pcre"
 
 . "${cwrecipe}/common.sh"
 
@@ -14,20 +14,32 @@ function cwconfigure_${rname}() {
 }
 "
 
-eval "            
+eval "
 function cwmake_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  make -j${cwmakejobs} CC=\"\${CC}\" CXX=\"\${CXX}\" CPPFLAGS=\"\" CFLAGS=\"-Wl,-static -fPIC\" CXXFLAGS=\"-Wl,-static -fPIC\" LDFLAGS=\"-static\"
+  env PATH=\"${cwsw}/pcre/current/bin:\${PATH}\" make -j${cwmakejobs} \
+    PREFIX=\"${ridir}\" \
+    CFGDIR=\"${ridir}/cfg\" \
+    HAVE_RULES=yes \
+    CC=\"\${CC}\" \
+    CXX=\"\${CXX}\" \
+    CPPFLAGS=\"\" \
+    CFLAGS=\"-Wl,-static -fPIC\" \
+    CXXFLAGS=\"-Wl,-static -fPIC -DNDEBUG\" \
+    LDFLAGS=\"-static\"
   popd >/dev/null 2>&1
-}                                
+}
 "
 
-eval "            
+eval "
 function cwmakeinstall_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  make install PREFIX=\"${ridir}\"
+  env PATH=\"${cwsw}/pcre/current/bin:\${PATH}\" make install \
+    PREFIX=\"${ridir}\" \
+    CFGDIR=\"${ridir}/cfg\"
+    HAVE_RULES=yes
   popd >/dev/null 2>&1
-}                                
+}
 "
 
 eval "
