0f363de847e5ec7c091cc75fe3153e99d2b9c9f0
ryan woodsmall <rwoodsmall@gmail.com>
2018-07-02 03:41:20 +0000

    python2 recipe: require gdbm, sqlite, bdb47; set dbm order to gdb:bdb47; make sure -fPIC is set for cc/c++; disable /usr/include path in setup.py

--- recipes/python2/python2.sh
+++ recipes/python2/python2.sh
@@ -17,17 +17,22 @@ rdir="Python-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.python.org/ftp/python/${rver}/${rfile}"
 rsha256="22d9b1ac5b26135ad2b8c2901a9413537e08749a753356ee913c84dbd2df5574"
-rreqs="make bzip2 zlib ncurses readline openssl"
+rreqs="make bzip2 zlib ncurses readline openssl gdbm sqlite bdb47"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-ensurepip=install LDFLAGS=\"\${LDFLAGS//-static/}\" CPPFLAGS=\"\${CPPFLAGS}\" CFLAGS='' CXXFLAGS=''
-  echo > Modules/Setup.local
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-ensurepip=install \
+    --with-dbmliborder=gdbm:bdb \
+    LDFLAGS=\"\${LDFLAGS//-static/}\" CPPFLAGS=\"\${CPPFLAGS}\" CFLAGS='-fPIC' CXXFLAGS='-fPIC'
+  echo >> Modules/Setup.local
   echo 'readline readline.c -lreadline -lncurses' >> Modules/Setup.local
   echo '_ssl _ssl.c -DUSE_SSL -lssl -lcrypto -lz' >> Modules/Setup.local
+  #echo \"dbm dbmmodule.c -lgdbm_compat -I${cwsw}/gdbm/current/include\" >> Modules/Setup.local
+  sed -i.ORIG 's#/usr/include#/no/usr/include#g' setup.py
   popd >/dev/null 2>&1
 }
 "
