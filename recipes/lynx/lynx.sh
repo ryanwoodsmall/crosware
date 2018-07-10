6ea49e892767751b9c8adb7921568e765637fa15
ryan woodsmall <rwoodsmall@gmail.com>
2018-07-10 15:18:57 +0000

    lynx: update recipe to version 2.8.9rel.1

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -1,9 +1,9 @@
 rname="lynx"
-rver="2.8.9pre.1"
+rver="2.8.9rel.1"
 rdir="${rname}${rver}"
 rfile="${rname}${rver}.tar.bz2"
 rurl="ftp://ftp.invisible-island.net/${rname}/tarballs/${rfile}"
-rsha256="a02267765a7677ffa77bf950b608dfb8d428080d7cf4311d59d1c0c57abe9ce1"
+rsha256="387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
 rreqs="make bzip2 slang ncurses openssl zlib"
 
 . "${cwrecipe}/common.sh"
