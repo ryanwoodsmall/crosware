f424881bce3978417b22c53704304e9e9276db95
ryan woodsmall <rwoodsmall@gmail.com>
2018-07-27 15:36:40 +0000

    jq: disable maintainer mode, req byacc instead of bison (neither are really needed, nor is flex, but, you know)

--- recipes/jq/jq.sh
+++ recipes/jq/jq.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/stedolan/${rname}/releases/download/${rdir}/${rfile}"
 rsha256="c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c"
-rreqs="make bison flex oniguruma"
+rreqs="make byacc flex oniguruma"
 
 . "${cwrecipe}/common.sh"
 
@@ -14,6 +14,7 @@ function cwconfigure_${rname}() {
   env PATH=\"${cwsw}/bison/current/bin:${cwsw}/flex/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
       --disable-docs \
+      --disable-maintainer-mode \
       --disable-valgrind \
       --enable-all-static \
       --with-oniguruma=\"${cwsw}/oniguruma/current\"
