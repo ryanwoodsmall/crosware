d11620d81c99dc30a913ac0e32df9b5fe798518b
ryan woodsmall <rwoodsmall@gmail.com>
2018-08-01 05:15:27 +0000

    iperf: update to 2.0.12

--- recipes/iperf/iperf.sh
+++ recipes/iperf/iperf.sh
@@ -1,9 +1,9 @@
 rname="iperf"
-rver="2.0.10"
+rver="2.0.12"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://sourceforge.net/projects/iperf2/files/${rfile}"
-rsha256="7fe4348dcca313b74e0aa9c34a8ccd713b84a5615b8578f4aa94cedce9891ef2"
+rsha256="367f651fb1264b13f6518e41b8a7e08ce3e41b2a1c80e99ff0347561eed32646"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
