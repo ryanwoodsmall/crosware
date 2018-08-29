e669c65f03b520a8635b0daea129a90bcc8edc17
ryan woodsmall <rwoodsmall@gmail.com>
2018-08-29 22:49:09 +0000

    byacc: do not require m4 or flex

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
 rsha256="5bbb0b3ec3da5981a2488383b652499d6c1e0236b47d8bac5fcdfa12954f749c"
-rreqs="make m4 flex"
+rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
