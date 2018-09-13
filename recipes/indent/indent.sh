e2f85498587fe3c15ac1f8a06ffb503855819de5
ryan woodsmall <rwoodsmall@gmail.com>
2018-09-13 20:21:05 +0000

    indent: update to 2.2.12, workaround -Werror mess, require and use slibtool, etc.

--- recipes/indent/indent.sh
+++ recipes/indent/indent.sh
@@ -1,13 +1,10 @@
 rname="indent"
-rver="2.2.10"
-#rver="2.2.11"
+rver="2.2.12"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="8a9b41be5bfcab5d8c1be74204b10ae78789fc3deabea0775fdced8677292639"
-#rurl="http://dev.alpinelinux.org/archive/${rname}/${rfile}"
-#rsha256="aaff60ce4d255efb985f0eb78cca4d1ad766c6e051666073050656b6753a0893"
-rreqs="make sed flex configgit"
+rsha256="e77d68c0211515459b8812118d606812e300097cfac0b4e9fb3472664263bb8b"
+rreqs="make sed flex configgit texinfo gettexttiny slibtool bison"
 
 . "${cwrecipe}/common.sh"
 
@@ -22,7 +19,29 @@ function cwconfigure_${rname}() {
   cd ..
   sed -i.ORIG 's/GNUC/GNUC_OFF/g' src/lexi.c
   sed -i.ORIG '/SUBDIRS/ s/doc//g' Makefile.in Makefile.am
-  ./configure ${cwconfigureprefix} --disable-nls --without-included-gettext
+  sed -i.ORIG 's/-Werror//g' src/Makefile.{in,am}
+  ./configure ${cwconfigureprefix} \
+    --disable-nls \
+    --without-included-gettext \
+    gl_cv_cc_vis_werror=no \
+    LIBTOOL=\"${cwsw}/slibtool/current/bin/slibtool-static -all-static\"
+  echo '#include <locale.h>' >> config.h
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make -j${cwmakejobs} LIBTOOL=\"${cwsw}/slibtool/current/bin/slibtool-static -all-static\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make install LIBTOOL=\"${cwsw}/slibtool/current/bin/slibtool-static -all-static\"
   popd >/dev/null 2>&1
 }
 "
