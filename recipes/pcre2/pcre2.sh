d0602505c77b83c539bd6e2e830792c9e0483887
ryan woodsmall <rwoodsmall@gmail.com>
2018-09-14 01:19:43 +0000

    pcre2: update to 10.32

--- recipes/pcre2/pcre2.sh
+++ recipes/pcre2/pcre2.sh
@@ -1,9 +1,9 @@
 rname="pcre2"
-rver="10.31"
+rver="10.32"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://ftp.pcre.org/pub/${rname//2/}/${rfile}"
-rsha256="e07d538704aa65e477b6a392b32ff9fc5edf75ab9a40ddfc876186c4ff4d68ac"
+rsha256="f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e"
 rreqs="make zlib bzip2 readline"
 
 . "${cwrecipe}/common.sh"
