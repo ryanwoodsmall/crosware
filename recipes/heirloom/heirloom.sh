7b698e48b9b0a031a8a4005020da007fc208f886
ryan woodsmall <rwoodsmall@gmail.com>
2018-09-18 05:19:49 +0000

    heirloom: update to 20180918-musl (with include ex/vi)

--- recipes/heirloom/heirloom.sh
+++ recipes/heirloom/heirloom.sh
@@ -9,34 +9,15 @@
 #   - use zz_00_heirloom.sh to come before 9base?
 #
 rname="heirloom"
-rver="20180817-musl"
+rver="20180918-musl"
 rdir="${rname}-project-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/ryanwoodsmall/${rname}-project/archive/${rfile}"
-rsha256="289f583dda2f70edbe4cc4fd29529cf59cb384b28b0b3529a724293ebbe468ad"
+rsha256="7c6d2ad7e99b9f312120f9c087f94086ace29788c41a6ca9ae5c077c76270848"
 rreqs="make sed ncurses zlib bzip2 flex bison ed"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
 . "${cwrecipe}/common.sh"
-
-eval "
-function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${cwdl}/${rname}/${rfile}\" \"${rsha256}\"
-  cwfetchcheck \"${rurl//-project/-ex-vi}\" \"${cwdl}/${rname}/${rname}-ex-vi-${rfile}\" \"4339790d0b7ba545bb2646c2f0d67c06fd2c2834578d215b282d26c1c19297f5\"
-}
-"
-
-eval "
-function cwextract_${rname}() {
-  local exvidir=\"${rdir//-project/-ex-vi}\"
-  cwextract \"${cwdl}/${rname}/${rfile}\" \"${cwbuild}\"
-  cwextract \"${cwdl}/${rname}/${rname}-ex-vi-${rfile}\" \"${rbdir}\"
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  rm -rf ${rname}-ex-vi
-  mv \${exvidir} ${rname}-ex-vi
-  popd >/dev/null 2>&1
-}
-"
 
 eval "
 function cwconfigure_${rname}() {
@@ -51,8 +32,8 @@ eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   local d=''
-  for d in '' -{sh,devtools,doctools,ex-vi} ; do
-    pushd ${rname}\${d}
+  for d in ${rname}{,-{sh,devtools,doctools,ex-vi}} ; do
+    pushd \${d}
     make
     make install
     popd
