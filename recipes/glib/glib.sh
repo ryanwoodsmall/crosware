059846c110e392e5424204fbdd87fb80c9b5f32a
ryan woodsmall <rwoodsmall@gmail.com>
2018-09-27 05:26:54 +0000

    glib: update to 2.56.3

--- recipes/glib/glib.sh
+++ recipes/glib/glib.sh
@@ -1,9 +1,9 @@
 rname="glib"
-rver="2.56.2"
+rver="2.56.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="http://ftp.gnome.org/pub/gnome/sources/glib/2.56/${rfile}"
-rsha256="d64abd16813501c956c4e123ae79f47f1b58de573df9fdd3b0795f1e2c1aa789"
+rsha256="a9a4c5b4c81b6c75bc140bdf5e32120ef3ce841b7413214ecf5f987acec74cb2"
 rreqs="gettexttiny libffi make perl pkgconfig python2 zlib"
 
 . "${cwrecipe}/common.sh"
