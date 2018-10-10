56e326603489fe8cc8798b5931707be2d5d1e523
ryan woodsmall <rwoodsmall@gmail.com>
2018-10-10 05:50:24 +0000

    git: update to 2.58.1

--- recipes/glib/glib.sh
+++ recipes/glib/glib.sh
@@ -1,23 +1,30 @@
 rname="glib"
-rver="2.56.3"
+rver="2.58.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
-rurl="http://ftp.gnome.org/pub/gnome/sources/glib/2.56/${rfile}"
-rsha256="a9a4c5b4c81b6c75bc140bdf5e32120ef3ce841b7413214ecf5f987acec74cb2"
-rreqs="gettexttiny libffi make perl pkgconfig python2 zlib"
+rurl="http://ftp.gnome.org/pub/gnome/sources/glib/${rver%.*}/${rfile}"
+rsha256="97d6a9d926b6aa3dfaadad3077cfb43eec74432ab455dff14250c769d526d7d6"
+rreqs="gettexttiny libffi make perl pkgconfig python2 zlib autoconf automake libtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
+  cat > gtk-doc.make <<EOF
+EXTRA_DIST =
+CLEANFILES =
+EOF
+  touch INSTALL
+  env PATH=\"${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:${PATH}\" \
+    autoreconf -fiv -I${cwsw}/libtool/current/share/aclocal -I${cwsw}/pkgconfig/current/share/aclocal
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --with-pcre=internal \
     --disable-libmount \
     --disable-gtk-doc \
     --disable-compile-warnings \
     --disable-fam \
-    --with-python=python2.7
+    --with-python=\"${cwsw}/python2/current/bin/python2.7\"
   popd >/dev/null 2>&1
 }
 "
