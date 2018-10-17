41b9bce241cdd5d63047b638405166ddb21e6a55
ryan woodsmall <rwoodsmall@gmail.com>
2018-10-17 23:39:48 +0000

    tmux: update to 2.8

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -1,9 +1,9 @@
 rname="tmux"
-rver="2.7"
+rver="2.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
-rsha256="9ded7d100313f6bc5a87404a4048b3745d61f2332f99ec1400a7c4ed9485d452"
+rsha256="7f6bf335634fafecff878d78de389562ea7f73a7367f268b66d37ea13617a2ba"
 rreqs="make libevent ncurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
