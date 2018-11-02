5f2c565c5db2338d74ba5086ee5e2ce09d9216a4
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-02 02:13:10 +0000

    slibtool: update to 0.5.26

--- recipes/slibtool/slibtool.sh
+++ recipes/slibtool/slibtool.sh
@@ -1,9 +1,9 @@
 rname="slibtool"
-rver="0.5.25"
+rver="0.5.26"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/midipix-project/${rname}/archive/${rfile}"
-rsha256="351b19e60fd45be592de05dc766d6ae100fa727eb2e890ccd1a2bec7de41e1ea"
+rsha256="0ce89a6b3ad3a75dd066b4bb7d089addc7e0db3fbbf9b6ea86495c95cc9989e0"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
