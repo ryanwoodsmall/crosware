19ca9091ce75f36694c5a5d4ba8d2bddeb335575
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-10 00:43:54 +0000

    bison: update to 3.2.1; require gawk

--- recipes/bison/bison.sh
+++ recipes/bison/bison.sh
@@ -1,10 +1,10 @@
 rname="bison"
-rver="3.2"
+rver="3.2.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="deec377b95aa72ec4e1a33fe2c938d2480749d740b5291a7cc1d77808d3710bf"
-rreqs="make m4 flex perl sed"
+rsha256="8ba8bd5d6e935d01b89382fa5c2fa7602e03bbb435575087bfdc3c450d4d9ecd"
+rreqs="make m4 flex perl sed gawk"
 
 . "${cwrecipe}/common.sh"
 
