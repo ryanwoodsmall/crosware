492986aac88337387f5b427ca9e94e7b41d9288e
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-15 08:04:04 +0000

    jython: require bash and busybox, make sure JAVA_HOME is set, force use of full bash 4 and busybox userspace to avoid python2.7 jython.py wrapper

--- recipes/jython/jython.sh
+++ recipes/jython/jython.sh
@@ -5,6 +5,7 @@ rfile="$(basename ${rurl})"
 rdir="${rname}-${rver}"
 rsha256="6e58dad0b8565b95c6fb14b4bfbf570523d1c5290244cfb33822789fa53b1d25"
 rprof="${cwetcprofd}/zz_${rname}.sh"
+rreqs="bash busybox"
 
 . "${cwrecipe}/common.sh"
 
@@ -12,7 +13,9 @@ eval "
 function cwmakeinstall_${rname}() {
   unset JYTHON_HOME
   test -e \"${ridir}\" && mv \"${ridir}\"{,.PRE-${TS}}
-  env PATH=\"\$(echo \${PATH} | tr : '\n' | egrep -v '/(j|p)ython' | xargs echo | tr ' ' :)\" java -jar \"${cwdl}/${rname}/${rfile}\" -s -t all -d \"${ridir}\"
+  env \
+    PATH=\"${cwsw}/bash/current/bin:${cwsw}/busybox/current/bin\" \
+    \"\${JAVA_HOME}/bin/java\" -jar \"${cwdl}/${rname}/${rfile}\" -s -t all -d \"${ridir}\"
 }
 "
 
@@ -25,7 +28,13 @@ function cwgenprofd_${rname}() {
 
 eval "
 function cwinstall_${rname}() {
+  : \${JAVA_HOME:=\"\"}
+  if [ -z \"\${JAVA_HOME}\" ] ; then
+    cwscriptecho \"JAVA_HOME not set, cannot install ${rname}\"
+    return
+  fi
   cwfetch_${rname}
+  cwcheckreqs_${rname}
   cwsourceprofile
   cwmakeinstall_${rname}
   cwlinkdir_${rname}
