d842f7c2d6077cda92d7e311b026980fc5337615
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-16 00:22:58 +0000

    jruby: update to 9.2.4.0

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="9.2.0.0"
+rver="9.2.4.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/${rname}/${rname}-dist/${rver}/${rfile}"
-rsha256="42718dea5fc90b7696cb3fccf8e8d546729173963ad0bc477d66545677d00684"
+rsha256="b9638c82c85d89f6e8b2da1b876ac235bb9ed47f2163b3c851f0496c9bd58a0c"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
