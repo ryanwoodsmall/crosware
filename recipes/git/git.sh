4ab557dbd9122bdc12dfc89629cc77dd33c92784
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-27 01:38:24 +0000

    git: update to 2.19.2

--- recipes/git/git.sh
+++ recipes/git/git.sh
@@ -1,9 +1,9 @@
 rname="git"
-rver="2.19.1"
+rver="2.19.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.kernel.org/pub/software/scm/${rname}/${rfile}"
-rsha256="345056aa9b8084280b1b9fe1374d232dec05a34e8849028a20bfdb56e920dbb5"
+rsha256="fce9a3a3297db5f3756c4553a2fc1fec209ee08178f8491e76ff4ff8fe7b8be9"
 rreqs="make bzip2 zlib openssl curl expat pcre2 perl gettexttiny libssh2"
 
 . "${cwrecipe}/common.sh"
@@ -11,7 +11,7 @@ rreqs="make bzip2 zlib openssl curl expat pcre2 perl gettexttiny libssh2"
 eval "
 function cwfetch_${rname}() {
   cwfetchcheck \"${rurl}\" \"${cwdl}/${rname}/${rfile}\" \"${rsha256}\"
-  cwfetchcheck \"${rurl//${rname}-${rver}/${rname}-manpages-${rver}}\" \"${cwdl}/${rname}/${rfile//${rname}-${rver}/${rname}-manpages-${rver}}\" \"27af909c7a43ffc8b1736af19a6a68d8a5b177963ec4ddd2b2f9f0ed53bcc6ee\"
+  cwfetchcheck \"${rurl//${rname}-${rver}/${rname}-manpages-${rver}}\" \"${cwdl}/${rname}/${rfile//${rname}-${rver}/${rname}-manpages-${rver}}\" \"25fd8ba1914f5859b59f72d6c6aa2324abe84891e071adae2195faa526a510eb\"
 }
 "
 
