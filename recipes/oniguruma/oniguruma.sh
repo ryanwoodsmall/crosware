667706586b4a1f25e702000b78206760d58ccf62
ryan woodsmall <rwoodsmall@gmail.com>
2018-12-16 02:32:10 +0000

    oniguruma: update to 6.9.1

--- recipes/oniguruma/oniguruma.sh
+++ recipes/oniguruma/oniguruma.sh
@@ -1,9 +1,9 @@
 rname="oniguruma"
-rver="6.9.0"
+rver="6.9.1"
 rdir="onig-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/kkos/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="91bfb25e050ce3c301483204dd3f0d03a7c05472e20d48fe227a383d4534e7c9"
+rsha256="c7c3feb7be45a5cc9f2dec239b4a317a422e6ffea299cf91ffab1b926633ea12"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
