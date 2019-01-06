5de6fda6d753e1815a242c6e0b6471bd39303446
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-06 00:53:33 +0000

    lunzip: update to 1.11

--- recipes/lzip/lunzip.sh
+++ recipes/lzip/lunzip.sh
@@ -1,9 +1,9 @@
 rname="lunzip"
-rver="1.10"
+rver="1.11"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://download.savannah.gnu.org/releases/lzip/${rname}/${rfile}"
-rsha256="7416393bac6a67460c55fe97075d2720f3be0747b01c74033dba6b6bf24a85c7"
+rsha256="21ad5f051cc516c5fc05cb6dfc9bb66737d9810cfa9349709e42c545eb1bf8a7"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
