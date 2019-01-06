4268a2565a602c7f6dd0b8dff34d5b92b3272e10
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-06 02:03:48 +0000

    pdlzip: update to 1.10

--- recipes/lzip/pdlzip.sh
+++ recipes/lzip/pdlzip.sh
@@ -1,9 +1,9 @@
 rname="pdlzip"
-rver="1.9"
+rver="1.10"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://download.savannah.gnu.org/releases/lzip/${rname}/${rfile}"
-rsha256="922b6968c520ee529edb54807d75a0cb4622717656a82cc888a193f3a9042e54"
+rsha256="c1545fbea02c8c127ee97f0a7f8c0bcb076f2d8cb0ad902720dddcead5517750"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
