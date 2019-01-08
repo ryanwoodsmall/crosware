f133685b4ce16e32a9718019cefcf20f83d2fd03
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-08 04:08:01 +0000

    bash: recreate cleaner bash recipe for 5.x release

---
+++ recipes/bash/bash.sh
@@ -0,0 +1,40 @@
+rname="bash"
+rver="5.0"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
+rsha256="b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
+rreqs="make bison flex sed"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} \
+    --disable-nls \
+    --disable-separate-helpfiles \
+    --enable-readline \
+    --enable-static-link \
+    --without-bash-malloc \
+    --without-curses \
+      LDFLAGS='-static' CPPFLAGS=''
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make install
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/${rname}5\"
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/sh\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+}
+"
