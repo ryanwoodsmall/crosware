de3b4aff6f376b716dcf63f6cf90cad33d507c0f
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-13 01:20:40 +0000

    libatomicops: update to 7.6.8

--- recipes/libatomicops/libatomicops.sh
+++ recipes/libatomicops/libatomicops.sh
@@ -1,9 +1,9 @@
 rname="libatomicops"
-rver="7.6.6"
+rver="7.6.8"
 rdir="libatomic_ops-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ivmai/libatomic_ops/releases/download/v${rver}/${rfile}"
-rsha256="99feabc5f54877f314db4fadeb109f0b3e1d1a54afb6b4b3dfba1e707e38e074"
+rsha256="1d6a279edf81767e74d2ad2c9fce09459bc65f12c6525a40b0cb3e53c089f665"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
