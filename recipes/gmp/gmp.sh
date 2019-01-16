b28ec0ee884c4a3250bbed9b3bfac772298036bd
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-16 02:52:17 +0000

    gmp: fix etc/profile.d/gmp.sh

--- recipes/gmp/gmp.sh
+++ recipes/gmp/gmp.sh
@@ -18,6 +18,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' > \"${rprof}\"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
 }
 "
