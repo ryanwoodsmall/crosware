2352c25ac3958fbc40c098cb60dec032807625d7
ryan woodsmall <rwoodsmall@gmail.com>
2019-01-24 22:56:43 +0000

    show-recent-gnu-ftp-files.sh: add script so i can stop looking at ftp.gnu.org obessively

---
+++ scripts/show-recent-gnu-ftp-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# shows a list of recently updated (this month) files on ftp.gnu.org
+#
+# awk would be better here but who has the time?
+#
+
+set -eu
+
+ftpgnuls="https://ftp.gnu.org/ls-lrRt.txt.gz"
+month="$(date '+%b')"
+
+curl -kLs "${ftpgnuls}" \
+| gzip -dc \
+| egrep -- '(^-|:$)' \
+| tr '\t' ' ' \
+| tr -s ' ' \
+| cut -f6- -d' ' \
+| egrep "(^${month} .*:|^/.*:$)" \
+| grep -B1 "${month} " \
+| grep -v -- '^--$'
