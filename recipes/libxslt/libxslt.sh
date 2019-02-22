9841615f85fb743c1d0c8f9f5e4b2ce9961b7d1d
ryan woodsmall <rwoodsmall@gmail.com>
2019-02-22 01:43:40 +0000

    libxslt: update to 1.1.33

--- recipes/libxslt/libxslt.sh
+++ recipes/libxslt/libxslt.sh
@@ -1,9 +1,9 @@
 rname="libxslt"
-rver="1.1.32"
+rver="1.1.33"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="ftp://xmlsoft.org/${rname}/${rfile}"
-rsha256="526ecd0abaf4a7789041622c3950c0e7f2c4c8835471515fd77eec684a355460"
+rsha256="8e36605144409df979cab43d835002f63988f3dc94d5d3537c12796db90e38c8"
 rreqs="make libgcrypt libxml2 slibtool xz zlib"
 
 . "${cwrecipe}/common.sh"
