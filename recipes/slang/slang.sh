f468c5ef3a5bf1b460cc8cfb256550a02f23a9b1
ryan woodsmall <rwoodsmall@gmail.com>
2019-03-13 07:36:31 +0000

    slang: require ncurses, note about switching to netbsdcurses

--- recipes/slang/slang.sh
+++ recipes/slang/slang.sh
@@ -3,6 +3,7 @@
 #  oniguruma: --with-onig
 #  pcre: --with-pcre
 #  zlib: --with-z
+# XXX - switch over to netbsdcurses
 #
 rname="slang"
 rver="2.3.2"
@@ -10,7 +11,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://www.jedsoft.org/releases/${rname}/${rfile}"
 rsha256="fc9e3b0fc4f67c3c1f6d43c90c16a5c42d117b8e28457c5b46831b8b5d3ae31a"
-rreqs="make zlib"
+rreqs="make zlib ncurses"
 
 . "${cwrecipe}/common.sh"
 
