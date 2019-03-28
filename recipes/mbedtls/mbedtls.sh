2c7f4437a0488dab640416e4592e5f93bc7ef475
ryan woodsmall <rwoodsmall@gmail.com>
2019-03-28 00:14:49 +0000

    mbedtls: update to 2.16.1

--- recipes/mbedtls/mbedtls.sh
+++ recipes/mbedtls/mbedtls.sh
@@ -3,11 +3,11 @@
 #
 
 rname="mbedtls"
-rver="2.16.0"
+rver="2.16.1"
 rdir="${rname}-${rname}-${rver}"
 rfile="${rname}-${rver}.tar.gz"
 rurl="https://github.com/ARMmbed/${rname}/archive/${rfile}"
-rsha256="cd14ff7b422eac01516973e41252c67c274814783b32fafb91a63ebe946beaff"
+rsha256="daf0d40f3016c34eb42d1e4b3b52be047e976d566aba8668977723c829af72f3"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
