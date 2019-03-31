50cb02eb37f3f2dad11fee383e343dc99fb3d132
ryan woodsmall <rwoodsmall@gmail.com>
2019-03-31 08:51:49 +0000

    lynx: use https invisible mirror instead of ftp; curl is bailing out with a "server refused to chdir" or some such

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -2,7 +2,7 @@ rname="lynx"
 rver="2.8.9rel.1"
 rdir="${rname}${rver}"
 rfile="${rname}${rver}.tar.bz2"
-rurl="ftp://ftp.invisible-island.net/${rname}/tarballs/${rfile}"
+rurl="https://invisible-mirror.net/archives/${rname}/tarballs/${rfile}"
 rsha256="387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
 rreqs="make bzip2 ncurses slang openssl zlib"
 
