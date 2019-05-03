17cc655e8b44fc95584b156173e82dcf2dfc72f7
ryan woodsmall <rwoodsmall@gmail.com>
2019-05-03 03:00:30 +0000

    tmux: update to 2.9a

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -6,11 +6,11 @@
 #
 
 rname="tmux"
-rver="2.9"
+rver="2.9a"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
-rsha256="34901232f486fd99f3a39e864575e658b5d49f43289ccc6ee57c365f2e2c2980"
+rsha256="839d167a4517a6bffa6b6074e89a9a8630547b2dea2086f1fad15af12ab23b25"
 rreqs="make libevent ncurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
