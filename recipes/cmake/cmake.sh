465408eb046a02eafa5602acdf766960f381369c
ryan woodsmall <rwoodsmall@gmail.com>
2019-05-26 03:50:36 +0000

    cmake: ignore system /usr/include dir, severely limit path, disable cpp/ld/pkg-config vars

--- recipes/cmake/cmake.sh
+++ recipes/cmake/cmake.sh
@@ -9,19 +9,24 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/Kitware/CMake/releases/download/v${rver}/${rfile}"
 rsha256="00b4dc9b0066079d10f16eed32ec592963a44e7967371d2f5077fd1670ff36d9"
-rreqs="make"
+rreqs="make bash busybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./bootstrap \
-    ${cwconfigureprefix} \
-    --no-system-libs \
-    --parallel=${cwmakejobs} \
-      CFLAGS=\"\${CFLAGS} -static\" \
-      CXXFLAGS=\"\${CXXFLAGS} -static\"
+  find . -type f -name '*.cmake' -print0 | xargs -0 sed -i.ORIG 's#/usr/include#/no/usr/include#g'
+  sed -i.ORIG 's#-DCMAKE_BOOTSTRAP=1#-DCMAKE_BOOTSTRAP=1 -DCMAKE_IGNORE_PATH=/usr/include#g' bootstrap
+  env CPPFLAGS= LDFLAGS= PKG_CONFIG_LIBDIR= PKG_CONFIG_PATH= \
+      PATH=\"${cwsw}/bash/current/bin:${cwsw}/busybox/current/bin:${cwsw}/make/current/bin:${cwsw}/statictoolchain/current/bin\" \
+      ./bootstrap \
+        ${cwconfigureprefix} \
+        --no-system-libs \
+        --parallel=${cwmakejobs} \
+        --verbose \
+          CFLAGS=\"\${CFLAGS} -static\" \
+          CXXFLAGS=\"\${CXXFLAGS} -static\"
   popd >/dev/null 2>&1
 }
 "
