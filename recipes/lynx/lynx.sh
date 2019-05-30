2952e129a78b456dd6e6c85b51e3c9a70d135e46
ryan woodsmall <rwoodsmall@gmail.com>
2019-05-30 00:56:16 +0000

    lynx: recipe changes few newer openssl, quoting, use pkgconfig

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -4,34 +4,34 @@ rdir="${rname}${rver}"
 rfile="${rname}${rver}.tar.bz2"
 rurl="https://invisible-mirror.net/archives/${rname}/tarballs/${rfile}"
 rsha256="387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
-rreqs="make bzip2 ncurses slang openssl zlib"
+rreqs="make bzip2 ncurses slang openssl zlib pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
 
 eval "
 function cwbuild_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  local commonopts=\"--with-ssl --enable-widec --with-zlib --with-bzlib --disable-idna\"
-  ./configure ${cwconfigureprefix} --with-screen=ncurses \${commonopts} LIBS=\"-lcrypto -lssl -lz\"
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  local commonopts=\"--with-ssl=${cwsw}/openssl/current --without-gnutls --enable-widec --with-pkg-config --with-zlib --with-bzlib --disable-idna\"
+  env PATH=\"${cwsw}/pkgconfig/current/bin:${PATH}\" ./configure ${cwconfigureprefix} --with-screen=ncurses \${commonopts} LIBS=\"-lcrypto -lssl -lz\"
   make
   make install
-  mv "${ridir}/bin/${rname}" "${ridir}/bin/${rname}-ncurses"
+  mv \"${ridir}/bin/${rname}\" \"${ridir}/bin/${rname}-ncurses\"
   make clean
-  ./configure ${cwconfigureprefix} --with-screen=slang \${commonopts} LIBS=\"-lcrypto -lssl -lz\"
+  env PATH=\"${cwsw}/pkgconfig/current/bin:${PATH}\" ./configure ${cwconfigureprefix} --with-screen=slang \${commonopts} LIBS=\"-lcrypto -lssl -lz\"
   make
   make install
-  mv "${ridir}/bin/${rname}" "${ridir}/bin/${rname}-slang"
-  ln -sf "${ridir}/bin/${rname}-ncurses" "${ridir}/bin/${rname}"
-  sed -i.DEFAULT 's/#ACCEPT_ALL_COOKIES:FALSE/ACCEPT_ALL_COOKIES:TRUE/g' "${ridir}/etc/lynx.cfg"
-  sed -i 's/#FORCE_SSL_PROMPT:PROMPT/FORCE_SSL_PROMPT:yes/g' "${ridir}/etc/lynx.cfg"
-  sed -i 's/#FORCE_COOKIE_PROMPT:PROMPT/FORCE_COOKIE_PROMPT:yes/g' "${ridir}/etc/lynx.cfg"
-  sed -i 's/#NO_PAUSE:FALSE/NO_PAUSE:TRUE/g' "${ridir}/etc/lynx.cfg"
+  mv \"${ridir}/bin/${rname}\" \"${ridir}/bin/${rname}-slang\"
+  ln -sf \"${ridir}/bin/${rname}-ncurses\" \"${ridir}/bin/${rname}\"
+  sed -i.DEFAULT 's/#ACCEPT_ALL_COOKIES:FALSE/ACCEPT_ALL_COOKIES:TRUE/g' \"${ridir}/etc/lynx.cfg\"
+  sed -i 's/#FORCE_SSL_PROMPT:PROMPT/FORCE_SSL_PROMPT:yes/g' \"${ridir}/etc/lynx.cfg\"
+  sed -i 's/#FORCE_COOKIE_PROMPT:PROMPT/FORCE_COOKIE_PROMPT:yes/g' \"${ridir}/etc/lynx.cfg\"
+  sed -i 's/#NO_PAUSE:FALSE/NO_PAUSE:TRUE/g' \"${ridir}/etc/lynx.cfg\"
   popd >/dev/null 2>&1
 }
 "
