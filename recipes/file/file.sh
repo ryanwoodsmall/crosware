50139c8ba114eb387d0dfb2cce738db99b56908c
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-05 00:23:20 +0000

    file: update to 5.37

--- recipes/file/file.sh
+++ recipes/file/file.sh
@@ -1,9 +1,9 @@
 rname="file"
-rver="5.36"
+rver="5.37"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="ftp://ftp.astron.com/pub/${rname}/${rfile}"
-rsha256="fb608290c0fd2405a8f63e5717abf6d03e22e183fb21884413d1edd918184379"
+rsha256="e9c13967f7dd339a3c241b7710ba093560b9a33013491318e88e6b8b57bae07f"
 rreqs="make zlib"
 
 . "${cwrecipe}/common.sh"
