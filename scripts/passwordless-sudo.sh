2a2db4f2e0408c2d7d28e97c29bf6a099c63f350
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-05 23:17:56 +0000

    scripts/passwordless-sudo.sh: make passwordless sudo wrapper block/check fail fast

--- scripts/passwordless-sudo.sh
+++ scripts/passwordless-sudo.sh
@@ -10,14 +10,13 @@ function sudo() {
   local tmpdir="/usr/local/tmp"
   mount | grep -q " ${sudoersddir}/${sudoersdfile} " || {
     echo "bind mounting ${sudoersddir}/${sudoersdfile}" 1>&2
-    ${realsudo} mkdir -p ${tmpdir}
-    ${realsudo} chmod 3775 ${tmpdir}
-    ${realsudo} chgrp chronos ${tmpdir}
-    echo 'chronos ALL=(ALL:ALL) NOPASSWD: ALL' \
-    | ${reasudo} tee ${tmpdir}/${sudoersdfile} >/dev/null 2>&1
-    ${realsudo} chown root:root ${tmpdir}/${sudoersdfile}
-    ${realsudo} chmod 440 ${tmpdir}/${sudoersdfile}
-    ${realsudo} mount -o bind ${tmpdir}/${sudoersdfile} ${sudoersddir}/${sudoersdfile}
+    ${realsudo} mkdir -p ${tmpdir} \
+    && ${realsudo} chmod 3775 ${tmpdir} \
+    && ${realsudo} chgrp chronos ${tmpdir} \
+    && echo 'chronos ALL=(ALL:ALL) NOPASSWD: ALL' | ${realsudo} tee ${tmpdir}/${sudoersdfile} >/dev/null 2>&1 \
+    && ${realsudo} chown root:root ${tmpdir}/${sudoersdfile} \
+    && ${realsudo} chmod 440 ${tmpdir}/${sudoersdfile} \
+    && ${realsudo} mount -o bind ${tmpdir}/${sudoersdfile} ${sudoersddir}/${sudoersdfile}
   }
   ${realsudo} "${@}"
 }
