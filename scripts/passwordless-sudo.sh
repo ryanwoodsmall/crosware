ed4103e65f611b7fb1a5fada790ef6105ca51986
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-10 19:53:15 +0000

    scripts/passwordless-sudo.sh: only run if chrome/chromium os specific /etc/sudoers.d/96_cros_base exists

--- scripts/passwordless-sudo.sh
+++ scripts/passwordless-sudo.sh
@@ -8,15 +8,17 @@ function sudo() {
   local sudoersddir="/etc/sudoers.d"
   local sudoersdfile="95_cros_base"
   local tmpdir="/usr/local/tmp"
-  mount | grep -q " ${sudoersddir}/${sudoersdfile} " || {
-    echo "bind mounting ${sudoersddir}/${sudoersdfile}" 1>&2
-    ${realsudo} mkdir -p ${tmpdir} \
-    && ${realsudo} chmod 3775 ${tmpdir} \
-    && ${realsudo} chgrp chronos ${tmpdir} \
-    && echo 'chronos ALL=(ALL:ALL) NOPASSWD: ALL' | ${realsudo} tee ${tmpdir}/${sudoersdfile} >/dev/null 2>&1 \
-    && ${realsudo} chown root:root ${tmpdir}/${sudoersdfile} \
-    && ${realsudo} chmod 440 ${tmpdir}/${sudoersdfile} \
-    && ${realsudo} mount -o bind ${tmpdir}/${sudoersdfile} ${sudoersddir}/${sudoersdfile}
+  ${realsudo} test -e ${sudoersddir}/${sudoersdfile} && {
+    mount | grep -q " ${sudoersddir}/${sudoersdfile} " || {
+      echo "bind mounting ${sudoersddir}/${sudoersdfile}" 1>&2
+      ${realsudo} mkdir -p ${tmpdir} \
+      && ${realsudo} chmod 3775 ${tmpdir} \
+      && ${realsudo} chgrp chronos ${tmpdir} \
+      && echo 'chronos ALL=(ALL:ALL) NOPASSWD: ALL' | ${realsudo} tee ${tmpdir}/${sudoersdfile} >/dev/null 2>&1 \
+      && ${realsudo} chown root:root ${tmpdir}/${sudoersdfile} \
+      && ${realsudo} chmod 440 ${tmpdir}/${sudoersdfile} \
+      && ${realsudo} mount -o bind ${tmpdir}/${sudoersdfile} ${sudoersddir}/${sudoersdfile}
+    }
   }
   ${realsudo} "${@}"
 }
