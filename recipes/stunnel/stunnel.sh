68316c375e5034479d0a98a274ef81974488011d
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-17 03:41:49 +0000

    stunnel: update to 5.55, use crosware perl instead of hard-coded /usr/bin/perl

--- recipes/stunnel/stunnel.sh
+++ recipes/stunnel/stunnel.sh
@@ -1,16 +1,18 @@
 rname="stunnel"
-rver="5.54"
+rver="5.55"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="ftp://ftp.${rname}.org/${rname}/archive/${rver%%.*}.x/${rfile}"
-rsha256="5e8588a6c274b46b1d63e1b50f0725f4908dec736f6588eb48d1eb3d20c87902"
-rreqs="make openssl zlib"
+rsha256="90de69f41c58342549e74c82503555a6426961b29af3ed92f878192727074c62"
+rreqs="make openssl zlib toybox perl"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
+  grep -ril '/usr/bin/perl' . \
+  | xargs sed -i \"s#/usr/bin/perl#${cwsw}/perl/current/bin/perl#g\"
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --disable-fips \
     --disable-libwrap \
