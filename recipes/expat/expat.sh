abddd43113a92085f557cf65bb361879213de16b
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-21 01:06:40 +0000

    expat: fix pkg-config .pc file, more consistent quoting

--- recipes/expat/expat.sh
+++ recipes/expat/expat.sh
@@ -10,15 +10,16 @@ rreqs="make"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --without-docbook
+  sed -i \"s#${ridir}#${rtdir}/current#g\" ${rname}.pc
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
