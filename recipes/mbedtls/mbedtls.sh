68a83e08e1ceae5c1f2a783123cdb9a69ac85b28
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-21 01:18:51 +0000

    mbedtls: update to 2.16.2, more consistent quoting

--- recipes/mbedtls/mbedtls.sh
+++ recipes/mbedtls/mbedtls.sh
@@ -3,18 +3,18 @@
 #
 
 rname="mbedtls"
-rver="2.16.1"
+rver="2.16.2"
 rdir="${rname}-${rname}-${rver}"
 rfile="${rname}-${rver}.tar.gz"
 rurl="https://github.com/ARMmbed/${rname}/archive/${rfile}"
-rsha256="daf0d40f3016c34eb42d1e4b3b52be047e976d566aba8668977723c829af72f3"
+rsha256="4699f2509c3b2c65f6c27b9a7e114ad0e7df1e8c70b0f82bbad7585cad8aa47e"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   sed -i.ORIG \"s#^DESTDIR=.*#DESTDIR=${ridir}#g\" Makefile
   popd >/dev/null 2>&1
 }
@@ -22,7 +22,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   make -j${cwmakejobs} no_test CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\" LDFLAGS=\"-static\"
   popd >/dev/null 2>&1
 }
@@ -30,7 +30,7 @@ function cwmake_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
 }
