40985d2340c4d7b973eb6ace1a7b02b4d1efda1d
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-21 01:59:29 +0000

    gawk: update to 5.0.1

--- recipes/gawk/gawk.sh
+++ recipes/gawk/gawk.sh
@@ -1,9 +1,9 @@
 rname="gawk"
-rver="5.0.0"
+rver="5.0.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="50f091ed0eb485ad87dbb620d773a3e2c31a27f75f5e008f7bf065055f024406"
+rsha256="8e4e86f04ed789648b66f757329743a0d6dfb5294c3b91b756a474f1ce05a794"
 rreqs="make sed"
 
 . "${cwrecipe}/common.sh"
