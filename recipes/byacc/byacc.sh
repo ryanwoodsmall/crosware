e681891c578e0f74dc5361304f346f9c6a6dde0a
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-22 01:59:10 +0000

    byacc: update to 20190617, more consistent quoting

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -1,25 +1,26 @@
 rname="byacc"
-rver="20180609"
+rver="20190617"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
-rsha256="5bbb0b3ec3da5981a2488383b652499d6c1e0236b47d8bac5fcdfa12954f749c"
+rsha256="f87868167b920bf2cb30fc32b62f63ae15671181ef329226d1063100be02518d"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
+  rm -f \"${ridir}/bin/{b,}yacc\"
   make install
-  mv "${ridir}/bin/yacc" "${ridir}/bin/byacc"
-  ln -sf "${ridir}/bin/byacc" "${ridir}/bin/yacc"
+  mv \"${ridir}/bin/yacc\" \"${ridir}/bin/byacc\"
+  ln -sf \"${rtdir}/current/bin/byacc\" \"${ridir}/bin/yacc\"
   popd >/dev/null 2>&1
 }
 "
