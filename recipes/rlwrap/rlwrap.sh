5c763506db71bef7d59b75dc176f250fd2c3098d
ryan woodsmall <rwoodsmall@gmail.com>
2019-07-14 08:55:39 +0000

    rlwrap: swtich from ncurses and readline to netbsdcurses with bundled readline

--- recipes/rlwrap/rlwrap.sh
+++ recipes/rlwrap/rlwrap.sh
@@ -4,9 +4,20 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/hanslub42/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="8e86d0b7882d9b8a73d229897a90edc207b1ae7fa0899dca8ee01c31a93feb2f"
-rreqs="make readline"
+rreqs="make netbsdcurses"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
+    CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${cwsw}/netbsdcurses/current/include/readline\" \
+    LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" \
+    LIBS=\"-lreadline -lcurses -lterminfo\"
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
