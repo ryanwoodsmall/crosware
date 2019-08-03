c9163c256ee63e067b691059cb8a8c0d0ebef065
ryan woodsmall <rwoodsmall@gmail.com>
2019-08-03 01:02:55 +0000

    zutils: use default cwextract_

--- recipes/lzip/zutils.sh
+++ recipes/lzip/zutils.sh
@@ -10,15 +10,6 @@ rreqs="make busybox lunzip"
 . "${cwrecipe}/common.sh"
 
 eval "
-function cwextract_${rname}() {
-  pushd "${cwbuild}" >/dev/null 2>&1
-  cwscriptecho \"extracting ${rfile} in ${cwbuild}\"
-  lunzip -dc "${cwdl}/${rname}/${rfile}" | tar -xf -
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} CPPFLAGS=\"\${CPPFLAGS}\" LDFLAGS=\"\${LDFLAGS}\" CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\"
