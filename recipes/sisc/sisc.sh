5a91a00bc9dc7e2df924eca13e067c459e5ce89c
ryan woodsmall <rwoodsmall@gmail.com>
2019-08-03 02:01:48 +0000

    sisc: use rdlfile, fix quoting, elminate a basename

--- recipes/sisc/sisc.sh
+++ recipes/sisc/sisc.sh
@@ -1,8 +1,8 @@
 rname="sisc"
 rver="1.16.6"
-rurl="https://sourceforge.net/projects/${rname}/files/SISC/${rver}/${rname}-${rver}.tar.gz/download"
-rfile="$(basename ${rurl//\/download/})"
 rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://sourceforge.net/projects/${rname}/files/SISC/${rver}/${rfile}/download"
 rsha256="65e15ac81d96e97de3ecabd409b3d2bc9307e624f46908d9f74047175c527ecf"
 rreqs="rlwrap"
 
@@ -10,8 +10,8 @@ rreqs="rlwrap"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'export SISC_HOME=\"${rtdir}/current\"' > "${rprof}"
-  echo 'append_path \"\${SISC_HOME}\"' >> "${rprof}"
+  echo 'export SISC_HOME=\"${rtdir}/current\"' > \"${rprof}\"
+  echo 'append_path \"\${SISC_HOME}\"' >> \"${rprof}\"
 }
 "
 
@@ -20,8 +20,8 @@ function cwinstall_${rname}() {
   cwfetch_${rname}
   cwcheckreqs_${rname}
   cwsourceprofile
-  cwmkdir "${rtdir}"
-  cwextract "${cwdl}/${rname}/${rfile}" "${rtdir}"
+  cwmkdir \"${rtdir}\"
+  cwextract \"${rdlfile}\" \"${rtdir}\"
   cwlinkdir_${rname}
   cwgenprofd_${rname}
   cwmarkinstall_${rname}
