8996fad78cbdf24fd19fac08ebba0a40148bef34
ryan woodsmall <rwoodsmall@gmail.com>
2019-08-03 02:52:44 +0000

    jscheme: use rdlfile, eliminate a basename, fix quoting

--- recipes/jscheme/jscheme.sh
+++ recipes/jscheme/jscheme.sh
@@ -1,8 +1,8 @@
 rname="jscheme"
 rver="7.2"
-rurl="https://sourceforge.net/projects/${rname}/files/${rname}/${rver}/${rname}-${rver}.jar/download"
-rfile="$(basename ${rurl//\/download/})"
 rdir="${rname}-${rver}"
+rfile="${rdir}.jar"
+rurl="https://sourceforge.net/projects/${rname}/files/${rname}/${rver}/${rfile}/download"
 rsha256="5d3941a94a2fbfc6da76f4b42ccad8cdcc13dc525afe7f44a3ebdb9cadb26008"
 rreqs="rlwrap"
 
@@ -10,7 +10,7 @@ rreqs="rlwrap"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current\"' > \"${rprof}\"
 }
 "
 
@@ -19,12 +19,12 @@ function cwinstall_${rname}() {
   cwfetch_${rname}
   cwcheckreqs_${rname}
   cwsourceprofile
-  cwmkdir "${ridir}"
-  rm -f "${ridir}/${rname}.jar"
-  cp "${cwdl}/${rname}/${rfile}" "${ridir}/${rname}.jar"
-  echo '#!/bin/sh' > "${ridir}/${rname}"
-  echo "rlwrap -C ${rname} java -jar ${ridir}/${rname}.jar \\\"\\\${@}\\\"" >> "${ridir}/${rname}"
-  cwchmod "755" "${ridir}/${rname}"
+  cwmkdir \"${ridir}\"
+  rm -f \"${ridir}/${rname}.jar\"
+  cp \"${rdlfile}\" \"${ridir}/${rname}.jar\"
+  echo '#!/bin/sh' > \"${ridir}/${rname}\"
+  echo 'rlwrap -C ${rname} java -jar \"${rtdir}/current/${rname}.jar\" \"\${@}\"' >> \"${ridir}/${rname}\"
+  cwchmod \"755\" \"${ridir}/${rname}\"
   cwlinkdir_${rname}
   cwgenprofd_${rname}
   cwmarkinstall_${rname}
