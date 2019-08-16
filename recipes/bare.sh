0e6ea255207a3a94ce66d4b2595203750b9476fa
ryan woodsmall <rwoodsmall@gmail.com>
2019-08-16 02:33:17 +0000

    recipes/bare.sh: add small autotoolsed recipe template

---
+++ recipes/bare.sh
@@ -0,0 +1,18 @@
+rname=""
+rver=""
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar..."
+rurl=""
+rsha256=""
+rreqs="make"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+}
+"
+
+# echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+# echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
