c7c905c21f439d0423d88b5b4c67af31e78704fa
ryan woodsmall <rwoodsmall@gmail.com>
2019-08-16 17:40:07 +0000

    configgit: use cgit for now, explicitly download and install config.guess/config.sub

--- recipes/configgit/configgit.sh
+++ recipes/configgit/configgit.sh
@@ -4,41 +4,43 @@
 
 rname="configgit"
 rver="dd5d5dd697df579a5ebd119a88475b446c07c6b0"
-rdir="config-dd5d5dd"
-rfile="${rdir}.tar.gz"
-rurl="http://git.savannah.gnu.org/gitweb/\?p=config.git\;a=snapshot\;h=${rver}\;sf=tgz"
-rsha256="4eac11d10beaef441d1fe38bc83a59667a3aae85cd68ae82e3f33000a61a495f"
+rdir="${rname}-${rver}"
+rurl="http://git.savannah.gnu.org/cgit/config.git/plain"
+rfile=""
+rsha256=""
 rreqs=""
 
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"${rurl}/config.guess?id=${rver}\" \"${cwdl}/${rname}/config.guess\" \"917194b0e3c0e22e16b8416f9c48e465d4c84d7d49e0dfe60b7ed95e9a0fa7df\"
+  cwfetchcheck \"${rurl}/config.sub?id=${rver}\" \"${cwdl}/${rname}/config.sub\" \"d33ce42392950b609356a93f2fc91d2575d3b98d1b904b1add111398b51a530a\"
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  true
+}
+"
+
+eval "
 function cwconfigure_${rname}() {
-  #cwscriptecho \"cwconfigure_${rname} noop\"
-  echo -n
+  true
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  #cwscriptecho \"cwmake_${rname} noop\"
-  echo -n
+  true
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  cwmkdir "${ridir}"
-  install -m 0755 config.guess ${ridir}/config.guess
-  install -m 0755 config.sub ${ridir}/config.sub
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
-function cwgenprofd_${rname}() {
-  #cwscriptecho \"cwgenprofd_${rname} noop\"
-  echo -n
+  cwmkdir \"${ridir}\"
+  install -m 0755 \"${cwdl}/${rname}/config.guess\" \"${ridir}/config.guess\"
+  install -m 0755 \"${cwdl}/${rname}/config.sub\" \"${ridir}/config.sub\"
 }
 "
