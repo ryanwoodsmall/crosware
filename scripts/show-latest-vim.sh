78a762ec2a0f8c9fb47709c9fc5ad27dcc371599
ryan woodsmall <rwoodsmall@gmail.com>
2019-09-05 06:10:06 +0000

    scripts/show-latest-vim.sh: check for xmllint and curl

--- scripts/show-latest-vim.sh
+++ scripts/show-latest-vim.sh
@@ -6,6 +6,14 @@
 
 set -eu
 
+prereqs=( 'curl' 'xmllint' )
+for prereq in ${prereqs[@]} ; do
+  hash "${prereq}" >/dev/null 2>&1 || {
+    echo "$(basename ${BASH_SOURCE[0]}): ${prereq} not found"
+    exit 1
+  }
+done
+
 if [ ${#} -ne 1 ] ; then
   vmax="$(curl -kLs "https://github.com/vim/vim/releases" \
   | xmllint --format --html - 2>/dev/null \
