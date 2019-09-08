ed1defb97ce21559494746fe307b21acff91ecb3
ryan woodsmall <rwoodsmall@gmail.com>
2019-09-08 05:33:53 +0000

    loksh: fix errant ==

--- recipes/loksh/loksh.sh
+++ recipes/loksh/loksh.sh
@@ -23,7 +23,7 @@ function cwmake_${rname}() {
     CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
     LDFLAGS=\"-static -L${cwsw}/netbsdcurses/current/lib\" \
     PKG_CONFIG_PATH=\"${cwsw}/netbsdcurses/current/lib/pkgconfig\" \
-    PKG_CONFIG_LIBDIR==\"${cwsw}/netbsdcurses/current/lib/pkgconfig\" \
+    PKG_CONFIG_LIBDIR=\"${cwsw}/netbsdcurses/current/lib/pkgconfig\" \
     PREFIX=\"${ridir}\" \
       make -j${cwmakejobs}
   popd >/dev/null 2>&1
