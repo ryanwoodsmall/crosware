e7c858237d31cb81f4e3e03e2a541a5cc850316d
ryan woodsmall <rwoodsmall@gmail.com>
2019-09-23 06:07:15 +0000

    lynx: require cacertificates

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -4,7 +4,7 @@ rdir="${rname}${rver}"
 rfile="${rname}${rver}.tar.bz2"
 rurl="https://invisible-mirror.net/archives/${rname}/tarballs/${rfile}"
 rsha256="387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
-rreqs="make bzip2 ncurses slang openssl zlib pkgconfig"
+rreqs="make bzip2 ncurses slang openssl zlib pkgconfig cacertificates"
 
 . "${cwrecipe}/common.sh"
 
