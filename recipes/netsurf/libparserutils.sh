62d4d63c4b31d7e40202d2bf1b0aac0cd7afccb0
ryan woodsmall <rwoodsmall@gmail.com>
2019-09-28 04:08:02 +0000

    libparserutils: bison/flex?

--- recipes/netsurf/libparserutils.sh
+++ recipes/netsurf/libparserutils.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}-src.tar.gz"
 rurl="http://download.netsurf-browser.org/libs/releases/${rfile}"
 rsha256="322bae61b30ccede3e305bf6eae2414920649775bc5ff1d1b688012a3c4947d8"
-rreqs="make pkgconfig perl byacc reflex expat"
+rreqs="make pkgconfig perl bison flex byacc reflex expat"
 
 . "${cwrecipe}/common.sh"
 . "${cwrecipe}/netsurf/netsurf.sh.common"
