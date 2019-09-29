8a24b773ae4a32b9c409da91be6e7ca006ce51e2
ryan woodsmall <rwoodsmall@gmail.com>
2019-09-29 08:39:02 +0000

    duktape: add separate linenoise cmdline

--- recipes/duktape/duktape.sh
+++ recipes/duktape/duktape.sh
@@ -9,7 +9,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/svaarala/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="86a89307d1633b5cedb2c6e56dc86e92679fc34b05be551722d8cc69ab0771fc"
-rreqs="rlwrap"
+rreqs="rlwrap linenoise"
 
 . "${cwrecipe}/common.sh"
 
@@ -22,11 +22,12 @@ function cwconfigure_${rname}() {
 eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  env CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= \${CC} -fPIC -Os -c -o src/${rname}.{o,c}
-  env CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= \${CC} -fPIC -Os -g -c -o src/${rname}{d.o,.c} 
+  \${CC} -fPIC -Os -c -o src/${rname}.{o,c}
+  \${CC} -fPIC -Os -g -c -o src/${rname}{d.o,.c}
   ar rcs lib${rname}.a src/${rname}.o
   ar rcs lib${rname}d.a src/${rname}d.o
-  env CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= \${CC} -I./src examples/cmdline/duk_cmdline.c -o duk -L. -l${rname} -static
+  \${CC} -I./src examples/cmdline/duk_cmdline.c -o duk -L. -l${rname} -static
+  \${CC} -I./src examples/cmdline/duk_cmdline.c -g -DDUK_CMDLINE_FANCY -o duk-fancy -L. -l${rname}d -I\"${cwsw}/linenoise/current/include\" -L\"${cwsw}/linenoise/current/lib\" -llinenoise -static
   strip --strip-all duk
   popd >/dev/null 2>&1
 }
@@ -42,7 +43,7 @@ function cwmakeinstall_${rname}() {
   unset d
   install -m 644 lib${rname}{,d}.a \"${ridir}/lib/\"
   install -m 644 src/${rname}.h src/duk_config.h \"${ridir}/include/\"
-  install -m 755 duk \"${ridir}/bin/\"
+  install -m 755 duk{,-fancy} \"${ridir}/bin/\"
   echo '#!/usr/bin/env bash' > \"${ridir}/bin/${rname}\"
   echo 'rlwrap \"${rtdir}/current/bin/duk\" \"\${@}\"' >> \"${ridir}/bin/${rname}\"
   chmod 755 \"${ridir}/bin/${rname}\"
