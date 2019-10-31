ae1d0989efe2373f6a91e49c217c14370503baf1
ryan woodsmall <rwoodsmall@gmail.com>
2019-10-31 06:02:17 +0000

    openssh: update to 8.1p1

--- recipes/openssh/openssh.sh
+++ recipes/openssh/openssh.sh
@@ -4,11 +4,11 @@
 #
 
 rname="openssh"
-rver="8.0p1"
+rver="8.1p1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/${rfile}"
-rsha256="bd943879e69498e8031eb6b7f44d08cdc37d59a7ab689aa0b437320c3481fd68"
+rsha256="02f5dbef3835d0753556f973cd57b4c19b6b1f6cd24c03445e23ac77ca1b93ff"
 rreqs="make zlib openssl netbsdcurses"
 
 . "${cwrecipe}/common.sh"
