b3a88c590e449df945b5f867f1a0f6b066edfe71
ryan woodsmall <rwoodsmall@gmail.com>
2019-11-03 01:54:57 +0000

    python2: update to 2.7.17

--- recipes/python/python2.sh
+++ recipes/python/python2.sh
@@ -13,11 +13,11 @@
 #
 
 rname="python2"
-rver="2.7.16"
+rver="2.7.17"
 rdir="Python-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.python.org/ftp/python/${rver}/${rfile}"
-rsha256="f222ef602647eecb6853681156d32de4450a2c39f4de93bd5b20235f2e660ed7"
+rsha256="4d43f033cdbd0aa7b7023c81b0e986fd11e653b5248dac9144d508f11812ba41"
 rreqs="make bzip2 zlib ncurses readline openssl gdbm sqlite bdb47"
 
 . "${cwrecipe}/common.sh"
