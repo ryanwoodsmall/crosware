c325069ec9cf407406be19bf4988b3608e311c64
ryan woodsmall <rwoodsmall@gmail.com>
2019-11-06 01:49:48 +0000

    sbase: update to b29fc0b05f579d293043b130aef3338eaf052c8e

--- recipes/suckless/sbase.sh
+++ recipes/suckless/sbase.sh
@@ -1,5 +1,5 @@
 rname="sbase"
-rver="71ba97c175c805ab1e473c62a9581073cc791b66"
+rver="b29fc0b05f579d293043b130aef3338eaf052c8e"
 rdir="${rname}-${rver}"
 rurl="https://git.suckless.org/${rname}"
 rprof="${cwetcprofd}/zz_${rname}.sh"
