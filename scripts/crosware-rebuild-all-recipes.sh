94eba3e8662ccc2bb0b8b745a04624615714a56d
ryan woodsmall <rwoodsmall@gmail.com>
2019-11-07 02:56:29 +0000

    scripts/crosware-rebuild-all-recipes.sh: start/end times

--- scripts/crosware-rebuild-all-recipes.sh
+++ scripts/crosware-rebuild-all-recipes.sh
@@ -4,21 +4,26 @@
 # uninstall/rebuild/reinstall everything
 #
 
+st="$(date '+%Y%m%d%H%M%S')"
+out="/tmp/crosware_rebuild.out"
+
 function failexit() {
-  echo "${1}"
+  echo "${1}" 1>&2
   exit 1
 }
 
 td="$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)"
-test -e "${td}/bin/crosware" || {
-  failexit "could not find crosware top directory"
-}
+test -e "${td}/bin/crosware" || failexit "could not find crosware top directory/executable"
 cw="${td}/bin/crosware"
+
+echo "start: ${st}" >${out}
 
 ${cw} list-installed | cut -f1 -d: | xargs ${cw} uninstall
 
-{ echo statictoolchain make ccache ; ${cw} list-recipes ; } | while read -r r ; do
-  o="/tmp/${r}.out"
+for r in statictoolchain make ccache $(${cw} list-recipes) ; do
   echo ${r} 1>&2
-  ${cw} check-installed ${r} || ( time ( ${cw} install ${r} ; echo $? ) ) 2>&1 | tee ${o}
-done >/tmp/crosware_rebuild.out
+  ${cw} check-installed ${r} || ( time ( ${cw} install ${r} ; echo $? ) ) 2>&1 | tee /tmp/${r}.out
+done >>${out}
+
+et="$(date '+%Y%m%d%H%M%S')"
+echo "end: ${et}" >>${out}
