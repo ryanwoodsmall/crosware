86296f83a907406bac92fa05b4dd492df1b156b8
ryan woodsmall <rwoodsmall@gmail.com>
2019-11-21 08:28:08 +0000

    simh: update to a06fa9264f5efe1deec362e03ec571cbec0d98be

--- recipes/simh/simh.sh
+++ recipes/simh/simh.sh
@@ -1,25 +1,27 @@
+#
+# XXX - uses a github commit, download every time
+# XXX - pcap is super iffy, particulary static, don't bother with it, use a tap/tun device
+#
+
 rname="simh"
-rver="3.10"
+rver="a06fa9264f5efe1deec362e03ec571cbec0d98be"
 rdir="${rname}-${rver}"
-rfile="v${rver}.tar.gz"
-rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="21718eb59ffa7784a658ce62388b7dc83da888dfbb4888f6795eaa17cb62d7c9"
-rreqs="make libpcap busybox"
+rfile="${rver}.zip"
+rurl="https://github.com/simh/simh/archive/${rfile}"
+rsha256=""
+rreqs="make busybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwfetch_${rname}() {
+  cwfetch \"${rurl}\" \"${rdlfile}\"
+}
+"
+
+eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG '/LIBEXT = so/s/so/a/g' makefile
-  sed -i 's/ mkdir / mkdir -p /g' makefile
-  echo >> makefile
-  echo 'i1401: \${BIN}i1401\${EXE}' >> makefile
-  dos2unix makefile{,.ORIG}
-  mkdir -p sysroot/{include,lib}
-  ln -sf ${cwsw}/lib{nl,pcap}/current/include/* sysroot/include/
-  ln -sf ${cwsw}/lib{nl,pcap}/current/lib/*.a sysroot/lib/
-  popd >/dev/null 2>&1
+  true
 }
 "
 
@@ -27,10 +29,8 @@ eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make -j${cwmakejobs} all \
+    TESTS=0 \
     CC=\"\${CC} -I. -D_LARGEFILE64_SOURCE -static\" \
-    LIBPATH=\"\${PWD}/sysroot/lib\" \
-    INCPATH=\"\${PWD}/sysroot/include\" \
-    USE_NETWORK=1 \
     CFLAGS= \
     CPPFLAGS= \
     CXXFLAGS= \
@@ -43,16 +43,20 @@ eval "
 function cwmakeinstall_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   cwmkdir \"${ridir}/bin\"
+  cwmkdir \"${ridir}/share\"
   local b
   local t
-  for b in BIN/* ; do
+  for b in \$(find BIN/ -type f) ; do
     t=\"${ridir}/bin/${rname}-\$(basename \${b})\"
     rm -f \"\${t}\"
     install -m 0755 \"\${b}\" \"\${t}\"
     strip --strip-all \"\${t}\"
   done
+  for b in \$(find . -type f | egrep -i '\\.(exe|bin)\$' | grep -v /tests/) ; do
+    t=\"${ridir}/share/\$(basename \${b})\"
+    install -m 0644 \"\${b}\" \"\${t}\"
+  done
   unset b t
-  install -m 0644 VAX/ka655x.bin \"${ridir}/bin\"
   popd >/dev/null 2>&1
 }
 "
