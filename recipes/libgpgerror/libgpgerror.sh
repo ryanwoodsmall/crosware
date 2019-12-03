fb4f7ee9f6383facdab9c3c0df29ad8b33bb1d5c
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-03 04:25:00 +0000

    libgpgerror: add pkg-config setup

--- recipes/libgpgerror/libgpgerror.sh
+++ recipes/libgpgerror/libgpgerror.sh
@@ -25,5 +25,6 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
 }
 "
