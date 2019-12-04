cfe4e8d278519718ca6e26c675fd9c4dc3a403da
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-04 02:04:19 +0000

    tmux: update to 3.0a

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -6,11 +6,11 @@
 #
 
 rname="tmux"
-rver="2.9a"
+rver="3.0a"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
-rsha256="839d167a4517a6bffa6b6074e89a9a8630547b2dea2086f1fad15af12ab23b25"
+rsha256="4ad1df28b4afa969e59c08061b45082fdc49ff512f30fc8e43217d7b0e5f8db9"
 rreqs="make libevent ncurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
