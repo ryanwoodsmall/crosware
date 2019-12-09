c5422cee5a74e459ed61d193f2b0cbade97ddfe9
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-09 04:35:03 +0000

    minischeme: fix rurl

--- recipes/minischeme/minischeme.sh
+++ recipes/minischeme/minischeme.sh
@@ -2,7 +2,7 @@ rname="minischeme"
 rver="0.85ce1"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
-rurl="https://github.com/catseye/minischeme/archive/0.85ce1.tar.gz"
+rurl="https://github.com/catseye/${rname}/archive/${rfile}"
 rsha256="879bbf0fcc0b90ca52eda8013a3c8e1eb0fe7356a87247cb230ada18300fc6e2"
 rreqs="make"
 
