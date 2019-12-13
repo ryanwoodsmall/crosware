14284fb14151800197a5943061b9b6483831f812
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-13 01:53:43 +0000

    scripts/crosware-rebuild-all-recipes.sh: use tee so rebuild can be seen in console

--- scripts/crosware-rebuild-all-recipes.sh
+++ scripts/crosware-rebuild-all-recipes.sh
@@ -16,14 +16,14 @@ td="$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)"
 test -e "${td}/bin/crosware" || failexit "could not find crosware top directory/executable"
 cw="${td}/bin/crosware"
 
-echo "start: ${st}" >${out}
+echo "start: ${st}" | tee ${out}
 
 ${cw} list-installed | cut -f1 -d: | xargs ${cw} uninstall
 
 for r in statictoolchain make ccache $(${cw} list-recipes) ; do
   echo ${r} 1>&2
   ${cw} check-installed ${r} || ( time ( ${cw} install ${r} ; echo $? ) ) 2>&1 | tee /tmp/${r}.out
-done >>${out}
+done | tee -a ${out}
 
 et="$(date '+%Y%m%d%H%M%S')"
-echo "end: ${et}" >>${out}
+echo "end: ${et}" | tee -a ${out}
