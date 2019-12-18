8da04e1a470a56a29a05226fa9febdbee2508f8f
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-18 04:18:43 +0000

    abcl: update to 1.6.0

--- recipes/abcl/abcl.sh
+++ recipes/abcl/abcl.sh
@@ -3,11 +3,11 @@
 #       https://common-lisp.net/project/armedbear/doc/abcl-install-with-java.html#linux
 #
 rname="abcl"
-rver="1.5.0"
+rver="1.6.0"
 rdir="${rname}-bin-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://common-lisp.net/project/armedbear/releases/${rver}/${rfile}"
-rsha256="f053608efb15beab0e85857900e3e44946cb9efe68329dc99d2397e8263a0690"
+rsha256="2d8fc65db80afee81cce96f332ef6a6048294687a14c01e4c93d8a89f04c7bf2"
 rreqs="rlwrap"
 
 . "${cwrecipe}/common.sh"
