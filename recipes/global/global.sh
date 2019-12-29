e46a4f90f6f55e48195f6e4a23494aa27c04264d
ryan woodsmall <rwoodsmall@gmail.com>
2019-12-29 02:46:36 +0000

    global: update to 6.6.4

--- recipes/global/global.sh
+++ recipes/global/global.sh
@@ -1,9 +1,9 @@
 rname="global"
-rver="6.6.3"
+rver="6.6.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="cbee98ef6c1b064bc5b062d14a6d94dca67289e8374860817057db7688bc651c"
+rsha256="987e8cb956c53f8ebe4453b778a8fde2037b982613aba7f3e8e74bcd05312594"
 rreqs="make ncurses sed"
 
 . "${cwrecipe}/common.sh"
