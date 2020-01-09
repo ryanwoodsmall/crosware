78305282a8342b399d92d452ab491759387b3248
ryan woodsmall <rwoodsmall@gmail.com>
2020-01-09 00:29:15 +0000

    kawa: update to 3.1 with bin/kawa workaround

--- recipes/kawa/kawa.sh
+++ recipes/kawa/kawa.sh
@@ -1,17 +1,31 @@
+#
+# XXX - ugh, get old script from kawa 3.0 since it's not in the 3.1 zip and bootstrapping sucks
+#
+
 rname="kawa"
-rver="3.0"
+rver="3.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.zip"
 rurl="ftp://ftp.gnu.org/pub/gnu/${rname}/${rfile}"
-rsha256="63116eec4b2b2dd8fae0b30127639aa42ad7a7430c4970d3fd76b42a148e423c"
+rsha256="c4c3abb5d4f3eab526299150e073685f29e5416fa4f9b025e06d31ce17d1c739"
 # we need unzip, use the busybox version
 rreqs="busybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetchcheck \"${rurl//3.1/3.0}\" \"${rdlfile//3.1/3.0}\" \"63116eec4b2b2dd8fae0b30127639aa42ad7a7430c4970d3fd76b42a148e423c\"
+}
+"
+
+eval "
 function cwmakeinstall_${rname}() {
   cwextract \"${rdlfile}\" \"${rtdir}\"
+  cwextract \"${rdlfile//3.1/3.0}\" \"${ridir}\"
+  install -m 0755 \"${ridir}/${rdir//3.1/3.0}/bin/${rname}\" \"${ridir}/bin/${rname}\"
+  rm -rf \"${ridir}/${rdir//3.1/3.0}\"
 }
 "
 
