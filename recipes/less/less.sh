296f95af44d36bf4e486f65f6f2b1ee48c66c3cd
ryan woodsmall <rwoodsmall@gmail.com>
2020-01-18 00:35:53 +0000

    less: prefer busybox to toybox in $PATH

--- recipes/less/less.sh
+++ recipes/less/less.sh
@@ -16,14 +16,14 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://www.greenwoodsoftware.com/${rname}/${rfile}"
 rsha256="ff165275859381a63f19135a8f1f6c5a194d53ec3187f94121ecd8ef0795fe3d"
-rreqs="make netbsdcurses toybox"
+rreqs="make netbsdcurses busybox toybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  env PATH=\"${cwsw}/toybox/current/bin:\${PATH}\" \
+  env PATH=\"${cwsw}/busybox/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
       CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
       LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" \
