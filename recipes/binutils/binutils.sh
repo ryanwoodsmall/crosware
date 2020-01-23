dc4d2964efc2929a4698d6c7be49036ac6cc5f6c
ryan woodsmall <rwoodsmall@gmail.com>
2020-01-23 05:50:24 +0000

    binutils: failure to fix static build breakage on arm 32-bit

--- recipes/binutils/binutils.sh
+++ recipes/binutils/binutils.sh
@@ -1,6 +1,7 @@
 #
 # XXX - this needs custom ld.so stff just like statictoolchain built from musl-cross-make
 # XXX - i.e., https://github.com/ryanwoodsmall/musl-misc/blob/master/musl-cross-make-confs/patches/9999-crosware-ldso_binutils-2.27.diff
+# XXX - 2.33.1 breakage compiling static on arm 32-bit ugh
 #
 
 rname="binutils"
@@ -9,14 +10,24 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="ab66fc2d1c3ec0359b8e08843c9f33b63e8707efdff5e4cc5c200eae24722cbf"
-rreqs="make gmp mpfr mpc flex bison slibtool isl"
+rreqs="make gmp mpfr mpc flex bison slibtool isl zlib gawk"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --enable-install-libiberty
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-system-zlib \
+    --with-static-standard-libraries \
+    --with-pic \
+    --with-mmap \
+    --disable-lto \
+    --disable-multilib \
+    --disable-host-shared
+    #--enable-install-libiberty \
+    #--with-{boot,stage1}-ldflags=\"\${LDFLAGS}\" \
+    #--with-{boot,stage1}-libs=\"-static\"
   popd >/dev/null 2>&1
 }
 "
