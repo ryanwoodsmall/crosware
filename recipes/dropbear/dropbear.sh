c58a44897cb3ba38a2e74fe647f0ec0f38f057a2
ryan woodsmall <rwoodsmall@gmail.com>
2020-01-26 07:51:20 +0000

    dropbear: build a static sftp-server from lsh with nettle/mini-gmp

--- recipes/dropbear/dropbear.sh
+++ recipes/dropbear/dropbear.sh
@@ -1,5 +1,6 @@
 #
 # XXX - move config (keys) to $cwtop/etc/dropbear/
+# XXX - sftp-server is messy
 #
 rname="dropbear"
 rver="2019.78"
@@ -9,25 +10,44 @@ rfile="${rdir}.tar.bz2"
 rurl="https://dropbear.nl/mirror/releases/${rfile}"
 rsha256="525965971272270995364a0eb01f35180d793182e63dd0b0c3eb0292291644a4"
 # need a patch program, try toybox
-rreqs="make toybox zlib"
+rreqs="make toybox zlib configgit"
 
 . "${cwrecipe}/common.sh"
+
+lshver="2.1"
 
 eval "
 function cwfetch_${rname}() {
   cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
   cwfetch \"https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/options/${rname}-${rver}_localoptions.h\" \"${cwdl}/${rname}/${rname}-${rver}_localoptions.h\"
+  cwfetch_nettle
+  #cwfetch_lsh
+  cwfetchcheck \"https://ftp.gnu.org/pub/gnu/lsh/lsh-${lshver}.tar.gz\" \"${cwdl}/lsh/lsh-${lshver}.tar.gz\" \"8bbf94b1aa77a02cac1a10350aac599b7aedda61881db16606debeef7ef212e3\"
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  cwextract \"${rdlfile}\" \"${cwbuild}\"
+  cwextract \"\$(cwdlfile_nettle)\" \"${rbdir}\"
+  cwextract \"${cwdl}/lsh/lsh-${lshver}.tar.gz\" \"${rbdir}\"
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
+  for c in config.{guess,sub} ; do
+    for t in \$(find . -name \${c}) ; do
+      cp \"${cwsw}/configgit/current/\${c}\" \"\${t}\"
+      chmod 755 \"\${t}\"
+    done
+  done
   cat \"${cwdl}/${rname}/${rname}-${rver}_localoptions.h\" > localoptions.h
   cwscriptecho 'patching localoptions.h'
   sed -i \"s#/opt/${rname}#${rtdir}#g\" localoptions.h
   echo '#undef SFTPSERVER_PATH' >> localoptions.h
-  echo '#define SFTPSERVER_PATH \"${cwsw}/openssh/current/libexec/sftp-server\"' >> localoptions.h
+  echo '#define SFTPSERVER_PATH \"${rtdir}/current/libexec/sftp-server\"' >> localoptions.h
   ./configure \
     ${cwconfigureprefix} \
     --disable-lastlog \
@@ -67,6 +87,34 @@ function cwmakeinstall_${rname}() {
   ln -sf \"${ridir}/bin/dbclient\" \"${ridir}/bin/ssh\"
   cwmkdir \"${rtdir}/etc\"
   popd >/dev/null 2>&1
+  cwmakeinstall_sftpserver_${rname}
+}
+"
+
+eval "
+function cwmakeinstall_sftpserver_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cd \"\$(cwdir_nettle)\"
+  ./configure --prefix=\"${rbdir}/sftp-server\" ${cwconfigurelibopts} \
+    --disable-assembler \
+    --disable-documentation \
+    --disable-openssl \
+    --enable-mini-gmp \
+      LDFLAGS=-static \
+      CPPFLAGS=
+  make -j${cwmakejobs}
+  make install
+  cd -
+  cd lsh-${lshver}/src/sftp
+  ./configure --prefix=\"${rbdir}/sftp-server\" \
+    LDFLAGS=\"-L${rbdir}/sftp-server/lib -static\" \
+    CPPFLAGS=\"-I${rbdir}/sftp-server/include\"
+  make -j${cwmakejobs}
+  make install
+  cd -
+  cwmkdir \"${ridir}/libexec\"
+  install -m 0755 \"${rbdir}/sftp-server/sbin/sftp-server\" \"${ridir}/libexec/\"
+  popd >/dev/null 2>&1
 }
 "
 
@@ -86,3 +134,5 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/sbin\"' >> \"${rprof}\"
 }
 "
+
+unset lshver
