60602f4c1c8fecba69ced620444da85503328eac
ryan woodsmall <rwoodsmall@gmail.com>
2020-02-09 01:26:31 +0000

    tnftp: require and use configgit

--- recipes/tnftp/tnftp.sh
+++ recipes/tnftp/tnftp.sh
@@ -4,13 +4,15 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="ftp://ftp.netbsd.org/pub/NetBSD/misc/${rname}/${rfile}"
 rsha256="c94a8a49d3f4aec1965feea831d4d5bf6f90c65fd8381ee0863d11a5029a43a0"
-rreqs="make netbsdcurses"
+rreqs="make netbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
+  cat \"${cwsw}/configgit/current/config.guess\" > buildaux/config.guess
+  cat \"${cwsw}/configgit/current/config.sub\" > buildaux/config.sub
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} ${cwconfigurelibopts} \
     --disable-ssl \
     --enable-editcomplete \
