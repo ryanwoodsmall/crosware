5e22d11054378e26be838f96c98e0606dd45fee7
ryan woodsmall <rwoodsmall@gmail.com>
2020-02-28 00:19:02 +0000

    bootstrapmake: always prefer newer make by $PATH setting

--- recipes/make/bootstrapmake.sh
+++ recipes/make/bootstrapmake.sh
@@ -35,6 +35,7 @@ function cwmakeinstall_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${cwsw}/make/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' >> "${rprof}"
 }
 "
