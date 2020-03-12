313179597b5961a9f8f62010cfcab0771652954d
ryan woodsmall <rwoodsmall@gmail.com>
2020-03-12 15:49:17 +0000

    scripts/termina-penguin_etc-profile-00_local.sh: add profile.d file for SSH into penguin container on termina VM

---
+++ scripts/termina-penguin_etc-profile-00_local.sh
@@ -0,0 +1,8 @@
+test -z "${DBUS_SESSION_BUS_ADDRESS}" && export DBUS_SESSION_BUS_ADDRESS="/run/user/${UID}/bus"
+test -z "${DISPLAY_LOW_DENSITY}" && DISPLAY_LOW_DENSITY=":1"
+test -z "${DISPLAY}" && export DISPLAY=":0"
+test -z "${WAYLAND_DISPLAY_LOW_DENSITY}" && WAYLAND_DISPLAY_LOW_DENSITY="wayland-1"
+test -z "${WAYLAND_DISPLAY}" && WAYLAND_DISPLAY="wayland-0"
+test -z "${XCURSOR_SIZE_LOW_DENSITY}" && XCURSOR_SIZE_LOW_DENSITY="15"
+test -z "${XCURSOR_SIZE}" && XCURSOR_SIZE="30"
+test -z "${XDG_RUNTIME_DIR}" && export XDG_RUNTIME_DIR="/run/user/${UID}"
