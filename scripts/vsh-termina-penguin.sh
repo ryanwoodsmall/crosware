b5321ee693001db9e6e25c7c3abd98ace894e385
ryan woodsmall <rwoodsmall@gmail.com>
2020-03-13 15:09:58 +0000

    scripts/vsh-termina*: add vsh+termina+penguin scripts

---
+++ scripts/vsh-termina-penguin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+test -z "${CROS_USER_ID_HASH}" || {
+  echo "CROS_USER_ID_HASH not set"
+  exit 1
+}
+
+vsh \
+  --vm_name=termina \
+  --target_container=penguin \
+  --owner_id="${CROS_USER_ID_HASH}" \
+    "${@}"
---
+++ scripts/vsh-termina.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+test -z "${CROS_USER_ID_HASH}" || {
+  echo "CROS_USER_ID_HASH not set"
+  exit 1
+}
+
+vsh \
+  --vm_name=termina \
+  --owner_id="${CROS_USER_ID_HASH}" \
+    "${@}"
