9231a7dc71155452ba96c70fff7e44ab63608660
ryan woodsmall <rwoodsmall@gmail.com>
2020-03-18 03:46:22 +0000

    scripts/termina-penguin_etc-profile-00_local.sh: add sbin(s) to $PATH

--- scripts/termina-penguin_etc-profile-00_local.sh
+++ scripts/termina-penguin_etc-profile-00_local.sh
@@ -6,3 +6,9 @@ test -z "${WAYLAND_DISPLAY}" && WAYLAND_DISPLAY="wayland-0"
 test -z "${XCURSOR_SIZE_LOW_DENSITY}" && XCURSOR_SIZE_LOW_DENSITY="15"
 test -z "${XCURSOR_SIZE}" && XCURSOR_SIZE="30"
 test -z "${XDG_RUNTIME_DIR}" && export XDG_RUNTIME_DIR="/run/user/${UID}"
+
+for p in /usr/local/sbin /usr/sbin /sbin ; do
+  echo "${PATH}" | tr ':' '\n' | grep -q "^${p}$" || PATH="${PATH}:${p}"
+done
+unset p
+export PATH
