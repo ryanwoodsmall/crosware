70b977e6818d9cb748302eb383bea3e04c8445bf
ryan woodsmall <rwoodsmall@gmail.com>
2020-04-01 22:38:24 +0000

    scripts/environment-helper.sh: add wip script

---
+++ scripts/environment-helper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+envscript="/tmp/transient-environment.sh"
+touch envscript
+
+# ssh-agent
+if ! $(ps -U "${USER}" | grep -q 'ssh-agent') ; then
+  eval `ssh-agent -s`
+  for e in SSH_AGENT_PID SSH_AUTH_SOCK ; do
+    sed -i "/^export ${e}=.*/d" "${envscript}"
+    echo "export ${e}=${!e}" >> "${envscript}"
+  done
+fi
+. "${envscript}"
+
+# ssh-add default key
+: ${defsshkey:="${HOME}/.ssh/id_rsa"}
+if [ -e "${defsshkey}" ] ; then
+  ssh-add -q "${defsshkey}"
+fi
+
+# chrome/chromium os user id hash
+if [ ! -z "${CROS_USER_ID_HASH}" ] ; then
+  sed -i "/^export CROS_USER_ID_HASH=.*/d" "${envscript}"
+  echo "export CROS_USER_ID_HASH=${CROS_USER_ID_HASH}" >> "${envscript}"
+fi
+. "${envscript}"
