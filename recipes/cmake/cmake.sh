0afe5d7b49b8bbb82918d97dfa095e6d80762291
ryan woodsmall <rwoodsmall@gmail.com>
2020-04-29 01:00:02 +0000

    cmake: update to 3.17.2

--- recipes/cmake/cmake.sh
+++ recipes/cmake/cmake.sh
@@ -12,11 +12,11 @@
 #
 
 rname="cmake"
-rver="3.17.1"
+rver="3.17.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/Kitware/CMake/releases/download/v${rver}/${rfile}"
-rsha256="3aa9114485da39cbd9665a0bfe986894a282d5f0882b1dea960a739496620727"
+rsha256="fc77324c4f820a09052a7785549b8035ff8d3461ded5bbd80d252ae7d1cd3aa5"
 rreqs="make bash busybox toybox"
 
 . "${cwrecipe}/common.sh"
