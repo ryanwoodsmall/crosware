d2240ca993d3c1c2e90874e010ba72873f03ddc4
ryan woodsmall <rwoodsmall@gmail.com>
2020-04-30 07:10:49 +0000

    libidn2: add recipe

---
+++ recipes/libidn/libidn2.sh
@@ -0,0 +1,26 @@
+rname="libidn2"
+rver="2.3.0"
+rdir="${rname}-${rver}"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/libidn/${rfile}"
+rsha256="e1cb1db3d2e249a6a3eb6f0946777c2e892d5c5dc7bd91c74394fc3a01cab8b5"
+rreqs="make libunistring"
+
+. "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd "${rbdir}" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --disable-nls --disable-doc
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
+}
+"
