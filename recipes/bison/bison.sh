724b9063bd6c2ec13f1c21d8633887c235e27f70
ryan woodsmall <rwoodsmall@gmail.com>
2020-05-18 00:40:17 +0000

    bison: update to 3.6.2

--- recipes/bison/bison.sh
+++ recipes/bison/bison.sh
@@ -3,11 +3,11 @@
 #
 
 rname="bison"
-rver="3.6.1"
+rver="3.6.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="67ff4539783569884cbecaf781ceb665fc92e4ef3cf44c507a49d06a87421398"
+rsha256="4a164b5cc971b896ce976bf4b624fab7279e0729cf983a5135df7e4df0970f6e"
 rreqs="make m4 flex perl sed gawk"
 
 . "${cwrecipe}/common.sh"
