62a5df6889d2f46299ae0c770f8119692274da12
ryan woodsmall <rwoodsmall@gmail.com>
2020-05-29 05:00:27 +0000

    dockerstatic: update to 19.03.10

--- recipes/docker/dockerstatic.sh
+++ recipes/docker/dockerstatic.sh
@@ -1,5 +1,5 @@
 rname="dockerstatic"
-rver="19.03.9"
+rver="19.03.10"
 rdir="${rname//static/}-${rver}"
 rbdir="${cwbuild}/docker"
 rfile="${rdir}.tgz"
@@ -9,13 +9,13 @@ rsha256=""
 rburl="https://download.docker.com/linux/static/stable"
 if [[ ${karch} =~ ^aarch64 ]] ; then
   rurl="${rburl}/aarch64/${rfile}"
-  rsha256="5d6ede3368eac8e74ead70489aa7e4e663fe1ccfbb9763a6ac55991d55b70354"
+  rsha256="c949aef8c40beea732ec497d27b8d203799ee0f34b0d330c7001d57601e5c34d"
 elif [[ ${karch} =~ ^arm ]] ; then
   rurl="${rburl}/armhf/${rfile}"
-  rsha256="5e757cf65d99b0326f49cabbfc3b9a65151cb569f04fcb64a7a0c7424772c7cf"
+  rsha256="b745359f26c613ed962f1c5860bc07da641f451c6cbe92886ac67876a94c1edc"
 elif [[ ${karch} =~ ^x86_64 ]] ; then
   rurl="${rburl}/x86_64/${rfile}"
-  rsha256="1c03c78be198d9085e7dd6806fc5d93264baaf0c7ea17f584d00af48eae508ee"
+  rsha256="7c1576a0bc749418d1423d2b78c8920b5d61f849789904612862dd118742e82b"
 fi
 unset rburl
 
