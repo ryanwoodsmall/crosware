483f3a2c0bfe6ba0f8d82b5b8fa064ce79e86912
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-14 03:43:48 +0000

    patchelf: fix rname in rurl

--- recipes/patchelf/patchelf.sh
+++ recipes/patchelf/patchelf.sh
@@ -6,7 +6,7 @@ rname="patchelf"
 rver="0.11"
 rdir="${rname}-${rver}.20200609.d6b2a72"
 rfile="${rname}-${rver}.tar.bz2"
-rurl="https://nixos.org/releases/${rname}/${name}-${rver}/${rfile}"
+rurl="https://nixos.org/releases/${rname}/${rname}-${rver}/${rfile}"
 rsha256="1d3221d87a2073c7d2fa0c5ee6aa53e2a808f7628832d3874468c1c8641cba9a"
 rreqs="make"
 
