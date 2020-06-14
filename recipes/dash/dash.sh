ce3171c8cbd648418f27a97f039a7d73557f821a
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-14 01:58:34 +0000

    dash: update to 0.5.11

--- recipes/dash/dash.sh
+++ recipes/dash/dash.sh
@@ -1,9 +1,9 @@
 rname="dash"
-rver="0.5.10.2"
+rver="0.5.11"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://gondor.apana.org.au/~herbert/${rname}/files/${rfile}"
-rsha256="3c663919dc5c66ec991da14c7cf7e0be8ad00f3db73986a987c118862b5f6071"
+rsha256="4dd9a6ed5fe7546095157918fe5d784bb0b7887ae13de50e1e2d11e1b5a391cb"
 rreqs="make netbsdcurses byacc"
 
 . "${cwrecipe}/common.sh"
