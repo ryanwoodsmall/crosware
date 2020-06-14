9ded0b88b59cfde886955a2488ceaeb02c06e377
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-14 02:14:00 +0000

    patchelf: update to 0.11 with rdir/rfile workaround

--- recipes/patchelf/patchelf.sh
+++ recipes/patchelf/patchelf.sh
@@ -1,9 +1,13 @@
+#
+# XXX - dir/file mismatch popped up in 0.11
+#
+
 rname="patchelf"
-rver="0.10"
-rdir="${rname}-${rver}"
-rfile="${rdir}.tar.bz2"
+rver="0.11"
+rdir="${rname}-${rver}.20200609.d6b2a72"
+rfile="${rname}-${rver}.tar.bz2"
 rurl="https://nixos.org/releases/${rname}/${rdir}/${rfile}"
-rsha256="f670cd462ac7161588c28f45349bc20fb9bd842805e3f71387a320e7a9ddfcf3"
+rsha256="1d3221d87a2073c7d2fa0c5ee6aa53e2a808f7628832d3874468c1c8641cba9a"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
