9f7554db6908b3ddf35de8fd5acc57617c7b038e
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-24 01:59:47 +0000

    netbsdcurses: include all terms, screw it; fix quoting, use cwconfigure_ function

--- recipes/netbsdcurses/netbsdcurses.sh
+++ recipes/netbsdcurses/netbsdcurses.sh
@@ -5,19 +5,6 @@
 # XXX - readline version bundled in install - hacky but ehh
 # XXX - bundle slang?
 # XXX - curses pkg-config files don't set -L or -I?
-# XXX - include... all terms? in compiled lib? hmm
-# XXX - include all terms with a scriptlet like...
-#   sed -n '/^TERMLIST="$/,/^"$/d;p' libterminfo/genterms > libterminfo/genterms.NEW
-#   sed -i '/^echo$/a\
-#   TERMLIST=|\
-#   |' libterminfo/genterms.NEW
-#   egrep -v '^(#|$|\t)' terminfo/terminfo | grep '|' | cut -f1 -d'|' | sort -u | while read -r t ; do
-#     sed -i "/^TERMLIST=|/a\
-#     ${t}" libterminfo/genterms.NEW
-#   done
-#   sed -i 's/|/"/g' libterminfo/genterms.NEW
-#   cat libterminfo/genterms.NEW > libterminfo/genterms
-#   chmod 755 libterminfo/genterms
 #
 
 rname="netbsdcurses"
@@ -57,58 +44,41 @@ function cwgenprofd_${rname}() {
 #  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
 #  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
 
+# XXX - full term list:
+#       awk -F'|' '/\|/&&!/^(#|$|\t)/{print $1}' terminfo/terminfo | sort -u
+# XXX - diff -Naur libterminfo/genterms{.ORIG,} || true
 eval "
 function cwconfigure_${rname}() {
-  true
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  sed -i.ORIG '/(TERMINFODIR)/ s#TERMINFODIR=#TERMINFO=#g;s#(TERMINFODIR)#(PWD)/terminfo/terminfo#g' GNUmakefile
+  cat libterminfo/genterms > libterminfo/genterms.ORIG
+  sed -n '/^#/,/^TERMLIST=/p' libterminfo/genterms.ORIG > libterminfo/genterms
+  egrep -v '^(#|\\t)' terminfo/terminfo \
+  | cut -f1 -d'|' \
+  | tr -d ',' \
+  | sort -u \
+  | egrep '^[a-zA-Z0-9]' >> libterminfo/genterms
+  sed -n '/^\"$/,//p' libterminfo/genterms.ORIG >> libterminfo/genterms
+  popd >/dev/null 2>&1
 }
 "
 
-# XXX - full term list:
-#       awk -F'|' '/\|/&&!/^(#|$|\t)/{print $1}' terminfo/terminfo | sort -u
 eval "
 function cwmake_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG '/(TERMINFODIR)/ s#TERMINFODIR=#TERMINFO=#g;s#(TERMINFODIR)#(PWD)/terminfo/terminfo#g' GNUmakefile
-  sed -i.ORIG '/^screen$/a\\
-screen-256color\\
-tmux\\
-tmux-256color\\
-ansi+enq\\
-ansi+rca\\
-ansi+pp\\
-ansi+tabs\\
-dec+sl\\
-vt220\\
-vt220-w\\
-vt220-8bit\\
-vt220+keypad\\
-vt320\\
-vt340\\
-vt400\\
-vt420\\
-vt420f\\
-vt420pc\\
-vt420pcdos\\
-vt510\\
-vt510pc\\
-vt510pcdos\\
-vt520\\
-vt525\\
-vt520ansi' libterminfo/genterms
-  diff -Naur libterminfo/genterms{.ORIG,} || true
+  pushd \"${rbdir}\" >/dev/null 2>&1
   cd nbperf
   make nbperf CPPFLAGS='-I..' LDFLAGS='-static'
   cd ..
-  make -j${cwmakejobs} all-static PREFIX="${ridir}" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static'
+  make -j${cwmakejobs} all-static PREFIX=\"${ridir}\" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static'
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   # LN=echo will disable libncurses symlinks
-  make install-static PREFIX="${ridir}" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static' LN='echo'
+  make install-static PREFIX=\"${ridir}\" CPPFLAGS='-I./ -I./libterminfo' LDFLAGS='-static' LN='echo'
   rm -f ${ridir}/lib/pkgconfig/ncurses* ${ridir}/lib/pkgconfig/*w.pc
   make terminfo/terminfo.cdb
   cwmkdir \"${ridir}/share\"
@@ -121,7 +91,7 @@ function cwmakeinstall_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}_readline() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   cd readline-\$(cwver_readline)/
   env PATH=\"${ridir}/bin:\${PATH}\" ./configure \
     ${cwconfigureprefix} \
@@ -141,7 +111,7 @@ function cwmakeinstall_${rname}_readline() {
 
 eval "
 function cwmakeinstall_${rname}_libedit() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"${rbdir}\" >/dev/null 2>&1
   cd libedit-\$(cwver_libedit)/
   env PATH=\"${ridir}/bin:\${PATH}\" ./configure \
     ${cwconfigureprefix} \
