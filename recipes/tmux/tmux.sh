4b8effcf7b0bc2f34a177bb8b1f0f59925886264
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-24 02:22:59 +0000

    tmux: move to netbsdcurses, set YACC=byacc in configure

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -1,19 +1,24 @@
-#
-# can be built with netbsdcurses:
-#  CPPFLAGS=\"-I\${cwsw}/libevent/current/include -I\${cwsw}/netbsdcurses/current/include\"
-#  LDFLAGS=\"-L\${cwsw}/libevent/current/lib -L\${cwsw}/netbsdcurses/current/lib -static\"
-#  LIBS=\"-lcurses -lterminfo\"
-#
-
 rname="tmux"
 rver="3.1b"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
 rsha256="d93f351d50af05a75fe6681085670c786d9504a5da2608e481c47cf5e1486db9"
-rreqs="make libevent ncurses pkgconfig byacc"
+rreqs="make libevent netbsdcurses pkgconfig byacc"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
+  CPPFLAGS=\"-I${cwsw}/libevent/current/include -I${cwsw}/netbsdcurses/current/include\" \
+  LDFLAGS=\"-L${cwsw}/libevent/current/lib -L${cwsw}/netbsdcurses/current/lib -static\" \
+  LIBS=\"-lcurses -lterminfo\" \
+  YACC=byacc
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
