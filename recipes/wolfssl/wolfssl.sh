2cf56900d7bf87eb1cb30ed95488063432f4b227
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-24 22:01:26 +0000

    wolfssl: rip out --enable-all/--enable-distro and manually set everything, disabling all openssl compat/coexist stuff

--- recipes/wolfssl/wolfssl.sh
+++ recipes/wolfssl/wolfssl.sh
@@ -17,13 +17,79 @@ function cwconfigure_${rname}() {
   env PATH=${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:\${PATH} autoreconf -fiv -I${cwsw}/libtool/current/share/aclocal
   sed -i '/^#!/s#/bin/sh#/usr/bin/env bash#g' configure
   bash ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
-    --enable-all \
-    --enable-singlethreaded \
-    --enable-distro \
-    --disable-fips \
-    --enable-tlsv10 \
-    --enable-jobserver=no \
+    --disable-asm \
     --disable-examples \
+    --disable-fips \
+    --disable-opensslall \
+    --disable-opensslcoexist \
+    --disable-opensslextra \
+    --enable-aesccm \
+    --enable-aesctr \
+    --enable-aeskeywrap \
+    --enable-anon \
+    --enable-arc4 \
+    --enable-alpn \
+    --enable-atomicuser \
+    --enable-blake2 \
+    --enable-blake2s \
+    --enable-camellia \
+    --enable-certext \
+    --enable-certgen \
+    --enable-certreq \
+    --enable-cmac \
+    --enable-crl \
+    --enable-crl-monitor \
+    --enable-curve25519 \
+    --enable-curve448 \
+    --enable-des3 \
+    --enable-dhdefaultparams \
+    --enable-dsa \
+    --enable-dtls \
+    --enable-ecccustcurves \
+    --enable-eccencrypt \
+    --enable-ed25519 \
+    --enable-fpecc \
+    --enable-hc128 \
+    --enable-hkdf \
+    --enable-idea \
+    --enable-indef \
+    --enable-jobserver=no \
+    --enable-keygen \
+    --enable-maxfragment \
+    --enable-mcast \
+    --enable-md2 \
+    --enable-md4 \
+    --enable-nullcipher \
+    --enable-ocsp \
+    --enable-ocspstapling \
+    --enable-ocspstapling2 \
+    --enable-oldnames \
+    --enable-pkcallbacks \
+    --enable-pkcs7 \
+    --enable-pkcs11 \
+    --enable-pkcs12 \
+    --enable-psk \
+    --enable-pwdbased \
+    --enable-rabbit \
+    --enable-renegotiation-indication \
+    --enable-ripemd \
+    --enable-rsapss \
+    --enable-savecert \
+    --enable-savesession \
+    --enable-scep \
+    --enable-scrypt \
+    --enable-session-ticket \
+    --enable-singlethreaded \
+    --enable-sni \
+    --enable-srp \
+    --enable-ssh \
+    --enable-sslv3 \
+    --enable-tls13 \
+    --enable-tlsv10 \
+    --enable-tlsx \
+    --enable-trustedca \
+    --enable-truncatedhmac \
+    --enable-x963kdf \
       CFLAGS=\"\${CFLAGS}\" \
       LDFLAGS=-static \
       CPPFLAGS=
