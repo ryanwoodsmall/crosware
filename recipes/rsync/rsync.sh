58d2c0e3d6dd8245b38451150ffa3af37ba16d31
ryan woodsmall <rwoodsmall@gmail.com>
2020-06-24 00:57:00 +0000

    rsync: update to 3.2.0, turn off all options for now

--- recipes/rsync/rsync.sh
+++ recipes/rsync/rsync.sh
@@ -7,19 +7,27 @@
 #
 
 rname="rsync"
-rver="3.1.3"
+rver="3.2.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://download.samba.org/pub/${rname}/src/${rfile}"
-rsha256="55cc554efec5fdaad70de921cd5a5eeb6c29a95524c715f3bbf849235b0800c0"
-rreqs="make perl"
+rsha256="90127fdfb1a0c5fa655f2577e5495a40907903ac98f346f225f867141424fa25"
+rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} --with-included-popt --with-included-zlib
+  ./configure ${cwconfigureprefix} \
+    --with-included-popt \
+    --with-included-zlib \
+    --disable-asm \
+    --disable-lz4 \
+    --disable-openssl \
+    --disable-simd \
+    --disable-xxhash \
+    --disable-zstd
   popd >/dev/null 2>&1
 }
 "
