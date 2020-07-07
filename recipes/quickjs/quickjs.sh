f768567da7d99c7a966c79fdd4e5badef07ca6ef
ryan woodsmall <rwoodsmall@gmail.com>
2020-07-07 04:29:55 +0000

    quickjs: update to 2020-07-05, move back to bellard.org until github mirror catches up

--- recipes/quickjs/quickjs.sh
+++ recipes/quickjs/quickjs.sh
@@ -3,11 +3,14 @@
 #
 
 rname="quickjs"
-rver="20200412"
+rver="2020-07-05"
 rdir="${rname}-${rver}"
-rfile="${rver}.tar.gz"
-rurl="https://github.com/horhof/${rname}/archive/${rfile}"
-rsha256="99a267894a162fb21cdb95061432910a7c5f0268c7e10b57bebc507586a629a6"
+rfile="${rdir}.tar.xz"
+rurl="https://bellard.org/${rname}/${rfile}"
+rsha256="63e9f77a4283cede6b37b75dbbe85cb57edc7dd646367650ac2901d9ef268a99"
+#rfile="${rver}.tar.gz"
+#rurl="https://github.com/horhof/${rname}/archive/${rfile}"
+#rsha256="99a267894a162fb21cdb95061432910a7c5f0268c7e10b57bebc507586a629a6"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
