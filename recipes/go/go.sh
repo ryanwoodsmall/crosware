f3aaf70b2700f000f59d84a7f0745b8e132334f5
ryan woodsmall <rwoodsmall@gmail.com>
2020-07-19 01:12:48 +0000

    go: update to 1.14.6

--- recipes/go/go.sh
+++ recipes/go/go.sh
@@ -6,22 +6,22 @@
 #
 
 rname="go"
-rver="1.14.4"
+rver="1.14.6"
 if [[ ${karch} =~ ^x86_64$ ]] ; then
   rdir="${rname}${rver}-amd64"
-  rsha256="e4f9e99d68334fef5ffbd4bbf5c4ca8af6839511bae268efd173e47103a321ab"
+  rsha256="23f2d3f361f74ffd754fb0588f1e50a7ecec3557c3e90c89e4c3dd4189c4c77b"
 elif [[ ${karch} =~ ^i.86$ ]] ; then
   rdir="${rname}${rver}-386"
-  rsha256="07cfa7e29debe27838137db025d3f41870180b9673d29f1655f5b9f92f847f9d"
+  rsha256="0ea633510841bd9fcce73584134a405c79bac44cc177b791e994462d5e2b7810"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rdir="${rname}${rver}-arm64"
-  rsha256="ea7fac090efaea1e73a6881953998223b49722c58d4539967281c875a6a287ef"
+  rsha256="cc1359dc6d801226871fad2dbf5e26fb50262294f8b4ff7e194d8903835d5135"
 elif [[ ${karch} =~ ^arm ]] ; then
   rdir="${rname}${rver}-arm"
-  rsha256="45a239b5c1caef4d25e51f3f172600d6625666b2445a3fe404c8032b43d0d0aa"
+  rsha256="4d20ea5c11725fc5aad7808e3efb552f9099a710419c0412fb715b31549e82c8"
 fi
 rfile="${rdir}.tar.bz2"
-rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/20200603-${rname}${rver}/${rfile}"
+rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/20200718-${rname}${rver}/${rfile}"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
