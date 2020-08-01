525147ec1d0e38daff31278a063685e2862756fd
ryan woodsmall <rwoodsmall@gmail.com>
2020-08-01 06:50:45 +0000

    iperf3: update to 3.8

--- recipes/iperf3/iperf3.sh
+++ recipes/iperf3/iperf3.sh
@@ -1,9 +1,9 @@
 rname="iperf3"
-rver="3.7"
+rver="3.8"
 rdir="iperf-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/esnet/${rname}/archive/${rfile}"
-rsha256="c349924a777e8f0a70612b765e26b8b94cc4a97cc21a80ed260f65e9823c8fc5"
+rsha256="3016d225dfa6d0c79c5fd21c59d73806d591a65a9b7251301cf010d062d32685"
 rreqs="make openssl"
 
 . "${cwrecipe}/common.sh"
