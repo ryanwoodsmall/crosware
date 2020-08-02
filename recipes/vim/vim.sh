354547fa7b8488a5a9ea208a3dfcaafa9531fed3
ryan woodsmall <rwoodsmall@gmail.com>
2020-08-02 02:18:19 +0000

    vim: update to 8.2.1351

--- recipes/vim/vim.sh
+++ recipes/vim/vim.sh
@@ -3,11 +3,11 @@
 #
 
 rname="vim"
-rver="8.2.1350"
+rver="8.2.1351"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="beb7257c0c143b5aeacb0a1deede7c8f234a647c0b69b2ad80664d178e9c6352"
+rsha256="bde6cd5c635945e4b3c3a6ef256a414cf8ee37874419ce0ac8d4fbc4a54b92ec"
 rreqs="make ncurses lua gettexttiny"
 
 . "${cwrecipe}/common.sh"
