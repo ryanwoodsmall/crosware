fa90fd71d2ebab1d37dbe1b34d1342d4c16e7998
ryan woodsmall <rwoodsmall@gmail.com>
2020-08-05 00:22:59 +0000

    jruby: update to 9.2.13.0

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="9.2.12.0"
+rver="9.2.13.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/${rname}/${rname}-dist/${rver}/${rfile}"
-rsha256="307f6124c4301d723e2d0ff4c0105fa9eee8950c3f9971e14bbe8862030e6c04"
+rsha256="73a8c241a162e644c87e864c3485c55adedeb82a6fd80fa3cb538fdacda7af58"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
