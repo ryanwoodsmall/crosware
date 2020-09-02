5927ebb1e15da429dc0b387ebddeff414b6c174e
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-02 16:53:28 +0000

    htop: fixup scripts/MakeHeader.py after configure

--- recipes/htop/htop.sh
+++ recipes/htop/htop.sh
@@ -11,8 +11,8 @@ rreqs="make ncurses python3"
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG \"s|/usr/bin/env python.*|${cwsw}/python3/current/bin/python3|g\" scripts/MakeHeader.py
   ./configure ${cwconfigureprefix} CC=\"\${CC} \${CFLAGS} \${LDFLAGS}\"
+  sed -i.ORIG \"s|/usr/bin/env python.*|${cwsw}/python3/current/bin/python3|g\" scripts/MakeHeader.py
   popd >/dev/null 2>&1
 }
 "
