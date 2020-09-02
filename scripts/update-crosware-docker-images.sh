3be04e84c07a81bf90b1c8c6d1716f60abf561e3
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-02 04:30:00 +0000

    scripts/update-crosware-docker-images.sh: fix bang path, add other arch support

--- scripts/update-crosware-docker-images.sh
+++ scripts/update-crosware-docker-images.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env/bash
+#!/usr/bin/env bash
 
 docker image pull ryanwoodsmall/crosware
 if $(uname -m | grep -q x86_64) ; then
-  docker image pull ryanwoodsmall/crosware:zulu
   docker image pull ryanwoodsmall/crosware:amd64
+  docker image pull ryanwoodsmall/crosware:zulu
+elif $(uname -m | grep -q ^arm) ; then
+  docker image pull ryanwoodsmall/crosware:arm32v6
+elif $(uname -m | grep -q ^aarch64) ; then
+  docker image pull ryanwoodsmall/crosware:arm64v8
+elif $(uname -m | grep -q 86$) ; then
+  docker image pull ryanwoodsmall/crosware:i386
 fi
 time bash <(curl -kLs https://github.com/ryanwoodsmall/dockerfiles/raw/master/crosware/ccache/build.sh)
 # XXX - docker image prune -f
