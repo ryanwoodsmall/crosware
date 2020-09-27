6b23a47d2cc84cd8ff52d5b9d7a99980f3db74c3
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-27 17:13:33 +0000

    sharutils: require configgit

--- recipes/sharutils/sharutils.sh
+++ recipes/sharutils/sharutils.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/pub/gnu/${rname}/${rfile}"
 rsha256="6a22459983d32ef4cbc201b4f43aa5505bc1bd02f2be4bbb81ef56bcb059f8a5"
-rreqs="make"
+rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
 
