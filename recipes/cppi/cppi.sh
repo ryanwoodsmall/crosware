105fdabca32d96df22d00500cb470b7bd263b5cb
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-28 08:14:59 +0000

    cppi: require configgit

--- recipes/cppi/cppi.sh
+++ recipes/cppi/cppi.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="12a505b98863f6c5cf1f749f9080be3b42b3eac5a35b59630e67bea7241364ca"
-rreqs="make sed flex"
+rreqs="make sed flex configgit"
 
 . "${cwrecipe}/common.sh"
 
