7806f91cf57e9c3c0a49d4d5ac4b47d4b3af097a
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-28 03:58:03 +0000

    rlwrap: require configgit

--- recipes/rlwrap/rlwrap.sh
+++ recipes/rlwrap/rlwrap.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/hanslub42/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="8e86d0b7882d9b8a73d229897a90edc207b1ae7fa0899dca8ee01c31a93feb2f"
-rreqs="make netbsdcurses"
+rreqs="make netbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
