867ed93eaf14f42a421ce5cb772b1b3b45856eb1
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-28 07:42:21 +0000

    bison: disable nls

--- recipes/bison/bison.sh
+++ recipes/bison/bison.sh
@@ -13,6 +13,14 @@ rreqs="make m4 flex perl sed gawk"
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} --disable-nls
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
