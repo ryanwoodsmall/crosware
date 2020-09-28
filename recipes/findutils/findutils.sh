2a9ad2a509882e40b96679872ae8ede68639923e
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-28 04:25:06 +0000

    findutils: require configgit

--- recipes/findutils/findutils.sh
+++ recipes/findutils/findutils.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d"
-rreqs="make sed"
+rreqs="make sed configgit"
 
 . "${cwrecipe}/common.sh"
 
