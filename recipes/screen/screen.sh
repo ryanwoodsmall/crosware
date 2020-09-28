b41aa4007298cc9e2e6386369b5d94606930e3d3
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-28 02:13:14 +0000

    screen: convert to use netbsdcurses instead of ncurses

--- recipes/screen/screen.sh
+++ recipes/screen/screen.sh
@@ -4,14 +4,17 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="6e11b13d8489925fde25dfb0935bf6ed71f9eb47eff233a181e078fde5655aa1"
-rreqs="make ncurses sed"
+rreqs="make netbsdcurses sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} --enable-colors256
+  ./configure ${cwconfigureprefix} --enable-colors256 \
+    CPPFLAGS=\"-I${cwsw}/libevent/current/include -I${cwsw}/netbsdcurses/current/include\" \
+    LDFLAGS=\"-L${cwsw}/libevent/current/lib -L${cwsw}/netbsdcurses/current/lib -static\" \
+    LIBS=\"-lcurses -lterminfo\"
   popd >/dev/null 2>&1
 }
 "
