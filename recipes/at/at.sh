b80b07ee87f86d04320056a0a4f919780ba31812
ryan woodsmall <rwoodsmall@gmail.com>
2020-09-30 06:26:34 +0000

    at: come on

--- recipes/at/at.sh
+++ recipes/at/at.sh
@@ -12,7 +12,8 @@ rreqs="make byacc reflex configgit"
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG \"/INSTALL/s/-g root -o root/-g \${GROUPS[0]} -o \${UID}/g\" Makefile.in
+  sed -i.ORIG 's/-o root -g root/-g root -o root/g' Makefile.in
+  sed -i \"/INSTALL/s/-g root -o root/-g \${GROUPS[0]} -o \${UID}/g\" Makefile.in
   sed -i '/INSTALL.*etcdir/d' Makefile.in
   ./configure ${cwconfigureprefix} \
     --with-jobdir=\"${cwtop}/var/spool/at/atjobs\" \
