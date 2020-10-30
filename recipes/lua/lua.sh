8b02e9ff7fae284e2f672c940ce24bdf90b81bc2
ryan woodsmall <rwoodsmall@gmail.com>
2020-10-30 01:52:11 +0000

    lua: update to 5.3.6

--- recipes/lua/lua.sh
+++ recipes/lua/lua.sh
@@ -3,11 +3,11 @@
 #
 
 rname="lua"
-rver="5.3.5"
+rver="5.3.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.lua.org/ftp/${rfile}"
-rsha256="0c2eed3f960446e1a3e4b9a1ca2f3ff893b6ce41942cf54d5dd59ab4b3b058ac"
+rsha256="fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60"
 rreqs="make ncurses readline"
 
 . "${cwrecipe}/common.sh"
