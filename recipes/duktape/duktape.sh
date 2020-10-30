7020831119d8d159c6ebf24e86661617ac8d9809
ryan woodsmall <rwoodsmall@gmail.com>
2020-10-30 01:45:12 +0000

    duktape: update to 2.6.0

--- recipes/duktape/duktape.sh
+++ recipes/duktape/duktape.sh
@@ -5,11 +5,11 @@
 #
 
 rname="duktape"
-rver="2.5.0"
+rver="2.6.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/svaarala/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="83d411560a1cd36ea132bd81d8d9885efe9285c6bc6685c4b71e69a0c4329616"
+rsha256="96f4a05a6c84590e53b18c59bb776aaba80a205afbbd92b82be609ba7fe75fa7"
 rreqs="linenoise"
 
 . "${cwrecipe}/common.sh"
