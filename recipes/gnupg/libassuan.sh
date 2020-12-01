b191cb8abd8c0cebaefc0ce9abebe8661a6ee461
ryan woodsmall <rwoodsmall@gmail.com>
2020-12-01 04:19:13 +0000

    libassuan: update to 2.5.4

--- recipes/gnupg/libassuan.sh
+++ recipes/gnupg/libassuan.sh
@@ -1,9 +1,9 @@
 rname="libassuan"
-rver="2.5.3"
+rver="2.5.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="91bcb0403866b4e7c4bc1cc52ed4c364a9b5414b3994f718c70303f7f765e702"
+rsha256="c080ee96b3bd519edd696cfcebdecf19a3952189178db9887be713ccbcb5fbf0"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
