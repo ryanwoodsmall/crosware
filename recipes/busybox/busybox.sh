c020e6ca070dcba703a41f69a06be628647bfc44
ryanwoodsmall <rwoodsmall@gmail.com>
2020-12-30 00:11:44 +0000

    busybox: update to 1.33.0, make sure we can clean up after a build

--- recipes/busybox/busybox.sh
+++ recipes/busybox/busybox.sh
@@ -1,9 +1,9 @@
 rname="busybox"
-rver="1.32.0"
+rver="1.33.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="http://${rname}.net/downloads/${rfile}"
-rsha256="c35d87f1d04b2b153d33c275c2632e40d388a88f19a9e71727e0bbbff51fe689"
+rsha256="57af7883eee2bed2b56bcc6938439df2c09675ee33ae58e4ea545444cdb910a4"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -11,6 +11,7 @@ rreqs="bootstrapmake"
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
+  chmod -R u+w .
   curl -kLsO https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh
   sed -i.ORIG 's/^make/#make/g;s/^test/#test/g' bb_config_script.sh
   make defconfig HOSTCC=\"\${CC} -static\"
