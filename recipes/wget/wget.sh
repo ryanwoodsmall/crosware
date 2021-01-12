8e446cb996c7c6b4af0839997781843ea17e3d55
ryanwoodsmall <rwoodsmall@gmail.com>
2021-01-12 04:04:46 +0000

    wget: update to 1.21.1

--- recipes/wget/wget.sh
+++ recipes/wget/wget.sh
@@ -3,11 +3,11 @@
 #
 
 rname="wget"
-rver="1.21"
+rver="1.21.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="87ae105e76e5b550e03e009ba94341143c66623a5ecbba047f6ef850608b6596"
+rsha256="db9bbe5347e6faa06fc78805eeb808b268979455ead9003a608569c9d4fc90ad"
 rreqs="make lunzip openssl zlib pcre2 gettexttiny pkgconfig sed expat libmetalink cacertificates"
 
 . "${cwrecipe}/common.sh"
