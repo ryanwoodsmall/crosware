b571b7aa15a9fb1075d3453020f6573b53483004
ryanwoodsmall <rwoodsmall@gmail.com>
2021-02-04 05:43:24 +0000

    rlwrap: fix archive download

--- recipes/rlwrap/rlwrap.sh
+++ recipes/rlwrap/rlwrap.sh
@@ -1,9 +1,12 @@
+#
+# XXX - void sources has it too... https://sources.voidlinux.org/rlwrap-0.44/rlwrap-0.44.tar.gz
+#
+
 rname="rlwrap"
 rver="0.44"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-#rurl="https://github.com/hanslub42/${rname}/releases/download/v${rver}/${rfile}"
-rurl="https://github.com/hanslub42/${rname}/releases/download/7c1e432/${rfile}"
+rurl="https://github.com/hanslub42/${rname}/releases/download/${rver}/${rfile}"
 rsha256="cd7ff50cde66e443cbea0049b4abf1cca64a74948371fa4f1b5d9a5bbce1e13c"
 rreqs="make netbsdcurses configgit"
 
