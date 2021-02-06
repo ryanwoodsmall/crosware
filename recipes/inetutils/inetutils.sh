c2f37e32201dac9b38cb06cc8035a370db0cb98b
ryanwoodsmall <rwoodsmall@gmail.com>
2021-02-06 00:04:09 +0000

    inetutils: update to 2.0, fudge netbsdcurses instead of ncurses+readline, enable talk/talkd with glibc protocols/talkd.h, disable idn, explicitly enable ipv4+ipv6, use verbose build

--- recipes/inetutils/inetutils.sh
+++ recipes/inetutils/inetutils.sh
@@ -1,27 +1,49 @@
-#
-# XXX - move to netbsdcurses
-#
-
 rname="inetutils"
-rver="1.9.4"
+rver="2.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="849d96f136effdef69548a940e3e0ec0624fc0c81265296987986a0dd36ded37"
-rreqs="make sed ncurses readline configgit"
+rsha256="e573d566e55393940099862e7f8994164a0ed12f5a86c3345380842bdc124722"
+rreqs="make sed netbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetchcheck \"https://raw.githubusercontent.com/bminor/glibc/glibc-2.33/inet/protocols/talkd.h\" \"${cwdl}/${rname}/talkd.h\" \"d820ecf039d562d6c29325bf0c4839e2ce6d9f9ad8773bd243e656b5c8c48a38\"
+}
+"
+
+eval "
 function cwconfigure_${rname}() {
   pushd '${rbdir}' >/dev/null 2>&1
+  cwmkdir \"${rbdir}/include/protocols\"
+  cwmkdir \"${rbdir}/include/bits/types\"
+  install -m 0644 \"${cwdl}/${rname}/talkd.h\" \"${rbdir}/include/protocols/talkd.h\"
+  echo 'struct osockaddr { unsigned short int sa_family; unsigned char sa_data[14]; };' > include/bits/types/struct_osockaddr.h
+  sed -i.ORIG \"s/LIBTERMCAP=-lcurses/LIBTERMCAP='-lcurses -lterminfo'/g\" configure
+  sed -i 's/-lcurses/-lreadline -lcurses -lterminfo/g' configure
+  sed -i 's/-lterminfo -lterminfo/-lterminfo/g' configure
+  sed -i \"s/ LIBREADLINE=\$/LIBREADLINE='-lreadline -lcurses -lterminfo'/g\" configure
   ./configure ${cwconfigureprefix} \
     --libexecdir='${ridir}/sbin' \
+    --enable-ipv4 \
+    --enable-ipv6 \
     --enable-servers \
     --enable-clients \
+    --enable-talk \
+    --enable-talkd \
+    --disable-ncurses \
     --disable-rcp \
     --disable-rlogin \
-    --disable-rsh
+    --disable-rsh \
+    --disable-silent-rules \
+    --without-idn \
+    --with-libreadline-prefix=\"${cwsw}/netbsdcurses/current\" \
+      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${rbdir}/include\" \
+      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib/ -static\" \
+      LIBS=\"-L${cwsw}/netbsdcurses/current/lib/ -lreadline -lcurses -lterminfo\"
   popd >/dev/null 2>&1
 }
 "
