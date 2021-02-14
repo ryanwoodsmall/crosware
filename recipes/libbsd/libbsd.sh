317d97c58d03b4312c39f90280dceb498953d520
ryanwoodsmall <rwoodsmall@gmail.com>
2021-02-14 03:44:32 +0000

    libbsd: update to 0.11.0, require libmd

--- recipes/libbsd/libbsd.sh
+++ recipes/libbsd/libbsd.sh
@@ -1,17 +1,17 @@
 rname="libbsd"
-rver="0.10.0"
+rver="0.11.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://libbsd.freedesktop.org/releases/${rfile}"
-rsha256="34b8adc726883d0e85b3118fa13605e179a62b31ba51f676136ecb2d0bc1a887"
-rreqs="make"
+rsha256="9043e24f5898eae6e0ce97bea4f2d15197e90f6e9b91d0c6a8d10fb1405fd562"
+rreqs="make libmd"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS='-static' CPPFLAGS=''
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS='-L${cwsw}/libmd/current/lib -static' CPPFLAGS='-I${cwsw}/libmd/current/include'
   echo '#include <fcntl.h>' >> config.h
   popd >/dev/null 2>&1
 }
