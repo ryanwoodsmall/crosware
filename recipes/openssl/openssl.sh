9f6d962b12989fa73411f0b78ed761f32db7e88c
ryanwoodsmall <rwoodsmall@gmail.com>
2021-02-17 05:18:44 +0000

    openssl: update to 1.1.1j

--- recipes/openssl/openssl.sh
+++ recipes/openssl/openssl.sh
@@ -5,11 +5,11 @@
 #
 
 rname="openssl"
-rver="1.1.1i"
+rver="1.1.1j"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.openssl.org/source/${rfile}"
-rsha256="e8be6a35fe41d10603c3cc635e93289ed00bf34b79671a3a4de64fcee00d5242"
+rsha256="aaf2fcb575cdf6491b98ab4829abf78a3dec8402b8b81efc8f23c00d443981bf"
 rreqs="make perl zlib cacertificates"
 
 . "${cwrecipe}/common.sh"
