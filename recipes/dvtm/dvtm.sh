2236fe70e107149f982329104bf5951e2bcfa60c
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-18 20:02:03 +0000

    dvtm: move to ncurses instead of netbsdcurses

--- recipes/dvtm/dvtm.sh
+++ recipes/dvtm/dvtm.sh
@@ -1,14 +1,10 @@
-#
-# XXX - netbsdcurses is iffy here, need to test with default ncurses
-#
-
 rname="dvtm"
 rver="0.15"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/martanne/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="8f2015c05e2ad82f12ae4cf12b363d34f527a4bbc8c369667f239e4542e1e510"
-rreqs="make abduco netbsdcurses"
+rreqs="make abduco ncurses"
 
 . "${cwrecipe}/common.sh"
 
@@ -16,7 +12,6 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   sed -i.ORIG '/^PREFIX/s,^PREFIX.*,PREFIX=${ridir},g' config.mk
-  sed -i 's/-lncursesw/-lcurses -lterminfo/g' config.mk
   popd >/dev/null 2>&1
 }
 "
@@ -24,10 +19,10 @@ function cwconfigure_${rname}() {
 eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  env PATH=\"${cwsw}/netbsdcurses/current/bin:\${PATH}\" \
+  env PATH=\"${cwsw}/ncurses/current/bin:\${PATH}\" \
     make \
-      CC=\"\${CC} -I${cwsw}/netbsdcurses/current/include -L${cwsw}/netbsdcurses/current/lib\" \
-      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static\"
+      CC=\"\${CC} -I${cwsw}/ncurses/current/include -I${cwsw}/ncurses/current/include/ncursesw -L${cwsw}/ncurses/current/lib\" \
+      LDFLAGS=\"-L${cwsw}/ncurses/current/lib -lncursesw -static\"
   popd >/dev/null 2>&1
 }
 "
@@ -35,11 +30,11 @@ function cwmake_${rname}() {
 eval "
 function cwmakeinstall_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  env PATH=\"${cwsw}/netbsdcurses/current/bin:\${PATH}\" \
+  env PATH=\"${cwsw}/ncurses/current/bin:\${PATH}\" \
     make \
       install \
-      CC=\"\${CC} -I${cwsw}/netbsdcurses/current/include -L${cwsw}/netbsdcurses/current/lib\" \
-      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static\"
+      CC=\"\${CC} -I${cwsw}/ncurses/current/include -I${cwsw}/ncurses/current/include/ncursesw -L${cwsw}/ncurses/current/lib\" \
+      LDFLAGS=\"-L${cwsw}/ncurses/current/lib -lncursesw -static\"
   cwmkdir \"${ridir}/share/terminfo\"
   install -m 0644 ${rname}.info \"${ridir}/share/terminfo/${rname}.info\"
   popd >/dev/null 2>&1
