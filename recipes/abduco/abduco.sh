b1358b33819173e0d80b309cddcd86178c99ad2e
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-18 07:13:39 +0000

    abduco: fix hard-coded ${rname}

--- recipes/abduco/abduco.sh
+++ recipes/abduco/abduco.sh
@@ -2,7 +2,7 @@ rname="abduco"
 rver="0.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://github.com/martanne/abduco/releases/download/v${rver}/${rfile}"
+rurl="https://github.com/martanne/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="c90909e13fa95770b5afc3b59f311b3d3d2fdfae23f9569fa4f96a3e192a35f4"
 rreqs="make"
 
