1bc27ac0ff01c7b7c66f169e4ec704d9f9fa952f
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-18 23:42:50 +0000

    slibtool: require bootstrapmake instead of make

--- recipes/slibtool/slibtool.sh
+++ recipes/slibtool/slibtool.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/midipix-project/${rname}/archive/${rfile}"
 rsha256="0fefc07ca7e0e3164278f2c59a9a9682d0dcb18c2ea51556afd32a928c698dad"
-rreqs="make"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
