5b8a81bb6317e806c669c04d82220ffd70e7e9fa
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-27 09:06:47 +0000

    libressl: disable CPPFLAGS, LDFLAGS, PKG_CONFIG_{LIBDIR,PATH} for now

--- recipes/libressl/libressl.sh
+++ recipes/libressl/libressl.sh
@@ -114,8 +114,8 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${cwsw}/openssl/current/bin\"' > \"${rprof}\"
   echo 'append_path \"${cwsw}/curl/current/bin\"' >> \"${rprof}\"
   echo 'append_path \"${rtdir}/current/bin\"' >> \"${rprof}\"
-  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
-  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
-  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
 }
 "
+#  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+#  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+#  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
