e7eed5ae8c752574937834114be491b1041ba49e
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-27 02:57:37 +0000

    libressl: bundle libssh2 install, require zlib

--- recipes/libressl/libressl.sh
+++ recipes/libressl/libressl.sh
@@ -8,11 +8,25 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/${rfile}"
 rsha256="798a65fd61d385e09d559810cdfa46512f8def5919264cfef241a7b086ce7cfe"
-rreqs="make cacertificates configgit"
+rreqs="make cacertificates configgit zlib"
 # prefer openssl for now
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetch_libssh2
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  cwextract \"${rdlfile}\" \"${cwbuild}\"
+  cwextract \"\$(cwdlfile_libssh2)\" \"${rbdir}\"
+}
+"
 
 eval "
 function cwconfigure_${rname}() {
@@ -33,6 +47,23 @@ function cwmakeinstall_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make install ${rlibtool}
   ln -sf \"${rtdir}/current/bin/openssl\" \"${ridir}/bin/${rname}\"
+  cwmakeinstall_${rname}_libssh2
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_libssh2() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cd \$(cwdir_libssh2)
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-libssl-prefix=\"${ridir}\" \
+    --with-libz \
+    --with-pic \
+      LDFLAGS=\"-L${ridir}/lib -L${cwsw}/zlib/current/lib -static\" \
+      CPPFLAGS=\"-I${ridir}/include -I${cwsw}/zlib/current/include\"
+  make -j${cwmakejobs} ${rlibtool}
+  make install ${rlibtool}
   popd >/dev/null 2>&1
 }
 "
