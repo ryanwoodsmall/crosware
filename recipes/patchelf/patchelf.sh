fdd4f2a27f3e20e47fe5a2b32eeaad50fe3a5dfb
ryanwoodsmall <rwoodsmall@gmail.com>
2021-03-29 05:30:15 +0000

    patchelf: strip big binary, should be safe?

--- recipes/patchelf/patchelf.sh
+++ recipes/patchelf/patchelf.sh
@@ -15,6 +15,15 @@ rreqs="make"
 . "${cwrecipe}/common.sh"
 
 eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make install ${rlibtool}
+  \$(\${CC} -dumpmachine)-strip --strip-all \"${ridir}/bin/${rname}\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
