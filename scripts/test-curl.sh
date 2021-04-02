1b54d1eb3426d9cf08634b0010439ddf1421f0fd
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-02 01:34:23 +0000

    scripts/test-curl.sh: curl test scriptlet improvements

--- scripts/test-curl.sh
+++ scripts/test-curl.sh
@@ -9,10 +9,38 @@ if [ -z "${cwsw}" ] ; then
   exit 1
 fi
 
+sn="${0}"
+
+durl="https://www.google.com"
+
+: ${copts:="-fkILs"}
+
+if [[ ${@} =~ -h ]] ; then
+cat >/dev/stderr<<EOF
+${sn}: usage
+  ${sn} # HTTPS HEAD test against default url: ${durl}
+  ${sn} https://site.com/ # HTTPS HEAD test against site.com
+  env url=http://my.site ${sn} # HTTP HEAD test against my.site
+  # curl options in variable \${copts} - default: ${copts}
+  # note: \${url} environment variable takes precedence over cli argument
+EOF
+exit 1
+fi
+
+if [ ${#} -eq 0 ] ; then
+: ${url:="${durl}"}
+else
+  if [ -z "${url}" ] ; then
+    url="${1}"
+  fi
+fi
+
 for i in $(realpath ${cwsw}/curl/current/bin/curl-*{ssl,tls}* | sort -u) ; do
-  echo ${i}
-  ${i} --version
-  ${i} -kILs https://www.google.com/
-  echo --
+  echo "${i}"
+  echo "url: ${url}"
+  "${i}" --version
+  "${i}" ${copts} "${url}"
+  echo "${?}"
+  echo "--"
   echo
 done
