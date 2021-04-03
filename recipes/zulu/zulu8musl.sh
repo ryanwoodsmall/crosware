d58e837006044c01c435aaa893be0d5370604989
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-03 05:11:10 +0000

    zulu*musl: rename common file to zulumusl.sh.common

--- recipes/zulu/zulu8musl.sh
+++ recipes/zulu/zulu8musl.sh
@@ -3,6 +3,6 @@ mver="${rver%%.*}"
 rname="zulu${mver}musl"
 rsha256="51ba720f61e473ca6b31d9677d9b786eca510c593032d1c54f772ee7dda7b9c0"
 
-. "${cwrecipe}/${rname%${mver}musl}/${rname%${mver}musl}.sh.common"
+. "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
 unset mver
