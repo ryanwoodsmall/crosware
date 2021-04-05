da867a91737b160b5cc3dab9fe4b59b1225c0677
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-05 02:06:56 +0000

    kawa: move to https:// since google killed ftp in the curl distributed with chrome os :\

--- recipes/kawa/kawa.sh
+++ recipes/kawa/kawa.sh
@@ -2,7 +2,7 @@ rname="kawa"
 rver="3.1.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.zip"
-rurl="ftp://ftp.gnu.org/pub/gnu/${rname}/${rfile}"
+rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="dab1f41da968191fc68be856f133e3d02ce65d2dbd577a27e0490f18ca00fa22"
 # we need unzip, use the busybox version
 rreqs="busybox"
