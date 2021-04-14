6a3c73ef404af4a15330bb96a7047ccc15c17d0d
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-14 23:57:44 +0000

    mbedtls: add currently disabled libssh2 bundle - breaks curl w/openssl+libssh2 without lib renames

--- recipes/mbedtls/mbedtls.sh
+++ recipes/mbedtls/mbedtls.sh
@@ -15,9 +15,23 @@ rdir="${rname}-${rname}-${rver}"
 rfile="${rname}-${rver}.tar.gz"
 rurl="https://github.com/ARMmbed/${rname}/archive/${rfile}"
 rsha256="78c02e2d277a302454ada90274d16d80f88d761bdd4243528e4206cf7920be78"
-rreqs="make cacertificates"
+rreqs="make cacertificates zlib"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetch_libssh2
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  cwextract \"${rdlfile}\" \"${cwbuild}\"
+  cwextract \"\$(cwdlfile_libssh2)\" \"${rbdir}\"
+}
+"
 
 eval "
 function cwconfigure_${rname}() {
@@ -34,6 +48,35 @@ eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make -j${cwmakejobs} no_test CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\" LDFLAGS=\"-static\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make install ${rlibtool}
+  echo cwmakeinstall_${rname}_libssh2
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_libssh2() {
+  pushd \"${rbdir}/\$(cwdir_libssh2)\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-crypto=mbedtls \
+    --with-libmbedcrypto-prefix=\"${ridir}\" \
+    --with-libz=\"${cwsw}/zlib/current\" \
+      CPPFLAGS=\"-I${ridir}/include -I${cwsw}/zlib/current/include\" \
+      LDFLAGS=\"-L${ridir}/lib -L${cwsw}/zlib/current/lib -static\"
+  make -j${cwmakejobs} ${rlibtool}
+  rm -f \"${ridir}/lib/libssh2-${rname}.a\"
+  make install ${rlibtool}
+  mv \"${ridir}/lib/libssh2.a\" \"${ridir}/lib/libssh2-${rname}.a\"
+  sed '/^Name:/s/libssh2/libssh2-${rname}/g' \"${ridir}/lib/pkgconfig/libssh2.pc\" > \"${ridir}/lib/pkgconfig/libssh2-${rname}.pc\"
+  rm -f \"${ridir}/lib/pkgconfig/libssh2.pc\"
+  rm -f \"${ridir}/lib/libssh2.la\"
   popd >/dev/null 2>&1
 }
 "
