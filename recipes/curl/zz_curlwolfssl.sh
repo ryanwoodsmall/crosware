85569873a3b70199697c23c1012ba5eb5a7c8a65
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-15 18:11:11 +0000

    curlwolfssl: add recipe

---
+++ recipes/curl/zz_curlwolfssl.sh
@@ -0,0 +1,3 @@
+rname="curlwolfssl"
+rreqs="make zlib wolfssl cacertificates nghttp2 pkgconfig libgpgerror libgcrypt libssh2libgcrypt"
+. "${cwrecipe}/${rname%wolfssl}/${rname%wolfssl}tlsprovider.sh.common"
