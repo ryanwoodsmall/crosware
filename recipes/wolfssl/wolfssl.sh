02adb18544a62f60c39b79c81203f5ffb2e83cd8
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-15 18:31:07 +0000

    wolfssl: remove bundled wolfssh bits, should be separate recipe
    This reverts commit 643ce407816893ee69ccb846261042984530bd44.

--- recipes/wolfssl/wolfssl.sh
+++ recipes/wolfssl/wolfssl.sh
@@ -8,37 +8,13 @@ rreqs="make perl m4 autoconf automake libtool cacertificates"
 
 . "${cwrecipe}/common.sh"
 
-wolfsshver="1.4.6"
-wolfsshfile="v${wolfsshver}-stable.tar.gz"
-wolfsshdlfile="${cwdl}/${rname}/${wolfsshfile}"
-wolfsshdir="wolfssh-${wolfsshver}-stable"
-
-eval "
-function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
-  cwfetchcheck \"https://github.com/wolfSSL/wolfssh/archive/refs/tags/${wolfsshfile}\" \"${wolfsshdlfile}\" \"17446c213f99b64c052a2dde2ca986d9873f56c5c77db9fba4a3a27d1ac21ddc\"
-}
-"
-
-eval "
-function cwextract_${rname}() {
-  cwextract \"${rdlfile}\" \"${cwbuild}\"
-  cwextract \"${wolfsshdlfile}\" \"${rbdir}\"
-}
-"
-
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   touch wolfssl/wolfcrypt/fips.h
   touch cyassl/ctaocrypt/fips.h
-  for d in ${rbdir} ${wolfsshdir} ; do
-    pushd \${d}
-    pwd
-    env PATH=${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:\${PATH} libtoolize
-    env PATH=${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:\${PATH} autoreconf -fiv -I${cwsw}/libtool/current/share/aclocal
-    popd
-  done
+  env PATH=${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:\${PATH} libtoolize
+  env PATH=${cwsw}/autoconf/current/bin:${cwsw}/automake/current/bin:${cwsw}/libtool/current/bin:\${PATH} autoreconf -fiv -I${cwsw}/libtool/current/share/aclocal
   sed -i '/^#!/s#/bin/sh#/usr/bin/env bash#g' configure
   bash ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --disable-asm \
@@ -124,17 +100,6 @@ function cwmakeinstall_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make install ${rlibtool}
   test -e \"${ridir}/include/wolfssl/options.h\" || cp wolfssl/options.h \"${ridir}/include/wolfssl/\"
-  cwmakeinstall_${rname}_wolfssh
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
-function cwmakeinstall_${rname}_wolfssh() {
-  pushd \"${rbdir}/${wolfsshdir}\" >/dev/null 2>&1
-  bash ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --enable-all CPPFLAGS=\"-I${ridir}/include\" LDFLAGS=\"-L${ridir}/lib -static\"
-  make -j${cwmakejobs} ${rlibtool}
-  make install ${rlibtool}
   popd >/dev/null 2>&1
 }
 "
@@ -147,5 +112,3 @@ function cwgenprofd_${rname}() {
   echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
 }
 "
-
-unset wolfsshdlfile wolfsshdir wolfsshfile wolfsshver
