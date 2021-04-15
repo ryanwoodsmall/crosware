ce0f8239c31e59167101fa1882b0abd4daf039c5
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-15 18:08:43 +0000

    curlbearssl: add recipe

---
+++ recipes/curl/zz_curlbearssl.sh
@@ -0,0 +1,3 @@
+rname="curlbearssl"
+rreqs="make zlib bearssl cacertificates nghttp2 pkgconfig libgpgerror libgcrypt libssh2libgcrypt"
+. "${cwrecipe}/${rname%bearssl}/${rname%bearssl}tlsprovider.sh.common"
