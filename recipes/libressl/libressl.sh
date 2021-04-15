003b53c562031318b5e781fc2824c5ac2804ecbd
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-15 16:46:28 +0000

    libressl: remove bundled curl

--- recipes/libressl/libressl.sh
+++ recipes/libressl/libressl.sh
@@ -18,7 +18,6 @@ eval "
 function cwfetch_${rname}() {
   cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
   cwfetch_libssh2
-  cwfetch_curl
 }
 "
 
@@ -26,7 +25,6 @@ eval "
 function cwextract_${rname}() {
   cwextract \"${rdlfile}\" \"${cwbuild}\"
   cwextract \"\$(cwdlfile_libssh2)\" \"${rbdir}\"
-  cwextract \"\$(cwdlfile_curl)\" \"${rbdir}\"
 }
 "
 
@@ -52,7 +50,6 @@ function cwmakeinstall_${rname}() {
   mv \"${ridir}/bin/openssl\" \"${ridir}/bin/${rname}\"
   ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/openssl\"
   cwmakeinstall_${rname}_libssh2
-  cwmakeinstall_${rname}_curl
   popd >/dev/null 2>&1
 }
 "
@@ -74,45 +71,8 @@ function cwmakeinstall_${rname}_libssh2() {
 "
 
 eval "
-function cwmakeinstall_${rname}_curl() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cd \$(cwdir_curl)
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
-    --disable-dependency-tracking \
-    --disable-maintainer-mode \
-    --enable-ipv6 \
-    --with-libssh2 \
-    --with-nghttp2 \
-    --with-ssl=\"${ridir}\" \
-    --with-zlib \
-    --without-hyper \
-    --without-libidn2 \
-    --without-zstd \
-    --without-libmetalink \
-    --without-bearssl \
-    --without-wolfssl \
-    --without-gnutls \
-    --without-mbedtls \
-    --with-default-ssl-backend=openssl \
-    --with-ca-bundle=\"${cwetc}/ssl/cert.pem\" \
-    --with-ca-path=\"${cwetc}/ssl/certs\" \
-      LDFLAGS=\"-L${cwsw}/zlib/current/lib -L${ridir}/lib -static\" \
-      CPPFLAGS=\"-I${cwsw}/zlib/current/include -I${ridir}/include\" \
-      PKG_CONFIG_PATH=\"${cwsw}/nghttp2/current/lib/pkgconfig:${ridir}/lib/pkgconfig\" \
-      PKG_CONFIG_LIBDIR=\"${cwsw}/nghttp2/current/lib/pkgconfig:${ridir}/lib/pkgconfig\"
-  make -j${cwmakejobs} ${rlibtool}
-  rm -f \"${ridir}/bin/curl\" \"${ridir}/bin/curl-${rname}\"
-  make install ${rlibtool}
-  install -m 0755 src/curl \"${ridir}/bin/curl-${rname}\"
-  ln -sf \"${rtdir}/current/bin/curl-${rname}\" \"${ridir}/bin/curl\"
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
 function cwgenprofd_${rname}() {
   echo 'append_path \"${cwsw}/openssl/current/bin\"' > \"${rprof}\"
-  echo 'append_path \"${cwsw}/curl/current/bin\"' >> \"${rprof}\"
   echo 'append_path \"${rtdir}/current/bin\"' >> \"${rprof}\"
 }
 "
