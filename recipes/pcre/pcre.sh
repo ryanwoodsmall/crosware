2c4414b7d8d855ccd84e69202773a8481b673f6e
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-16 19:41:23 +0000

    pcre: disable readline, libedit for pcretest

--- recipes/pcre/pcre.sh
+++ recipes/pcre/pcre.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://ftp.pcre.org/pub/${rname}/${rfile}"
 rsha256="19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d"
-rreqs="make zlib bzip2 readline"
+rreqs="make zlib bzip2"
 
 . "${cwrecipe}/common.sh"
 . "${cwrecipe}/${rname}/${rname}.sh.common"
@@ -13,12 +13,12 @@ eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --disable-pcretest-lib{edit,readline} \
     --enable-pcre8 \
     --enable-pcre16 \
     --enable-pcre32 \
     --enable-pcregrep-libz \
     --enable-pcregrep-libbz2 \
-    --enable-pcretest-libreadline \
     --enable-unicode-properties \
     --enable-utf
   popd >/dev/null 2>&1
