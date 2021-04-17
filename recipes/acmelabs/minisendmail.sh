88e5a29d2acf70466ebb9f2fd9408adb202a1ac1
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-17 11:01:12 +0000

    minisendmail: note on alpine sh.c

--- recipes/acmelabs/minisendmail.sh
+++ recipes/acmelabs/minisendmail.sh
@@ -1,3 +1,8 @@
+#
+# XXX - checkout alpine limited sh.c:
+#   https://git.alpinelinux.org/aports/tree/community/mini-sendmail?h=master
+#
+
 rname="minisendmail"
 rver="1.3.9"
 rsha256="2e2741ade434808b2b95c7df7b2016a4a7e629c3d6c87468a7f58fb183a1967b"
