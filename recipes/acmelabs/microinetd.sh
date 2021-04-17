7e7a2881241ee9d099cfe3ea7f3fd367750e4db8
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-17 08:27:25 +0000

    micro{httpd,inetd,proxy}: rename without the underscore, somewhat onerous - maybe i should just allow underscores?

--- recipes/acmelabs/microinetd.sh
+++ recipes/acmelabs/microinetd.sh
@@ -1,3 +1,3 @@
-rname="micro_inetd"
+rname="microinetd"
 rsha256="15f5558753bb50ed18e4a1445b3e8a185f3b1840ec8e017a5e6fc7690616ec52"
 . "${cwrecipe}/acmelabs/acmelabs.sh.common"
--- recipes/acmelabs/microproxy.sh
+++ recipes/acmelabs/microproxy.sh
@@ -1,3 +1,3 @@
-rname="micro_proxy"
+rname="microproxy"
 rsha256="c9cf6605e22cee8a67564902ce2fb262d06da6d9b9c73a14a1c8127e083d7de3"
 . "${cwrecipe}/acmelabs/acmelabs.sh.common"
