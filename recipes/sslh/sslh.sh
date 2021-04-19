d6804118d19af5fe03b95fb5ff65b219c4fd7775
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-19 08:56:30 +0000

    sslh: require libcap, kill flags overrides

--- recipes/sslh/sslh.sh
+++ recipes/sslh/sslh.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/yrutschle/${rname}/archive/refs/tags/${rfile}"
 rsha256="2e457e59592f8e523cade8d9302b0fdc87f8ea0322beb674dd7f067547a93ea9"
-rreqs="make pcre libconfig"
+rreqs="make pcre libconfig libcap"
 
 . "${cwrecipe}/common.sh"
 
@@ -21,10 +21,21 @@ eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make -j${cwmakejobs} \
-    CPPFLAGS=\"\$(echo -I${cwsw}/{pcre,libconfig}/current/include)\" \
-    LDFLAGS=\"\$(echo -L${cwsw}/{pcre,libconfig}/current/lib) -static\" \
-    PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{pcre,libconfig}/current/include | tr ' ' ':')\" \
-    PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{pcre,libconfig}/current/lib/pkgconfig | tr ' ' ':')\"
+    ENABLE_REGEX=1 \
+    USELIBPCRE=1 \
+    USELIBCONFIG=1 \
+    USELIBCAP=1
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make install
+  install -m 0755 ${rname}-fork \"${ridir}/sbin/\"
+  install -m 0755 ${rname}-select \"${ridir}/sbin/\"
+  ln -sf ${rname}-fork \"${ridir}/sbin/${rname}\"
   popd >/dev/null 2>&1
 }
 "
