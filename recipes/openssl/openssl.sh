5ea07cae96f1197e23bc5b943b97a9dabb21dd46
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-24 10:06:00 +0000

    openssl: disable zlib

--- recipes/openssl/openssl.sh
+++ recipes/openssl/openssl.sh
@@ -2,6 +2,7 @@
 # XXX - our perl needs to be at front of path for config/make
 # XXX - other "openssl version -a" stuff?
 # XXX - 1.1.1j disables threads/pic with -static in LDFLAGS. WHY? COME ON
+# XXX - disable zlib to workaround some conflicts (redis) and pain with forcing libz.a
 #
 
 rname="openssl"
@@ -10,7 +11,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.openssl.org/source/${rfile}"
 rsha256="892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
-rreqs="make perl zlib cacertificates"
+rreqs="make perl cacertificates"
 
 . "${cwrecipe}/common.sh"
 
@@ -19,7 +20,7 @@ function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   sed -i.ORIG '/if.*eq.*-static/s/-static/-blahblahblah/g' Configure
   env PATH=\"${cwsw}/perl/current/bin:\${PATH}\" \
-    ./config --prefix=${ridir} --openssldir=${cwetc}/ssl no-asm no-shared zlib no-zlib-dynamic \${CFLAGS} \${LDFLAGS} \${CPPFLAGS} -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
+    ./config --prefix=${ridir} --openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic \${CFLAGS} \${LDFLAGS} \${CPPFLAGS} -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
   popd >/dev/null 2>&1
 }
 "
@@ -29,7 +30,6 @@ function cwmakeinstall_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   make install_sw
   sed -i '/^Libs:/s/$/ -lz/g' ${ridir}/lib/pkgconfig/*.pc
-  sed -i '/^Requires/s/\$/ zlib/g' ${ridir}/lib/pkgconfig/*.pc
   sed -i '/^Requires/s/\\.private:/:/g' ${ridir}/lib/pkgconfig/*.pc
   popd >/dev/null 2>&1
 }
