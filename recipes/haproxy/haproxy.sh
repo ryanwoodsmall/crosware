c790f32e4b4fda17895762487b840cd1553153d7
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-24 11:59:50 +0000

    haproxy: require and use lua

--- recipes/haproxy/haproxy.sh
+++ recipes/haproxy/haproxy.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://www.haproxy.org/download/${rver%.*}/src/${rfile}"
 rsha256="9e3e51441c70bedfb494fc9d4b4d3389a71be9a3c915ba3d6f7e8fd9a57ce160"
-rreqs="make openssl pcre2 zlib"
+rreqs="make openssl pcre2 zlib lua"
 
 . "${cwrecipe}/common.sh"
 
@@ -24,13 +24,15 @@ function cwmake_${rname}() {
     V=1 \
     PREFIX=\"${ridir}\" \
     TARGET=linux-musl \
-    USE_{THREAD,PTHREAD_PSHARED,OPENSSL,{,STATIC_}PCRE2,ZLIB,{,LINUX_}TPROXY,CRYPT_H,GETADDRINFO}=1 \
+    USE_{THREAD,PTHREAD_PSHARED,OPENSSL,{,STATIC_}PCRE2,ZLIB,{,LINUX_}TPROXY,CRYPT_H,GETADDRINFO,LUA}=1 \
     SSL_INC=\"${cwsw}/openssl/current/include\" \
     SSL_LIB=\"${cwsw}/openssl/current/include\" \
     ZLIB_INC=\"${cwsw}/zlib/current/include\" \
     ZLIB_LIB=\"${cwsw}/zlib/current/lib\" \
     PCRE2_INC=\"${cwsw}/pcre2/current/include\" \
     PCRE2_LIB=\"${cwsw}/pcre2/current/lib\" \
+    LUA_INC=\"${cwsw}/lua/current/include\" \
+    LUA_LIB=\"${cwsw}/lua/current/lib\" \
     CC=\"\${CC}\" \
     LDFLAGS=\"\${LDFLAGS}\"
   popd >/dev/null 2>&1
@@ -44,13 +46,15 @@ function cwmakeinstall_${rname}() {
     V=1 \
     PREFIX=\"${ridir}\" \
     TARGET=linux-musl \
-    USE_{THREAD,PTHREAD_PSHARED,OPENSSL,{,STATIC_}PCRE2,ZLIB,{,LINUX_}TPROXY,CRYPT_H,GETADDRINFO}=1 \
+    USE_{THREAD,PTHREAD_PSHARED,OPENSSL,{,STATIC_}PCRE2,ZLIB,{,LINUX_}TPROXY,CRYPT_H,GETADDRINFO,LUA}=1 \
     SSL_INC=\"${cwsw}/openssl/current/include\" \
     SSL_LIB=\"${cwsw}/openssl/current/include\" \
     ZLIB_INC=\"${cwsw}/zlib/current/include\" \
     ZLIB_LIB=\"${cwsw}/zlib/current/lib\" \
     PCRE2_INC=\"${cwsw}/pcre2/current/include\" \
     PCRE2_LIB=\"${cwsw}/pcre2/current/lib\" \
+    LUA_INC=\"${cwsw}/lua/current/include\" \
+    LUA_LIB=\"${cwsw}/lua/current/lib\" \
     CC=\"\${CC}\" \
     LDFLAGS=\"\${LDFLAGS}\"
   \$(\${CC} -dumpmachine)-strip --strip-all \"${ridir}/sbin/${rname}\"
