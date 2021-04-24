aadb9f3c5c918cace7b36e7291c6e790f3b6dd85
ryanwoodsmall <rwoodsmall@gmail.com>
2021-04-24 00:14:26 +0000

    zulu11musl: update to 11.48.21-ca-jdk11.0.11

--- recipes/zulu/zulu11musl.sh
+++ recipes/zulu/zulu11musl.sh
@@ -1,7 +1,7 @@
-rver="11.45.27-ca-jdk11.0.10"
+rver="11.48.21-ca-jdk11.0.11"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="efc94aa65985153c4400a86bcbb8d86031040fd0a8f6883739f569c3a01219f8"
+rsha256="406b76e53e8be7e19436dee6954ee903f50c0a2ed3ed86456de4c7b5072aa544"
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
