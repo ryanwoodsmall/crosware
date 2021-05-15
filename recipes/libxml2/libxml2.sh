21ac9b4cddf10415cbaf3f95c6615b28261c6a4b
ryanwoodsmall <rwoodsmall@gmail.com>
2021-05-15 00:27:07 +0000

    libxml2: update to 2.9.12

--- recipes/libxml2/libxml2.sh
+++ recipes/libxml2/libxml2.sh
@@ -1,11 +1,11 @@
 rname="libxml2"
-rver="2.9.11"
+rver="2.9.12"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="ftp://xmlsoft.org/${rname}/${rfile}"
 #rurl="https://sources.voidlinux.org/${rdir}/${rfile}"
 rurl="http://xmlsoft.org/sources/${rfile}"
-rsha256="886f696d5d5b45d780b2880645edf9e0c62a4fd6841b853e824ada4e02b4d331"
+rsha256="c8d6681e38c56f172892c85ddc0852e1fd4b53b4209e7f4ebf17f7e2eae71d92"
 rreqs="make xz zlib"
 
 . "${cwrecipe}/common.sh"
