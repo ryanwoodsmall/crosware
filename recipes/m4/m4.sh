4495b4e81ceb14360323245633df00fc3af3b690
ryanwoodsmall <rwoodsmall@gmail.com>
2021-05-29 03:28:50 +0000

    m4: update to 1.4.19, workaround for missing REG_SP def on riscv64

--- recipes/m4/m4.sh
+++ recipes/m4/m4.sh
@@ -3,14 +3,26 @@
 #
 
 rname="m4"
-rver="1.4.18"
+rver="1.4.19"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="6640d76b043bc658139c8903e293d5978309bf0f408107146505eca701e67cf6"
+rsha256="b306a91c0fd93bc4280cfc2e98cb7ab3981ff75a187bea3293811f452c89a8c8"
 rreqs="make sed"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts}
+  cat lib/sigsegv.c > lib/sigsegv.c.ORIG
+  echo '#include <config.h>' > lib/sigsegv.c
+  echo '#include <sys/reg.h>' >> lib/sigsegv.c
+  sed '/<config/d' lib/sigsegv.c.ORIG >> lib/sigsegv.c
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
