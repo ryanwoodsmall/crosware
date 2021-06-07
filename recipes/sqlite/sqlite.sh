db88cdcfa09120626c80189a7a86a3b325a74275
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-07 03:09:49 +0000

    sqlite: move to netbsdcurses

--- recipes/sqlite/sqlite.sh
+++ recipes/sqlite/sqlite.sh
@@ -9,14 +9,18 @@ rdir="${rname}-autoconf-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.sqlite.org/2021/${rfile}"
 rsha256="f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0"
-rreqs="make ncurses readline zlib"
+rreqs="make netbsdcurses zlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --enable-readline
+  sed -i.ORIG s/termcap/terminfo/g configure
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --enable-readline \
+    CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${cwsw}/zlib/current/include\" \
+    LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -L${cwsw}/zlib/current/lib -static\" \
+    LIBS=\"-L${cwsw}/netbsdcurses/current/lib -lreadline -lcurses -lterminfo -L${cwsw}/zlib/current/lib -lz\"
   popd >/dev/null 2>&1
 }
 "
