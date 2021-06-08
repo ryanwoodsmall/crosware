202d3ddb6e8c5f529777c216278d6cebf22db07b
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-08 23:48:04 +0000

    jruby: update to 9.2.18.0

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="9.2.17.0"
+rver="9.2.18.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/${rname}/${rname}-dist/${rver}/${rfile}"
-rsha256="7701d3537b3a606d2765ac6d5c40e675ddaa01d3cebad26a21a66e3aadd5c202"
+rsha256="425a5c970a6d918ae531275850634d12169e3557754b2b817fbfb7987a32c1a7"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
