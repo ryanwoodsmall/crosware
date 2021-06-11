3d21813a3d30860339cba312e85d5f7a50861fae
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-11 02:41:17 +0000

    tmux: update to 3.2a

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -1,9 +1,9 @@
 rname="tmux"
-rver="3.2"
+rver="3.2a"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
-rsha256="664d345338c11cbe429d7ff939b92a5191e231a7c1ef42f381cebacb1e08a399"
+rsha256="551553a4f82beaa8dadc9256800bcc284d7c000081e47aa6ecbb6ff36eacd05f"
 rreqs="make libevent netbsdcurses pkgconfig byacc"
 
 . "${cwrecipe}/common.sh"
