76e95b1086d951e90612a6db5e6505fa25dbcad7
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-18 07:00:31 +0000

    lua: use lua.sh.common

--- recipes/lua/lua.sh
+++ recipes/lua/lua.sh
@@ -5,25 +5,9 @@ rfile="${rdir}.tar.gz"
 rurl="https://www.lua.org/ftp/${rfile}"
 rsha256="fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60"
 rreqs="bootstrapmake"
+rdlfile="${cwdl}/${rname}/${rfile}"
 
-. "${cwrecipe}/common.sh"
-
-eval "
-function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG \"s#^INSTALL_TOP=.*#INSTALL_TOP= ${ridir}#g\" Makefile
-  sed -i.ORIG \"s#^CC= gcc #CC= \${CC} #g\" src/Makefile
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
-function cwmake_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  make -j${cwmakejobs} posix MYCFLAGS=\"\${CFLAGS} \${CPPFLAGS} \" MYLDFLAGS=\"\${LDFLAGS}\"
-  popd >/dev/null 2>&1
-}
-"
+. "${cwrecipe}/${rname}/${rname}.sh.common"
 
 eval "
 function cwgenprofd_${rname}() {
