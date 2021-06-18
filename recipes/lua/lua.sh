ec4e82eeb0ee2c3c038a1d716acbacab2f3e7e56
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-18 07:23:39 +0000

    lua: normalize ${CC} setting across versions, support dynamic modules(?) with -DLUA_USE_DLOPEN, make ${rreqs} overridable

--- recipes/lua/lua.sh
+++ recipes/lua/lua.sh
@@ -4,7 +4,6 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.lua.org/ftp/${rfile}"
 rsha256="fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60"
-rreqs="bootstrapmake"
 rdlfile="${cwdl}/${rname}/${rfile}"
 
 . "${cwrecipe}/${rname}/${rname}.sh.common"
