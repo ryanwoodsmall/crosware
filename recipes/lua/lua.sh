86b92adbe6200004dcc4540977ec06e08961ac88
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-18 06:22:18 +0000

    lua: just remove curses for now

--- recipes/lua/lua.sh
+++ recipes/lua/lua.sh
@@ -1,16 +1,10 @@
-#
-# XXX - lua 5.4.0
-# XXX - disable readline, remove ncurses reqs
-# XXX - or move to netbsdcurses?
-#
-
 rname="lua"
 rver="5.3.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.lua.org/ftp/${rfile}"
 rsha256="fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60"
-rreqs="make ncurses readline"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
