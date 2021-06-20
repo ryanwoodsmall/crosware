ec158d1369794117a1fe3e52f08aa3cae085d8cb
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-20 05:23:21 +0000

    plan9port9p: tweak a bit, remove 9pserve, quiet down extraction

--- recipes/plan9port/zz_plan9port9p.sh
+++ recipes/plan9port/zz_plan9port9p.sh
@@ -15,6 +15,13 @@ function cwfetch_${rname}() {
 }
 "
 
+# unzip is incredibly loud, add "-q"?
+eval "
+function cwextract_${rname}() {
+  cwextract \"${rdlfile}\" \"${cwbuild}\" >/dev/null 2>&1
+}
+"
+
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
@@ -32,7 +39,7 @@ function cwmake_${rname}() {
     ( cd lib\${l}/ ; env PLAN9=\"${rbdir}\" PATH=\"\${PATH}:${rbdir}/bin\" \"${cwsw}/9base/current/bin/mk\" CC9=\"\${CC}\" )
   done
   cd cmd
-  env PLAN9=\"${rbdir}\" PATH=\"\${PATH}:${rbdir}/bin\" \"${cwsw}/9base/current/bin/mk\" 9p.install 9pserve.install CC9=\"\${CC}\"
+  env PLAN9=\"${rbdir}\" PATH=\"\${PATH}:${rbdir}/bin\" \"${cwsw}/9base/current/bin/mk\" 9p.install CC9=\"\${CC}\"
   cd 9pfuse
   env PLAN9=\"${rbdir}\" PATH=\"\${PATH}:${rbdir}/bin\" \"${cwsw}/9base/current/bin/mk\" 9pfuse.install CC9=\"\${CC}\"
   popd >/dev/null 2>&1
@@ -46,10 +53,8 @@ function cwmakeinstall_${rname}() {
   cwmkdir \"${ridir}/man/man1\"
   cwmkdir \"${ridir}/man/man4\"
   install -m 0755 bin/9p \"${ridir}/bin/\"
-  install -m 0755 bin/9pserve \"${ridir}/bin/\"
   install -m 0755 bin/9pfuse \"${ridir}/bin/\"
   install -m 0644 man/man1/9p.1 \"${ridir}/man/man1/\"
-  install -m 0644 man/man4/9pserve.4 \"${ridir}/man/man4/\"
   install -m 0644 man/man4/9pfuse.4 \"${ridir}/man/man4/\"
   find \"${ridir}/bin/\" ! -type d | xargs \$(\${CC} -dumpmachine)-strip --strip-all || true
   popd >/dev/null 2>&1
