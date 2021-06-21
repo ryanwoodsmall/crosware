902e2d3a05fd0abc0c904b18c67a480952acd567
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-21 02:08:58 +0000

    vim: update to 8.2.3025

--- recipes/vim/vim.sh
+++ recipes/vim/vim.sh
@@ -3,11 +3,11 @@
 #
 
 rname="vim"
-rver="8.2.3024"
+rver="8.2.3025"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="0a9d7477ee5c3ad8ba5bec6f101b3330f42622d30445512b4c6f7c5213a174ca"
+rsha256="e282d2c7ffe7d9448bf9c730308aa222ef9a71d28cd61ad7e0398e1617ae8704"
 rreqs="make ncurses lua gettexttiny attr acl"
 
 . "${cwrecipe}/common.sh"
