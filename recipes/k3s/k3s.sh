6521e84105d7b6ba822df8b0eb2d0cd6bcd7f1e4
ryanwoodsmall <rwoodsmall@gmail.com>
2021-06-23 01:07:25 +0000

    k3s: version download file

--- recipes/k3s/k3s.sh
+++ recipes/k3s/k3s.sh
@@ -14,15 +14,17 @@ if [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rname}-arm64"
   rurl="${rburl}/${rfile}"
   rsha256="5257042d68f5c2a9caa148592e5d2fdbbbb468ea199c1128e636f227cfb6c74b"
+  rdlfile="${cwdl}/${rname}/${rfile}_${rver}"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rname}-armhf"
   rurl="${rburl}/${rfile}"
   rsha256="6383d4700faa1c0afa52d575e549ec9728298202e1737b02fcd8ef6f39f9d14e"
+  rdlfile="${cwdl}/${rname}/${rfile}_${rver}"
 elif [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rname}"
-  rdlfile="${cwdl}/${rname}/${rfile}-amd64"
   rurl="${rburl}/${rfile}"
   rsha256="5097d515e220f8e97ab13c56cb9142ee4526b4c9eade5ed098e2906c1db2a163"
+  rdlfile="${cwdl}/${rname}/${rfile}-amd64_${rver}"
 fi
 unset rburl
 
