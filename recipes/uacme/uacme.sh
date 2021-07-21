3666ecefbcf3f71400aa5e12ee91cea9b859ae5d
ryanwoodsmall <rwoodsmall@gmail.com>
2021-07-21 19:28:21 +0000

    uacme: remove expat and metalink deps

--- recipes/uacme/uacme.sh
+++ recipes/uacme/uacme.sh
@@ -4,7 +4,7 @@ rdir="${rname}-upstream-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/ndilieto/${rname}/archive/refs/tags/upstream/${rfile}"
 rsha256="36027a587256cbaa86650cec2a5b3eb000480e1150bd83941565661b392625ac"
-rreqs="make curl zlib openssl libssh2 expat libmetalink cacertificates nghttp2 pkgconfig"
+rreqs="make curl zlib openssl libssh2 cacertificates nghttp2 pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
@@ -19,7 +19,7 @@ function cwconfigure_${rname}() {
       --with-openssl=\"${cwsw}/openssl/current\" \
       --without-gnutls \
       --without-mbedtls \
-        LIBS='-lcurl -lssh2 -lnghttp2 -lz -lmetalink -lexpat -lssl -lcrypto -static' \
+        LIBS='-lcurl -lssh2 -lnghttp2 -lz -lssl -lcrypto -static' \
         PKG_CONFIG=\"${cwsw}/pkgconfig/current/bin/pkg-config\"
   popd >/dev/null 2>&1
 }
