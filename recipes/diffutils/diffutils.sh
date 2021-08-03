93b529a8c40200321a5231285017e7c9c42bc981
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-03 03:01:10 +0000

    diffutils: update to 3.8

--- recipes/diffutils/diffutils.sh
+++ recipes/diffutils/diffutils.sh
@@ -5,11 +5,11 @@
 #
 
 rname="diffutils"
-rver="3.7"
+rver="3.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="b3a7a6221c3dc916085f0d205abf6b8e1ba443d4dd965118da364a1dc1cb3a26"
+rsha256="a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
 rreqs="make sed gettexttiny"
 
 . "${cwrecipe}/common.sh"
