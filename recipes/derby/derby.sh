d678d0fb8865f5d0e8a55286a659421dd322723c
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-08 08:27:09 +0000

    derby: update url, note on mirror

--- recipes/derby/derby.sh
+++ recipes/derby/derby.sh
@@ -1,8 +1,13 @@
+#
+# XXX - use a mirror?
+#
+
 rname="derby"
 rver="10.14.2.0"
 rdir="db-${rname}-${rver}-bin"
 rfile="${rdir}.tar.gz"
-rurl="http://www-us.apache.org/dist//db/${rname}/db-${rname}-${rver}/${rfile}"
+#rurl="http://www-us.apache.org/dist//db/${rname}/db-${rname}-${rver}/${rfile}"
+rurl="https://archive.apache.org/dist/db/derby/db-${rname}-${rver}/${rfile}"
 rsha256="980fb0534c38edf4a529a13fb4a12b53d32054827b57b6c5f0307d10f17d25a8"
 rreqs=""
 
