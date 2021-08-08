25d5404513355614b34467809acb1953da7d274e
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-08 08:33:04 +0000

    derby: update to 10.15.1.3, note on jdk8/10.14

--- recipes/derby/derby.sh
+++ recipes/derby/derby.sh
@@ -1,14 +1,15 @@
 #
+# XXX - derbyjdk8 package with 10.14.x.x?
 # XXX - use a mirror?
 #
 
 rname="derby"
-rver="10.14.2.0"
+rver="10.15.1.3"
 rdir="db-${rname}-${rver}-bin"
 rfile="${rdir}.tar.gz"
 #rurl="http://www-us.apache.org/dist//db/${rname}/db-${rname}-${rver}/${rfile}"
 rurl="https://archive.apache.org/dist/db/derby/db-${rname}-${rver}/${rfile}"
-rsha256="980fb0534c38edf4a529a13fb4a12b53d32054827b57b6c5f0307d10f17d25a8"
+rsha256="eedb0293fea8b7d9cc813371c34935661e42ea8270e72fedd0ffe2a6a29c61ad"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
