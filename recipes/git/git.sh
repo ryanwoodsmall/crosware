1cbb0fbe8aff1bb1e758d1c7ebe49e73b9c19a07
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-17 02:43:03 +0000

    git: update to 2.33.0

--- recipes/git/git.sh
+++ recipes/git/git.sh
@@ -4,11 +4,11 @@
 #
 
 rname="git"
-rver="2.32.0"
+rver="2.33.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.kernel.org/pub/software/scm/${rname}/${rfile}"
-rsha256="68a841da3c4389847ecd3301c25eb7e4a51d07edf5f0168615ad6179e3a83623"
+rsha256="bf3c6ab5f82e072aad4768f647cfb1ef60aece39855f83f080f9c0222dd20c4f"
 rreqs="make bzip2 zlib openssl curl expat pcre2 perl gettexttiny libssh2 groff busybox less cacertificates nghttp2"
 
 . "${cwrecipe}/common.sh"
@@ -20,7 +20,7 @@ function cwfetch_${rname}() {
   cwfetchcheck \
     \"${rurl//${rname}-${rver}/${rname}-manpages-${rver}}\" \
     \"${rdlfile//${rname}-${rver}/${rname}-manpages-${rver}}\" \
-    \"19e3cb0425c94d4ad82984f41522e77c8e35093e15a891f8e7195617201f6ac1\"
+    \"d6d38abe3fe45b74359e65d53e51db3aa92d7f551240b7f7a779746f24c4bc31\"
 }
 "
 
