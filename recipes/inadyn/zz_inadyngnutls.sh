4bec476e2dfc907bb75675ace9eca2ada79aeb79
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-22 11:53:54 +0000

    inadyngnutls: remove unnecessary libgcrypt, libgpgerror reqs

--- recipes/inadyn/zz_inadyngnutls.sh
+++ recipes/inadyn/zz_inadyngnutls.sh
@@ -4,7 +4,7 @@ rdir="$(cwdir_inadyn)"
 rfile="$(cwfile_inadyn)"
 rurl="$(cwurl_inadyn)"
 rsha256=""
-rreqs="make libconfuse pkgconfig zlib gnutls libtasn1 libunistring nettle gmp libgcrypt libgpgerror"
+rreqs="make libconfuse pkgconfig zlib gnutls libtasn1 libunistring nettle gmp"
 
 . "${cwrecipe}/common.sh"
 
@@ -23,11 +23,11 @@ function cwconfigure_${rname}() {
   env PATH=\"${cwsw}/pkgconfig/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
       --without-systemd \
-        CPPFLAGS=\"\$(echo -I${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libgcrypt,libgpgerror,libconfuse,zlib}/current/include)\" \
-        LDFLAGS=\"\$(echo -L${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libgcrypt,libgpgerror,libconfuse,zlib}/current/lib) -static\" \
-        PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libgcrypt,libgpgerror,libconfuse,zlib}/current/lib/pkgconfig | tr ' ' ':')\" \
-        PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libgcrypt,libgpgerror,libconfuse,zlib}/current/lib/pkgconfig | tr ' ' ':')\" \
-        LIBS='-lgcrypt -lgpg-error -lz -lgnutls -ltasn1 -lunistring -lhogweed -lgmp -lnettle -static'
+        CPPFLAGS=\"\$(echo -I${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libconfuse,zlib}/current/include)\" \
+        LDFLAGS=\"\$(echo -L${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libconfuse,zlib}/current/lib) -static\" \
+        PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libconfuse,zlib}/current/lib/pkgconfig | tr ' ' ':')\" \
+        PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{gnutls,libtasn1,libunistring,nettle,gmp,libconfuse,zlib}/current/lib/pkgconfig | tr ' ' ':')\" \
+        LIBS='-lz -lgnutls -ltasn1 -lunistring -lhogweed -lgmp -lnettle -static'
   popd >/dev/null 2>&1
 }
 "
