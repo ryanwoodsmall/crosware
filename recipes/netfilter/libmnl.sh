4ce462ea96d8675bf84607cef01f23fce670ad4d
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-26 04:49:21 +0000

    libmnl: remove commented genprofd

--- recipes/netfilter/libmnl.sh
+++ recipes/netfilter/libmnl.sh
@@ -15,12 +15,3 @@ function cwconfigure_${rname}() {
   popd >/dev/null 2>&1
 }
 "
-
-#eval "
-#function cwgenprofd_${rname}() {
-#  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
-#  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
-#  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
-#  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
-#}
-#"
