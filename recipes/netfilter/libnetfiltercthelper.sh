6c7f221a6126529ea4eea0a0ca26e7d2b774c3f4
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-26 08:05:51 +0000

    libnetfiltercthelper: require configgit

--- recipes/netfilter/libnetfiltercthelper.sh
+++ recipes/netfilter/libnetfiltercthelper.sh
@@ -4,7 +4,7 @@ rdir="${rname//rc/r_c}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://www.netfilter.org/pub/${rname//rc/r_c}/${rfile}"
 rsha256="07618e71c4d9a6b6b3dc1986540486ee310a9838ba754926c7d14a17d8fccf3d"
-rreqs="bootstrapmake pkgconfig libmnl"
+rreqs="bootstrapmake pkgconfig configgit libmnl"
 
 . "${cwrecipe}/common.sh"
 
