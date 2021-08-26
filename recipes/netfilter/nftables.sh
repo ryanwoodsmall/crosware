134fa46b2faa62af18a51e544fe7404ac398c8ce
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-26 08:37:09 +0000

    nftables: require and use jansson for json

--- recipes/netfilter/nftables.sh
+++ recipes/netfilter/nftables.sh
@@ -1,14 +1,10 @@
-#
-# XXX - json support requires jansson
-#
-
 rname="nftables"
 rver="1.0.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
 rsha256="58bf547daf967a2b88ecb4f425f126006ebde22711db806b25c1d6cf84fe45f4"
-rreqs="bootstrapmake pkgconfig byacc netbsdcurses libpcap libnl libmnl libnetfilterconntrack libnfnetlink libnftnl iptables"
+rreqs="bootstrapmake pkgconfig byacc netbsdcurses libpcap libnl jansson libmnl libnetfilterconntrack libnfnetlink libnftnl iptables"
 
 . "${cwrecipe}/common.sh"
 
@@ -16,7 +12,7 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
-    --with-mini-gmp --with-xtables --disable-man-doc --disable-python --without-json --without-python-bin \
+    --with-json --with-mini-gmp --with-xtables --disable-man-doc --disable-python --without-python-bin \
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
