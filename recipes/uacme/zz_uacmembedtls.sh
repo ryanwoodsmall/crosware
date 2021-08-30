e90b01bce8e7d792d13cb7bdf30765ba471920d7
ryanwoodsmall <rwoodsmall@gmail.com>
2021-08-30 00:16:16 +0000

    uacmembedtls: switch to libssh2mbedtls instead of libssh2libgcrypt/libgcrypt/libgpgerror

--- recipes/uacme/zz_uacmembedtls.sh
+++ recipes/uacme/zz_uacmembedtls.sh
@@ -1,6 +1,6 @@
 rprovider="mbedtls"
 rname="uacme${rprovider}"
-rreqs="make curl${rprovider} ${rprovider} nghttp2 zlib libssh2libgcrypt libgcrypt libgpgerror pkgconfig"
+rreqs="make curl${rprovider} ${rprovider} nghttp2 zlib libssh2mbedtls pkgconfig"
 . "${cwrecipe}/${rname%${rprovider}}/${rname%${rprovider}}tlsprovider.sh.common"
 
 eval "
@@ -14,9 +14,9 @@ function cwconfigure_${rname}() {
       --with-${rprovider}=\"${cwsw}/${rprovider}/current\" \
       --without-openssl \
       --without-gnutls \
-        CPPFLAGS=\"\$(echo -I${cwsw}/{curl${rprovider},zlib,nghttp2,libssh2libgcrypt,libgcrypt,libgpgerror,${rprovider}}/current/include)\" \
-        LDFLAGS=\"\$(echo -L${cwsw}/{curl${rprovider},zlib,nghttp2,libssh2libgcrypt,libgcrypt,libgpgerror,${rprovider}}/current/lib) -static\" \
-        LIBS='-lcurl -lssh2 -lgcrypt -lgpg-error -lnghttp2 -lz -lmbedx509 -lmbedtls -lmbedcrypto -static' \
+        CPPFLAGS=\"\$(echo -I${cwsw}/{curl${rprovider},zlib,nghttp2,libssh2mbedtls,${rprovider}}/current/include)\" \
+        LDFLAGS=\"\$(echo -L${cwsw}/{curl${rprovider},zlib,nghttp2,libssh2mbedtls,${rprovider}}/current/lib) -static\" \
+        LIBS='-lcurl -lssh2 -lnghttp2 -lz -lmbedx509 -lmbedtls -lmbedcrypto -static' \
         PKG_CONFIG=\"${cwsw}/pkgconfig/current/bin/pkg-config\" \
         PKG_CONFIG_LIBDIR= \
         PKG_CONFIG_PATH=
