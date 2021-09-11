3e4e61395e9d3e390fa59e8bfdae45f785ab3f7a
ryanwoodsmall <rwoodsmall@gmail.com>
2021-09-11 03:17:19 +0000

    libedit: update to 20210910-3.1

--- recipes/libedit/libedit.sh
+++ recipes/libedit/libedit.sh
@@ -1,9 +1,9 @@
 rname="libedit"
-rver="20210714-3.1"
+rver="20210910-3.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.thrysoee.dk/editline/${rfile}"
-rsha256="3023b498ad593fd7745ae3b20abad546de506b67b8fbb5579637ca69ab82dbc9"
+rsha256="6792a6a992050762edcca28ff3318cdb7de37dccf7bc30db59fcd7017eed13c5"
 rreqs="make ncurses"
 
 . "${cwrecipe}/common.sh"
