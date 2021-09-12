1c71badb0a024c7e341e3edeafcd01e9bc205c11
ryanwoodsmall <rwoodsmall@gmail.com>
2021-09-12 04:06:10 +0000

    sbase: update to 371f3cb5ec3b8ef3135b3729326bfd6c7b7cb85c

--- recipes/suckless/sbase.sh
+++ recipes/suckless/sbase.sh
@@ -1,10 +1,10 @@
 rname="sbase"
-rver="7d60e2cabbe07495ee74f3d5e705f1e3470148eb"
+rver="371f3cb5ec3b8ef3135b3729326bfd6c7b7cb85c"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rprof="${cwetcprofd}/zz_${rname}.sh"
-rsha256="dadf515a121804c9f789529e9028683b651cd9f0b2ebc040854b996f4221702a"
+rsha256="a3988cedab5c38efe076671e5a5d5785208594353dc11a7048566f320ed68fad"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
