a11f23e0d8e726d1a93596f66dfdb6bf24b5987b
ryanwoodsmall <rwoodsmall@gmail.com>
2021-09-20 22:05:39 +0000

    scripts/reset-cwenv.sh: add scriptlet

---
+++ scripts/reset-cwenv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+: ${cwtop:="/usr/local/crosware"}
+export PATH="$(echo $PATH | tr : \\n | grep -v $cwtop/ | paste -s -d: -)"
+for e in CFLAGS CPPFLAGS CXXFLAGS LDFLAGS PKG_CONFIG_LIBDIR PKG_CONFIG_PATH ; do
+  unset ${e}
+done
+unset e
+test -e "${cwtop}/etc/profile" && . "${cwtop}/etc/profile" || true
