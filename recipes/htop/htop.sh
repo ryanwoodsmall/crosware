0b904f20ccc4bc63c871675fca38bc9410df5aae
ryanwoodsmall <rwoodsmall@gmail.com>
2021-09-22 05:22:26 +0000

    htop: update to 3.1.0

--- recipes/htop/htop.sh
+++ recipes/htop/htop.sh
@@ -1,10 +1,10 @@
 rname="htop"
-rver="3.0.5"
+rver="3.1.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://bintray.com/${rname}/source/download_file?file_path=${rfile}"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="19535f8f01ac08be2df880c93c9cedfc50fa92320d48e3ef92a30b6edc4d1917"
+rsha256="9b90697c091b2981c9123530e46a93d45d4277c22200fdaee41cd2e73ebe0420"
 rreqs="make ncurses"
 
 . "${cwrecipe}/common.sh"
@@ -12,7 +12,10 @@ rreqs="make ncurses"
 eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} CC=\"\${CC} \${CFLAGS} \${LDFLAGS}\"
+  ./configure ${cwconfigureprefix} \
+    --enable-static \
+    --disable-dependency-tracking \
+      CC=\"\${CC} \${CFLAGS} \${LDFLAGS}\"
   popd >/dev/null 2>&1
 }
 "
