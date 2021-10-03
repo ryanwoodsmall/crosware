64b837d983ead6c0af609a569f9e7656dc3066f8
ryanwoodsmall <rwoodsmall@gmail.com>
2021-10-03 02:13:30 +0000

    git: notes on ssh

--- recipes/git/git.sh
+++ recipes/git/git.sh
@@ -2,6 +2,29 @@
 # XXX - mandoc with/instead of groff?
 # XXX - add html docs?
 #
+# git will use whatever "ssh" command is in the $PATH
+# this can break with dropbear's dbclient -> ssh symlink
+# git can be configured to use "openssh"
+# these are provided by the openssh/opensshlibressl packages
+#
+#   export GIT_SSH_COMMAND='openssh'
+#   git config --global core.sshCommand 'openssh'
+#
+# these can be used to pass additional options to the ssh cli
+# to i.e. use a specific key, set IdentitiesOnly options, etc.
+#
+# additionally, the "simple" ssh variant can be used to turn off openssh specifics
+# i.e. spuriuos SendEnv=GIT_PROTOCOL
+# note that this won't make dropbear work
+#
+#   export GIT_SSH_VARIANT=simple
+#   git config --global ssh.variant simple
+#
+# links:
+# - https://git-scm.com/docs/git-config#Documentation/git-config.txt-coresshCommand
+# - https://git-scm.com/docs/git-config#Documentation/git-config.txt-sshvariant
+# - https://stackoverflow.com/questions/4565700/how-to-specify-the-private-ssh-key-to-use-when-executing-shell-command-on-git
+#
 
 rname="git"
 rver="2.33.0"
