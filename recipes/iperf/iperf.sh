3e8fe88dcff5f6f40c9bfd3bce02bc813b8a6220
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-04 19:44:49 +0000

    iperf: update to 2.1.4, add cwpatch_ for netinet/if_ether.h workaround

--- recipes/iperf/iperf.sh
+++ recipes/iperf/iperf.sh
@@ -1,12 +1,22 @@
 rname="iperf"
-rver="2.1.3"
+rver="2.1.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://sourceforge.net/projects/iperf2/files/${rfile}"
-rsha256="dfe2197e2842fe9c9d9677bf1cb20a5a9ccfcb9a9de79f9927c39f73204ba003"
+rsha256="062b392e87b8e227aca74fef0a99b04fe0382d4518957041b508a56885b4d4f9"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwpatch_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cat include/headers.h > include/headers.h.ORIG
+  echo '#include <netinet/if_ether.h>' > include/headers.h
+  cat include/headers.h.ORIG >> include/headers.h
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
