ae0f1e90681abbda220e0c4d6739e3e5edb756eb
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-04 19:33:36 +0000

    iperf: update to 2.1.3

--- recipes/iperf/iperf.sh
+++ recipes/iperf/iperf.sh
@@ -1,9 +1,9 @@
 rname="iperf"
-rver="2.1.1"
+rver="2.1.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://sourceforge.net/projects/iperf2/files/${rfile}"
-rsha256="9873b540131049dac04887296ebb69f4992036715e9045c7efe00e7a3c7f3308"
+rsha256="dfe2197e2842fe9c9d9677bf1cb20a5a9ccfcb9a9de79f9927c39f73204ba003"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
