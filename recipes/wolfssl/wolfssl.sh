2dbc5aeef9315edd4db5ec67177e8dda8ec14278
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-07 04:11:36 +0000

    wolfssl: enable and install examples, fixup certs/ installation, require toybox

--- recipes/wolfssl/wolfssl.sh
+++ recipes/wolfssl/wolfssl.sh
@@ -8,7 +8,7 @@ rdir="${rname}-${rver}-stable"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rsha256="1356d8f93a3bd9b86c12bc7b88277bdfcfa260aa41a7959682fe725b1d3440aa"
-rreqs="make cacertificates configgit slibtool"
+rreqs="make cacertificates configgit slibtool toybox"
 
 . "${cwrecipe}/common.sh"
 
@@ -19,8 +19,8 @@ function cwconfigure_${rname}() {
   touch cyassl/ctaocrypt/fips.h
   sed -i '/^#!/s#/bin/sh#/usr/bin/env bash#g' configure
   bash ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --enable-examples \
     --disable-asm \
-    --disable-examples \
     --disable-fips \
     --disable-opensslall \
     --disable-opensslcoexist \
@@ -98,10 +98,30 @@ function cwconfigure_${rname}() {
 "
 
 eval "
+function cwpatch_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cat wolfssl/test.h > wolfssl/test.h.ORIG
+  sed -i 's,\./certs/,certs/,g' wolfssl/test.h
+  sed -i 's,\"certs/,\"${rtdir}/current/certs/,g' wolfssl/test.h
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
 function cwmakeinstall_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
   make install ${rlibtool}
   test -e \"${ridir}/include/wolfssl/options.h\" || cp wolfssl/options.h \"${ridir}/include/wolfssl/\"
+  mkdir -p \"${ridir}/bin\"
+  for i in \$(find examples/ -type f -exec \"${cwsw}/toybox/current/bin/toybox\" file {} + | awk -F: '/ELF.*executable/{print \$1}') ; do
+    n=\"\$(basename \${i})\"
+    install -m 0755 \"\${i}\" \"${ridir}/bin/${rname}-\${n}\"
+    \$(\${CC} -dumpmachine)-strip --strip-all \"${ridir}/bin/${rname}-\${n}\"
+  done
+  unset i n
+  rm -rf \"${ridir}/certs\"
+  mkdir \"${ridir}/certs\"
+  ( cd certs ; tar -cf - . ) | ( cd \"${ridir}/certs/\" ; tar -xf - )
   popd >/dev/null 2>&1
 }
 "
