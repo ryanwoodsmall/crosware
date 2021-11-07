088751a64e3b2007cd5aeb441667b60999d07d37
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-07 08:02:20 +0000

    wolfmqtt: remove commented lib/include/pkg-config stuff

--- recipes/wolfmqtt/wolfmqtt.sh
+++ recipes/wolfmqtt/wolfmqtt.sh
@@ -42,7 +42,3 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
-
-# echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
-# echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
-# echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
