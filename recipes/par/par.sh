680d220bf7d474da36461fc607c2406cd8e78397
ryanwoodsmall <rwoodsmall@gmail.com>
2021-11-09 06:00:04 +0000

    par: update to 1.53.0 and move to a (mirrored) upstream archive

--- recipes/par/par.sh
+++ recipes/par/par.sh
@@ -1,36 +1,41 @@
-#
-# XXX - move to upstream? http://www.nicemice.net/par/
-#
-
 rname="par"
-rver="1.52"
-rdir="${rname}-${rver}.orig"
-rfile="${rdir//-/_}.tar.gz"
-rurl="http://deb.debian.org/debian/pool/main/p/${rname}/${rfile}"
-rsha256="d5822ed7297c29034f415d1cb0a958bc4476139fe196ff36bd8b705dcb9fdf3f"
-rreqs="make"
-rdldfile="${rfile//.orig.tar.gz/-3.diff.gz}"
+rver="1.53.0"
+rdir="Par-${rver}"
+rfile="${rdir}.tar.gz"
+#rurl="http://www.nicemice.net/${rname}/${rfile}"
+rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
+rsha256="c809c620eb82b589553ac54b9898c8da55196d262339d13c046f2be44ac47804"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
-function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
-  cwfetchcheck \"${rurl//${rfile}/${rdldfile}}\" \"${cwdl}/${rname}/${rdldfile}\" \"fd90eed9489f5da841bb1d7f726c3fbabe3872614ad84903c09f8b32ae744ec4\"
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cat protoMakefile > protoMakefile.ORIG
+  sed -i \"/^LINK1/s/=.*/= \${CC} -static/g\" protoMakefile
+  sed -i \"s/ cc / \${CC} \${CFLAGS} /g\" protoMakefile
+  popd >/dev/null 2>&1
 }
 "
 
 eval "
-function cwconfigure_${rname}() {
+function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  gzip -dc < \"${cwdl}/${rname}/${rdldfile}\" | patch -p1
-  for p in \$(cat debian/patches/series) ; do
-    patch -p1 < debian/patches/\${p}
-  done
-  sed -i.ORIG \"/^LINK1/s/=.*/= \${CC} -static/g\" protoMakefile
-  sed -i.ORIG \"s#/usr#${ridir}#g\" Makefile
-  sed -i 's/install -o/install -D -o/g' Makefile
-  sed -i '/install /s/-o root -g root//g' Makefile
+  make -f protoMakefile
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cwmkdir \"${ridir}/bin\"
+  cwmkdir \"${ridir}/share/doc/${rname}\"
+  cwmkdir \"${ridir}/share/man/man1\"
+  install -m 0755 \"${rname}\" \"${ridir}/bin/${rname}\"
+  install -m 0644 \"${rname}.doc\" \"${ridir}/share/doc/${rname}/${rname}.doc\"
+  install -m 0644 \"${rname}.1\" \"${ridir}/share/man/man1/${rname}.1\"
   popd >/dev/null 2>&1
 }
 "
@@ -40,5 +45,3 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
-
-unset rdldfile
