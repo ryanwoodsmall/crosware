796f04b59e435e428fbc5a9f7f9685a772680489
ryanwoodsmall <rwoodsmall@gmail.com>
2021-12-12 04:10:38 +0000

    bmake: update to 20211207

--- recipes/bmake/bmake.sh
+++ recipes/bmake/bmake.sh
@@ -4,11 +4,11 @@
 #
 
 rname="bmake"
-rver="20211024"
+rver="20211207"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://www.crufty.net/ftp/pub/sjg/${rfile}"
-rsha256="b5a3305bdb328b1383982125e90785baa30bc2ff024a1b0fda3d5d5beca82ac9"
+rsha256="05533f9249398d3947c9f3b9f854c544283802cf5aafe798cc60bd031f0875b3"
 rreqs=""
 rbdir="${cwbuild}/${rdir}/build"
 rprof="${cwetcprofd}/zz_${rname}.sh"
