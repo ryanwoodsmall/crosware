d60d901f8afb36d595bca89f580953b9dc99c28d
ryanwoodsmall <rwoodsmall@gmail.com>
2021-12-12 05:41:39 +0000

    tig: explicitly require pkgconfig

--- recipes/tig/tig.sh
+++ recipes/tig/tig.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jonas/tig/releases/download/${rdir}/${rfile}"
 rsha256="c48284d30287a6365f8a4750eb0b122e78689a1aef8ce1d2961b6843ac246aa7"
-rreqs="make ncurses readline git"
+rreqs="make ncurses readline git pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
