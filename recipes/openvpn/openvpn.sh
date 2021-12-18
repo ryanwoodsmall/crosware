71290b14680b35ab966ae7e5b50cea8d2910706f
ryanwoodsmall <rwoodsmall@gmail.com>
2021-12-18 09:45:56 +0000

    openvpn: update to 2.5.5

--- recipes/openvpn/openvpn.sh
+++ recipes/openvpn/openvpn.sh
@@ -1,9 +1,9 @@
 rname="openvpn"
-rver="2.5.4"
+rver="2.5.5"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://swupdate.openvpn.org/community/releases/${rfile}"
-rsha256="56c0dcd27ab938c4ad07469c86eb8b7408ef64c3e68f98497db8c03f11792436"
+rsha256="119bd69fa0210838f6cdaa273696dc738efa200f454dbe11eb6dfb75dfb6003b"
 rreqs="make openssl zlib lzo lz4 pkgconfig"
 
 . "${cwrecipe}/common.sh"
