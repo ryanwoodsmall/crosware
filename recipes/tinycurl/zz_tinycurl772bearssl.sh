a9c2964c5c1e63ca4f42de709d8d1de57a49795a
ryanwoodsmall <rwoodsmall@gmail.com>
2021-12-18 00:10:41 +0000

    tinycurl772bearssl: add recipe

---
+++ recipes/tinycurl/zz_tinycurl772bearssl.sh
@@ -0,0 +1,19 @@
+rname="tinycurl772bearssl"
+rver="$(cwver_tinycurl772)"
+rdir="$(cwdir_tinycurl772)"
+rfile="$(cwfile_tinycurl772)"
+rdlfile="$(cwdlfile_tinycurl772)"
+rurl="$(cwurl_tinycurl772)"
+rsha256=""
+rreqs="make bearssl zlib"
+
+. "${cwrecipe}/tinycurl/tinycurl.sh.common"
+
+for f in fetch clean extract make ; do
+  eval "
+  function cw${f}_${rname}() {
+    cw${f}_${rname%bearssl}
+  }
+  "
+done
+unset f
