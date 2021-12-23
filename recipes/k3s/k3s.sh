4302e83204b5e72e21a9f7ca1eaddb395f59cd42
ryanwoodsmall <rwoodsmall@gmail.com>
2021-12-23 21:40:14 +0000

    k3s: update to 1.22.5

--- recipes/k3s/k3s.sh
+++ recipes/k3s/k3s.sh
@@ -6,7 +6,7 @@
 #
 
 rname="k3s"
-rver="1.22.4_${rname}1"
+rver="1.22.5_${rname}1"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
@@ -15,17 +15,17 @@ rburl="https://github.com/k3s-io/k3s/releases/download/v${rver//_/%2B}"
 if [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rname}-arm64"
   rurl="${rburl}/${rfile}"
-  rsha256="86180811cc9a3aad14c68238e90f26df94b71ae308bc871e8d3aa271d931d4d0"
+  rsha256="2eb878cbc660613c2b06745eaabfa97c6b6932e2822e09ab89bccf1f016a1e0a"
   rdlfile="${cwdl}/${rname}/${rfile}_${rver}"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rname}-armhf"
   rurl="${rburl}/${rfile}"
-  rsha256="2ae4de4c27b496fc1e807480a95e9967c3af306c754631ad2bc011a8ffaa3ff1"
+  rsha256="8d5964532a66757f65ff784491727a8c283aec176e004688e8ac40c0554dc15e"
   rdlfile="${cwdl}/${rname}/${rfile}_${rver}"
 elif [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rname}"
   rurl="${rburl}/${rfile}"
-  rsha256="84bc5241f76d9468c25bb5982624df21ba7f1d6fb142d5986912dca82577d6f7"
+  rsha256="f18e0e17f11d3052f00ca4428bd548b6bf936bcfb71d8ad49a91154a6b5e460f"
   rdlfile="${cwdl}/${rname}/${rfile}-amd64_${rver}"
 fi
 unset rburl
