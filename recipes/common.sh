8899896f4697b2f9f07fe5d2037f4144faee6500
ryanwoodsmall <rwoodsmall@gmail.com>
2022-01-16 08:44:13 +0000

    bin/crosware: revamped upgrades to use a marking system, add and use a slew of new utility functions

--- recipes/common.sh
+++ recipes/common.sh
@@ -276,7 +276,8 @@ function cwupgrade_${rname}() {
   cwuninstall_${rname}
   cwscriptecho \"installing ${rname}\"
   cwinstall_${rname}
-  cwupgraded[${rname}]=1
+  cwupgraded[\"${rname}\"]=1
+  cwtoupgrade[\"${rname}\"]=0
 }
 "
 
@@ -284,22 +285,11 @@ eval "
 function cwupgradereqs_${rname}() {
   cwscriptecho \"checking req upgrades for ${rname}\"
   local r
-  declare -A u
   for r in ${rreqs} ; do
-    u[\${r}]=0
-  done
-  for r in \$(cwlistupgradable | tr -d ' ' | cut -f1 -d:) ; do
-    u[\${r}]=1
-  done
-  for r in ${rreqs} ; do
-    if [ \${u[\${r}]} -eq 1 ] ; then
-      if [ \${cwupgraded[\${r}]} -eq 0 ] ; then
-        cwupgrade_\${r}
-      fi
-    fi
+    cwcheckinstalled \${r} || cwinstall_\${r}
+    cwcheckupgradable \${r} && cwupgrade_\${r}
   done
   unset r
-  unset u
 }
 "
 
