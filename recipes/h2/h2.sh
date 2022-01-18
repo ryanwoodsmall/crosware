738ab68f411736abf8565017a9faa7c40ffa4ed3
ryanwoodsmall <rwoodsmall@gmail.com>
2022-01-18 06:03:36 +0000

    h2: update to 2.1.210

--- recipes/h2/h2.sh
+++ recipes/h2/h2.sh
@@ -7,13 +7,13 @@
 #
 
 rname="h2"
-rver="2.0.206"
+rver="2.1.210"
 rdir="${rname}-${rver}"
 rbdir="${cwbuild}/${rname}"
-rfile="${rname}-2022-01-04.zip"
+rfile="${rname}-2022-01-17.zip"
 #rurl="https://h2database.com/${rfile}"
 rurl="https://github.com/h2database/h2database/releases/download/version-${rver}/${rfile}"
-rsha256="3306748f1ba3e00b415bb6b50a9d61fd0026d6e55396940eb60c1d7c49947b6f"
+rsha256="f3a82b598526270b60ea60d439e21bd290bacc2d18d262512bb9b37748055691"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
