9f3c58c3430f1ab1b9e8548690e80615d66d289a
ryanwoodsmall <rwoodsmall@gmail.com>
2022-01-29 07:44:04 +0000

    libgpgerror: update to 1.44

--- recipes/gnupg/libgpgerror.sh
+++ recipes/gnupg/libgpgerror.sh
@@ -1,9 +1,9 @@
 rname="libgpgerror"
-rver="1.43"
+rver="1.44"
 rdir="libgpg-error-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/libgpg-error/${rfile}"
-rsha256="a9ab83ca7acc442a5bd846a75b920285ff79bdb4e3d34aa382be88ed2c3aebaf"
+rsha256="8e3d2da7a8b9a104dd8e9212ebe8e0daf86aa838cc1314ba6bc4de8f2d8a1ff9"
 rreqs="make slibtool busybox"
 
 . "${cwrecipe}/common.sh"
