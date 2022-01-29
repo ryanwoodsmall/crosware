6c3272ac4b11089d7f9e371c59d60edcbe04ecea
ryanwoodsmall <rwoodsmall@gmail.com>
2022-01-29 08:42:20 +0000

    zulu17musl: update to 17.32.13-ca-jdk17.0.2

--- recipes/zulu/zulu17musl.sh
+++ recipes/zulu/zulu17musl.sh
@@ -1,10 +1,10 @@
 # default is x86_64
-rver="17.30.15-ca-jdk17.0.1"
-rsha256="02764bb49dc7e1a01bfa3b99ba7cc20ca8403dc41e914d1a5ae43a32eb25f7b1"
+rver="17.32.13-ca-jdk17.0.2"
+rsha256="bcc5342011bd9f3643372aadbdfa68d47463ff0d8621668a0bdf2910614d95c6"
 # per-arch override rver/rdir/rfile/rurl/rsha256 here
 if [[ ${karch} =~ aarch64 ]] ; then
   rdir="zulu${rver}-linux_musl_aarch64"
-  rsha256="74e9ffa86b8cf7757501794b847133502f33d916ce07a07171317bcf97c646f1"
+  rsha256="6b920559abafbe9bdef386a20ecf3a2f318bc1f0d8359eb1f95aee26606bbc70"
 fi
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
