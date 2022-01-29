082c5f1cb159a723cc1ceb756dc9fb9fd18befa7
ryanwoodsmall <rwoodsmall@gmail.com>
2022-01-29 02:35:36 +0000

    lua54: update to 5.4.4

--- recipes/lua/lua54.sh
+++ recipes/lua/lua54.sh
@@ -1,7 +1,7 @@
-rver="5.4.3"
+rver="5.4.4"
 sv="${rver%.*}"
 sv="${sv//./}"
 rname="lua${sv}"
-rsha256="f8612276169e3bfcbcfb8f226195bfc6e466fe13042f1076cbde92b7ec96bbfb"
+rsha256="164c7849653b80ae67bec4b7473b884bf5cc8d2dca05653475ec2ed27b9ebf61"
 . "${cwrecipe}/${rname%${sv}}/${rname%${sv}}.sh.common"
 unset sv
