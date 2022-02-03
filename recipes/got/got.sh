8badf272b1a4d59b4e18318b2ff50497f0e31e0d
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-03 19:00:17 +0000

    got: notes on git interop

--- recipes/got/got.sh
+++ recipes/got/got.sh
@@ -5,6 +5,21 @@
 # XXX - need a mirror in github?
 # XXX - /usr/bin/ssh path is hardcoded in lib/dial.c as GOT_DIAL_PATH_SSH with execv used by default
 # XXX - would a git<->http(s) proxy be possible with libgit2? hmm...
+# XXX - git interop (with .cvsignore+.gitignore)
+#   mkdir blah
+#   got clone blah@blah:blah.git blah/.git
+#   got checkout -E .git .
+#   # edit
+#   got add blah
+#   got commit -m blah
+#   got send
+#   # do changes in git
+#   got fetch -a -t -v origin
+#   got update -b origin/master
+#   got rebase master
+#   # edit
+#   got commit -m msg
+#   got send
 #
 
 rname="got"
