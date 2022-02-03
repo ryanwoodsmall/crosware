dc2394c545855e44054d02fca45482f88af4e4c0
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-03 04:58:31 +0000

    rcs: update to 5.10.1, move to .lz archive and require lunzip

--- recipes/rcs/rcs.sh
+++ recipes/rcs/rcs.sh
@@ -1,10 +1,10 @@
 rname="rcs"
-rver="5.10.0"
+rver="5.10.1"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.xz"
+rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="3a0d9f958c7ad303e475e8634654974edbe6deb3a454491f3857dc1889bac5c5"
-rreqs="make sed gettexttiny ed diffutils"
+rsha256="43ddfe10724a8b85e2468f6403b6000737186f01e60e0bd62fde69d842234cc5"
+rreqs="make sed gettexttiny ed diffutils lunzip"
 
 . "${cwrecipe}/common.sh"
 
