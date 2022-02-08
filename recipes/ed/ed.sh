650a06720cca7e21aa3fa23aaae8f4c81c3eb7d6
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-08 06:21:23 +0000

    ed: update to 1.18

--- recipes/ed/ed.sh
+++ recipes/ed/ed.sh
@@ -1,9 +1,9 @@
 rname="ed"
-rver="1.17"
+rver="1.18"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="71de39883c25b6fab44add80635382a10c9bf154515b94729f4a6529ddcc5e54"
+rsha256="aca8efad9800c587724a20b97aa8fc47e6b5a47df81606feaba831b074462b4f"
 rreqs="make lunzip"
 
 . "${cwrecipe}/common.sh"
