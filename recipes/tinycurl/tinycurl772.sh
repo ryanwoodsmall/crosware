6f2dfea7ee83837dd6e7e7d0e3830d20f712423e
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-09 03:01:33 +0000

    tinycurl772: fixup wolfssl+libssh2wolfssl build, not sure why this broke (on centos? other? ugh)

--- recipes/tinycurl/tinycurl772.sh
+++ recipes/tinycurl/tinycurl772.sh
@@ -6,3 +6,11 @@ rurl="https://curl.se/tiny/${rfile}"
 rsha256="f903cfcd008615d194f80a2b1c1f261609cea9c764eb9daaff92bf99040f7730"
 rreqs="wolfssl libssh2wolfssl"
 . "${cwrecipe}/${rname%772}/${rname%772}.sh.common"
+
+eval "
+function cwpatch_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  sed -i.ORIG '/addcflags.*wolfssl/s,\$addcflags,-I${cwsw}/wolfssl/current/include,g' configure
+  popd >/dev/null 2>&1
+}
+"
