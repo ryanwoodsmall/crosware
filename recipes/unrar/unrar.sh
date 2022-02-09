08d8eb4dc821050333e6400bbb538a66419fb1a0
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-09 03:25:45 +0000

    unrar: update to 6.1.4

--- recipes/unrar/unrar.sh
+++ recipes/unrar/unrar.sh
@@ -1,9 +1,9 @@
 rname="unrar"
-rver="6.1.3"
+rver="6.1.4"
 rdir="${rname}"
 rfile="${rname}src-${rver}.tar.gz"
 rurl="https://www.rarlab.com/rar/${rfile}"
-rsha256="d05022442009202a792e588bec58921c123ff046fc755f7f2272871a5bd79636"
+rsha256="c0ed58629243961c3f1ec90c08b11ff93261e568dbfdce2bf3b759ee7a4a3b7c"
 rreqs="make"
 ridir="${cwsw}/${rname}/${rname}-${rver}"
 
