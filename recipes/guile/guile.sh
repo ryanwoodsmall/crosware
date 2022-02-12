6e762f387514eacc64a52d0fa08ff9f40e866526
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-12 04:55:13 +0000

    guile: update to 3.0.8

--- recipes/guile/guile.sh
+++ recipes/guile/guile.sh
@@ -1,9 +1,9 @@
 rname="guile"
-rver="3.0.7"
+rver="3.0.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="f57d86c70620271bfceb7a9be0c81744a033f08adc7ceba832c9917ab3e691b7"
+rsha256="daa7060a56f2804e9b74c8d7e7fe8beed12b43aab2789a38585183fcc17b8a13"
 rreqs="make sed gawk gmp libtool slibtool pkgconfig libffi gc readline ncurses libunistring"
 
 . "${cwrecipe}/common.sh"
