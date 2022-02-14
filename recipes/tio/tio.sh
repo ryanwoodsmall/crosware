aee6b0d7f6eac1e9c30ba22a5f66963fea2c71b0
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-14 01:40:47 +0000

    tio: update to 1.33, move to meson (ugh), fudge out install because meson (ugh)

--- recipes/tio/tio.sh
+++ recipes/tio/tio.sh
@@ -1,12 +1,39 @@
 rname="tio"
-rver="1.32"
+rver="1.33"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="a8f5ed6994cacb96780baa416b19e5a6d7d67e8c162a8ea4fd9eccd64984ae44"
-rreqs="make"
+rsha256="f5aec6c7ee68a3fa2a6939a4cdc6c2b6fc74e3c33bc408c22ae787468b9bef1a"
+rreqs="meson"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  \"${cwsw}/meson/current/bin/meson\" build
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  \"${cwsw}/meson/current/bin/meson\" compile -C build
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  cwmkdir \"${ridir}/bin\"
+  cwmkdir \"${ridir}/share/man/man1\"
+  install -m 755 \"build/src/${rname}\" \"${ridir}/bin/${rname}\"
+  install -m 644 \"build/man/${rname}.1\" \"${ridir}/share/man/man1/${rname}.1\"
+  popd >/dev/null 2>&1
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
