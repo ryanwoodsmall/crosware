90c55a6c62a07e092ad986121a88f824ae9dec44
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-15 15:39:17 +0000

    tio: update to 1.34

--- recipes/tio/tio.sh
+++ recipes/tio/tio.sh
@@ -1,9 +1,9 @@
 rname="tio"
-rver="1.33"
+rver="1.34"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="f5aec6c7ee68a3fa2a6939a4cdc6c2b6fc74e3c33bc408c22ae787468b9bef1a"
+rsha256="ec5f659fcab6ebab5ff3aa8737d67809441e6f69f62fc8f38ab11986908254e1"
 rreqs="meson"
 
 . "${cwrecipe}/common.sh"
