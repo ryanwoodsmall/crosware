61722a666c9cff4f028b33a19502488393b297cf
ryanwoodsmall <rwoodsmall@gmail.com>
2022-02-27 04:21:49 +0000

    wget: update to 1.21.3, pull out metalink+expat for now to avoid pulling in gpgme+deps, comments

--- recipes/wget/wget.sh
+++ recipes/wget/wget.sh
@@ -1,16 +1,20 @@
 #
 # XXX - wget2: wolfssl, bison, flex, xz, ...
+# XXX - c-ares support
+# XXX - libpsl support
 # XXX - libressl variant
 # XXX - gnutls variant
+# XXX - disabled metalink+expat 20220226
+# XXX - metalink has gpgme support, probably not worth supporting for now
 #
 
 rname="wget"
-rver="1.21.2"
+rver="1.21.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="1727a330a86acacb3e57615ce268f5f29978bf7adec4abe6a30d370207bc91b3"
-rreqs="make lunzip openssl zlib pcre2 gettexttiny pkgconfig sed expat libmetalink cacertificates"
+rsha256="dbd2fb5e47149d4752d0eaa0dac68cc49cf20d46df4f8e326ffc8f18b2af4ea5"
+rreqs="make lunzip openssl zlib pcre2 gettexttiny pkgconfig sed cacertificates"
 
 . "${cwrecipe}/common.sh"
 
@@ -20,14 +24,13 @@ function cwconfigure_${rname}() {
   ./configure ${cwconfigureprefix} \
     --disable-nls \
     --with-ssl=openssl \
-    --with-metalink \
     --with-openssl=yes \
     --with-zlib \
     --disable-pcre \
     --enable-pcre2 \
     --with-included-libunistring \
     --with-included-regex \
-      LIBS='-lexpat'
+    --without-metalink
   popd >/dev/null 2>&1
 }
 "
