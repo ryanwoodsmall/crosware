5a2dbdab3817ed819a855ed62c6e0d0a299f7466
ryanwoodsmall <rwoodsmall@gmail.com>
2022-03-06 04:45:56 +0000

    zulu11musl: update to 11.54.25-ca-jdk11.0.14.1

--- recipes/zulu/zulu11musl.sh
+++ recipes/zulu/zulu11musl.sh
@@ -1,7 +1,7 @@
-rver="11.54.23-ca-jdk11.0.14"
+rver="11.54.25-ca-jdk11.0.14.1"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="250622e83688ac2e66cb1379a00dc47708f9fbde757056329e45ac4391db14c4"
+rsha256="06b1855b5ea44407940bf2a85bc2094dc7fccd561a5058c87c57c9f1a3caff65"
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
