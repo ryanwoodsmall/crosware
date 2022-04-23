4a04013b4878a542583b17e8e28b884f2a07be6c
ryanwoodsmall <rwoodsmall@gmail.com>
2022-04-23 23:27:44 +0000

    zulu8musl: update to 8.62.0.19-ca-jdk8.0.332

--- recipes/zulu/zulu8musl.sh
+++ recipes/zulu/zulu8musl.sh
@@ -1,7 +1,7 @@
-rver="8.60.0.21-ca-jdk8.0.322"
+rver="8.62.0.19-ca-jdk8.0.332"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="168f7dbee05a8786ff381810af7b1ea6737e61833703ceca85aaafbd27d5574a"
+rsha256="62ba8f2fb152dd3a04bc8b0ea89ecf076da46779744f7997fe2720796a68c625"
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
