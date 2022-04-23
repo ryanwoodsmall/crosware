b7199b5bb61666157d008c86abbb622cc02b9b64
ryanwoodsmall <rwoodsmall@gmail.com>
2022-04-23 23:38:24 +0000

    zulu11musl: update to 11.56.19-ca-jdk11.0.15, add aarch64 support

--- recipes/zulu/zulu11musl.sh
+++ recipes/zulu/zulu11musl.sh
@@ -1,7 +1,12 @@
-rver="11.54.25-ca-jdk11.0.14.1"
+rver="11.56.19-ca-jdk11.0.15"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="06b1855b5ea44407940bf2a85bc2094dc7fccd561a5058c87c57c9f1a3caff65"
+rsha256="2179fa2d2b3016c36793e2c84de53185d2177bf2c572de0177334277f4a5e536"
+# per-arch override rver/rdir/rfile/rurl/rsha256 here
+if [[ ${karch} =~ aarch64 ]] ; then
+  rdir="zulu${rver}-linux_musl_aarch64"
+  rsha256="fcb46f70726fec697dd9628f686ccc62cb8efdeb680a653d3e36f9ba354ba3d6"
+fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
