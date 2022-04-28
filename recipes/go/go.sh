e6e26e4a4b1050fe26471f3a7c70aac6d2b2ff87
ryanwoodsmall <rwoodsmall@gmail.com>
2022-04-28 03:55:26 +0000

    go: update to 1.17.9

--- recipes/go/go.sh
+++ recipes/go/go.sh
@@ -4,30 +4,29 @@
 # XXX - generic go recipe w/CGO enabled
 # XXX - date should be moved to version?
 #
-
 rname="go"
-rver="1.17.8"
+rver="1.17.9"
 if [[ ${karch} =~ ^x86_64$ ]] ; then
   rdir="${rname}${rver}-amd64"
-  rsha256="ce1a61a7693ff22ce1388ad9bc207c2c9c9398fcc2f4da1054990bbdb5428bc9"
+  rsha256="72588b76af44dbf76036913fd4fdc0a21e2d48c4e3bae3cddb1652784e605cf3"
 elif [[ ${karch} =~ ^i.86$ ]] ; then
   rdir="${rname}${rver}-386"
-  rsha256="3e358314924fed39956720a56aaa7219915e12e34fb3676a1ee3605b944d33a1"
+  rsha256="2a6f8cf3090a7531adaa8caf1f9538386a26e449c4fda0ae0316cd416c609f0e"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rdir="${rname}${rver}-arm64"
-  rsha256="d15ab2036718dd3f15679e93648031a45858664281fda03e69c9af3dfed0f664"
+  rsha256="87869de80803494b67cb151288393bc02a7ff97f3d2d437a91945d6e5d29a220"
 elif [[ ${karch} =~ ^arm ]] ; then
   rdir="${rname}${rver}-arm"
-  rsha256="78051c30bf8168d8a7d5c0e0d5226dfcdd77c821581d984a3eb7bb2c3be1f865"
+  rsha256="c8d6fca9f798ce42cb030cec8dda9ff5313d73051d02dfcfc15d12e0480f0045"
 elif [[ ${karch} =~ ^riscv64 ]] ; then
   rdir="${rname}${rver}-riscv64"
-  rsha256="37ab11b4728423b53b2215958f54fb3d48beda5782ca6b579e4824a3ef604d13"
+  rsha256="c59a474ae0e8239eb27f3cbb447b53655eef0ecf3bbfc666ed517bd410917a7a"
 else
   rdir="none"
   rsha256="none"
 fi
 rfile="${rdir}.tar.xz"
-rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/20220319-${rname}${rver}/${rfile}"
+rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/20220427-${rname}${rver}/${rfile}"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
