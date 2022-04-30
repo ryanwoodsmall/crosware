1bb7b6f3c7f1a52f568b032feee5d955ec630903
ryanwoodsmall <rwoodsmall@gmail.com>
2022-04-30 07:32:06 +0000

    scripts/update-crosware-from-tar.sh: add script

---
+++ scripts/update-crosware-from-tar.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# update crosware from a tarball
+#
+
+set -eu
+set -o pipefail
+
+function failexit() {
+  echo "${BASH_SOURCE[0]}: ${@}" 1>&2
+  exit 1
+}
+
+reqs=( 'tar' )
+for req in ${reqs[@]} ; do
+  which ${req} >/dev/null 2>&1 || failexit "req ${req} not found"
+done
+
+: ${version:="master"}
+: ${tarfile:="${version}.tgz"}
+: ${tarurl:="https://github.com/ryanwoodsmall/crosware/tarball/${version}"}
+: ${cwtop:="/usr/local/crosware"}
+: ${cwtmp:="${cwtop}/tmp"}
+
+if [ ! -e "${cwtmp}" ] ; then
+  mkdir -p "${cwtmp}" || failexit "could not ${cwtmp} or it does not exist"
+fi
+
+pushd "${cwtmp}"
+rm -f "${tarfile}"
+curl -fkLo "${tarfile}" "${tarurl}" || wget -O "${tarfile}" "${tarurl}"
+extdir="$(tar -ztvf ${tarfile} | grep '/$' | awk '{print $NF}' | sort | head -1 || true)"
+rm -rf "${extdir}"
+tar -zxf "${tarfile}"
+( cd "${extdir}" ; tar -cf - . | ( cd "${cwtop}" ; tar -xvf - ) )
+rm -f "${tarfile}"
+rm -rf "${extdir}"
+popd
