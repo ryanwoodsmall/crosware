2534e394ec48231a58ecdc1879347d65ef70b213
ryanwoodsmall <rwoodsmall@gmail.com>
2022-05-09 08:09:40 +0000

    zulu8glibc: update to 8.62.0.19-ca-jdk8.0.332, fix glibc version to url check

--- recipes/zulu/zulu8glibc.sh
+++ recipes/zulu/zulu8glibc.sh
@@ -1,16 +1,16 @@
-rver="8.60.0.21-ca-jdk8.0.322"
+rver="8.62.0.19-ca-jdk8.0.332"
 mver="${rver%%.*}"
 rname="zulu${mver}glibc"
 rsha256=""
 
 if [[ ${uarch} =~ ^aarch64 ]] ; then
-  rsha256="0d970a27ac8f33ce70672d193f5ae6af4e5e618f0d3958d99c6d848d20a2c088"
+  rsha256="c4519172d6f5323192561108ee25c2cdef2b7ab0d7f9c807d95a7cf5d4209e84"
 elif [[ ${uarch} =~ ^arm ]] ; then
-  rsha256="d2973dd90bac84458b8f87676ee9bd3eddced3fddd11dc3f435945ef379bd7be"
+  rsha256="cc775897116405bc8405e8b7db444d9b2e4e4aaaa8bcd7ff8b2a2b87758cd6c3"
 elif [[ ${uarch} =~ ^i.86 ]] ; then
-  rsha256="52c76aa10203cad319d5737076ccadef95ba345cf6a1b097f0358e0683f9b0aa"
+  rsha256="9a755765d58e4d8ab14cf080a206815c7070cc719086f1fe41a22cdbf1374b05"
 elif [[ ${uarch} =~ ^x86_64 ]] ; then
-  rsha256="69cac729f07a383aa96e54a669715b3b488130abf8395db1c66cdf82dcbcd54e"
+  rsha256="bfe6fa049797ea996e5a7252279f4bb99d5e8f2227be6c3b98e9c78e3d916fc9"
 fi
 
 . "${cwrecipe}/${rname%${mver}glibc}/${rname//${mver}glibc/glibc}.sh.common"
