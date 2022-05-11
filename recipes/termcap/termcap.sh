8ddbbf6f50f9b80d1dad4c6728adf9dfa184920d
ryanwoodsmall <rwoodsmall@gmail.com>
2022-05-11 05:40:34 +0000

    termcap: clean up CPPFLAGS, LDFLAGS

--- recipes/termcap/termcap.sh
+++ recipes/termcap/termcap.sh
@@ -20,9 +20,18 @@ function cwpatch_${rname}() {
 eval "
 function cwconfigure_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
-    --enable-install-termcap \
-    --with-termcap=\"${ridir}/etc/termcap\"
+  env CPPFLAGS= LDFLAGS=-static \
+    ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
+      --enable-install-termcap \
+      --with-termcap=\"${ridir}/etc/termcap\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd \"${rbdir}\" >/dev/null 2>&1
+  make -j${cwmakejobs} ${rlibtool} CPPFLAGS= LDFLAGS=-static
   popd >/dev/null 2>&1
 }
 "
