86c36799ec8589054c5d239b7fa631d6c6a9086f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-06-03 04:49:59 +0000

    iptables: update to 1.8.8, workaround for ethhdr/u_int16_t, explicitly specify -lmnl in LIBS, require slibtool

--- recipes/netfilter/iptables.sh
+++ recipes/netfilter/iptables.sh
@@ -1,27 +1,36 @@
 #
 # XXX - dynamic support for modules?
+# XXX - ugly workarounds for ethhdr, u_int16_t
+# XXX - see: https://patchwork.ozlabs.org/project/openwrt/patch/1434902002-18745-1-git-send-email-amery@geeks.cl/
 #
 
 rname="iptables"
-rver="1.8.7"
+rver="1.8.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
-rsha256="c109c96bb04998cd44156622d36f8e04b140701ec60531a10668cfdff5e8d8f0"
-rreqs="bootstrapmake pkgconfig libpcap libnl libmnl libnetfilterconntrack libnfnetlink libnftnl"
+rsha256="71c75889dc710676631553eb1511da0177bbaaf1b551265b912d236c3f51859f"
+rreqs="bootstrapmake pkgconfig libpcap libnl libmnl libnetfilterconntrack libnfnetlink libnftnl slibtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --enable-bpf-compiler --enable-nfsynproxy --enable-libipq \
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
-      LIBS='-lpcap -lnl-3 -lnl-genl-3 -static'
+      LIBS='-lpcap -lnl-3 -lnl-genl-3 -lmnl -static'
+  echo '#include <sys/types.h>' >> config.h
+  echo '#include <netinet/if_ether.h>' >> config.h
+  cat iptables/xshared.h > iptables/xshared.h.ORIG
+  echo -n > iptables/xshared.h
+  echo '#include <sys/types.h>' >> iptables/xshared.h
+  echo '#include <netinet/if_ether.h>' >> iptables/xshared.h
+  cat iptables/xshared.h.ORIG >> iptables/xshared.h
   popd >/dev/null 2>&1
 }
 "
