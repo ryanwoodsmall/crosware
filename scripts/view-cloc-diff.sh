19c058c9548edb32fe54e552ded58dfc234a2ba2
ryanwoodsmall <rwoodsmall@gmail.com>
2022-06-04 05:19:08 +0000

    scripts/view-cloc-diff.sh: note on diffutils

--- scripts/view-cloc-diff.sh
+++ scripts/view-cloc-diff.sh
@@ -2,5 +2,7 @@
 #
 # show the differnces between runs of ${cwtop}/scripts/crosware-cloc-source.sh
 #
+# XXX - you _really_ want diffutils diff here. busybox is slowww...
+#
 
 vim <(diff -Naur ${cwtop}/tmp/crosware.set.out{.old,} | egrep -v -- '^(-|\+| )(cw.*=\(|(PATH|PKG_CONFIG_(LIBDIR|PATH)|(C(|XX|PP)|LD)FLAGS)=)')
