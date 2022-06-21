56fef97ad527b50169910b70f7f131350a2c7139
ryanwoodsmall <rwoodsmall@gmail.com>
2022-06-21 02:50:36 +0000

    tio: update to 1.40

--- recipes/tio/tio.sh
+++ recipes/tio/tio.sh
@@ -1,9 +1,9 @@
 rname="tio"
-rver="1.39"
+rver="1.40"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="28cffbe6a09440b233e941688b5828459878a4c390720bf444f3d3905c2a7094"
+rsha256="beafc4763552ff7cf3a368e8af36258018ef85fac32133c4635051d7615ab527"
 rreqs="ninja meson inih pkgconfig"
 
 . "${cwrecipe}/common.sh"
