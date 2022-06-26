d6c78b77f0248ebfe8df4f3d3cec9c1a9ce1ad39
ryanwoodsmall <rwoodsmall@gmail.com>
2022-06-26 05:21:51 +0000

    scripts/fakecurl.sh: add overridable ${wgetopts}, notes on insecure downloads, comment on methodology

--- scripts/fakecurl.sh
+++ scripts/fakecurl.sh
@@ -3,11 +3,15 @@
 # fake curl that uses busybox/gnu wget
 # might be enough for cwfetch()?
 #
+# XXX - add --no-check-certificate for curl -k interop by default?
+#
 
 echo "this is fake curl" 2>&1
 
 set -eu
 set -o pipefail
+
+: ${wgetopts:=""}
 
 declare -a a=()
 for i in $(seq 0 $((${#}-1))) ; do
@@ -15,10 +19,13 @@ for i in $(seq 0 $((${#}-1))) ; do
   shift
 done
 
+# args come in from cwfetch as:
+#   cwfetch "scheme://url/file.ext" "/full/path/to/save/file.ext"
+# figure target item as last input array element, url item as next to last
 ti="${#a[@]}"
 ((ti--))
 ui="${#a[@]}"
 ((ui--))
 ((ui--))
 
-wget -O "${a[${ti}]}" "${a[${ui}]}"
+wget ${wgetopts} -O "${a[${ti}]}" "${a[${ui}]}"
