8048594b85ca5309ad373cf446c5029e906a63fa
ryanwoodsmall <rwoodsmall@gmail.com>
2022-07-09 03:59:15 +0000

    gnupg: update to 2.2.36, use cw..._ functions

--- recipes/gnupg/gnupg.sh
+++ recipes/gnupg/gnupg.sh
@@ -4,18 +4,18 @@
 #
 
 rname="gnupg"
-rver="2.2.35"
+rver="2.2.36"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="340bc255938971e6e729b3d9956fa2ef4db8215d77693bf300df2bb302498690"
+rsha256="bdfe783810fceca9703b9e811817acca63ee9ef0174e616598e8ea6590aa4c9c"
 rreqs="make libgpgerror libgcrypt libksba libassuan npth ntbtls sqlite readline ncurses slibtool zlib bzip2 pkgconfig pinentry configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/libgpgerror/current/bin:${cwsw}/libgcrypt/current/bin:${cwsw}/libassuan/current/bin:${cwsw}/libksba/current/bin:${cwsw}/npth/current/bin:${cwsw}/ntbtls/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} \
       --disable-ccid-driver \
@@ -40,12 +40,12 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
   local p
-  for p in \$(find ${ridir}/bin/ ! -type d) ; do
+  for p in \$(find \$(cwidir_${rname})/bin/ ! -type d) ; do
     p=\$(basename \${p})
-    ln -sf \${p} ${ridir}/bin/\${p}${rver%%.*}
+    ln -sf \${p} \$(cwidir_${rname})/bin/\${p}${rver%%.*}
   done
   unset p
   popd >/dev/null 2>&1
