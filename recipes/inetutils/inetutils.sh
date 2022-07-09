2b8b1b82e656c1fd5808eb7eec7d6c1bb66dedb4
ryanwoodsmall <rwoodsmall@gmail.com>
2022-07-09 03:11:38 +0000

    inetutils: update to 2.3, use cw?..._ funcs

--- recipes/inetutils/inetutils.sh
+++ recipes/inetutils/inetutils.sh
@@ -16,35 +16,35 @@
 #
 
 rname="inetutils"
-rver="2.2"
+rver="2.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="d547f69172df73afef691a0f7886280fd781acea28def4ff4b4b212086a89d80"
+rsha256="0b01bb08e29623c4e3b940f233c961451d9af8c5066301add76a52a95d51772c"
 rreqs="make sed netbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(cwsha256_${rname})\"
   cwfetchcheck \"https://raw.githubusercontent.com/bminor/glibc/glibc-2.33/inet/protocols/talkd.h\" \"${cwdl}/${rname}/talkd.h\" \"d820ecf039d562d6c29325bf0c4839e2ce6d9f9ad8773bd243e656b5c8c48a38\"
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
-  pushd '${rbdir}' >/dev/null 2>&1
-  cwmkdir \"${rbdir}/include/protocols\"
-  cwmkdir \"${rbdir}/include/bits/types\"
-  install -m 0644 \"${cwdl}/${rname}/talkd.h\" \"${rbdir}/include/protocols/talkd.h\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwmkdir \"\$(cwbdir_${rname})/include/protocols\"
+  cwmkdir \"\$(cwbdir_${rname})/include/bits/types\"
+  install -m 0644 \"${cwdl}/${rname}/talkd.h\" \"\$(cwbdir_${rname})/include/protocols/talkd.h\"
   echo 'struct osockaddr { unsigned short int sa_family; unsigned char sa_data[14]; };' > include/bits/types/struct_osockaddr.h
   sed -i.ORIG \"s/LIBTERMCAP=-lcurses/LIBTERMCAP='-lcurses -lterminfo'/g\" configure
   sed -i 's/-lcurses/-lreadline -lcurses -lterminfo/g' configure
   sed -i 's/-lterminfo -lterminfo/-lterminfo/g' configure
   sed -i \"s/ LIBREADLINE=\$/LIBREADLINE='-lreadline -lcurses -lterminfo'/g\" configure
   ./configure ${cwconfigureprefix} \
-    --libexecdir='${ridir}/sbin' \
+    --libexecdir=\"\$(cwidir_${rname})/sbin\" \
     --enable-ipv4 \
     --enable-ipv6 \
     --enable-servers \
@@ -58,7 +58,7 @@ function cwconfigure_${rname}() {
     --disable-silent-rules \
     --without-idn \
     --with-libreadline-prefix=\"${cwsw}/netbsdcurses/current\" \
-      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${rbdir}/include\" \
+      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I\$(cwbdir_${rname})/include\" \
       LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib/ -static\" \
       LIBS=\"-L${cwsw}/netbsdcurses/current/lib/ -lreadline -lcurses -lterminfo\"
   popd >/dev/null 2>&1
@@ -67,9 +67,9 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install-strip
-  find ${ridir}/bin/ ${ridir}/sbin/ | xargs chmod a+x
+  find \$(cwidir_${rname})/bin/ \$(cwidir_${rname})/sbin/ | xargs chmod a+x
   popd >/dev/null 2>&1
 }
 "
