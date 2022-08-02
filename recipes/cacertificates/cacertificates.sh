0593a49fa0fd450fccc9c762dff50eb861fcbfac
ryanwoodsmall <rwoodsmall@gmail.com>
2022-08-02 05:40:24 +0000

    cacertificates: update to 20220614-r0

--- recipes/cacertificates/cacertificates.sh
+++ recipes/cacertificates/cacertificates.sh
@@ -5,13 +5,13 @@
 # XXX - move to curl cacert https://curl.haxx.se/docs/caextract.html
 #
 rname="cacertificates"
-rver="20211220-r0"
+rver="20220614-r0"
 rdir="${rname}-${rver}"
 #rfile="ca-certificates-cacert-${rver}.apk"
 #rurl="https://mirrors.edge.kernel.org/alpine/v3.11/main/x86_64/${rfile}"
 rfile="ca-certificates-bundle-${rver}.apk"
-rurl="https://mirrors.edge.kernel.org/alpine/v3.15/main/x86_64/${rfile}"
-rsha256="2612c7d0f8b18b1be1c7e49f2318214e47edaf7076ee1b63ddbf9bd011a3ea35"
+rurl="https://mirrors.edge.kernel.org/alpine/v3.16/main/x86_64/${rfile}"
+rsha256="9434141d9289fba97c8b10b4e68c7c5664f36636694430a6655c001f1fe3e8cc"
 rreqs=""
 rdlfile="${cwdl}/${rname}/${rfile}.tar.gz"
 
