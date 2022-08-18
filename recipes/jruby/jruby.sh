aeaacc6c8331e0ca3d58859d46a6a8139699707d
ryanwoodsmall <rwoodsmall@gmail.com>
2022-08-18 05:13:29 +0000

    jruby: update to 9.3.7.0, use cw?dir_ funcs

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="9.3.6.0"
+rver="9.3.7.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/${rname}/${rname}-dist/${rver}/${rfile}"
-rsha256="747af6af99a674f208f40da8db22d77c6da493a83280e990b52d523abd9499e2"
+rsha256="94a7a8b3beeac2253a8876e73adfac6bececb2b54d2ddfa68f245dc81967d0c1"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
@@ -20,7 +20,7 @@ function cwinstall_${rname}() {
   cwfetch_${rname}
   cwcheckreqs_${rname}
   cwsourceprofile
-  cwextract \"${rdlfile}\" \"${rtdir}\"
+  cwextract \"\$(cwdlfile_${rname})\" \"${rtdir}\"
   cwlinkdir_${rname}
   cwgenprofd_${rname}
   cwmarkinstall_${rname}
