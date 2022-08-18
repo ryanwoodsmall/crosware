ca5a95e863179ac196874341e7a96a759817b07c
ryanwoodsmall <rwoodsmall@gmail.com>
2022-08-18 01:17:26 +0000

    lz4: update to 1.9.4, use cw?dir_ funcs

--- recipes/lz4/lz4.sh
+++ recipes/lz4/lz4.sh
@@ -1,16 +1,16 @@
 rname="lz4"
-rver="1.9.3"
+rver="1.9.4"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
+rsha256="0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's/ln -s/ln -sf/g' Makefile.inc
   sed -i 's/ln -sff/ln -sf/g' Makefile.inc
   popd >/dev/null 2>&1
@@ -19,16 +19,16 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make -j${cwmakejobs} ${rlibtool} {PREFIX,prefix}=\"${ridir}\" BUILD_SHARED=no
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make -j${cwmakejobs} ${rlibtool} {PREFIX,prefix}=\"\$(cwidir_${rname})\" BUILD_SHARED=no
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make install ${rlibtool} {PREFIX,prefix}=\"${ridir}\" BUILD_SHARED=no
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install ${rlibtool} {PREFIX,prefix}=\"\$(cwidir_${rname})\" BUILD_SHARED=no
   popd >/dev/null 2>&1
 }
 "
