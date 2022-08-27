9149030f74a1a78f3956f15480cab359ac6f8f15
ryanwoodsmall <rwoodsmall@gmail.com>
2022-08-27 06:16:00 +0000

    tig: update to 2.5.7

--- recipes/tig/tig.sh
+++ recipes/tig/tig.sh
@@ -1,9 +1,9 @@
 rname="tig"
-rver="2.5.6"
+rver="2.5.7"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jonas/tig/releases/download/${rdir}/${rfile}"
-rsha256="50bb5f33369b50b77748115c730c52b13e79b2de49cba7167bb634eb683d965f"
+rsha256="dbc7bac86b29098adaa005a76161e200f0734dda36de9f6bd35a861c7c29ca76"
 rreqs="make ncurses readline git pkgconfig pcre2"
 
 . "${cwrecipe}/common.sh"
