1a07b6315caabc8fc36e5f4290352b1f6b1a40dc
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-03 03:51:14 +0000

    zulu8musl: update to 8.0.345, add aarch64 support

--- recipes/zulu/zulu8musl.sh
+++ recipes/zulu/zulu8musl.sh
@@ -1,7 +1,12 @@
-rver="8.62.0.19-ca-jdk8.0.332"
+rver="8.64.0.19-ca-jdk8.0.345"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="62ba8f2fb152dd3a04bc8b0ea89ecf076da46779744f7997fe2720796a68c625"
+rsha256="991d08ace913943640dc2f27826a33bd4c4c3eef1950ff7e2b51ebe0ffea05e8"
+# per-arch override rver/rdir/rfile/rurl/rsha256 here
+if [[ ${karch} =~ aarch64 ]] ; then
+  rdir="zulu${rver}-linux_musl_aarch64"
+  rsha256="2b8fc4a048494244f9b6ccde6a9b89c159f8027ae1b441163c2b13fae9e911b8"
+fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
 
