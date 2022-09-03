086d7f19d8a7cd5092f26a54a22c96966cab344f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-03 01:15:32 +0000

    go117: update to 1.17.13

--- recipes/go/go117.sh
+++ recipes/go/go117.sh
@@ -1,24 +1,24 @@
 rname="go117"
-rver="1.17.10"
+rver="1.17.13"
 if [[ ${karch} =~ ^x86_64$ ]] ; then
   rdir="go${rver}-amd64"
-  rsha256="18325c11cb9ed74e21cd5f408afc0347441a7e7e3aed81007422c2ae9a27db37"
+  rsha256="b6593b673a2988e50211322ea0ebf9ed0c210f90ce51ea14fc69cb95f7766fc5"
 elif [[ ${karch} =~ ^i.86$ ]] ; then
   rdir="go${rver}-386"
-  rsha256="3f012472ab732ffd355d200671aeffba80524c61d5176638392cf66ecc703ff0"
+  rsha256="dff190615646d4e48269bc40d6c54a3c1127182fc73e8cb47c4ff271324e15a6"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rdir="go${rver}-arm64"
-  rsha256="be41b9d13ad4742da3259951cfed8a5a1ac78a4b1514ed17864dae883449d1d9"
+  rsha256="19cdb0a2288d0d6624d5e4c551d595f2dff564c783cb0b3e85ab9a1751da99ac"
 elif [[ ${karch} =~ ^arm ]] ; then
   rdir="go${rver}-arm"
-  rsha256="43aaee1fb0e925d520148fce1de7395b0474116235f37cb4b41b6a22aa3dc30d"
+  rsha256="fabad595a46edc97c3348a583be85215c84a08d9f44c59f225038616ae8fd16f"
 elif [[ ${karch} =~ ^riscv64 ]] ; then
   rdir="go${rver}-riscv64"
-  rsha256="4dccbe0b7de386699e860eefffcc44e4939fac7435d00076990f418490a4ee07"
+  rsha256="cd9731c57cc483a878fbe9dfc50339f9d423fa112d5b3e5724e32038edb7e2ba"
 fi
-rdate="20220511"
+rdate="20220902"
 rfile="${rdir}.tar.xz"
-rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/${rdate}-go1.18.2/${rfile}"
+rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/${rdate}-go1.19/${rfile}"
 
 . "${cwrecipe}/go/go.sh.common"
 
