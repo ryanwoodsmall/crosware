554bc548abc141530c6d81c2c5315a51407d9c5e
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-04 00:01:30 +0000

    grep: update to 3.8, use cw?dir_ funcs, require pcre2 and pkgconfig, comment out "obsolescent" message (come on)

--- recipes/grep/grep.sh
+++ recipes/grep/grep.sh
@@ -1,16 +1,16 @@
 rname="grep"
-rver="3.7"
+rver="3.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="5c10da312460aec721984d5d83246d24520ec438dd48d7ab5a05dbc0d6d6823c"
-rreqs="make pcre sed"
+rsha256="498d7cc1b4fb081904d87343febb73475cf771e424fb7e6141aff66013abc382"
+rreqs="make pcre2 sed pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} --program-prefix=g --disable-nls
   popd >/dev/null 2>&1
 }
@@ -18,11 +18,13 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install
-  ln -sf g${rname} ${ridir}/bin/${rname}
-  ln -sf ge${rname} ${ridir}/bin/e${rname}
-  ln -sf gf${rname} ${ridir}/bin/f${rname}
+  ln -sf g${rname} \"\$(cwidir_${rname})/bin/${rname}\"
+  ln -sf ge${rname} \"\$(cwidir_${rname})/bin/e${rname}\"
+  ln -sf gf${rname} \"\$(cwidir_${rname})/bin/f${rname}\"
+  sed -i '/obsolescent/s,^,#,g' \"\$(cwidir_${rname})/bin/ge${rname}\"
+  sed -i '/obsolescent/s,^,#,g' \"\$(cwidir_${rname})/bin/gf${rname}\"
   popd >/dev/null 2>&1
 }
 "
