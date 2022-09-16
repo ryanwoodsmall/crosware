cae78244054e4a8f5bfacba8b5232540f4b879d5
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-16 02:42:46 +0000

    hitch: update to 1.7.3, use cw?dir_ funcs

--- recipes/hitch/hitch.sh
+++ recipes/hitch/hitch.sh
@@ -5,18 +5,18 @@
 #
 
 rname="hitch"
-rver="1.7.2"
+rver="1.7.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://hitch-tls.org/source/${rfile}"
-rsha256="dcd65cbec6287c622a5a317d5f37e1ea5526daa93fb4db28f4206fc9911a1785"
+rsha256="1a1bf4955d775b718dc31c89a1a05176530b0ca856f95ee42a2bc7a23f289787"
 rreqs="make openssl libev pkgconfig zlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --enable-tcp-fastopen
   popd >/dev/null 2>&1
