71a42328fc56097c66c3271e7c5b8eb33f108deb
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-17 02:44:27 +0000

    etcd: udpate to 3.5.5, use cw?dir_ funcs

--- recipes/etcd/etcd.sh
+++ recipes/etcd/etcd.sh
@@ -1,34 +1,21 @@
 rname="etcd"
-rver="3.5.4"
+rver="3.5.5"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/etcd-io/${rname}/archive/refs/tags/${rfile}"
-rsha256="10e8c1604578d4a8557263d61d4fbbd2428c8cd8dda3c8a23a8755314af6ef10"
+rsha256="1a16ab899c2381f3ed68f7c38ab878edd391397f7ff987b22a9cd50aab092b79"
 rreqs="go"
 
 . "${cwrecipe}/common.sh"
 
-# XXX - segfaults on compile/link for riscv64, problem with my go? qemu? cross-compile problem?
-if [[ ${karch} =~ ^riscv64 ]] ; then
-eval "
-function cwinstall_${rname}() {
-  cwscriptecho \"recipe ${rname} does not support architecture ${karch}\"
-}
-"
-fi
-
-eval "
-function cwconfigure_${rname}() {
-  true
-}
-"
+cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  : \${GOCACHE=\"${rbdir}/gocache\"}
-  : \${GOMODCACHE=\"${rbdir}/gomodcache\"}
-  cwmkdir \"${rbdir}/bin\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
+  : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
+  cwmkdir \"\$(cwbdir_${rname})/bin\"
   for s in server:etcd etcdutl:etcdutl etcdctl:etcdctl ; do
     d=\${s%:*}
     p=\${s#*:}
@@ -37,11 +24,11 @@ function cwmake_${rname}() {
       PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
-      GOPATH=\"${rbdir}/gopath\" \
+      GOPATH=\"\$(cwbdir_${rname})/gopath\" \
       GOROOT=\"${cwsw}/go/current\" \
-      COMMIT=\"${rver}\" \
-        \"${cwsw}/go/current/bin/go\" build -v -ldflags='-w -s' -o=\"${rbdir}/bin/\${p}\"
-    cd \"${rbdir}\"
+      COMMIT=\"\$(cwver_${rname})\" \
+        \"${cwsw}/go/current/bin/go\" build -v -ldflags='-w -s' -o=\"\$(cwbdir_${rname})/bin/\${p}\"
+    cd \"\$(cwbdir_${rname})\"
   done
   chmod -R u+rw . || true
   popd >/dev/null 2>&1
@@ -50,9 +37,9 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/bin\"
-  install -m 0755 bin/${rname}* \"${ridir}/bin/\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 0755 bin/${rname}* \"\$(cwidir_${rname})/bin/\"
   popd >/dev/null 2>&1
 }
 "
