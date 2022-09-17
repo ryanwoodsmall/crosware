0005f744c01d48e3f6f2268b06ff45cd2c4e15e7
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-17 02:14:55 +0000

    kuroko: update to 1.3.0, use cw?dir_ funcs

--- recipes/kuroko/kuroko.sh
+++ recipes/kuroko/kuroko.sh
@@ -3,11 +3,11 @@
 #
 
 rname="kuroko"
-rver="1.2.5"
+rver="1.3.0"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}-lang/${rname}/archive/refs/tags/${rfile}"
-rsha256="fa068e5a12c5153e1fc4c667f5efb83651c7b491c804870bb873c65c484e6e0a"
+rsha256="7a41b0caa9b56c810ee3c5de3aefc6e71b95f4e6a667add92306f7b3e59515bc"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -20,23 +20,23 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwpatch_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG \"s,/usr/local,${ridir},g\" Makefile
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  sed -i.ORIG \"s,/usr/local,\$(cwidir_${rname}),g\" Makefile
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  env CPPFLAGS= LDFLAGS= CFLAGS='-Wl,-rpath,${rtdir}/current/lib -fPIC' make
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  env CPPFLAGS= LDFLAGS= CFLAGS=\"-Wl,-rpath,\$(cwidir_${rname})/current/lib -fPIC\" make
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install-strip ${rlibtool}
   cwmakeinstall_${rname}_static
   popd >/dev/null 2>&1
@@ -45,10 +45,10 @@ function cwmakeinstall_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}_static() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make clean
   cat Makefile.ORIG > Makefile
-  sed -i.ORIG \"s,/usr/local,${ridir}/static,g\" Makefile
+  sed -i.ORIG \"s,/usr/local,\$(cwidir_${rname})/static,g\" Makefile
   env CPPFLAGS= LDFLAGS=-static CFLAGS=\"\${CFLAGS} -static\" CC=\"\${CC} -DSTATIC_ONLY\" make
   make install-strip ${rlibtool}
   popd >/dev/null 2>&1
