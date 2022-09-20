83b35f55b3e711431c08425ffd2f7fc7318aa928
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-20 18:54:37 +0000

    expat: update to 2.4.9, use cw?dir_ funcs

--- recipes/expat/expat.sh
+++ recipes/expat/expat.sh
@@ -1,16 +1,16 @@
 rname="expat"
-rver="2.4.8"
+rver="2.4.9"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://github.com/libexpat/libexpat/releases/download/R_${rver//./_}/${rfile}"
-rsha256="a247a7f6bbb21cf2ca81ea4cbb916bfb9717ca523631675f99b3d4a5678dcd16"
+rsha256="7f44d1469b110773a94b0d5abeeeffaef79f8bd6406b07e52394bcf48126437a"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --without-docbook
   popd >/dev/null 2>&1
 }
@@ -18,9 +18,9 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
-  sed -i \"s#${ridir}#${rtdir}/current#g\" \"${ridir}/lib/lib${rname}.la\" \"${ridir}/lib/pkgconfig/${rname}.pc\"
+  sed -i \"s#\$(cwidir_${rname})#${rtdir}/current#g\" \"\$(cwidir_${rname})/lib/lib${rname}.la\" \"\$(cwidir_${rname})/lib/pkgconfig/${rname}.pc\"
   popd >/dev/null 2>&1
 }
 "
