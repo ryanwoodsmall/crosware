332ed4999527c68cbae7d40ff5ab387216e44894
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-21 00:57:57 +0000

    libffi: update to 3.4.3, use cw?dir_ funcs

--- recipes/libffi/libffi.sh
+++ recipes/libffi/libffi.sh
@@ -1,16 +1,16 @@
 rname="libffi"
-rver="3.4.2"
+rver="3.4.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="540fb721619a6aba3bdeef7d940d8e9e0e6d2c193595bc243241b77ff9e93620"
+rsha256="4416dd92b6ae8fcb5b10421e711c4d3cb31203d77521a77d85d0102311e6c3b8"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
   sed -i.ORIG 's/__gnu_linux__/__linux__/g' src/closures.c
   popd >/dev/null 2>&1
