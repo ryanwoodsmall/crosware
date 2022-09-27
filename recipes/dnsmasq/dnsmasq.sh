767e801a64149dd3b3ee45e95af68e239e08ba03
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-27 20:28:45 +0000

    dnsmasq: update to 2.87, use cw?dir_ funcs

--- recipes/dnsmasq/dnsmasq.sh
+++ recipes/dnsmasq/dnsmasq.sh
@@ -2,19 +2,19 @@
 # XXX - libidn/libidn2 stuff
 #
 rname="dnsmasq"
-rver="2.86"
+rver="2.87"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="http://www.thekelleys.org.uk/${rname}/${rfile}"
-rsha256="28d52cfc9e2004ac4f85274f52b32e1647b4dbc9761b82e7de1e41c49907eb08"
+rsha256="0228c0364a7f2356fd7e7f1549937cbf3099a78d3b2eb1ba5bb0c31e2b89de7a"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG 's#^PREFIX.*#PREFIX = ${ridir}#g' Makefile
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  sed -i.ORIG \"s#^PREFIX.*#PREFIX = \$(cwidir_${rname})#g\" Makefile
   sed -i.ORIG 's#<sys/poll.h>#<poll.h>#g' src/dnsmasq.h
   sed -i.ORIG 's#/etc/dnsmasq.conf#${cwetc}/${rname}.conf#g' src/config.h
   popd >/dev/null 2>&1
@@ -23,7 +23,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make -j${cwmakejobs} CFLAGS=\"\${CFLAGS} -Wall -W -O2\" LDFLAGS=\"-static\"
   popd >/dev/null 2>&1
 }
@@ -31,7 +31,7 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
   test -e \"${cwetc}/${rname}.conf\" || install -m 0644 ${rname}.conf.example \"${cwetc}/${rname}.conf\"
   popd >/dev/null 2>&1
