2fb722d5ac15cde066ebb663eff3984541402ed6
ryanwoodsmall <rwoodsmall@gmail.com>
2022-09-27 20:20:11 +0000

    readline: update to 8.2, use cw?dir_ funcs

--- recipes/readline/readline.sh
+++ recipes/readline/readline.sh
@@ -1,16 +1,16 @@
 rname="readline"
-rver="8.1.2"
+rver="8.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="7589a2381a8419e68654a47623ce7dfcb756815c8fee726b98f90bf668af7bc6"
+rsha256="3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35"
 rreqs="make ncurses sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --with-curses ${cwconfigurefpicopts}
   popd >/dev/null 2>&1
 }
