87316c757181829e4e95dfa47ab2b8e031461a2b
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-01 04:10:56 +0000

    wolfmqtt: update to 1.14.1, use cw?dir_ funcs

--- recipes/wolfmqtt/wolfmqtt.sh
+++ recipes/wolfmqtt/wolfmqtt.sh
@@ -1,16 +1,16 @@
 rname="wolfmqtt"
-rver="1.14.0"
+rver="1.14.1"
 rdir="${rname//mqtt/MQTT}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="b26577186c2d5e8d543dc7e575569271d298c1120aa6f97fbc4193e7a902247a"
+rsha256="658938e87db0a5a06559f4fb7c3fe270ae4d1b6f5e1ff48840d0e1d8f422f812"
 rreqs="make wolfssl configgit toybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --enable-all \
     --with-libwolfssl-prefix=\"${cwsw}/wolfssl/current\" \
@@ -23,16 +23,16 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
-  mkdir -p \"${ridir}/bin\"
+  mkdir -p \"\$(cwidir_${rname})/bin\"
   for i in \$(find examples/ -type f -exec \"${cwsw}/toybox/current/bin/toybox\" file {} + | awk -F: '/ELF.*executable/{print \$1}') ; do
     n=\"\$(basename \${i})\"
-    install -m 0755 \"\${i}\" \"${ridir}/bin/${rname}-\${n}\"
+    install -m 0755 \"\${i}\" \"\$(cwidir_${rname})/bin/${rname}-\${n}\"
   done
   unset i n
-  sed -i 's,${ridir},${rtdir}/current,g' \"${ridir}/bin/${rname}-config\"
-  sed -i 's,-lwolfssl,-L${cwsw}/wolfssl/current/lib -lwolfssl,g' \"${ridir}/bin/${rname}-config\"
+  sed -i \"s,\$(cwidir_${rname}),${rtdir}/current,g\" \"\$(cwidir_${rname})/bin/${rname}-config\"
+  sed -i 's,-lwolfssl,-L${cwsw}/wolfssl/current/lib -lwolfssl,g' \"\$(cwidir_${rname})/bin/${rname}-config\"
   popd >/dev/null 2>&1
 }
 "
