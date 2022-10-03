026e4ac0fe22d765c840bccf738478182ef59c20
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-03 03:33:49 +0000

    libxml2: update to 2.10.0, new config options, use cw?dir_funcs

--- recipes/libxml2/libxml2.sh
+++ recipes/libxml2/libxml2.sh
@@ -1,27 +1,27 @@
 rname="libxml2"
-rver="2.9.14"
+rver="2.10.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
-#rurl="ftp://xmlsoft.org/${rname}/${rfile}"
-#rurl="https://sources.voidlinux.org/${rdir}/${rfile}"
-#rurl="http://xmlsoft.org/sources/${rfile}"
 rurl="https://download.gnome.org/sources/${rname}/${rver%.*}/${rfile}"
-rsha256="60d74a257d1ccec0475e749cba2f21559e48139efba6ff28224357c7c798dfee"
+rsha256="2dd33110ea778676de14bea4999ee1173c4ca55d5ff1452bca224e06f0152595"
 rreqs="make xz zlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --without-python
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --with-ftp \
+    --with-legacy \
+    --without-python
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include/${rname}\"' >> \"${rprof}\"
