189a3b9723d67fd7e908682b1ec64e70973f40d2
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 21:33:49 +0000

    gnupg1: require readlinenetbsdcurses, fixup flags

--- recipes/gnupg/gnupg1.sh
+++ recipes/gnupg/gnupg1.sh
@@ -4,23 +4,24 @@ rdir="${rname%1}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname%1}/${rfile}"
 rsha256="c9462f17e651b6507848c08c430c791287cd75491f8b5a8b50c6ed46b12678ba"
-rreqs="make zlib bzip2 netbsdcurses slibtool configgit"
+rreqs="make zlib bzip2 netbsdcurses readlinenetbsdcurses slibtool configgit"
+rprof="${cwetcprofd}/zz_${rname}.sh"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} \
     --program-suffix=1 \
     --disable-ldap \
     --disable-nls \
     --without-libcurl \
     --with-bzip2=\"${cwsw}/bzip2/current\" \
-    --with-readline=\"${cwsw}/netbsdcurses/current\" \
+    --with-readline=\"${cwsw}/readlinenetbsdcurses/current\" \
     --with-zlib=\"${cwsw}/zlib/current\" \
-      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
-      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib\" \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       LIBS='-lreadline -lterminfo -lcurses -static'
   popd >/dev/null 2>&1
 }
@@ -28,7 +29,10 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+  rm -f \"${cwetcprofd}/${rname}.sh\"
+  echo 'append_path \"${cwsw}/gnupg/current/bin\"' > \"${rprof}\"
+  echo 'append_path \"${cwsw}/gnupg/current/sbin\"' >> \"${rprof}\"
+  echo 'append_path \"${rtdir}/current/bin\"' >> \"${rprof}\"
   echo 'append_path \"${rtdir}/current/sbin\"' >> \"${rprof}\"
 }
 "
