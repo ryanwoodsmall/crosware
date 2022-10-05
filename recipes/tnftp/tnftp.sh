ada05289058a36c1d2b9d813af577ef6a4db2d71
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 22:54:46 +0000

    tnftp: require libeditnetbsdcurses, fixup flags

--- recipes/tnftp/tnftp.sh
+++ recipes/tnftp/tnftp.sh
@@ -4,29 +4,29 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.netbsd.org/pub/NetBSD/misc/${rname}/${rfile}"
 rsha256="101901e90b656c223ec8106370dd0d783fb63d26aa6f0b2a75f40e86a9f06ea2"
-rreqs="make netbsdcurses configgit"
+rreqs="make netbsdcurses libeditnetbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} ${cwconfigurelibopts} \
     --disable-ssl \
     --enable-editcomplete \
     --without-socks \
-      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
-      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib/ -static\" \
-      LIBS=\"-L${cwsw}/netbsdcurses/current/lib/ -ledit -lcurses -lterminfo\"
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+      LIBS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -ledit -lcurses -lterminfo\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make -j${cwmakejobs} ${rlibtool} \
-    CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I. -I..\"
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include) -I. -I..\"
   popd >/dev/null 2>&1
 }
 "
