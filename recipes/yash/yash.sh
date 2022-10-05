210bafce46d4d6b6e3ef3c85ef0c45f17af4b4a9
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 22:57:50 +0000

    yash: require libeditnetbsdcurses, fixup flags

--- recipes/yash/yash.sh
+++ recipes/yash/yash.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/magicant/${rname}/releases/download/${rver}/${rfile}"
 rsha256="e430ee845dfd7711c4f864d518df87dd78b40560327c494f59ccc4731585305d"
-rreqs="make netbsdcurses"
+rreqs="make netbsdcurses libeditnetbsdcurses"
 
 . "${cwrecipe}/common.sh"
 
@@ -15,8 +15,8 @@ function cwconfigure_${rname}() {
     --disable-nls \
     --enable-{array,dirstack,double-bracket,help,history,lineedit,printf,socket,test,ulimit} \
     --with-term-lib='edit curses terminfo' \
-      CC=\"\${CC} \${CFLAGS} -L${cwsw}/netbsdcurses/current/lib -I${cwsw}/netbsdcurses/current/include\" \
-      LDFLAGS=\"-static\" \
+      CC=\"\${CC} \${CFLAGS} \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) \$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=-static \
       LIBS='-ledit -lcurses -lterminfo -static' \
       CPPFLAGS=
   popd >/dev/null 2>&1
