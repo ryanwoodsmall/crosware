32fc3a3505c26ad1c64c08db07f18b9858ad2389
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 21:23:57 +0000

    dash: require libeditnetbsdcurses, fixup flags

--- recipes/dash/dash.sh
+++ recipes/dash/dash.sh
@@ -4,20 +4,20 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://gondor.apana.org.au/~herbert/${rname}/files/${rfile}"
 rsha256="db778110891f7937985f29bf23410fe1c5d669502760f584e54e0e7b29e123bd"
-rreqs="make netbsdcurses byacc"
+rreqs="make netbsdcurses libeditnetbsdcurses byacc"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     --enable-static \
     --with-libedit \
     --disable-silent-rules \
-      CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
-      CFLAGS=\"\${CFLAGS} -O2 -L${cwsw}/netbsdcurses/current/lib\" \
-      LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -ledit -lcurses -lterminfo -static\" \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      CFLAGS=\"\${CFLAGS} -O2 \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -ledit -lcurses -lterminfo -static\" \
       LIBS=\"-ledit -lcurses -lterminfo -static\" \
       YACC=byacc
   popd >/dev/null 2>&1
