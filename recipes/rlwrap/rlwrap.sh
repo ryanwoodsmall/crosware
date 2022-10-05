225cd33a4aff5495ad8aa4383113d7b21a6b0319
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 22:34:46 +0000

    rlwrap: require readlinenetbsdcurses, fixup flags

--- recipes/rlwrap/rlwrap.sh
+++ recipes/rlwrap/rlwrap.sh
@@ -1,9 +1,3 @@
-#
-# XXX - void sources has it too... https://sources.voidlinux.org/rlwrap-0.44/rlwrap-0.44.tar.gz
-# XXX - fossies: https://fossies.org/linux/privat/rlwrap-0.45.tar.gz/
-# XXX - old 'termcap' variant? very limited, buggy/crashy?
-#
-
 rname="rlwrap"
 rver="0.45.2"
 rdir="${rname}-${rver}"
@@ -11,16 +5,16 @@ rfile="${rdir}.tar.gz"
 #rurl="https://github.com/hanslub42/${rname}/releases/download/${rver}/${rfile}"
 rurl="https://github.com/ryanwoodsmall/${rname}/releases/download/v${rver}/${rfile}"
 rsha256="626ff6260dd3dd9d93b98b149a563067a2b22da70dcee39475387c0eefd2e70f"
-rreqs="make netbsdcurses configgit"
+rreqs="make netbsdcurses readlinenetbsdcurses configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
-    CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${cwsw}/netbsdcurses/current/include/readline\" \
-    LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" \
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
     LIBS=\"-lreadline -lcurses -lterminfo\"
   which pod2man || sed -i.ORIG 's/pod2man/echo pod2man/g' filters/Makefile
   popd >/dev/null 2>&1
