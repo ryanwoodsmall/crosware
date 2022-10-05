75cda50eb9b58e07e98c1cfec339def9890dc852
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 22:31:33 +0000

    rc: require readlinenetbsdcurses, fixup flags

--- recipes/rc/rc.sh
+++ recipes/rc/rc.sh
@@ -4,16 +4,16 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rsha256="070cd078090da69a90aadbb613a6148569c9692106ddf9f2c5df9ba08d41fb24"
-rreqs="make netbsdcurses byacc"
+rreqs="make netbsdcurses readlinenetbsdcurses byacc"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   cat Makefile > Makefile.ORIG
-  sed -i '/^DESCRIPTION/s,^DESCRIPTION.*,DESCRIPTION=${rname}-${rver},g' Makefile
-  sed -i '/^PREFIX/s,^PREFIX.*,PREFIX=${ridir},g' Makefile
+  sed -i \"/^DESCRIPTION/s,^DESCRIPTION.*,DESCRIPTION=${rname}-\$(cwver_${rname}),g\" Makefile
+  sed -i \"/^PREFIX/s,^PREFIX.*,PREFIX=\$(cwidir_${rname}),g\" Makefile
   sed -i 's,-lreadline,-lreadline -lcurses -lterminfo,g' Makefile
   popd >/dev/null 2>&1
 }
@@ -21,8 +21,12 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" CFLAGS=\"\${CFLAGS}\" CC=\"\${CC} \${CFLAGS} -static\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make \
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+    CFLAGS=\"\${CFLAGS}\" \
+    CC=\"\${CC} \${CFLAGS} -static\"
   make history mksignal mkstatval tripping LDFLAGS=-static CC=\"\${CC} \${CFLAGS} -static\"
   popd >/dev/null 2>&1
 }
@@ -30,12 +34,16 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make install CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" CFLAGS=\"\${CFLAGS}\" CC=\"\${CC} \${CFLAGS} -static\"
-  cwmkdir \"${ridir}/share/${rname}/helpers\"
-  rm -f ${ridir}/share/${rname}/helpers/*
-  cp -a history mksignal mkstatval tripping \"${ridir}/share/${rname}/helpers/\"
-  \$(\${CC} -dumpmachine)-strip \"${ridir}/bin/${rname}\" ${ridir}/share/${rname}/helpers/*
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install \
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+    CFLAGS=\"\${CFLAGS}\" \
+    CC=\"\${CC} \${CFLAGS} -static\"
+  cwmkdir \"\$(cwidir_${rname})/share/${rname}/helpers\"
+  rm -f \$(cwidir_${rname})/share/${rname}/helpers/*
+  cp -a history mksignal mkstatval tripping \"\$(cwidir_${rname})/share/${rname}/helpers/\"
+  \$(\${CC} -dumpmachine)-strip \"\$(cwidir_${rname})/bin/${rname}\" \$(cwidir_${rname})/share/${rname}/helpers/*
   popd >/dev/null 2>&1
 }
 "
