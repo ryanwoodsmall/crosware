ca85fb6b445b3b89c7a2cf5a09b6f40225c7e4e1
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-05 21:26:16 +0000

    es: require readlinenetbsdcurses, fixup flags

--- recipes/es/es.sh
+++ recipes/es/es.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/wryun/es-shell/releases/download/v${rver}/${rfile}"
 rsha256="c926482b42084e903eb871ee1eb0cefc09dae6f1adeb8408dd9e933035c4f5dd"
-rreqs="make byacc netbsdcurses"
+rreqs="make byacc netbsdcurses readlinenetbsdcurses"
 
 . "${cwrecipe}/common.sh"
 
@@ -19,9 +19,9 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
-    CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
-    LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" \
-    PKG_CONFIG_{LIBDIR,PATH}=\"${cwsw}/netbsdcurses/current/lib/pkgconfig\" \
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
     YACC=\"${cwsw}/byacc/current/bin/byacc\"
   popd >/dev/null 2>&1
 }
