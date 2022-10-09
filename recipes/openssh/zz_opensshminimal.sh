f6c1fcad2c7f8d6a4edcd959f646d280928f4332
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-09 08:47:17 +0000

    opensshminimal: use libeditminimal for now

--- recipes/openssh/zz_opensshminimal.sh
+++ recipes/openssh/zz_opensshminimal.sh
@@ -8,7 +8,7 @@ rfile="$(cwfile_openssh)"
 rdlfile="$(cwdlfile_openssh)"
 rurl="$(cwurl_openssh)"
 rsha256="$(cwsha256_openssh)"
-rreqs="bootstrapmake zlibng netbsdcurses libeditnetbsdcurses"
+rreqs="bootstrapmake zlibng libeditminimal"
 
 . "${cwrecipe}/common.sh"
 
@@ -24,10 +24,11 @@ unset f
 eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  sed -i.ORIG 's,-lcurses,-ltermcap,g' configure
   ./configure ${cwconfigureprefix} \
     --disable-security-key \
     --sysconfdir=\"${cwetc}/openssh\" \
-    --with-libedit=\"${cwsw}/libeditnetbsdcurses/current\" \
+    --with-libedit=\"${cwsw}/libeditminimal/current\" \
     --with-mantype=man \
     --with-privsep-path=\"${cwtmp}/empty\" \
     --without-openssl \
@@ -38,7 +39,7 @@ function cwconfigure_${rname}() {
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static -s\" \
       PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
-      LIBS='-lz -ledit -lcurses -lterminfo'
+      LIBS='-lz -ledit -ltermcap'
   popd >/dev/null 2>&1
 }
 "
