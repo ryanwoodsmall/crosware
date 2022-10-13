36bda68c45f7b2c21ea3d9a7fceaa8116c74ba62
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-13 00:23:24 +0000

    openssl: update to 1.1.1r, use a couple cw?dir_ funcs, need to clean this one up...

--- recipes/openssl/openssl.sh
+++ recipes/openssl/openssl.sh
@@ -8,31 +8,31 @@
 #
 
 rname="openssl"
-rver="1.1.1q"
+rver="1.1.1r"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.openssl.org/source/${rfile}"
-rsha256="d7939ce614029cdff0b6c20f0e2e5703158a489a72b2507b8bd51bf8c8fd10ca"
+rsha256="e389352ae3d5ae4d38597bf8a54f1dcb6fb3c8b50f4fe58a94bb1bf7f85d82a0"
 rreqs="make perl cacertificates"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG '/if.*eq.*-static/s/-static/-blahblahblah/g' Configure
   env PATH=\"${cwsw}/perl/current/bin:\${PATH}\" \
-    ./config --prefix=${ridir} --openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic \${CFLAGS} \${LDFLAGS} \${CPPFLAGS} -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
+    ./config --prefix=\"\$(cwidir_${rname})\" --openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic \${CFLAGS} \${LDFLAGS} \${CPPFLAGS} -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install_sw
-  sed -i '/^Libs:/s/$/ -lz/g' ${ridir}/lib/pkgconfig/*.pc
-  sed -i '/^Requires/s/\\.private:/:/g' ${ridir}/lib/pkgconfig/*.pc
+  sed -i '/^Libs:/s/$/ -lz/g' \$(cwidir_${rname})/lib/pkgconfig/*.pc
+  sed -i '/^Requires/s/\\.private:/:/g' \$(cwidir_${rname})/lib/pkgconfig/*.pc
   popd >/dev/null 2>&1
 }
 "
