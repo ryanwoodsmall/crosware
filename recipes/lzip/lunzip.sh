2d1b5fcc9b32006700c8b50ba671c1fd5fccb05f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-16 04:42:19 +0000

    lunzip: update to 1.13, use cw?dir_ funcs, clean up some quotes

--- recipes/lzip/lunzip.sh
+++ recipes/lzip/lunzip.sh
@@ -1,16 +1,16 @@
 rname="lunzip"
-rver="1.12"
+rver="1.13"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://download.savannah.gnu.org/releases/lzip/${rname}/${rfile}"
-rsha256="e55b4aaac5666b6ccad8459e52f8adb6869ce259f686e960129ae1e1bdf52ad2"
+rsha256="3c7d8320b947d2eb3c6081caf9b6c91b12debecb089ee544407cd14c8e517894"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} CPPFLAGS=\"\${CPPFLAGS}\" LDFLAGS=\"\${LDFLAGS}\" CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\"
   popd >/dev/null 2>&1
 }
@@ -18,6 +18,6 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
