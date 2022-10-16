79945e1b9856440c44210d200dadf883fc43ec09
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-16 11:40:41 +0000

    libksba: update to 1.6.2, use cw?dir_ funcs, fix some quoting

--- recipes/gnupg/libksba.sh
+++ recipes/gnupg/libksba.sh
@@ -1,25 +1,25 @@
 rname="libksba"
-rver="1.6.1"
+rver="1.6.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="40750864c3ca11fa0833227938db6603661f6c1a3cc267485e81fa140cc6ea04"
+rsha256="fce01ccac59812bddadffacff017dac2e4762bdb6ebc6ffe06f6ed4f6192c971"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
-    --with-libgpg-error-prefix=\${cwsw}/libgpgerror/current/
+    --with-libgpg-error-prefix=\"\${cwsw}/libgpgerror/current/\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
   echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
