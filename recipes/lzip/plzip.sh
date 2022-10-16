d494fe110884421224e5a8ab7257bc29b055dcc2
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-16 05:11:35 +0000

    plzip: update to 1.10, use cw?dir_ funcs, clean up some quotes

--- recipes/lzip/plzip.sh
+++ recipes/lzip/plzip.sh
@@ -1,16 +1,16 @@
 rname="plzip"
-rver="1.9"
+rver="1.10"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://download.savannah.gnu.org/releases/lzip/${rname}/${rfile}"
-rsha256="14d8d1db8dde76bdd9060b59d50b2943417eb4c0fbd2b84736546b78fab5f1a7"
+rsha256="43faa58265d8b89ad75bd4ed11d347ef10065a8070748bc1ed0e06f191458098"
 rreqs="make lzlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} CPPFLAGS=\"\${CPPFLAGS}\" CXX=\"\${CXX}\" CXXFLAGS=\"\${CXXFLAGS}\" LDFLAGS=\"\${LDFLAGS}\"
   popd >/dev/null 2>&1
 }
@@ -18,6 +18,6 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
