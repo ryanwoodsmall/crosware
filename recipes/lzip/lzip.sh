d45fdef2475e139ae448dcc01005e4e55471d6aa
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-16 04:33:30 +0000

    lzip: update to 1.23, use cw?dir_ funcs

--- recipes/lzip/lzip.sh
+++ recipes/lzip/lzip.sh
@@ -1,16 +1,16 @@
 rname="lzip"
-rver="1.22"
+rver="1.23"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://download.savannah.gnu.org/releases/${rname}/${rfile}"
-rsha256="c3342d42e67139c165b8b128d033b5c96893a13ac5f25933190315214e87a948"
+rsha256="4792c047ddf15ef29d55ba8e68a1a21e0cb7692d87ecdf7204419864582f280d"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} CPPFLAGS=\"\${CPPFLAGS}\" CXX=\"\${CXX}\" CXXFLAGS=\"\${CXXFLAGS}\" LDFLAGS=\"\${LDFLAGS}\"
   popd >/dev/null 2>&1
 }
