f899d07b600aef341e4e1a4cb46fef6a839b9915
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-16 09:36:32 +0000

    libgpgerror: update to 1.46, use cw?dir_ funcs, fix some quoting

--- recipes/gnupg/libgpgerror.sh
+++ recipes/gnupg/libgpgerror.sh
@@ -1,9 +1,9 @@
 rname="libgpgerror"
-rver="1.45"
+rver="1.46"
 rdir="libgpg-error-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/libgpg-error/${rfile}"
-rsha256="570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
+rsha256="b7e11a64246bbe5ef37748de43b245abd72cfcd53c9ae5e7fc5ca59f1c81268d"
 rreqs="make slibtool busybox"
 
 . "${cwrecipe}/common.sh"
@@ -12,7 +12,7 @@ rreqs="make slibtool busybox"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG '/_gpgrt_logv_printhex.*NULL,.*NULL/ s/NULL,.*NULL/NULL, arg_ptr/g' src/logging.c
   #sed -i 's/va_list arg_ptr;/va_list arg_ptr = {};/g' src/logging.c
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --disable-nls AWK='busybox awk'
@@ -22,7 +22,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
   echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
