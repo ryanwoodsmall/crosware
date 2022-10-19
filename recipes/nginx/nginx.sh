a957bdea5f71f962409658654b141c8c6459e5ab
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-19 18:02:16 +0000

    nginx: update to 1.23.2, use cw?dir_ funcs

--- recipes/nginx/nginx.sh
+++ recipes/nginx/nginx.sh
@@ -9,18 +9,18 @@
 #
 
 rname="nginx"
-rver="1.23.1"
+rver="1.23.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://nginx.org/download/${rfile}"
-rsha256="5eee1bd1c23e3b9477a45532f1f36ae6178b43d571a9607e6953cef26d5df1e2"
+rsha256="a80cc272d3d72aaee70aa8b517b4862a635c0256790434dbfc4d618a999b0b46"
 rreqs="make perl slibtool pcre2"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(cwsha256_${rname})\"
   cwfetch_openssl
   cwfetch_pcre2
   cwfetch_zlib
@@ -30,30 +30,30 @@ function cwfetch_${rname}() {
 
 eval "
 function cwextract_${rname}() {
-  cwextract \"${rdlfile}\" \"${cwbuild}\"
-  cwextract \"\$(cwdlfile_openssl)\" \"${rbdir}\"
-  cwextract \"\$(cwdlfile_pcre2)\" \"${rbdir}\"
-  cwextract \"\$(cwdlfile_zlib)\" \"${rbdir}\"
-  cwextract \"\$(cwdlfile_njs)\" \"${rbdir}\"
+  cwextract \"\$(cwdlfile_${rname})\" \"${cwbuild}\"
+  cwextract \"\$(cwdlfile_openssl)\" \"\$(cwbdir_${rname})\"
+  cwextract \"\$(cwdlfile_pcre2)\" \"\$(cwbdir_${rname})\"
+  cwextract \"\$(cwdlfile_zlib)\" \"\$(cwbdir_${rname})\"
+  cwextract \"\$(cwdlfile_njs)\" \"\$(cwbdir_${rname})\"
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG '/if.*eq.*-static/s/-static/-blahblahblah/g' \"\$(cwdir_openssl)/Configure\"
   sed -i.ORIG 's/-march=armv7-a/-march=armv7-a -static/g' \"\$(cwdir_openssl)/config\"
   env PATH=\"${cwsw}/perl/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
-      --add-module=\"${rbdir}/\$(cwdir_njs)/nginx\" \
+      --add-module=\"\$(cwbdir_${rname})/\$(cwdir_njs)/nginx\" \
       --with-cc=\"\$(which \${CC})\" \
       --with-cc-opt=\"-fPIC -Wl,-static\" \
       --with-ld-opt=\"-static\" \
-      --with-openssl=\"${rbdir}/\$(cwdir_openssl)\" \
+      --with-openssl=\"\$(cwbdir_${rname})/\$(cwdir_openssl)\" \
       --with-openssl-opt=\"--openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS -static\" \
       --with-perl=\"${cwsw}/perl/current/bin/perl\" \
-      --with-pcre=\"${rbdir}/\$(cwdir_pcre2)\" \
-      --with-zlib=\"${rbdir}/\$(cwdir_zlib)\" \
+      --with-pcre=\"\$(cwbdir_${rname})/\$(cwdir_pcre2)\" \
+      --with-zlib=\"\$(cwbdir_${rname})/\$(cwdir_zlib)\" \
       --with-compat \
       --with-http_addition_module \
       --with-http_auth_request_module \
@@ -83,7 +83,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make -j${cwmakejobs} ${rlibtool} CC=\"\${CC}\" CPPFLAGS= LDFLAGS= PKG_CONFIG_LIBDIR= PKG_CONFIG_PATH= NJS_CFLAGS=-static
   popd >/dev/null 2>&1
 }
@@ -91,9 +91,9 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool} CC=\"\${CC}\" CPPFLAGS= LDFLAGS= PKG_CONFIG_LIBDIR= PKG_CONFIG_PATH=
-  \$(\${CC} -dumpmachine)-strip --strip-all \"${ridir}/sbin/${rname}\"
+  \$(\${CC} -dumpmachine)-strip --strip-all \"\$(cwidir_${rname})/sbin/${rname}\"
   popd >/dev/null 2>&1
 }
 "
