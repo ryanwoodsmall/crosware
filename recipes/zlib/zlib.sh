00ee09bfc5c7d1e2bf4acf94a7203f489c016bdf
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-21 05:08:07 +0000

    zlib: move to bootstrapmake

--- recipes/zlib/zlib.sh
+++ recipes/zlib/zlib.sh
@@ -7,7 +7,7 @@ rfile="${rdir}.tar.gz"
 #rurl="https://downloads.sourceforge.net/project/libpng/${rname}/${rver}/${rfile}"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rsha256="b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
-rreqs="make"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
