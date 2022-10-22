9e26cbccfa21adcf19eea93b9a95d47094699759
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-22 01:56:26 +0000

    zulu11: update zulu/zulu11{glibc,musl} to 11.60.19-ca-jdk11.0.17

--- recipes/zulu/zulu11glibc.sh
+++ recipes/zulu/zulu11glibc.sh
@@ -1,16 +1,16 @@
-rver="11.56.19-ca-jdk11.0.15"
+rver="11.60.19-ca-jdk11.0.17"
 mver="${rver%%.*}"
 rname="zulu${mver}glibc"
 rsha256=""
 
 if [[ ${uarch} =~ ^aarch64 ]] ; then
-  rsha256="fc7c41a0005180d4ca471c90d01e049469e0614cf774566d4cf383caa29d1a97"
+  rsha256="48632fb921cae8fd3914f7297b349b66c602455af26cf1fd1d09a866330c3de1"
 elif [[ ${uarch} =~ ^arm ]] ; then
-  rsha256="1c637012c5fce079a820ab3cc8b74695722c10e07342336055042aebb8bd8420"
+  rsha256="498180dcf87e89b23071a8136da977a0b50052ac2deb222eae77784810ad78ac"
 elif [[ ${uarch} =~ ^i.86 ]] ; then
-  rsha256="1ee1654aa592f48ce2920092638d70bee4b6997dc42cf629a9717fb11e468d58"
+  rsha256="78ce988091f50a1f153909770872ad8e514d447ba663e6de688a82c9884ce6c2"
 elif [[ ${uarch} =~ ^x86_64 ]] ; then
-  rsha256="e064b61d93304012351242bf0823c6a2e41d9e28add7ea7f05378b7243d34247"
+  rsha256="d9e9d34d64bc63cd846c88117a731e6550c17866df9501d0d1ae0329f965d78a"
 fi
 
 . "${cwrecipe}/${rname%${mver}glibc}/${rname//${mver}glibc/glibc}.sh.common"
--- recipes/zulu/zulu11musl.sh
+++ recipes/zulu/zulu11musl.sh
@@ -1,12 +1,11 @@
-rver="11.58.23-ca-jdk11.0.16.1"
+rver="11.60.19-ca-jdk11.0.17"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="6f6e2cbf71982a5540c83c77441e3b26e7706672e7e8dd8a7e473b8825832db6"
+rsha256="c88f438c07a38fb49b8a3a0a3cfd152dd09c24158ac43b024c72ebcfbe31124d"
 # per-arch override rver/rdir/rfile/rurl/rsha256 here
 if [[ ${karch} =~ aarch64 ]] ; then
-  rver="11.58.25-ca-jdk11.0.16.1"
   rdir="zulu${rver}-linux_musl_aarch64"
-  rsha256="e82a555db6a9a19433a1f2589d86a429b191dbe25e12550e8f801f9a84237239"
+  rsha256="17ee2eb126a15b489a397f14440a3740594bb139a9ddd506dfcc4101cdb4e014"
 fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
