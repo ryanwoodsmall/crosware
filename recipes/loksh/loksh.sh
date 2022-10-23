1b2bebf4645fa410ad7ae8ff9ece1b4f66c5c7b7
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-23 05:39:09 +0000

    loksh: update to 7.2, use cw?dir_ funcs, catch a couple of potential errors, make git an optional req

--- recipes/loksh/loksh.sh
+++ recipes/loksh/loksh.sh
@@ -1,26 +1,30 @@
 rname="loksh"
-rver="7.1"
+rver="7.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/dimkr/${rname}/releases/download/${rver}/${rfile}"
-rsha256="ab7d274a057b83bc64643664094031126641aa91ee440a96765279991aab6938"
-rreqs="ncurses pkgconfig python3 meson ninja git"
+rsha256="0083df24b3916c2f34ab63f8b4de6720ba9799e0e9d139c4b2d4ae41ad6aa53d"
+rreqs="ncurses pkgconfig python3 meson ninja"
 rprof="${cwetcprofd}/zz_${rname}.sh"
+
+if ! command -v git &>/dev/null ; then
+  rreqs="${rreqs} git"
+fi
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"\$(echo -n ${cwsw}/{python3,meson,ninja}/current/bin | tr ' ' ':'):\${PATH}\" \
-    \"${cwsw}/meson/current/bin/meson\" setup --prefix=\"${ridir}\" build \"${rbdir}\"
+    \"${cwsw}/meson/current/bin/meson\" setup --prefix=\"\$(cwidir_${rname})\" build \"\$(cwbdir_${rname})\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"\$(echo -n ${cwsw}/{python3,meson,ninja}/current/bin | tr ' ' ':'):\${PATH}\" \
     \"${cwsw}/ninja/current/bin/ninja\" -C build
   popd >/dev/null 2>&1
@@ -29,14 +33,14 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  find ${ridir}/bin/ ! -type d | grep 'sh$' | xargs rm -f
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  find \$(cwidir_${rname})/bin/ ! -type d | grep 'sh$' | xargs rm -f || true
   env PATH=\"\$(echo -n ${cwsw}/{python3,meson,ninja}/current/bin | tr ' ' ':'):\${PATH}\" \
     \"${cwsw}/ninja/current/bin/ninja\" -C build install
-  mv \"${ridir}/bin/ksh\" \"${ridir}/bin/${rname}\"
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/ksh\"
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/sh\"
-  find \"${ridir}/bin/\" -type f | xargs \$(\${CC} -dumpmachine)-strip --strip-all
+  mv \"\$(cwidir_${rname})/bin/ksh\" \"\$(cwidir_${rname})/bin/${rname}\"
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/ksh\"
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/sh\"
+  find \"\$(cwidir_${rname})/bin/\" -type f | xargs \$(\${CC} -dumpmachine)-strip --strip-all || true
   popd >/dev/null 2>&1
 }
 "
