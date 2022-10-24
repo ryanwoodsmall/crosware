e6b600834ba59003f85f6d3c6f207af262b2d1b6
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-24 01:32:57 +0000

    libffi: update to 3.4.4

--- recipes/libffi/libffi.sh
+++ recipes/libffi/libffi.sh
@@ -1,9 +1,9 @@
 rname="libffi"
-rver="3.4.3"
+rver="3.4.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="4416dd92b6ae8fcb5b10421e711c4d3cb31203d77521a77d85d0102311e6c3b8"
+rsha256="d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
