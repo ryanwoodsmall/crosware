ad89f2ef56faa4c215297a31d6097c5bffed76ba
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-25 04:06:07 +0000

    libidn2: update to 2.3.4, use cw?dir_ funcs

--- recipes/libidn/libidn2.sh
+++ recipes/libidn/libidn2.sh
@@ -1,16 +1,16 @@
 rname="libidn2"
-rver="2.3.3"
+rver="2.3.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/libidn/${rfile}"
-rsha256="f3ac987522c00d33d44b323cae424e2cffcb4c63c6aa6cd1376edacbf1c36eb0"
+rsha256="93caba72b4e051d1f8d4f5a076ab63c99b77faee019b72b9783b267986dbb45f"
 rreqs="make libunistring"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --disable-nls --disable-doc
   popd >/dev/null 2>&1
 }
@@ -18,9 +18,9 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
-  sed -i 's/-lidn2/-lidn2 -lunistring/g' \"${ridir}/lib/pkgconfig/${rname}.pc\"
+  sed -i 's/-lidn2/-lidn2 -lunistring/g' \"\$(cwidir_${rname})/lib/pkgconfig/${rname}.pc\"
   popd >/dev/null 2>&1
 }
 "
