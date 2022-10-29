d8b4965b2f293a0ef5fd56de83a1e5feb222ca3f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-29 01:44:44 +0000

    scripts/show-latest-vim.sh: fix filename matching again, need to figure out xmllint/xmlstarlet/... (xpath?) selectors for this :\

--- scripts/show-latest-vim.sh
+++ scripts/show-latest-vim.sh
@@ -18,7 +18,7 @@ if [ ${#} -ne 1 ] ; then
   vmax="$(curl -kLs "https://github.com/vim/vim/tags" \
   | xmllint --format --html - 2>/dev/null \
   | awk -F'"' '/\/vim\/vim\/archive\/refs\/tags\//{print $4}' \
-  | grep \\.tar\\.gz \
+  | grep -E 'v[0-9]+\.[0-9]+\.[0-9]{4}\.tar\.gz' \
   | head -1 \
   | xargs basename \
   | sed 's/^v//g;s/\.tar\.gz//g')"
