deaae2c8cc248290e47e3418ebd68bfd637d09c4
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-29 01:07:42 +0000

    oksh: update to 7.2, use cw?dir_ funcs

--- recipes/oksh/oksh.sh
+++ recipes/oksh/oksh.sh
@@ -1,9 +1,9 @@
 rname="oksh"
-rver="7.1"
+rver="7.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ibara/${rname}/releases/download/${rdir}/${rfile}"
-rsha256="9dc0b0578d9d64d10c834f9757ca11f526b562bc5454da64b2cb270122f52064"
+rsha256="3340ca98b1d5a2800ebe7dba75312d8a4971a3fcff20fcd0d0ec5cf7b719427e"
 rreqs="make netbsdcurses"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
@@ -11,7 +11,7 @@ rprof="${cwetcprofd}/zz_${rname}.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's/ncurses\.h/curses.h/g' configure emacs.c var.c vi.c
   sed -i 's/-lncurses/-lcurses -lterminfo/g' configure
   env \
@@ -19,9 +19,9 @@ function cwconfigure_${rname}() {
     CFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
     LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static\" \
       ./configure \
-        --prefix=\"${ridir}\" \
-        --bindir=\"${ridir}/bin\" \
-        --mandir=\"${ridir}/share/man\" \
+        --prefix=\"\$(cwidir_${rname})\" \
+        --bindir=\"\$(cwidir_${rname})/bin\" \
+        --mandir=\"\$(cwidir_${rname})/share/man\" \
         --enable-curses \
         --enable-ksh \
         --enable-sh \
@@ -32,7 +32,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make -j${cwmakejobs} CPPFLAGS=
   popd >/dev/null 2>&1
 }
@@ -40,12 +40,12 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  find ${ridir}/bin/ ! -type d | grep 'sh$' | xargs rm -f
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  find \$(cwidir_${rname})/bin/ ! -type d | grep 'sh$' | xargs rm -f &>/dev/null || true
   make install
-  mv \"${ridir}/bin/ksh\" \"${ridir}/bin/${rname}\"
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/ksh\"
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/sh\"
+  mv \"\$(cwidir_${rname})/bin/ksh\" \"\$(cwidir_${rname})/bin/${rname}\"
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/ksh\"
+  ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/sh\"
   popd >/dev/null 2>&1
 }
 "
