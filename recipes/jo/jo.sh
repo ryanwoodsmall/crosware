c510fac414940895d2a53d8cd33178c6ca18ab6b
ryanwoodsmall <rwoodsmall@gmail.com>
2022-10-30 00:53:07 +0000

    jo: update to 1.7

--- recipes/jo/jo.sh
+++ recipes/jo/jo.sh
@@ -1,15 +1,15 @@
 rname="jo"
-rver="1.6"
+rver="1.7"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jpmens/${rname}/releases/download/${rver}/${rfile}"
-rsha256="eb15592f1ba6d5a77468a1438a20e3d21c3d63bb7d045fb3544f223340fcd1a1"
+rsha256="74c3032f0650bbaf69c17ef71ba3240e51262963843cdc8452d4e917ab8e8656"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
