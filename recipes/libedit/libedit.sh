9e9eb685ab5318af181ec9520bf98b3b8976d4d7
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-02 06:28:36 +0000

    libedit: update to 20221030-3.1, remove cdefs patch

--- recipes/libedit/libedit.sh
+++ recipes/libedit/libedit.sh
@@ -1,21 +1,13 @@
 rname="libedit"
-rver="20221009-3.1"
+rver="20221030-3.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://www.thrysoee.dk/editline/${rfile}"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="b7b135a5112ce4344c9ac3dff57cc057b2b0e1b912619a36cf1d13fce8e88626"
+rsha256="f0925a5adf4b1bf116ee19766b7daa766917aec198747943b1c4edf67a4be2bb"
 rreqs="make ncurses"
 
 . "${cwrecipe}/common.sh"
-
-eval "
-function cwpatch_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  sed -i.ORIG '/cdefs/d' src/reallocarr.c
-  popd >/dev/null 2>&1
-}
-"
 
 eval "
 function cwconfigure_${rname}() {
