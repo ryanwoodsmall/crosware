4f50e46d7c9d430c605ef3a9c49d9c5bc429148f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-04 14:36:33 +0000

    jo: update to 1.9

--- recipes/jo/jo.sh
+++ recipes/jo/jo.sh
@@ -1,9 +1,9 @@
 rname="jo"
-rver="1.7"
+rver="1.9"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jpmens/${rname}/releases/download/${rver}/${rfile}"
-rsha256="74c3032f0650bbaf69c17ef71ba3240e51262963843cdc8452d4e917ab8e8656"
+rsha256="0195cd6f2a41103c21544e99cd9517b0bce2d2dc8cde31a34867977f8a19c79f"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
