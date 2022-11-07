827aa84ec865b1a85ab5ec1b3004e703393c138f
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-07 02:34:51 +0000

    ccache4: update to 4.7.3

--- recipes/ccache/ccache4.sh
+++ recipes/ccache/ccache4.sh
@@ -3,11 +3,11 @@
 #
 
 rname="ccache4"
-rver="4.7.2"
+rver="4.7.3"
 rdir="${rname%4}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname%4}/${rname%4}/releases/download/v${rver}/${rfile}"
-rsha256="6b346f441342a25a6c1d7e010957a593f416e94b5d66fdf2e2992953b3860b9d"
+rsha256="577841df9e9d9659d58a2f4e0f6eaceb7e29816988ffb2b12390e17b109b4ac4"
 rreqs="cmake make zstd"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
