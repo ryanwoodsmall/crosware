1c31fbbfc39e0f465194977b5aa8ac0e9aa732a2
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-09 23:53:02 +0000

    pdpmake: update to 1.4.0, enable ENABLE_FEATURE_MAKE_POSIX_202X, size optimize, turn off debug symbols and strip bins

--- recipes/pdpmake/pdpmake.sh
+++ recipes/pdpmake/pdpmake.sh
@@ -1,13 +1,13 @@
 #
-# XXX - check macros: ENABLE_FEATURE_MAKE_POSIX_202X ENABLE_FEATURE_CLEAN_UP
+# XXX - check macros: ENABLE_FEATURE_CLEAN_UP
 #
 
 rname="pdpmake"
-rver="1.3.0"
+rver="1.4.0"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/rmyorston/${rname}/archive/refs/tags/${rfile}"
-rsha256="1242d2171398a03a82a4b57c9b710de74e4efff41e8f5b1e4f60f2dd7c6451c1"
+rsha256="d17600f2699938980cc8734ed9f1cd993b11264e22b6fe2c18cdb8ed6ab5045d"
 rreqs="bootstrapmake"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
@@ -25,10 +25,10 @@ eval "
 function cwmake_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   rm -f ${rname} posixmake make
-  make CC=\"\${CC} \${CFLAGS}\" CPPFLAGS= LDFLAGS=-static
+  make CC=\"\${CC} \${CFLAGS} -Os -g0 -Wl,-s\" CPPFLAGS= LDFLAGS='-static -s'
   mv make posixmake
   make clean
-  make CC=\"\${CC} \${CFLAGS} -DENABLE_FEATURE_MAKE_EXTENSIONS=1\" CPPFLAGS= LDFLAGS=-static
+  make CC=\"\${CC} \${CFLAGS} -Os -g0 -Wl,-s -DENABLE_FEATURE_MAKE_EXTENSIONS=1 -DENABLE_FEATURE_MAKE_POSIX_202X=1\" CPPFLAGS= LDFLAGS='-static -s'
   mv make ${rname}
   popd >/dev/null 2>&1
 }
@@ -40,6 +40,8 @@ function cwmakeinstall_${rname}() {
   cwmkdir \"\$(cwidir_${rname})/bin\"
   install -m 755 ${rname} \"\$(cwidir_${rname})/bin/${rname}\"
   install -m 755 posixmake \"\$(cwidir_${rname})/bin/posixmake\"
+  \$(\${CC} -dumpmachine)-strip --strip-all \"\$(cwidir_${rname})/bin/${rname}\"
+  \$(\${CC} -dumpmachine)-strip --strip-all \"\$(cwidir_${rname})/bin/posixmake\"
   popd >/dev/null 2>&1
 }
 "
