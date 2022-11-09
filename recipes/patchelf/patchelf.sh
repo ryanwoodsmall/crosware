fd4dcc5d05f13004adc7443c211e84a224676c3c
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-09 04:28:11 +0000

    patchelf: update to 0.17.0

--- recipes/patchelf/patchelf.sh
+++ recipes/patchelf/patchelf.sh
@@ -1,9 +1,9 @@
 rname="patchelf"
-rver="0.16.1"
+rver="0.17.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-${rver}.tar.gz"
 rurl="https://github.com/NixOS/${rname}/releases/download/${rver}/${rfile}"
-rsha256="1a562ed28b16f8a00456b5f9ee573bb1af7c39c1beea01d94fc0c7b3256b0406"
+rsha256="eeda23de42bfcd6368615d00dc0700e595493fba6af5d98ed5d7d9d00a214abe"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
