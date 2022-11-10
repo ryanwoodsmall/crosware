083bc6eef268797cd06f1315e997f1ff5f471781
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-10 00:42:00 +0000

    byacc: update to 20221106, use cw?dir_ funcs

--- recipes/byacc/byacc.sh
+++ recipes/byacc/byacc.sh
@@ -1,26 +1,26 @@
 rname="byacc"
-rver="20220128"
+rver="20221106"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
-rsha256="42c1805cc529314e6a76326fe1b33e80c70862a44b01474da362e2f7db2d749c"
+rsha256="a899be227bbcac9cf7700f7dbb5a8494688f1f9f0617b510762daeace47b9d12"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
-function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+function cwmakeinstall_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  rm -f \"\$(cwidir_${rname})/bin/{b,}yacc\"
+  make install
+  mv \"\$(cwidir_${rname})/bin/yacc\" \"\$(cwidir_${rname})/bin/${rname}\"
+  ln -sf ${rname} \"\$(cwidir_${rname})/bin/yacc\"
+  popd >/dev/null 2>&1
 }
 "
 
 eval "
-function cwmakeinstall_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  rm -f \"${ridir}/bin/{b,}yacc\"
-  make install
-  mv \"${ridir}/bin/yacc\" \"${ridir}/bin/byacc\"
-  ln -sf \"${rtdir}/current/bin/byacc\" \"${ridir}/bin/yacc\"
-  popd >/dev/null 2>&1
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
