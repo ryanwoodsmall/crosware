1022918d6a2ee153fbf854c5a83a415ba1d32fcb
ryanwoodsmall <rwoodsmall@gmail.com>
2022-11-16 07:35:01 +0000

    libnftnl: update to 1.2.4

--- recipes/netfilter/libnftnl.sh
+++ recipes/netfilter/libnftnl.sh
@@ -1,9 +1,9 @@
 rname="libnftnl"
-rver="1.2.3"
+rver="1.2.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
-rsha256="e916ea9b79f9518560b9a187251a7c042442a9ecbce7f36be7908888605d0255"
+rsha256="c0fe233be4cdfd703e7d5977ef8eb63fcbf1d0052b6044e1b23d47ca3562477f"
 rreqs="bootstrapmake pkgconfig libmnl"
 
 . "${cwrecipe}/common.sh"
