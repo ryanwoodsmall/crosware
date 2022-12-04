414e0eb1efac676bcb0fae7586966bbd925ae9fb
ryanwoodsmall <rwoodsmall@gmail.com>
2022-12-04 06:25:34 +0000

    beanshell2: update to 2.1.1, use cwstubfunc, use cw?{dir,file}_ funcs

--- recipes/beanshell/beanshell2.sh
+++ recipes/beanshell/beanshell2.sh
@@ -1,38 +1,34 @@
 rname="beanshell2"
-rver="2.1.0"
+rver="2.1.1"
 rdir="${rname}-${rver}"
 rfile="bsh-${rver}.jar"
 rurl="https://github.com/beanshell/beanshell/releases/download/${rver}/${rfile}"
-rsha256="e9a68515dd69d54a648c4497875264a874f6c69ff52d0ddaf4be2204f0f18652"
+rsha256="71192cbbe49e7a269cfcba05dc5cb959c33b9b26dafcd6266ca3288b461f86a3"
 rreqs="rlwrap"
 
 . "${cwrecipe}/common.sh"
 
-for f in clean extract configure make ; do
-  eval "
-  function cw${f}_${rname}() {
-    true
-  }
-  "
-done
-unset f
+cwstubfunc "cwclean_${rname}"
+cwstubfunc "cwextract_${rname}"
+cwstubfunc "cwconfigure_${rname}"
+cwstubfunc "cwmake_${rname}"
 
 eval "
 function cwmakeinstall_${rname}() {
-  local c=\"${ridir}/bin/${rname}\"
-  cwmkdir \"${ridir}/bin\"
-  install -m 644 \"${rdlfile}\" \"${ridir}/${rfile}\"
-  ln -sf \"${rfile}\" \"${ridir}/${rname}.jar\"
-  ln -sf \"${rfile}\" \"${ridir}/beanshell.jar\"
-  ln -sf \"${rfile}\" \"${ridir}/bsh.jar\"
-  ln -sf \"${rfile}\" \"${ridir}/bsh2.jar\"
+  local c=\"\$(cwidir_${rname})/bin/${rname}\"
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 644 \"\$(cwdlfile_${rname})\" \"\$(cwidir_${rname})/\$(cwfile_${rname})\"
+  ln -sf \"\$(cwfile_${rname})\" \"\$(cwidir_${rname})/${rname}.jar\"
+  ln -sf \"\$(cwfile_${rname})\" \"\$(cwidir_${rname})/beanshell.jar\"
+  ln -sf \"\$(cwfile_${rname})\" \"\$(cwidir_${rname})/bsh.jar\"
+  ln -sf \"\$(cwfile_${rname})\" \"\$(cwidir_${rname})/bsh2.jar\"
   echo > \"\${c}\"
   echo '#!/usr/bin/env bash' >> \"\${c}\"
   echo 'rlwrap -C ${rname} java -cp \"${rtdir}/current/${rname}.jar\" bsh.Interpreter \"\${@}\"' >> \"\${c}\"
   chmod 755 \"\${c}\"
-  ln -sf \"${rname}\" \"${ridir}/bin/beanshell\"
-  ln -sf \"${rname}\" \"${ridir}/bin/bsh\"
-  ln -sf \"${rname}\" \"${ridir}/bin/bsh2\"
+  ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/beanshell\"
+  ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/bsh\"
+  ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/bsh2\"
   unset c
 }
 "
