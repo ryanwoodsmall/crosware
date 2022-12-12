8b092ea9a4bbf61435f01e0aa32a2a86b7a84a93
ryanwoodsmall <rwoodsmall@gmail.com>
2022-12-12 17:44:28 +0000

    pcre2: update to 10.42

--- recipes/pcre/pcre2.sh
+++ recipes/pcre/pcre2.sh
@@ -1,9 +1,9 @@
 rname="pcre2"
-rver="10.41"
+rver="10.42"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://github.com/PCRE2Project/${rname}/releases/download/${rname}-${rver}/${rfile}"
-rsha256="0f78cebd3e28e346475fb92e95fe9999945b4cbaad5f3b42aca47b887fb53308"
+rsha256="8d36cd8cb6ea2a4c2bb358ff6411b0c788633a2a45dabbf1aeb4b701d1b5e840"
 rreqs="make zlib bzip2"
 
 . "${cwrecipe}/common.sh"
