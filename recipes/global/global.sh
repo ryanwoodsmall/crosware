922bc5498cc4feb749513484631becdec896d604
ryanwoodsmall <rwoodsmall@gmail.com>
2022-12-12 00:52:46 +0000

    global: update to 6.6.9, use cw?dir_ funcs

--- recipes/global/global.sh
+++ recipes/global/global.sh
@@ -1,16 +1,16 @@
 rname="global"
-rver="6.6.8"
+rver="6.6.9"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="6f93d9732a07175817907d26640a90dc1009918e02be761bba09d1fa068357cd"
+rsha256="aacba0fa8d60ca645e62f312dcd23b47ed48a081aa0fb1563cff4702d9d1fad5"
 rreqs="make ncurses sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's/-lcurses/-lncurses/g;s/makeinfo/true/g' configure
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
   popd >/dev/null 2>&1
