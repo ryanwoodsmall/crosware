ec1b3d6b9da5663989a57826be30d91e3c1a417a
ryanwoodsmall <rwoodsmall@gmail.com>
2022-12-19 05:49:14 +0000

    cacertificates: update to 20220614-r2 from alpine 3.17, use cwstubfunc, use cw?{dir,file}_ funcs

--- recipes/cacertificates/cacertificates.sh
+++ recipes/cacertificates/cacertificates.sh
@@ -5,23 +5,27 @@
 # XXX - move to curl cacert https://curl.haxx.se/docs/caextract.html
 #
 rname="cacertificates"
-rver="20220614-r0"
+rver="20220614-r2"
 rdir="${rname}-${rver}"
 #rfile="ca-certificates-cacert-${rver}.apk"
 #rurl="https://mirrors.edge.kernel.org/alpine/v3.11/main/x86_64/${rfile}"
 rfile="ca-certificates-bundle-${rver}.apk"
-rurl="https://mirrors.edge.kernel.org/alpine/v3.16/main/x86_64/${rfile}"
-rsha256="9434141d9289fba97c8b10b4e68c7c5664f36636694430a6655c001f1fe3e8cc"
+rurl="https://mirrors.edge.kernel.org/alpine/v3.17/main/x86_64/${rfile}"
+rsha256="302bb29f5d43cbd60b02aebfdbf585db9575b4371a40a8593fd13a83e93c5f6c"
 rreqs=""
 rdlfile="${cwdl}/${rname}/${rfile}.tar.gz"
 
 . "${cwrecipe}/common.sh"
 
+cwstubfunc "cwclean_${rname}"
+cwstubfunc "cwconfigure_${rname}"
+cwstubfunc "cwmake_${rname}"
+
 eval "
 function cwextract_${rname}() {
-  cwmkdir \"${ridir}\"
-  pushd \"${ridir}\" >/dev/null 2>&1
-  cwextract \"${rdlfile}\" \"${ridir}\"
+  cwmkdir \"\$(cwidir_${rname})\"
+  pushd \"\$(cwidir_${rname})\" >/dev/null 2>&1
+  cwextract \"\$(cwdlfile_${rname})\" \"\$(cwidir_${rname})\"
   popd >/dev/null 2>&1
 }
 "
@@ -52,12 +56,3 @@ function cwuninstall_${rname}() {
   popd >/dev/null 2>&1
 }
 "
-
-for f in configure make ; do
-eval "
-function cw${f}_${rname}() {
-  true
-}
-"
-done
-unset f
