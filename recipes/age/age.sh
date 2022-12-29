1cc5c32154716963caa2deae060720da9876fd10
ryanwoodsmall <rwoodsmall@gmail.com>
2022-12-29 06:06:35 +0000

    age: update to 1.1.1, ungate riscv64, use cwstubfunc, use cw?dir_ funcs

--- recipes/age/age.sh
+++ recipes/age/age.sh
@@ -1,41 +1,30 @@
 rname="age"
-rver="1.0.0"
+rver="1.1.1"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/FiloSottile/${rname}/archive/refs/tags/${rfile}"
-rsha256="8d27684f62f9dc74014035e31619e2e07f8b56257b1075560456cbf05ddbcfce"
+rsha256="f1f3dbade631976701cd295aa89308681318d73118f5673cced13f127a91178c"
 rreqs="go cacertificates"
 
 . "${cwrecipe}/common.sh"
 
-if [[ ${karch} =~ ^riscv64 ]] ; then
-eval "
-function cwinstall_${rname}() {
-  cwscriptecho \"recipe ${rname} does not support architecture ${karch}\"
-}
-"
-fi
 
-eval "
-function cwconfigure_${rname}() {
-  true
-}
-"
+cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
   pushd \"${cwbuild}\" >/dev/null 2>&1
-  test -e \"${rbdir}\" && chmod -R u+rw \"${rbdir}/\" || true
-  rm -rf \"${rbdir}\"
+  test -e \"\$(cwbdir_${rname})\" && chmod -R u+rw \"\$(cwbdir_${rname})/\" || true
+  rm -rf \"\$(cwbdir_${rname})\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  : \${GOCACHE=\"${rbdir}/gocache\"}
-  : \${GOMODCACHE=\"${rbdir}/gomodcache\"}
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
+  : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
   mkdir -p build
   local p
   for p in ${rname}{,-keygen} ; do
@@ -44,7 +33,7 @@ function cwmake_${rname}() {
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
-        go build -x -ldflags '-s -w -extldflags \"-s -static\"' -o build/ \"${rbdir}/cmd/\${p}\"
+        go build -x -ldflags '-s -w -extldflags \"-s -static\"' -o build/ \"\$(cwbdir_${rname})/cmd/\${p}\"
   done
   unset p
   popd >/dev/null 2>&1
@@ -53,15 +42,15 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/bin\"
-  cwmkdir \"${ridir}/share/man/man1\"
-  cwmkdir \"${ridir}/share/doc\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  cwmkdir \"\$(cwidir_${rname})/share/man/man1\"
+  cwmkdir \"\$(cwidir_${rname})/share/doc\"
   local p
   for p in ${rname}{,-keygen} ; do
-    install -m 0755 build/\${p} \"${ridir}/bin/\${p}\"
-    install -m 0644 doc/\${p}.1 \"${ridir}/share/man/man1/\${p}.1\"
-    install -m 0644 doc/\${p}.1.html \"${ridir}/share/doc/\${p}.1.html\"
+    install -m 0755 build/\${p} \"\$(cwidir_${rname})/bin/\${p}\"
+    install -m 0644 doc/\${p}.1 \"\$(cwidir_${rname})/share/man/man1/\${p}.1\"
+    install -m 0644 doc/\${p}.1.html \"\$(cwidir_${rname})/share/doc/\${p}.1.html\"
   done
   unset p
   popd >/dev/null 2>&1
