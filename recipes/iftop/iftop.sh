19847dcaee2348ffdcb546ec8070f1a77c10ea7d
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-01 09:55:22 +0000

    iftop: use older libpcap19 library since i cannot figure out 1.10.x incompat

--- recipes/iftop/iftop.sh
+++ recipes/iftop/iftop.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://www.ex-parrot.com/~pdw/${rname}/download/${rfile}"
 rsha256="d032547c708307159ff5fd0df23ebd3cfa7799c31536fa0aea1820318a8e0eac"
-rreqs="make ncurses libpcap autoconf automake libtool libnl"
+rreqs="make ncurses libpcap19 autoconf automake libtool libnl"
 
 . "${cwrecipe}/common.sh"
 
@@ -12,7 +12,12 @@ eval "
 function cwconfigure_${rname}() {
   pushd "${rbdir}" >/dev/null 2>&1
   autoreconf -fiv
-  ./configure ${cwconfigureprefix} LIBS=\"\$(${cwsw}/libpcap/current/bin/pcap-config --static --libs)\"
+  env PATH=\"${cwsw}/libpcap19/current/bin:\${PATH}\" \
+    ./configure ${cwconfigureprefix} \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include) -I${cwsw}/ncurses/current/include/ncurses\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static -s\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
+      LIBS=\"\$(${cwsw}/libpcap19/current/bin/pcap-config --static --libs)\"
   popd >/dev/null 2>&1
 }
 "
