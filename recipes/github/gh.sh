2833caf4d9bb8d9c86c0829aff29325a3c443709
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-04 06:47:32 +0000

    gh: update to 2.21.2

--- recipes/github/gh.sh
+++ recipes/github/gh.sh
@@ -1,9 +1,9 @@
 rname="gh"
-rver="2.21.1"
+rver="2.21.2"
 rdir="cli-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/cli/cli/archive/refs/tags/${rfile}"
-rsha256="7c57f77ce5c8f1f418582b891224cedb364672f397251a1c593b8875f7c34f87"
+rsha256="225cdeeaf7cacd837abd58ec1b29a5351de02509d82a70921842214ebf2389be"
 rreqs="go bootstrapmake"
 
 if ! command -v git &>/dev/null ; then
