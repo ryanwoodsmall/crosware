6a7675addd6c1325d7e2a75b34050434a1dad0c5
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-09 05:58:08 +0000

    vile: update to 9.8x, use cw?dir_ funcs

--- recipes/vile/vile.sh
+++ recipes/vile/vile.sh
@@ -1,16 +1,16 @@
 rname="vile"
-rver="9.8w"
+rver="9.8x"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/current/${rfile}"
-rsha256="78253ec3f7ae5f4f9d4799a3c8bc35b85b47d456f2ac172810008a48e4609815"
+rsha256="8fe0dfa60179d4b7dd2750f116cd4396d4cd3e07d8a54d142a36c84f4a82feef"
 rreqs="make flex ncurses"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} \
     --with-screen=ncurses \
     --with-builtin-filters=all \
@@ -22,7 +22,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make LEX=\"${cwsw}/flex/current/bin/flex\"
   popd >/dev/null 2>&1
 }
