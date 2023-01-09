801d9df2c72d1f9bc64b3a7c9d35bbfe582be366
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-09 08:02:33 +0000

    gnutls: separate out patch function, use cw?dir_ funcs, install tlsproxy program from examples

--- recipes/gnutls/gnutls.sh
+++ recipes/gnutls/gnutls.sh
@@ -4,17 +4,26 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.gnupg.org/ftp/gcrypt/${rname}/v${rver%.*}/${rfile}"
 rsha256="1b79b381ac283d8b054368b335c408fedcb9b7144e0c07f531e3537d4328f3b3"
-rreqs="make sed byacc nettle gmp libtasn1 libunistring pkgconfig slibtool"
+rreqs="make sed byacc nettle gmp libtasn1 libunistring pkgconfig slibtool cacertificates"
 
 . "${cwrecipe}/common.sh"
 
 eval "
-function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+function cwpatch_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   cat configure > configure.ORIG
   sed -i \"s,/etc/${rname}/config,${cwetc}/${rname}/config,g\" configure
   sed -i \"s,/etc/unbound/root.key,${cwetc}/${rname}/unbound/root.key,g\" configure
-  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
+  sed -i s,-Wmissing-include-dirs,,g configure
+  sed -i.ORIG \"s,/etc/ssl/certs/ca-certificates.crt,${cwetc}/ssl/cert.pem,g\" doc/examples/*.c doc/examples/*.h doc/examples/tlsproxy/*.c doc/examples/tlsproxy/*.h
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --disable-doc \
     --disable-gtk-doc{,-{html,pdf}} \
     --disable-guile \
@@ -28,10 +37,27 @@ function cwconfigure_${rname}() {
     --enable-ssl3-support \
     --with-default-trust-store-dir=\"${cwetc}/ssl/certs\" \
     --with-default-trust-store-file=\"${cwetc}/ssl/cert.pem\" \
+    --with-unbound-root-key-file=\"${cwetc}/${rname}/unbound/root.key\" \
     --without-idn \
     --without-p11-kit \
     --without-tpm \
       PKG_CONFIG=\"${cwsw}/pkgconfig/current/bin/pkg-config\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install ${rlibtool}
+  cd doc/examples/tlsproxy
+  \${CC} \
+    buffer.c crypto-gnutls.c tlsproxy.c \
+    -o ${rname}-tlsproxy \
+    -I../../../ -I\$(cwidir_${rname})/include \$(echo -I${cwsw}/{${rreqs// /,}}/current/include) \
+    -L\$(cwidir_${rname})/lib \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) \
+    -lgnutls -lhogweed -lnettle -lgmp -ltasn1 -lunistring -static
+  install -m 755 ${rname}-tlsproxy \$(cwidir_${rname})/bin/
   popd >/dev/null 2>&1
 }
 "
