064e5d041d1c030f254b07f30a686abf2f2023cf
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-13 03:05:47 +0000

    xz: update to 5.2.10, note on 5.4.x

--- recipes/xz/xz.sh
+++ recipes/xz/xz.sh
@@ -1,9 +1,12 @@
+#
+# XXX - xz 5.4.x is released, need to test compat
+#
 rname="xz"
-rver="5.2.9"
+rver="5.2.10"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://tukaani.org/${rname}/${rfile}"
-rsha256="e982ea31b81543d7ee2b6fa34c2ad11760e1c50c6f4475add8ba0f2f005f07b4"
+rsha256="eb7a3b2623c9d0135da70ca12808a214be9c019132baaa61c9e1d198d1d9ded3"
 rreqs="make gettexttiny slibtool patch"
 
 . "${cwrecipe}/common.sh"
