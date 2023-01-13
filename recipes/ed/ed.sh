5e7843a0a62c88e5dc4cf844633e9b1c29d45bd1
ryanwoodsmall <rwoodsmall@gmail.com>
2023-01-13 02:17:14 +0000

    ed: update to 1.19, limit CPPFLAGS and LDFLAGS, use cw?dir_ funcs

--- recipes/ed/ed.sh
+++ recipes/ed/ed.sh
@@ -1,17 +1,17 @@
 rname="ed"
-rver="1.18"
+rver="1.19"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="aca8efad9800c587724a20b97aa8fc47e6b5a47df81606feaba831b074462b4f"
+rsha256="ce2f2e5c424790aa96d09dacb93d9bbfdc0b7eb6249c9cb7538452e8ec77cd48"
 rreqs="make lunzip"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\" CPPFLAGS=\"\${CPPFLAGS}\" LDFLAGS=\"\${LDFLAGS}\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} CC=\"\${CC}\" CFLAGS=\"\${CFLAGS}\" LDFLAGS=-static CPPFLAGS=
   popd >/dev/null 2>&1
 }
 "
