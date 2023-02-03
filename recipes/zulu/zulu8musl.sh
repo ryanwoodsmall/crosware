efa95bec2820abd0a4ff90165b1eb0768caa9574
ryanwoodsmall <rwoodsmall@gmail.com>
2023-02-03 17:35:03 +0000

    zulu8musl: update to 8.68.0.21-ca-jdk8.0.362

--- recipes/zulu/zulu8musl.sh
+++ recipes/zulu/zulu8musl.sh
@@ -1,11 +1,11 @@
-rver="8.66.0.15-ca-jdk8.0.352"
+rver="8.68.0.21-ca-jdk8.0.362"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="424e7b72997fddbb1dc519857b9bd1d5574b9a531eab768602c880823b00959e"
+rsha256="cdcf6db607e3218889a45a0507f1439158b53f18921778dfc3be8dd9e6ade8c9"
 # per-arch override rver/rdir/rfile/rurl/rsha256 here
 if [[ ${karch} =~ aarch64 ]] ; then
   rdir="zulu${rver}-linux_musl_aarch64"
-  rsha256="359a2ed589f67a641c79b01efd8e5b7685771434c604bc04ef768eb4a4c64357"
+  rsha256="d4d5eeedefad7ca46cda005293372e8b53d64d2ce1d1ae57758e21776deef1fa"
 fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
