7aeb6d4ba039c0c052943a80e4a8af240221d183
ryanwoodsmall <rwoodsmall@gmail.com>
2023-02-04 04:41:35 +0000

    e2fsprogs: update to 1.46.6, use cw?dir_ funcs

--- recipes/e2fsprogs/e2fsprogs.sh
+++ recipes/e2fsprogs/e2fsprogs.sh
@@ -1,16 +1,16 @@
 rname="e2fsprogs"
-rver="1.46.5"
+rver="1.46.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/${rname}/v${rver}/${rfile}"
-rsha256="2f16c9176704cf645dc69d5b15ff704ae722d665df38b2ed3cfc249757d8d81e"
+rsha256="a77517f19ff5e4e97ede63536566865dd5d48654e13fc145f5f2249ef7c4f4fc"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     --disable-nls \
     --disable-bsd-shlibs \
@@ -19,7 +19,7 @@ function cwconfigure_${rname}() {
     --enable-libuuid \
     --enable-symlink-install \
     --enable-verbose-makecmds \
-    --with-crond-dir=\"${ridir}/etc/cron.d\" \
+    --with-crond-dir=\"\$(cwidir_${rname})/etc/cron.d\" \
       LDFLAGS=-static \
       CPPFLAGS= \
       PKG_CONFIG_LIBDIR= \
@@ -30,7 +30,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool} MKDIR_P='mkdir -p' INSTALL='install -D'
   popd >/dev/null 2>&1
 }
