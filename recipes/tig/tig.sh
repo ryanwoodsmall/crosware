4a0148be8f21498bc548e0486dcd5bac04d36e55
ryanwoodsmall <rwoodsmall@gmail.com>
2023-02-06 02:32:45 +0000

    tig: update to 2.5.8, use cw?dir_ funcs

--- recipes/tig/tig.sh
+++ recipes/tig/tig.sh
@@ -1,16 +1,16 @@
 rname="tig"
-rver="2.5.7"
+rver="2.5.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jonas/tig/releases/download/${rdir}/${rfile}"
-rsha256="dbc7bac86b29098adaa005a76161e200f0734dda36de9f6bd35a861c7c29ca76"
+rsha256="b70e0a42aed74a4a3990ccfe35262305917175e3164330c0889bd70580406391"
 rreqs="make ncurses readline git pkgconfig pcre2"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} \
     --with-pcre
   popd >/dev/null 2>&1
