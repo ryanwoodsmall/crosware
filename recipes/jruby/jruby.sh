b036579124bccd8526c1993d996c371678ac8e8d
ryanwoodsmall <rwoodsmall@gmail.com>
2023-03-09 01:45:22 +0000

    jruby: update to 9.4.2.0

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="9.4.1.0"
+rver="9.4.2.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/${rname}/${rname}-dist/${rver}/${rfile}"
-rsha256="5e0cce40b7c42f8ad0f619fdd906460fe3ef13444707f70eb8abfc6481e0d6b6"
+rsha256="c2b065c5546d398343f86ddea68892bb4a4b4345e6c8875e964a97377733c3f1"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
