f7d83cf7c7ecd59a8e6c05406b21f5ded51927a6
ryanwoodsmall <rwoodsmall@gmail.com>
2023-03-21 05:32:03 +0000

    gpgme: update to 1.19.0

--- recipes/gnupg/gpgme.sh
+++ recipes/gnupg/gpgme.sh
@@ -1,9 +1,9 @@
 rname="gpgme"
-rver="1.18.0"
+rver="1.19.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/gpgme/${rfile}"
-rsha256="361d4eae47ce925dba0ea569af40e7b52c645c4ae2e65e5621bf1b6cdd8b0e9e"
+rsha256="cb58494dc415fba9eeb12b826550ad3190dc92e265c5bb2ae1a21c92841cfd38"
 rreqs="make gnupg libgpgerror libgcrypt libksba libassuan npth ntbtls sqlite readline ncurses slibtool zlib bzip2 pkgconfig pinentry configgit"
 
 . "${cwrecipe}/common.sh"
