02e14fe65964e23c15eebb54d2b3a511c35bf0cc
ryanwoodsmall <rwoodsmall@gmail.com>
2023-03-29 03:33:08 +0000

    most: update to 5.2.0, use cw?dir_ funcs

--- recipes/most/most.sh
+++ recipes/most/most.sh
@@ -1,16 +1,16 @@
 rname="most"
-rver="5.1.0"
+rver="5.2.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://www.jedsoft.org/releases/${rname}/${rfile}"
-rsha256="db805d1ffad3e85890802061ac8c90e3c89e25afb184a794e03715a3ed190501"
+rsha256="9455aeb8f826fa8385c850dc22bf0f22cf9069b3c3423fba4bf2c6f6226d9903"
 rreqs="make slang configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG s/ncurses5-config/ncurses6-config/g configure
   sed -i s/ncursesw5-config/ncursesw6-config/g configure
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
