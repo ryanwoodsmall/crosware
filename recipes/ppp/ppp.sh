df69c9ddcf2753954c0e5c81327717cc38484fb5
ryanwoodsmall <rwoodsmall@gmail.com>
2023-04-05 00:56:22 +0000

    ppp: update to 2.5.0, move to autotools archive, split out openssl/libressl builds

--- recipes/ppp/ppp.sh
+++ recipes/ppp/ppp.sh
@@ -1,61 +1,45 @@
 #
-# XXX - eap-tls support, openssl conflicting with md5/sha#/... headers
+# XXX - move config to ${cwetc}/ppp
 #
-
 rname="ppp"
-rver="2.4.9"
+rver="2.5.0"
 rdir="${rname}-${rver}"
-rfile="${rver}.tar.gz"
-rurl="https://github.com/ppp-project/${rname}/archive/refs/tags/${rfile}"
-rsha256="565bd23aeaf020b00287c4730fad75bfeb87c8e060462a2958c51e5a0f7963e2"
-rreqs="make libpcap libnl"
+rfile="${rdir}.tar.gz"
+rurl="https://download.samba.org/pub/ppp/${rfile}"
+rsha256="5cae0e8075f8a1755f16ca290eb44e6b3545d3f292af4da65ecffe897de636ff"
+rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
-# XXX - config placement?
 eval "
 function cwpatch_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cat pppd/Makefile.linux > pppd/Makefile.linux.ORIG
-  sed -i '/USE_EAPTLS=y/d' pppd/Makefile.linux
-  sed -i 's/^#USE_CRYPT=y/USE_CRYPT=y/g' pppd/Makefile.linux
-  sed -i 's/-lpcap/-lpcap -lnl-3 -lnl-genl-3/g' pppd/Makefile.linux
-  cat configure > configure.ORIG
-  sed -i '/^DESTDIR=/s,=.*,=${ridir},g' configure
-  sed -i '/^SYSCONF=/s,=.*,=${ridir}/etc,g' configure
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  echo '#include <sys/types.h>' >> pppd/config.h.in
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  ./configure
-  popd >/dev/null 2>&1
-}
-"
-
-eval "
-function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make \
-    CC=\"\${CC} \${CFLAGS}\" \
-    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
-    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib -static)\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
+    --enable-multilink \
+    --disable-{plugins,systemd,eaptls,openssl-engine,peap,microsoft-extensions} \
+    --without-{openssl,srp,atm,pam,pcap,gtk} \
+      LDFLAGS=-static \
+      CPPFLAGS= \
+      PKG_CONFIG_{LIBDIR,PATH}=
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make install \
-    CC=\"\${CC} \${CFLAGS}\" \
-    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
-    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib -static)\"
-  rm -rf \"${ridir}/etc/ppp\"
-  cwmkdir \"${ridir}/etc/ppp\"
-  ( cd etc.ppp ; tar cf - . ) | ( cd \"${ridir}/etc/ppp\" ; tar xvf - )
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install
+  rm -rf \"\$(cwidir_${rname})/etc/ppp\"
+  cwmkdir \"\$(cwidir_${rname})/etc/ppp\"
+  ( cd etc.ppp ; tar cf - . ) | ( cd \"\$(cwidir_${rname})/etc/ppp\" ; tar xvf - )
   popd >/dev/null 2>&1
 }
 "
---
+++ recipes/ppp/zz_pppopenssl.sh
@@ -0,0 +1,53 @@
+rname="pppopenssl"
+rver="$(cwver_ppp)"
+rdir="$(cwdir_ppp)"
+rfile="$(cwfile_ppp)"
+rdlfile="$(cwdlfile_ppp)"
+rurl="$(cwurl_ppp)"
+rsha256=""
+rreqs="make openssl libpcap libnl pkgconfig"
+rprof="${cwetcprofd}/zz_${rname}.sh"
+
+. "${cwrecipe}/common.sh"
+
+local f
+for f in clean fetch extract patch make ; do
+  eval "function cw${f}_${rname}() { cw${f}_ppp ; }"
+done
+unset f
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
+    --enable-{multilink,plugins,eaptls,peap,microsoft-extensions} \
+    --disable-{systemd,openssl-engine} \
+    --with-openssl=\"${cwsw}/openssl/current\" \
+    --with-pcap=\"${cwsw}/libpcap/current\" \
+    --without-{srp,atm,pam,gtk} \
+      CFLAGS=-fPIC \
+      CXXFLAGS=-fPIC \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
+      LIBS='-lpcap -lnl-3 -lnl-genl-3 -lssl -lcrypto'
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install
+  rm -rf \"\$(cwidir_${rname})/etc/ppp\"
+  cwmkdir \"\$(cwidir_${rname})/etc/ppp\"
+  ( cd etc.ppp ; tar cf - . ) | ( cd \"\$(cwidir_${rname})/etc/ppp\" ; tar xvf - )
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/sbin\"' > \"${rprof}\"
+}
+"
