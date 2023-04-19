2e484928599a69c01a4aa0052bbee4018a3583c1
ryanwoodsmall <rwoodsmall@gmail.com>
2023-04-19 03:00:40 +0000

    coreutils: update to 9.3

--- recipes/coreutils/coreutils.sh
+++ recipes/coreutils/coreutils.sh
@@ -3,11 +3,11 @@
 #
 
 rname="coreutils"
-rver="9.2"
+rver="9.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="6885ff47b9cdb211de47d368c17853f406daaf98b148aaecdf10de29cc04b0b3"
+rsha256="adbcfcfe899235b71e8768dcf07cd532520b7f54f9a8064843f8d199a904bbaa"
 rreqs="make gettexttiny sed attr acl perl gmp openssl utillinux libcap"
 
 . "${cwrecipe}/common.sh"
