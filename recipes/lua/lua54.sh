7840eaa316d24a007c60dc47520fd7f985aedd37
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-03 21:54:07 +0000

    lua54: move back to 5.4.4

--- recipes/lua/lua54.sh
+++ recipes/lua/lua54.sh
@@ -1,7 +1,7 @@
-rver="5.4.5"
+rver="5.4.4"
 sv="${rver%.*}"
 sv="${sv//./}"
 rname="lua${sv}"
-rsha256="59df426a3d50ea535a460a452315c4c0d4e1121ba72ff0bdde58c2ef31d6f444"
+rsha256="164c7849653b80ae67bec4b7473b884bf5cc8d2dca05653475ec2ed27b9ebf61"
 . "${cwrecipe}/${rname%${sv}}/${rname%${sv}}.sh.common"
 unset sv
