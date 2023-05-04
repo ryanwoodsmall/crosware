663b834500f48d3ae8fd39e216a87b3bb118f7b3
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-04 02:42:30 +0000

    svnkit: update to 1.10.10

--- recipes/svnkit/svnkit.sh
+++ recipes/svnkit/svnkit.sh
@@ -1,9 +1,9 @@
 rname="svnkit"
-rver="1.10.7"
+rver="1.10.10"
 rdir="${rname}-${rver}"
 rfile="org.tmatesoft.svn_${rver}.standalone.nojna.zip"
 rurl="https://www.svnkit.com/${rfile}"
-rsha256="a85a508feddd99bd1e460a37a97364016f08175727eeb018701bcf90fd6b3fb7"
+rsha256="e4f9b91874c70f11c4380d0b256a1cb8aa9d1089c75a40b54d415c4723c2b224"
 # we need unzip, use the busybox version
 rreqs=""
 if ! command -v unzip &>/dev/null ; then
