563601562b041bbcc84ec1220d523da9b2520912
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-11 04:40:57 +0000

    muslstandalone: update to 1.2.4, use cw?dir_ funcs

--- recipes/musl/muslstandalone.sh
+++ recipes/musl/muslstandalone.sh
@@ -3,11 +3,11 @@
 #   -L/usr/local/crosware/software/statictoolchain/current/lib/$(${CC} -dumpmachine)/lib
 #
 rname="muslstandalone"
-rver="1.2.3"
+rver="1.2.4"
 rdir="musl-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://musl.libc.org/releases/${rfile}"
-rsha256="7d5b0b6062521e4627e099e4c9dc8248d32a30285e959b7eecaa780cf8cfd4a4"
+rsha256="7a35eae33d5372a7c0da1188de798726f68825513b7ae3ebe97aaaa52114f039"
 rreqs="make"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
@@ -15,24 +15,24 @@ rprof="${cwetcprofd}/zz_${rname}.sh"
 
 eval "
 function cwfetch_${rname}() {
-  cwfetchcheck \"${rurl}\" \"${rdlfile}\" \"${rsha256}\"
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(cwsha256_${rname})\"
   cwfetch_kernelheaders
 }
 "
 
 eval "
 function cwextract_${rname}() {
-  cwextract \"${rdlfile}\" \"${cwbuild}\"
-  cwextract \$(cwdlfile_kernelheaders) \"${rbdir}\"
+  cwextract \"\$(cwdlfile_${rname})\" \"${cwbuild}\"
+  cwextract \$(cwdlfile_kernelheaders) \"\$(cwbdir_${rname})\"
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's#\\\$ldso#${rtdir}/current/lib/ld.so#g' tools/musl-gcc.specs.sh
   ./configure ${cwconfigureprefix} \
-    --syslibdir=\"${ridir}/lib\" \
+    --syslibdir=\"\$(cwidir_${rname})/lib\" \
     --enable-debug \
     --enable-warnings \
     --enable-wrapper=gcc \
@@ -48,7 +48,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   local a
   if [[ ${karch} =~ ^i ]] ; then
     a=x86
@@ -59,18 +59,18 @@ function cwmakeinstall_${rname}() {
   fi
   make install
   cd \$(cwdir_kernelheaders)
-  make ARCH=\${a} prefix=\"${ridir}\" install
-  ln -sf musl-gcc \"${ridir}/bin/cc\"
-  ln -sf musl-gcc \"${ridir}/bin/gcc\"
-  ln -sf musl-gcc \"${ridir}/bin/musl-cc\"
-  ln -sf musl-gcc \"${ridir}/bin/${statictoolchain_triplet[${karch}]}-cc\"
-  ln -sf musl-gcc \"${ridir}/bin/${statictoolchain_triplet[${karch}]}-gcc\"
-  ln -sf libc.so \"${ridir}/lib/ldd\"
-  ln -sf \"${rtdir}/current/lib/ldd\" \"${ridir}/bin/musl-ldd\"
-  ln -sf libc.so \"${ridir}/lib/ld.so\"
-  sed -i.ORIG 's#${ridir}#${rtdir}/current#g' \"${ridir}/lib/musl-gcc.specs\"
+  make ARCH=\${a} prefix=\"\$(cwidir_${rname})\" install
+  ln -sf musl-gcc \"\$(cwidir_${rname})/bin/cc\"
+  ln -sf musl-gcc \"\$(cwidir_${rname})/bin/gcc\"
+  ln -sf musl-gcc \"\$(cwidir_${rname})/bin/musl-cc\"
+  ln -sf musl-gcc \"\$(cwidir_${rname})/bin/${statictoolchain_triplet[${karch}]}-cc\"
+  ln -sf musl-gcc \"\$(cwidir_${rname})/bin/${statictoolchain_triplet[${karch}]}-gcc\"
+  ln -sf libc.so \"\$(cwidir_${rname})/lib/ldd\"
+  ln -sf \"${rtdir}/current/lib/ldd\" \"\$(cwidir_${rname})/bin/musl-ldd\"
+  ln -sf libc.so \"\$(cwidir_${rname})/lib/ld.so\"
+  sed -i.ORIG \"s#\$(cwidir_${rname})#${rtdir}/current#g\" \"\$(cwidir_${rname})/lib/musl-gcc.specs\"
   for a in \$(find \"${cwsw}/statictoolchain/current/\$(\${CC} -dumpmachine)/lib/\" -mindepth 1 -maxdepth 1 | grep '\\.a\$') ; do
-    test -e \"${ridir}/lib/\$(basename \${a})\" || ln -s \"\${a}\" \"${ridir}/lib/\" || true
+    test -e \"\$(cwidir_${rname})/lib/\$(basename \${a})\" || ln -s \"\${a}\" \"\$(cwidir_${rname})/lib/\" || true
   done
   unset a
   popd >/dev/null 2>&1
