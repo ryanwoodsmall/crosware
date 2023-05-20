477de5772ad8c35abb2f65d0b0bf99724f74a568
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-20 02:11:44 +0000

    libuv: update to 1.45.0, move to upstreamed autotools-ed dist file (avoid auto{conf,make}/libtool/perl reqs), normalize flags, use cw?dir_ funcs

--- recipes/libuv/libuv.sh
+++ recipes/libuv/libuv.sh
@@ -1,18 +1,20 @@
 rname="libuv"
-rver="1.44.2"
+rver="1.45.0"
 rdir="${rname}-${rver}"
-rfile="v${rver}.tar.gz"
-rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="e6e2ba8b4c349a4182a33370bb9be5e23c51b32efb9b9e209d0e8556b73a48da"
-rreqs="make libtool autoconf automake slibtool"
+rfile="${rname}-v${rver}-dist.tar.gz"
+rurl="https://dist.libuv.org/dist/v${rver}/${rfile}"
+rsha256="3793d8c0d6fa587721d010d0555b7e82443fd4e8b3c91e529eb6607592f52b87"
+rreqs="make slibtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  ./autogen.sh
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts}
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
+    LDFLAGS=-static \
+    CPPFLAGS= \
+    PKG_CONFIG_{LIBDIR,PATH}=
   popd >/dev/null 2>&1
 }
 "
