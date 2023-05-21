c25f94595ad0e516af776be3f0f45567d4ac7432
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-21 01:26:40 +0000

    cwstaticbinaries: update to 3cf9a83e753f319eef428385064386aeb0de15c1

--- recipes/crosware/cwstaticbinaries.sh
+++ recipes/crosware/cwstaticbinaries.sh
@@ -4,7 +4,7 @@
 #
 
 rname="cwstaticbinaries"
-rver="638dd716705cabe27dfa4281c4bfe7714138378d"
+rver="3cf9a83e753f319eef428385064386aeb0de15c1"
 rdir="${rname}-${rver}"
 rfile=""
 rurl="https://github.com/ryanwoodsmall/static-binaries"
