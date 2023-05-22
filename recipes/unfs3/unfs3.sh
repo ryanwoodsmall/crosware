71fc642c6d07fb48e245354cea5dd64ffe907c1d
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-22 04:10:21 +0000

    unfs3: update to 0.10.0, move to upstream github, fixup alpine patches, use cw?dir_ funcs, normalize flags, require pkgconfig

--- recipes/unfs3/unfs3.sh
+++ recipes/unfs3/unfs3.sh
@@ -1,5 +1,4 @@
 #
-# XXX - move to official github? https://github.com/unfs3/unfs3
 # XXX - tcp seems sketchy? udp works?
 # XXX - write an exports file: echo '/usr/local/crosware 0.0.0.0/0(ro)' | tee ${cwtop}/tmp/exports
 # XXX - start with: unfsd -d -e ${cwtop}/tmp/exports -i /tmp/unfsd.pid -u -n 2049 -m 2049 -p -s
@@ -7,37 +6,35 @@
 #
 
 rname="unfs3"
-rver="0.9.22"
+rver="0.10.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c"
-rreqs="make byacc flex libtirpc"
+rurl="https://github.com/unfs3/unfs3/releases/download/${rdir}/${rfile}"
+rsha256="9bc6568fba2e43f3e2181f1d1802afec41929c1e55a6e53da982aacc7ce79ebb"
+rreqs="make byacc flex libtirpc pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   cat configure > configure.ORIG
   sed -i '/LDFLAGS=/ s,-R/usr/ucblib,,' ./configure
-  sed -i 's/rpcsoc/tirpc/g' ./configure
   env PATH=\"${cwsw}/byacc/current/bin:${cwsw}/flex/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
       CC=\"\${CC} \${CFLAGS} -fcommon -I${cwsw}/libtirpc/current/include/tirpc\" \
-      LDFLAGS=\"-L${cwsw}/libtirpc/current/lib -L${cwsw}/flex/current/lib -static\" \
-      LIBS='-ltirpc' \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       LEX=\"${cwsw}/flex/current/bin/flex\" \
       YACC=\"${cwsw}/byacc/current/bin/byacc\" \
-      CPPFLAGS= \
-      PKG_CONFIG_{LIBDIR,PATH}=
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make LDFLAGS=\"-L${cwsw}/libtirpc/current/lib -L${cwsw}/flex/current/lib -ltirpc -lfl -static\" CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
   popd >/dev/null 2>&1
 }
