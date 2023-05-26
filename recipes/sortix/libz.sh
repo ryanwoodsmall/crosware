1a75ef60c80516299f1c062927a187825251fe5f
ryanwoodsmall <rwoodsmall@gmail.com>
2023-05-26 08:11:18 +0000

    libz: move to mirror, use cw?dir_ funcs

--- recipes/sortix/libz.sh
+++ recipes/sortix/libz.sh
@@ -7,7 +7,8 @@ rname="libz"
 rver="1.2.8.2015.12.26"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://sortix.org/${rname}/release/${rfile}"
+#rurl="https://sortix.org/${rname}/release/${rfile}"
+rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/sortix/libz/${rfile}"
 rsha256="abcc2831b7a0e891d0875fa852e9b9510b420d843d3d20aad010f65493fe4f7b"
 rreqs="bootstrapmake"
 
@@ -15,7 +16,7 @@ rreqs="bootstrapmake"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} \
      --enable-shared \
      --enable-static \
