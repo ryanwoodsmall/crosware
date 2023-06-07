ee4ce8ce4db4302b00c725ec99ab282de0505848
ryanwoodsmall <rwoodsmall@gmail.com>
2023-06-07 16:26:30 +0000

    go120: update to 1.20.5

--- recipes/go/go120.sh
+++ recipes/go/go120.sh
@@ -1,21 +1,22 @@
+
 rname="go120"
-rver="1.20.4"
-rdate="20230503"
+rver="1.20.5"
+rdate="20230607"
 if [[ ${karch} =~ ^x86_64$ ]] ; then
   rdir="go${rver}-amd64"
-  rsha256="e1f12e25aaf10217f8b419db984f9212d12f552b6e026ee1d7b2ab3c9c74e862"
+  rsha256="3c87fe1ba42c61e6adc6ca970f341f1821363932228aec3676a02a6c2caafa2c"
 elif [[ ${karch} =~ ^i.86$ ]] ; then
   rdir="go${rver}-386"
-  rsha256="f441007619c5a7943c1a39c86ecd47dcabad2bcb8952fbe3017d4c4b408956c6"
+  rsha256="edbd192ee8b6c5ea6e76023c549fcb188a68b3bd7be26cf49bb0476db81be65b"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rdir="go${rver}-arm64"
-  rsha256="d9bd8d39bee821eb8d7240c9421152259ebeb911337a887139bc3aa920b021d7"
+  rsha256="88d1a2f259074e149c8a8cab1d3dcf0fadb01962690027dfedfbc063eec3c19a"
 elif [[ ${karch} =~ ^arm ]] ; then
   rdir="go${rver}-arm"
-  rsha256="816ddc22316a46b8a6bcd364dcedb1fa3b897658ea0e0d6f6b9128d89720e8de"
+  rsha256="4168b887ae60457bf314d7abbf71e82a6ed35dab8a4958e0df6865fc077c5831"
 elif [[ ${karch} =~ ^riscv64 ]] ; then
   rdir="go${rver}-riscv64"
-  rsha256="9ff976c53b6e63a0cf0677915d5afba0a5474300b2c431bc8f0dca7a71dc02c3"
+  rsha256="a5c6a468c7611b5ac3d4a5563b8e1fb5020a3e448daf5e24420de399eada990e"
 fi
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/ryanwoodsmall/go-misc/releases/download/${rdate}-go${rver}/${rfile}"
