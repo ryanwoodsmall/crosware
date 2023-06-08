a0c2b0450ced9e68c77c84529567062463399612
ryanwoodsmall <rwoodsmall@gmail.com>
2023-06-08 06:59:55 +0000

    ksh93: env $TMPDIR?

--- recipes/ksh93/ksh93.sh
+++ recipes/ksh93/ksh93.sh
@@ -19,12 +19,12 @@ function cwmake_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env \
     PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/dashtiny/current/bin:${cwsw}/busybox/current/bin\" \
+    TMPDIR=\"${cwtop}/tmp\" \
       \"${cwsw}/dashtiny/current/bin/dash\" ./bin/package make \
         CC=\"\${CC} -static -Wl,-static\" \
         CCFLAGS=\"\${CFLAGS} -Wl,-s -Wl,-static -Os\" \
         LDFLAGS=\"-static -s\" \
-        SHELL=\"${cwsw}/dashtiny/current/bin/dash\" \
-        TMPDIR=\"${cwtop}/tmp\"
+        SHELL=\"${cwsw}/dashtiny/current/bin/dash\"
   popd >/dev/null 2>&1
 }
 "
@@ -37,12 +37,12 @@ function cwmakeinstall_${rname}() {
   rm -f \"\$(cwidir_${rname})/bin/${rname%93}\"
   env \
     PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/dashtiny/current/bin:${cwsw}/busybox/current/bin\" \
+    TMPDIR=\"${cwtop}/tmp\" \
       \"${cwsw}/dashtiny/current/bin/dash\" ./bin/package install \"\$(cwidir_${rname})\" \
         CC=\"\${CC} -static -Wl,-static\" \
         CCFLAGS=\"\${CFLAGS} -Wl,-s -Wl,-static -Os\" \
         LDFLAGS=\"-static -s\" \
-        SHELL=\"${cwsw}/dashtiny/current/bin/dash\" \
-        TMPDIR=\"${cwtop}/tmp\"
+        SHELL=\"${cwsw}/dashtiny/current/bin/dash\"
   cat \"\$(cwidir_${rname})/bin/${rname%93}\" > \"\$(cwidir_${rname})/bin/${rname}\"
   chmod 755 \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/${rname%93}\"
