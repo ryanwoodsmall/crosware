f3eefcb2767dfd66a344a30999ea911d190c2434
ryanwoodsmall <rwoodsmall@gmail.com>
2023-06-13 05:33:05 +0000

    python3: update to 3.8.17, limit flags, fix uuid module with e2fsprogs instead of utillinux, notes

--- recipes/python/python3.sh
+++ recipes/python/python3.sh
@@ -3,21 +3,21 @@
 # XXX - conditionally set PYTHONSTARTUP in profile.d file
 # XXX - example at https://gist.github.com/ryanwoodsmall/72b60ec679e4a1680c7eb7639694afd1
 # XXX - full shared: add "--enable-shared" and set LDFLAGS=\"\${LDFLAGS//-static/} -Wl,-rpath,${rtdir}/current/lib\"
-# XXX - uuid support is in e2fsprogs
-# XXX - move to libressl to avoid perl dep in openssl?
+# XXX - move to libressl to avoid perl dep in openssl? (libressl support removed in 3.10)
 # XXX - shared build should enable venv to work
 # XXX - "minimal" variant: libressl (no perl prereq), netbsdcurses+readline, dbm/gdbm/ndbm (via gdbm), sqlite, libffi, ...
 # XXX - sabotage patch for netbsd-curses https://github.com/sabotage-linux/sabotage/blob/06a4a815/KEEP/python2710-curses.patch
 # XXX - standalone builds, crib?: https://github.com/indygreg/python-build-standalone
 # XXX - ninja/meson cruft is ugly, need real separation
+# XXX - remove bdb - gdbm is plenty good for the dbm module
 #
 rname="python3"
-rver="3.7.16"
+rver="3.8.17"
 rdir="Python-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.python.org/ftp/python/${rver}/${rfile}"
-rsha256="8338f0c2222d847e904c955369155dc1beeeed806e8d5ef04b00ef4787238bfd"
-rreqs="make bzip2 zlib ncurses readline openssl gdbm sqlite bdb47 expat libffi xz utillinux pkgconfig"
+rsha256="2e54b0c68191f16552f6de2e97a2396540572a219f6bbb28591a137cecc490a9"
+rreqs="make bzip2 zlib ncurses readline openssl gdbm sqlite bdb47 expat libffi xz e2fsprogs pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
@@ -28,17 +28,17 @@ function cwconfigure_${rname}() {
     --with-ensurepip=install \
     --with-dbmliborder=gdbm:bdb \
     --with-system-{expat,ffi} \
-      LDFLAGS=\"\${LDFLAGS//-static/}\" \
       CFLAGS='-fPIC' \
       CXXFLAGS='-fPIC' \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include -I${cwsw}/ncurses/current/include/ncurses{,w} -I${cwsw}/e2fsprogs/current/include/uuid)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       LIBS='-lssl -lcrypto -lz -lncurses -lncursesw -lffi -llzma -lexpat'
   echo >> Modules/Setup.local
   echo 'readline readline.c -lreadline -lncurses -lncursesw' >> Modules/Setup.local
   echo '_ssl _ssl.c -DUSE_SSL -lssl -lcrypto -lz' >> Modules/Setup.local
-  echo '_curses _cursesmodule.c -lncurses -lncursesw' >> Modules/Setup.local
-  echo '_curses_panel _curses_panel.c -lpanel -lpanelw -lncurses -lncursesw' >> Modules/Setup.local
-  #echo '_uuid _uuidmodule.c -luuid' >> Modules/Setup.local
-  #echo \"dbm dbmmodule.c -lgdbm_compat -I${cwsw}/gdbm/current/include\" >> Modules/Setup.local
+  echo '_curses _cursesmodule.c -lncurses -lncursesw -I${cwsw}/ncurses/current/include/ncurses -I${cwsw}/ncurses/current/include/ncursesw' >> Modules/Setup.local
+  echo '_curses_panel _curses_panel.c -lpanel -lpanelw -lncurses -lncursesw -I${cwsw}/ncurses/current/include/ncurses -I${cwsw}/ncurses/current/include/ncursesw' >> Modules/Setup.local
   sed -i.ORIG 's#/usr/include#/no/usr/include#g' setup.py
   local sld
   sld=\"\${LDFLAGS//-L}\"
@@ -56,7 +56,9 @@ function cwmake_${rname}() {
   make -j${cwmakejobs} \
     CFLAGS='-fPIC' \
     CXXFLAGS'=-fPIC' \
-    LDFLAGS=\"\${LDFLAGS//-static/}\"
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include -I${cwsw}/ncurses/current/include/ncurses{,w} -I${cwsw}/e2fsprogs/current/include/uuid)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
@@ -67,7 +69,9 @@ function cwmakeinstall_${rname}() {
   make install \
     CFLAGS='-fPIC' \
     CXXFLAGS='-fPIC' \
-    LDFLAGS=\"\${LDFLAGS//-static/}\"
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include -I${cwsw}/ncurses/current/include/ncurses{,w} -I${cwsw}/e2fsprogs/current/include/uuid)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   for p in ninja meson ; do
     if \$(cwcheckinstalled \${p}) ; then
       cwlinkdir_${rname}
