056e0f8cb4049ad8725de2b21b88ea016002bc71
ryanwoodsmall <rwoodsmall@gmail.com>
2023-06-23 16:42:39 +0000

    libassuan: udpate to 2.5.6, use cw?dir_ funcs

--- recipes/gnupg/libassuan.sh
+++ recipes/gnupg/libassuan.sh
@@ -1,18 +1,18 @@
 rname="libassuan"
-rver="2.5.5"
+rver="2.5.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="8e8c2fcc982f9ca67dcbb1d95e2dc746b1739a4668bc20b3a3c5be632edb34e4"
+rsha256="e9fd27218d5394904e4e39788f9b1742711c3e6b41689a31aa3380bd5aa4f426"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
-    --with-libgpg-error-prefix=\${cwsw}/libgpgerror/current/
+    --with-libgpg-error-prefix=\"\${cwsw}/libgpgerror/current/\"
   popd >/dev/null 2>&1
 }
 "
