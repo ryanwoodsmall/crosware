8dbf0f578dcf82fe6289a26ad8a8bc2678a68413
ryanwoodsmall <rwoodsmall@gmail.com>
2023-06-27 03:28:39 +0000

    strace: update to 6.4, limit flags

--- recipes/strace/strace.sh
+++ recipes/strace/strace.sh
@@ -1,9 +1,9 @@
 rname="strace"
-rver="6.3"
+rver="6.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="e17878e301506c1cc301611118ad14efee7f8bcef63b27ace5d290acce7bb731"
+rsha256="27987dbac57fdfd260c6db4dc8328df35c95c6867c8a3d4371d59cdcf4eb9238"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
@@ -12,7 +12,13 @@ rreqs="make"
 eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} --enable-mpers=no --disable-gcc-Werror CFLAGS=\"\${CFLAGS} -Dsigcontext_struct=sigcontext\"
+  ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
+    --enable-mpers=no \
+    --disable-gcc-Werror \
+      CFLAGS=\"\${CFLAGS} -Dsigcontext_struct=sigcontext\" \
+      CPPFLAGS= \
+      LDFLAGS= \
+      PKG_CONFIG_{LIBDIR,PATH}=
   touch config.h
   if \$(uname -m | grep -q ^aarch64) ; then
     echo '#undef __ASM_SIGCONTEXT_H' >> src/config.h
