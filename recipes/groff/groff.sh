dd184bcc62a8c70866d28c884f140ec2588ed5c4
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-09 07:46:57 +0000

    groff: update to 1.23.0, use cw?dir_ funcs

--- recipes/groff/groff.sh
+++ recipes/groff/groff.sh
@@ -1,16 +1,16 @@
 rname="groff"
-rver="1.22.4"
+rver="1.23.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293"
+rsha256="6b9757f592b7518b4902eb6af7e54570bdccba37a871fddb2d30ae3863511c13"
 rreqs="gawk make perl sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} \
     --without-doc \
     --with-doc=no \
@@ -22,7 +22,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make
   popd >/dev/null 2>&1
 }
