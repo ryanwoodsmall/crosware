27834ed8098fba45de96392c9f60af71618204ee
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-10 01:40:30 +0000

    inih: update to r57, patch in version manually, use cw?dir_ funcs

--- recipes/inih/inih.sh
+++ recipes/inih/inih.sh
@@ -1,25 +1,34 @@
 rname="inih"
-rver="r56"
+rver="r57"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/benhoyt/${rname}/archive/refs/tags/${rfile}"
-rsha256="4f2ba6bd122d30281a8c7a4d5723b7af90b56aa828c0e88256d7fceda03a491a"
+rsha256="f03f98ca35c3adb56b2358573c8d3eda319ccd5287243d691e724b7eafa970b3"
 rreqs="meson ninja"
 
 . "${cwrecipe}/common.sh"
 
+# XXX - remove when fixed upstream
+eval "
+function cwpatch_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  sed -i.ORIG s,56,57,g meson.build
+  popd >/dev/null 2>&1
+}
+"
+
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/meson/current/bin:${cwsw}/ninja/current/bin:\${PATH}\" \
-    \"${cwsw}/meson/current/bin/meson\" --prefix=\"${ridir}\" --libdir=\"${ridir}/lib\" -Ddefault_library=static build
+    \"${cwsw}/meson/current/bin/meson\" --prefix=\"\$(cwidir_${rname})\" --libdir=\"\$(cwidir_${rname})/lib\" -Ddefault_library=static build
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/meson/current/bin:${cwsw}/ninja/current/bin:\${PATH}\" \
     \"${cwsw}/meson/current/bin/meson\" compile -C build
   popd >/dev/null 2>&1
@@ -28,7 +37,7 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/meson/current/bin:${cwsw}/ninja/current/bin:\${PATH}\" \
     \"${cwsw}/meson/current/bin/meson\" install -C build
   popd >/dev/null 2>&1
