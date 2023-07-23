c62c70018c6d12cb1585a6c84a2aca487caa5bf0
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-23 00:31:58 +0000

    libgcrypt: update to 1.10.2, use cw?dir_ funcs, normalize FLAGS

--- recipes/gnupg/libgcrypt.sh
+++ recipes/gnupg/libgcrypt.sh
@@ -1,19 +1,22 @@
 rname="libgcrypt"
-rver="1.8.10"
+rver="1.10.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="6896915501f951e23d02dcb0453469c2cc22aa4d77a001ff73a2647c2d29e7dd"
+rsha256="3b9c02a004b68c256add99701de00b383accccf37177e0d6c58289664cce0c03"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --disable-asm \
-    --with-libgpg-error-prefix=\${cwsw}/libgpgerror/current/
+    --with-libgpg-error-prefix=\${cwsw}/libgpgerror/current/ \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
