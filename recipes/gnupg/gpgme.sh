5b018b225eea56330eaa06878240899e474aecfd
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-23 00:41:27 +0000

    gpgme: update to 1.21.0, use cw?dir_ funcs, limit FLAGS

--- recipes/gnupg/gpgme.sh
+++ recipes/gnupg/gpgme.sh
@@ -1,27 +1,30 @@
 rname="gpgme"
-rver="1.20.0"
+rver="1.21.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/gpgme/${rfile}"
-rsha256="25a5785a5da356689001440926b94e967d02e13c49eb7743e35ef0cf22e42750"
+rsha256="416e174e165734d84806253f8c96bda2993fd07f258c3aad5f053a6efd463e88"
 rreqs="make gnupg libgpgerror libgcrypt libksba libassuan npth ntbtls sqlite readline ncurses slibtool zlib bzip2 pkgconfig pinentry configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/gnupg/current/bin:\${PATH}\" \
     ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
       --with-libgpg-error-prefix=\"${cwsw}/libgpgerror/current\" \
-      --with-libassuan-prefix=\"${cwsw}/libassuan/current\"
+      --with-libassuan-prefix=\"${cwsw}/libassuan/current\" \
+        CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+        LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+        PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/gnupg/current/bin:\${PATH}\" \
     make -j${cwmakejobs} ${rlibtool}
   popd >/dev/null 2>&1
@@ -30,7 +33,7 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/gnupg/current/bin:\${PATH}\" \
     make install ${rlibtool}
   popd >/dev/null 2>&1
