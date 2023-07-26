6ef51fb12607482f43da82f944c4ceea54d7599e
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-26 01:34:46 +0000

    ibaramg: update to 7.3, use cw?dir_ funcs

--- recipes/ibaramg/ibaramg.sh
+++ recipes/ibaramg/ibaramg.sh
@@ -1,16 +1,16 @@
 rname="ibaramg"
-rver="7.0"
+rver="7.3"
 rdir="mg-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ibara/mg/releases/download/mg-${rver}/${rfile}"
-rsha256="650dbdf9c9a72ec1922486ce07112d6181fc88a30770913d71d5c99c57fb2ac5"
+rsha256="1fd52feed9a96b93ef16c28ec4ff6cb25af85542ec949867bffaddee203d1e95"
 rreqs="make libbsd netbsdcurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwpatch_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   grep -ril sys/queue . | xargs sed -i.ORIG s,sys/queue,bsd/sys/queue,g
   popd >/dev/null 2>&1
 }
@@ -18,8 +18,8 @@ function cwpatch_${rname}() {
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  env CPPFLAGS= LDFLAGS=-static ./configure --enable-static --prefix=\"${ridir}\" --mandir=\"${ridir}/share/man\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  env CPPFLAGS= LDFLAGS=-static ./configure --enable-static --prefix=\"\$(cwidir_${rname})\" --mandir=\"\$(cwidir_${rname})/share/man\"
   sed -i.ORIG 's/-lncursesw/-lcurses -lterminfo/g' Makefile
   popd >/dev/null 2>&1
 }
@@ -27,7 +27,7 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make CPPFLAGS= LDFLAGS=-static CC=\"\${CC} \$(pkg-config --libs --cflags libbsd) -I${cwsw}/netbsdcurses/current/include -L${cwsw}/netbsdcurses/current/lib -Wl,-static\"
   popd >/dev/null 2>&1
 }
@@ -35,9 +35,9 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install CPPFLAGS= LDFLAGS=-static CC=\"\${CC} \$(pkg-config --libs --cflags libbsd) -I${cwsw}/netbsdcurses/current/include -L${cwsw}/netbsdcurses/current/lib -Wl,-static\"
-  ln -sf mg \"${ridir}/bin/${rname}\"
+  ln -sf mg \"\$(cwidir_${rname})/bin/${rname}\"
   popd >/dev/null 2>&1
 }
 "
