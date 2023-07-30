13e212e346945635e564e9b28f41cb9b9d98abdd
ryanwoodsmall <rwoodsmall@gmail.com>
2023-07-30 03:38:33 -0500

    zulu11musl: update to 11.66.15-ca-jdk11.0.20

--- recipes/zulu/zulu11musl.sh
+++ recipes/zulu/zulu11musl.sh
@@ -1,11 +1,11 @@
-rver="11.64.19-ca-jdk11.0.19"
+rver="11.66.15-ca-jdk11.0.20"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="600cbe8ea898cc6106105b61263db1f10177b935812d0b1678689c45fc179bfc"
+rsha256="8a54827da4f3aaac7c7e321ccdc45edea7e4d995297daab6ce89081d51ab2d05"
 # per-arch override rver/rdir/rfile/rurl/rsha256 here
 if [[ ${karch} =~ aarch64 ]] ; then
   rdir="zulu${rver}-linux_musl_aarch64"
-  rsha256="8950727298734f7b8b571a9b9577558e028a738d69005e1ae8e42d2dc0fc4fc6"
+  rsha256="f674a9d2d72d366c7bd9b4947dada0478d41a28d8b9a97946e2946c028e884b2"
 fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
