8b7b9297acb5eb95e54b665af8b8fa26a685d3a9
ryanwoodsmall <rwoodsmall@gmail.com>
2023-08-03 01:37:01 +0000

    xz: update to 5.4.4, move to github release

--- recipes/xz/xz.sh
+++ recipes/xz/xz.sh
@@ -1,9 +1,10 @@
 rname="xz"
-rver="5.4.3"
+rver="5.4.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://tukaani.org/${rname}/${rfile}"
-rsha256="1c382e0bc2e4e0af58398a903dd62fff7e510171d2de47a1ebe06d1528e9b7e9"
+#rurl="https://tukaani.org/${rname}/${rfile}"
+rurl="https://github.com/tukaani-project/xz/releases/download/v${rver}/${rfile}"
+rsha256="aae39544e254cfd27e942d35a048d592959bd7a79f9a624afb0498bb5613bdf8"
 rreqs="make gettexttiny slibtool patch"
 
 . "${cwrecipe}/common.sh"
