b6a62561abfc961c6fcc32ef2cbe8f7e700fe062
ryanwoodsmall <rwoodsmall@gmail.com>
2023-08-11 06:30:30 +0000

    nftables: update to 1.0.8

--- recipes/netfilter/nftables.sh
+++ recipes/netfilter/nftables.sh
@@ -14,11 +14,11 @@
 #
 
 rname="nftables"
-rver="1.0.7"
+rver="1.0.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
-rsha256="c12ac941fff9adaedf17367d5ce213789b98a0d314277bc22b3d71e10891f412"
+rsha256="9373740de41a82dbc98818e0a46a073faeb8a8d0689fa4fa1a74399c32bf3d50"
 rreqs="bootstrapmake pkgconfig byacc netbsdcurses readlinenetbsdcurses libpcap libnl jansson libmnl libnetfilterconntrack libnfnetlink libnftnl iptables slibtool"
 
 . "${cwrecipe}/common.sh"
