b90a17e0a864e1809690c30e1f04d1bd4f499d31
ryanwoodsmall <rwoodsmall@gmail.com>
2023-08-12 05:43:28 +0000

    otools: move to pkgconf, include fts support via muslfts, tear out otools- symlinks, notes

--- recipes/otools/otools.sh
+++ recipes/otools/otools.sh
@@ -1,10 +1,8 @@
 #
-# XXX - ${rname}-${p} symlink... hmm. might be better as a plan9 `9`-like wrapper?
-# XXX - do other receipes need this? baseutils, outils, ...
+# XXX - removed ${rname}-${p} symlinks... hmm. might be better as a plan9 `9`-like wrapper?
+# XXX - do other recipes need this? baseutils, outils, ...
 # XXX - bearssl libtls in use here - not extensively tested...
 # XXX - make libressl default? libretls (openssl) and libtlsbearssl variants?
-# XXX - turn on muslfts support?
-# XXX - otools- symlinks are kind of annoying, eliminate?
 #
 rname="otools"
 rver="1.5.1"
@@ -12,7 +10,7 @@ rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/CarbsLinux/${rname}/archive/refs/tags/${rfile}"
 rsha256="37dfe4f0ae14a9acf1453661f8c9afb2a2cf0a3c8bdb80abd51a7c5722479b08"
-rreqs="bootstrapmake zlib bearssl libtlsbearssl pkgconfig"
+rreqs="bootstrapmake zlib bearssl libtlsbearssl muslfts pkgconf"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
 . "${cwrecipe}/common.sh"
@@ -25,8 +23,9 @@ function cwconfigure_${rname}() {
     LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
     PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
     CC=\"\${CC} \${CFLAGS} \$(echo -I${cwsw}/{${rreqs// /,}}/current/include) \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\" \
+    PKG_CONFIG=\"${cwsw}/pkgconf/current/bin/pkgconf\" \
       ./configure ${cwconfigureprefix} \
-        --with-fts=none \
+        --with-fts=musl-fts \
         --with-less-t=no \
         --with-netcat=yes \
         --with-system-zlib
@@ -51,12 +50,6 @@ function cwmakeinstall_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool} \
     CC=\"\${CC} \${CFLAGS} \$(echo -I${cwsw}/{${rreqs// /,}}/current/include) \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib)\"
-  cd \"\$(cwidir_${rname})/bin/\"
-  for p in \$(find . ! -type d | grep -v ${rname}-) ; do
-    rm -f ${rname}-\${p}
-    ln -sf \${p} ${rname}-\$(basename \${p})
-  done
-  cd -
   popd >/dev/null 2>&1
 }
 "
