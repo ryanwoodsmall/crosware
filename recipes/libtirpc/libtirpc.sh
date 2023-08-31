9e972d7008e8619d646f6dc60e888753a92073a3
ryanwoodsmall <rwoodsmall@gmail.com>
2023-08-31 22:11:24 +0000

    libtirpc: update to 1.3.3, use cw?dir_ funcs

--- recipes/libtirpc/libtirpc.sh
+++ recipes/libtirpc/libtirpc.sh
@@ -1,16 +1,16 @@
 rname="libtirpc"
-rver="1.3.2"
+rver="1.3.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://sourceforge.net/projects/${rname}/files/${rname}/${rver}/${rfile}/download"
-rsha256="e24eb88b8ce7db3b7ca6eb80115dd1284abc5ec32a8deccfed2224fc2532b9fd"
+rsha256="6474e98851d9f6f33871957ddee9714fdcd9d8a5ee9abb5a98d63ea2e60e12f3"
 rreqs="make libbsd pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's,/etc/netconfig,${rtdir}/current/etc/netconfig,g' tirpc/netconfig.h
   sed -i.ORIG 's,/etc/bindresvport.blacklist,${rtdir}/current/etc/bindresvport.blacklist,g' src/bindresvport.c
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
@@ -26,11 +26,11 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   make install ${rlibtool}
-  cwmkdir \"${ridir}/etc\"
-  install -m 644 doc/netconfig \"${ridir}/etc/\"
-  install -m 644 doc/bindresvport.blacklist \"${ridir}/etc/\"
+  cwmkdir \"\$(cwidir_${rname})/etc\"
+  install -m 644 doc/netconfig \"\$(cwidir_${rname})/etc/\"
+  install -m 644 doc/bindresvport.blacklist \"\$(cwidir_${rname})/etc/\"
   popd >/dev/null 2>&1
 }
 "
