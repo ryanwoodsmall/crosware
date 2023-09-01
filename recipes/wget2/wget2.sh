775348ce56e8700da92ea31b79592c84996393f8
ryanwoodsmall <rwoodsmall@gmail.com>
2023-09-01 00:08:19 +0000

    wget2: update to 2.1.0, leave wget2libressl at 2.0.1 due to some (currently) missing API

--- recipes/wget2/wget2.sh
+++ recipes/wget2/wget2.sh
@@ -2,12 +2,13 @@
 # XXX - gpgme support
 # XXX - wolfssl seems to leave a port open and never completes a retrieval?
 # XXX - --with-included-regex : default, safe to specify/force?
+# XXX - libressl (at least 3.7.x, haven't tested with 3.8.x) breaks with wget2: https://github.com/rockdaboot/wget2/issues/271
 #
 rname="wget2"
-rver="2.0.1"
+rver="2.1.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname%2}/${rfile}"
-rsha256="2c942fba6a547997aa7aae0053b7c46a5203e311e4e62d305d575b6d2f06411f"
+rsha256="bc034194b512bb83ce0171d15a8db33e1c5c3ab8b3e343e1e6f2cf48f9154fad"
 
 . "${cwrecipe}/${rname}/${rname}.sh.common"
--- recipes/wget2/zz_wget2libressl.sh
+++ recipes/wget2/zz_wget2libressl.sh
@@ -1,8 +1,42 @@
+#
+# XXX - if wget2 2.1.x or libressl gets fixed, revert back!
+#
 rname="wget2libressl"
-rver="$(cwver_wget2)"
-rdir="$(cwdir_wget2)"
-rfile="$(cwfile_wget2)"
-rurl="$(cwurl_wget2)"
-rsha256="$(cwsha256_wget2)"
+rver="2.0.1"
+rdir="${rname%libressl}-${rver}"
+rfile="${rdir}.tar.lz"
+rurl="https://ftp.gnu.org/gnu/${rname%2libressl}/${rfile}"
+rsha256="2c942fba6a547997aa7aae0053b7c46a5203e311e4e62d305d575b6d2f06411f"
 
 . "${cwrecipe}/${rname%libressl}/${rname%libressl}.sh.common"
+
+# XXX - remove!!!
+cwstubfunc "cwpatch_${rname}"
+
+eval "
+function cwclean_${rname}() {
+  pushd \"${cwbuild}\" >/dev/null 2>&1
+  rm -rf \"${rbdir}\"
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwfetch_${rname}() {
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(cwsha256_${rname})\"
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  cwextract \"\$(cwdlfile_${rname})\" \"${cwbuild}\"
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make -j${cwmakejobs} ${rlibtool}
+  popd >/dev/null 2>&1
+}
+"
