f74a1180a8c2b23351ba826d7a837d1734b5ab42
ryanwoodsmall <rwoodsmall@gmail.com>
2023-09-13 05:23:37 +0000

    openssl: move to a virtual recipe defaulting to 1.1.1 until i can get 3.0 into shape

---
+++ recipes/openssl/zz_openssl.sh
@@ -0,0 +1,45 @@
+#
+# XXX - our perl needs to be at front of path for config/make
+# XXX - other "openssl version -a" stuff?
+# XXX - 1.1.1j disables threads/pic with -static in LDFLAGS. WHY? COME ON
+# XXX - disable zlib to workaround some conflicts (redis) and pain with forcing libz.a
+# XXX - remove `-lz` "fix" - this shouldn't be necessary but will need a full rebuild
+# XXX - need openssl11, openssl3, etc.?
+#
+
+v="111"
+rname="openssl"
+rver="$(cwver_${rname}${v})"
+rdir="$(cwdir_${rname}${v})"
+rfile="$(cwfile_${rname}${v})"
+rurl="$(cwurl_${rname}${v})"
+rsha256=""
+rreqs="openssl${v}"
+
+. "${cwrecipe}/common.sh"
+
+cwstubfunc "cwfetch_${rname}"
+cwstubfunc "cwextract_${rname}"
+cwstubfunc "cwconfigure_${rname}"
+cwstubfunc "cwmake_${rname}"
+
+eval "
+function cwmakeinstall_${rname}() {
+  cwmkdir \"${rtdir}\"
+  rm -rf \"${rtdir}/current\"
+  rm -rf \"\$(cwidir_${rname})\"
+  ln -sf \"${cwsw}/openssl${v}/\$(cwdir_${rname})\" \"\$(cwidir_${rname})\"
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
+  echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
+  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
+  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
+  #echo 'append_cppflags \"-I${rtdir}/current/include/${rname}\"' >> \"${rprof}\"
+}
+"
+
+unset v
