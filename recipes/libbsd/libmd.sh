f31a393c5a7b66bed4fbd20c0130ef968653971f
ryanwoodsmall <rwoodsmall@gmail.com>
2023-09-21 02:16:34 +0000

    libmd: update to 1.1.0, limit flags, use cw?dir_ funcs

--- recipes/libbsd/libmd.sh
+++ recipes/libbsd/libmd.sh
@@ -1,13 +1,12 @@
 #
 # XXX - safe to enable pkg-config bits *only*?
 #
-
 rname="libmd"
-rver="1.0.4"
+rver="1.1.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://libbsd.freedesktop.org/releases/${rfile}"
-rsha256="f51c921042e34beddeded4b75557656559cf5b1f2448033b4c1eec11c07e530f"
+rsha256="1bd6aa42275313af3141c7cf2e5b964e8b1fd488025caf2f971f43b00776b332"
 rreqs="make"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
@@ -15,16 +14,8 @@ rprof="${cwetcprofd}/zz_${rname}.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS='-static' CPPFLAGS=''
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
   popd >/dev/null 2>&1
 }
 "
-
-#eval "
-#function cwgenprofd_${rname}() {
-#  echo 'append_ldflags \"-L${rtdir}/current/lib\"' > \"${rprof}\"
-#  echo 'append_pkgconfigpath \"${rtdir}/current/lib/pkgconfig\"' >> \"${rprof}\"
-#  echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
-#}
-#"
