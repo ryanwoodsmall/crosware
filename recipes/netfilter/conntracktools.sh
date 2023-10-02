c8343f3080c3bea2866a3d683a1aee92c3686071
ryanwoodsmall <rwoodsmall@gmail.com>
2023-10-02 01:02:10 +0000

    conntracktools: update to 1.4.8, move archive to .tar.xz, use cw?dir_ funcs, couple of cleanups

--- recipes/netfilter/conntracktools.sh
+++ recipes/netfilter/conntracktools.sh
@@ -1,25 +1,23 @@
 #
 # XXX - dynamic???
 #
-
 rname="conntracktools"
-rver="1.4.7"
+rver="1.4.8"
 rdir="${rname//kt/k-t}-${rver}"
-rfile="${rdir}.tar.bz2"
+rfile="${rdir}.tar.xz"
 rurl="https://www.netfilter.org/pub/${rname//kt/k-t}/${rfile}"
-rsha256="099debcf57e81690ced57f516b493588a73518f48c14d656f823b29b4fc24b5d"
+rsha256="067677f4c5f6564819e78ed3a9d4a8980935ea9273f3abb22a420ea30ab5ded6"
 rreqs="bootstrapmake pkgconfig byacc flex slibtool libtirpc libmnl libnfnetlink libnetfilterconntrack libnetfiltercttimeout libnetfiltercthelper libnetfilterqueue"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
     LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
-    PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
-    PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
     LIBS='-lmnl -static'
   grep -ril -- -lnetfilter_cttimeout . | grep /Makefile | xargs sed -i 's,-lnetfilter_cttimeout,-lnetfilter_cttimeout -lmnl,g'
   popd >/dev/null 2>&1
