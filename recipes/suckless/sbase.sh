7c1f107d3dc4e87e4ef50eb1caf7d0cac6c21328
ryanwoodsmall <rwoodsmall@gmail.com>
2023-10-21 02:51:15 +0000

    sbase: update to 090490b81d01f4e1da005560669fbb1239c88989, work around "mkproto" stuff(?)/manually install, limit FLAGS

--- recipes/suckless/sbase.sh
+++ recipes/suckless/sbase.sh
@@ -1,10 +1,10 @@
 rname="sbase"
-rver="53040766d1a09baa7412c73c9a93afac2bfd6acc"
+rver="090490b81d01f4e1da005560669fbb1239c88989"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rprof="${cwetcprofd}/zz_${rname}.sh"
-rsha256="3016002557c3c43619b542a6af682288bde83a1480729ab0a7b794400a39bb1f"
+rsha256="f7778d8e83ebe44ebfcd293f0c25158a8ceb0182a6ae5682c0773c241cf816e0"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -12,16 +12,22 @@ rreqs="bootstrapmake"
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make ${rname}-box
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  ( unset CPPFLAGS ; make sbase-box ) || cwfailexit \"${rname}: build failed\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make ${rname}-box-install
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwscriptecho \"${rname}: manually installing\"
+  rm -rf \$(cwidir_${rname})/{bin,share} || true
+  cwmkdir \$(cwidir_${rname})/bin
+  install -m 755 sbase-box \$(cwidir_${rname})/bin/sbase-box
+  ( cd \$(cwidir_${rname})/bin ; for a in \$(./sbase-box) ; do ln -sf sbase-box \${a} ; done )
+  cwmkdir \$(cwidir_${rname})/share/man/man1
+  install -m 644 *.1 \$(cwidir_${rname})/share/man/man1/
   popd >/dev/null 2>&1
 }
 "
