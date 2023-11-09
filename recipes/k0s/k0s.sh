409a7606dac0a90a0c61e9c2d47a6f7bd4ca7842
ryanwoodsmall <rwoodsmall@gmail.com>
2023-11-09 02:17:57 +0000

    k0s: update to 1.28.3

--- recipes/k0s/k0s.sh
+++ recipes/k0s/k0s.sh
@@ -4,7 +4,7 @@
 #
 
 rname="k0s"
-rver="1.28.2_${rname}.0"
+rver="1.28.3_${rname}.0"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
@@ -13,13 +13,13 @@ rbfile="${rname}-v${rver//_/+}"
 rburl="https://github.com/${rname}project/${rname}/releases/download/v${rver//_/%2B}"
 if [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rbfile}-arm64"
-  rsha256="ef11e82e5c360b8e417223d7256305cffa262be62ef92b04ef284d6ebb893eca"
+  rsha256="415e7482ed5c06b1d326662368ee56622fff9c75c32fe7d83fb9a68d98e741c5"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rbfile}-arm"
-  rsha256="6164f4921dedef7a17cb6582eb548539eb8b82f96d848a20243432f8cc0a684c"
+  rsha256="1b79007ce55cbc7682da0f4e8eaecbb69f7c29f062dd4f26ff62f71d8cf17081"
 elif [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rbfile}-amd64"
-  rsha256="4bcd69e4d55352b27bfa45b900ee8b0923e54f0720be701b18e9b5496e68ab70"
+  rsha256="876731478af62e196a37cdbb1836fe8e08cb97de7122e21ddfb557a8e332d79b"
 fi
 rurl="${rburl}/${rfile//+/%2B}"
 unset rbfile
