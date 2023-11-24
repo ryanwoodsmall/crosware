f2dfcb0abf9576965cdca8e93278291bd8c7337a
ryanwoodsmall <rwoodsmall@gmail.com>
2023-11-24 06:07:40 +0000

    libksba: update to 1.6.5

--- recipes/gnupg/libksba.sh
+++ recipes/gnupg/libksba.sh
@@ -1,9 +1,9 @@
 rname="libksba"
-rver="1.6.4"
+rver="1.6.5"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="bbb43f032b9164d86c781ffe42213a83bf4f2fee91455edfa4654521b8b03b6b"
+rsha256="a564628c574c99287998753f98d750babd91a4e9db451f46ad140466ef2a6d16"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
