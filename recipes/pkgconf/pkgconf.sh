dae0207e5e264a850f555b400d166f095520fa23
ryanwoodsmall <rwoodsmall@gmail.com>
2023-11-24 08:47:58 +0000

    pkgconf: update to 2.1.0

--- recipes/pkgconf/pkgconf.sh
+++ recipes/pkgconf/pkgconf.sh
@@ -4,13 +4,13 @@
 #
 
 rname="pkgconf"
-rver="2.0.3"
+rver="2.1.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 #rurl="https://github.com/${rname}/${rname}/archive/refs/tags/${rfile}"
 #rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rurl="https://distfiles.dereferenced.org/${rname}/${rfile}"
-rsha256="cabdf3c474529854f7ccce8573c5ac68ad34a7e621037535cbc3981f6b23836c"
+rsha256="266d5861ee51c52bc710293a1d36622ae16d048d71ec56034a02eb9cf9677761"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
