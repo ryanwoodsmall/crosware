bed65321ba8b3da4088d240b2dbe59906c0385e1
ryanwoodsmall <rwoodsmall@gmail.com>
2023-12-02 04:32:28 +0000

    iperf3: update to 3.16, use cw?dir_ funcs, limit flags

--- recipes/iperf3/iperf3.sh
+++ recipes/iperf3/iperf3.sh
@@ -1,18 +1,21 @@
 rname="iperf3"
-rver="3.15"
+rver="3.16"
 rdir="iperf-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/esnet/${rname}/archive/${rfile}"
-rsha256="d287baa6f0ef4fc27160e2c9ae2fd7a03ce3ae303292e3b5455bce7ae633ad58"
+rsha256="be859e7168d0c867029e7013a8de273a169b4842ec7c24cd6570af75d5080aa3"
 rreqs="make openssl configgit zlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG 's/-pg//g' src/Makefile.in
-  ./configure ${cwconfigureprefix}
+  ./configure ${cwconfigureprefix} \
+    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static -s\" \
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
