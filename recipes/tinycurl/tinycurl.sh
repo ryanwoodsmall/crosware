94d945e1613094299f6379c998c5e710ff1b645f
ryanwoodsmall <rwoodsmall@gmail.com>
2023-12-07 08:34:51 +0000

    tinycurl: centralize main recipe

--- recipes/tinycurl/tinycurl.sh
+++ recipes/tinycurl/tinycurl.sh
@@ -1,22 +1,4 @@
 rname="tinycurl"
 rver="8.4.0"
-rdir="tiny-curl-${rver}"
-rfile="${rdir}.tar.gz"
-rurl="https://curl.se/tiny/${rfile}"
 rsha256="0b349aba6be4c1cd6216eb96062b2d405da3f04fb6798704b6ed971b67efef2e"
-rreqs="${rname}wolfssl"
-
-. "${cwrecipe}/common.sh"
-
-cwstubfunc "cwconfigure_${rname}"
-cwstubfunc "cwmake_${rname}"
-
-eval "
-function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  mkdir -p ${rtdir}
-  rm -rf \$(cwidir_${rname}) || true
-  ln -sf \$(cwidir_\$(cwreqs_${rname} | cut -f1 -d' ')) \$(cwidir_${rname})
-  popd >/dev/null 2>&1
-}
-"
+. "${cwrecipe}/tinycurl/tinycurl.sh.main"
--- recipes/tinycurl/tinycurl772.sh
+++ recipes/tinycurl/tinycurl772.sh
@@ -1,22 +1,4 @@
 rname="tinycurl772"
 rver="7.72.0"
-rdir="tiny-curl-${rver}"
-rfile="${rdir}.tar.gz"
-rurl="https://curl.se/tiny/${rfile}"
 rsha256="f903cfcd008615d194f80a2b1c1f261609cea9c764eb9daaff92bf99040f7730"
-rreqs="${rname}wolfssl"
-
-. "${cwrecipe}/common.sh"
-
-cwstubfunc "cwconfigure_${rname}"
-cwstubfunc "cwmake_${rname}"
-
-eval "
-function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  mkdir -p ${rtdir}
-  rm -rf \$(cwidir_${rname}) || true
-  ln -sf \$(cwidir_\$(cwreqs_${rname} | cut -f1 -d' ')) \$(cwidir_${rname})
-  popd >/dev/null 2>&1
-}
-"
+. "${cwrecipe}/tinycurl/tinycurl.sh.main"
