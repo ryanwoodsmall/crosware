e8fc6b1b4eabe708be35e3ade45cf121ccb972c2
ryanwoodsmall <rwoodsmall@gmail.com>
2023-12-16 02:18:47 +0000

    libatomicops: update to 7.8.2

--- recipes/libatomicops/libatomicops.sh
+++ recipes/libatomicops/libatomicops.sh
@@ -1,9 +1,9 @@
 rname="libatomicops"
-rver="7.8.0"
+rver="7.8.2"
 rdir="libatomic_ops-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ivmai/libatomic_ops/releases/download/v${rver}/${rfile}"
-rsha256="15676e7674e11bda5a7e50a73f4d9e7d60452271b8acf6fd39a71fefdf89fa31"
+rsha256="d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
