bd7bb4a3b9e71359d9595762d0c6fc7d456f474c
ryanwoodsmall <rwoodsmall@gmail.com>
2023-12-27 07:11:53 +0000

    cjson: update to 1.7.17, use cw?dir_ funcs, make sure compiler, assembler, etc. vars are set for make

--- recipes/cjson/cjson.sh
+++ recipes/cjson/cjson.sh
@@ -1,9 +1,9 @@
 rname="cjson"
-rver="1.7.16"
+rver="1.7.17"
 rdir="${rname//json/JSON}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/DaveGamble/${rname//json/JSON}/archive/refs/tags/${rfile}"
-rsha256="451131a92c55efc5457276807fc0c4c2c2707c9ee96ef90c47d68852d5384c6c"
+rsha256="c91d1eeb7175c50d49f6ba2a25e69b46bd05cffb798382c19bfb202e467ec51c"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -16,19 +16,19 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make lib${rname}{,_utils}.a CFLAGS=-fPIC CXXFLAGS=-fPIC LDFLAGS= PKG_CONFIG_LIBDIR= PKG_CONFIG_PATH=
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make lib${rname}{,_utils}.a CC=\"\${CC}\" CXX=\"\${CXX}\" CFLAGS=-fPIC CXXFLAGS=-fPIC AR=\"\${AR}\" LD=\"\${LD}\" LDFLAGS= PKG_CONFIG_LIBDIR= PKG_CONFIG_PATH=
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/include/${rname}\"
-  cwmkdir \"${ridir}/lib\"
-  install -m 0644 c*.h \"${ridir}/include/${rname}/\"
-  install -m 0644 lib*.a \"${ridir}/lib/\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwmkdir \"\$(cwidir_${rname})/include/${rname}\"
+  cwmkdir \"\$(cwidir_${rname})/lib\"
+  install -m 0644 c*.h \"\$(cwidir_${rname})/include/${rname}/\"
+  install -m 0644 lib*.a \"\$(cwidir_${rname})/lib/\"
   popd >/dev/null 2>&1
 }
 "
