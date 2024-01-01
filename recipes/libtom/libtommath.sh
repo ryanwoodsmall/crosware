c5cfeaae1883cf59d23aef310cf107b66aa49f89
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-01 03:14:58 +0000

    libtommath: update to 1.2.1

--- recipes/libtom/libtommath.sh
+++ recipes/libtom/libtommath.sh
@@ -1,9 +1,9 @@
 rname="libtommath"
-rver="1.2.0"
+rver="1.2.1"
 rdir="${rname}-${rver}"
 rfile="ltm-${rver}.tar.xz"
 rurl="https://github.com/libtom/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="b7c75eecf680219484055fcedd686064409254ae44bc31a96c5032843c0e18b1"
+rsha256="986025d7b374276fee2e30e99f3649e4ac0db8a02257a37ee10eae72abed0d1f"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
