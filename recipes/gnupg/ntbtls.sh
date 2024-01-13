62174f7a1b4564b2d43253b79324955838eb0b90
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-13 05:37:08 +0000

    ntbtls: update to 0.3.2, use cw?dir_ funcs, limit flags

--- recipes/gnupg/ntbtls.sh
+++ recipes/gnupg/ntbtls.sh
@@ -1,23 +1,24 @@
 rname="ntbtls"
-rver="0.3.1"
+rver="0.3.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="8922181fef523b77b71625e562e4d69532278eabbd18bc74579dbe14135729ba"
+rsha256="bdfcb99024acec9c6c4b998ad63bb3921df4cfee4a772ad6c0ca324dbbf2b07c"
 rreqs="make slibtool libgpgerror libgcrypt libksba zlib configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --with-libgpg-error-prefix=\"${cwsw}/libgpgerror/current\" \
     --with-libgcrypt-prefix=\"${cwsw}/libgcrypt/current\" \
     --with-ksba-prefix=\"${cwsw}/libksba/current\" \
     --with-zlib=\"${cwsw}/zlib/current\" \
-      CPPFLAGS=\"-I${cwsw}/libgcrypt/current/include\" \
-      LDFLAGS=-static
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
