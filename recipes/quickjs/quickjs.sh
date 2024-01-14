730e4133f34f0924708332d0cd4e34ffe6b8c80f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-14 11:17:36 +0000

    quickjs: update to 2024-01-13, use cw?dir_ funcs, make sure PREFIX is set

--- recipes/quickjs/quickjs.sh
+++ recipes/quickjs/quickjs.sh
@@ -7,11 +7,11 @@
 #
 
 rname="quickjs"
-rver="2023-12-09"
+rver="2024-01-13"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://bellard.org/${rname}/${rfile}"
-rsha256="e8afe386f875d0e52310ea91aa48e2b0e04182e821f19147794e3e272f4c8d8c"
+rsha256="3c4bf8f895bfa54beb486c8d1218112771ecfc5ac3be1036851ef41568212e03"
 #rfile="${rver}.tar.gz"
 #rurl="https://github.com/horhof/${rname}/archive/${rfile}"
 #rsha256="99a267894a162fb21cdb95061432910a7c5f0268c7e10b57bebc507586a629a6"
@@ -21,15 +21,16 @@ rreqs="make"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG '/CONFIG_LTO=y/d' Makefile
-  sed -i 's#^prefix=.*#prefix=${ridir}#g' Makefile
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cat Makefile > Makefile.ORIG
+  sed -i \"/^PREFIX/s#=.*#=\$(cwidir_${rname})#g\" Makefile
+  sed -i \"/^CFLAGS_OPT=/s/-O2/-O2 \${CFLAGS}/g\" Makefile
+  sed -i '/CONFIG_LTO=y/d' Makefile
   sed -i 's/-shared/-static/g' Makefile
   sed -i 's/-rdynamic/-static/g' Makefile
   sed -i '/^PROGS.*so$/s/PROGS/DISABLED_/g' Makefile
   sed -i '/^PROGS.*test_fib/s/PROGS/DISABLED_/g' Makefile
   sed -i '/^PROGS=/s/run-test262//g' Makefile
-  sed -i \"/^CFLAGS_OPT=/s/-O2/-O2 \${CFLAGS}/g\" Makefile
   sed -i 's/-lpthread/-lpthread -latomic/g' Makefile
   popd >/dev/null 2>&1
 }
@@ -38,16 +39,16 @@ function cwconfigure_${rname}() {
 eval "
 function cwmake_${rname}() {
   pushd \"${rbdir}\" >/dev/null 2>&1
-  make -j${cwmakejobs} CC=\"\${CC}\" CXX=\"\${CXX}\" LDFLAGS=-static CONFIG_CC=\${CC}
+  make -j${cwmakejobs} CC=\"\${CC}\" CXX=\"\${CXX}\" LDFLAGS=-static CONFIG_CC=\"\${CC}\" PREFIX=\"\$(cwidir_${rname})\"
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make install ${rlibtool}
-  ln -sf qjs \"${ridir}/bin/${rname}\"
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install ${rlibtool} PREFIX=\"\$(cwidir_${rname})\"
+  ln -sf qjs \"\$(cwidir_${rname})/bin/${rname}\"
   popd >/dev/null 2>&1
 }
 "
