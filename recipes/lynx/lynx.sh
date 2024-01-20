94dcef6c8d0e16e28024bbd21c89560ca61b2283
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-20 04:36:01 +0000

    lynx: update to 2.9.0, fix -lssl linkage order, workaround missing delete_screen in netbsdcurses

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -1,13 +1,13 @@
 #
 # XXX - gnutls variant
+# XXX - netbsdcurses (non-slang) HAVE_DELSCREEN/delete_screen/stop_curses breakage
 #
-
 rname="lynx"
-rver="2.8.9rel.1"
+rver="2.9.0"
 rdir="${rname}${rver}"
-rfile="${rname}${rver}.tar.bz2"
+rfile="${rname}${rver}.tar.gz"
 rurl="https://invisible-mirror.net/archives/${rname}/tarballs/${rfile}"
-rsha256="387f193d7792f9cfada14c60b0e5c0bff18f227d9257a39483e14fa1aaf79595"
+rsha256="746c926e28d50571a42d2477f9c50784b27fc8cba4c7db7f3e6c9e00dde89070"
 rreqs="ncurses openssl"
 
 . "${cwrecipe}/lynx/lynx.sh.common"
