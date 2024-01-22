5e13e2bb0f2494bd766eabf81be539fd50aceb53
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-22 09:24:01 +0000

    ed: update to 1.20

--- recipes/ed/ed.sh
+++ recipes/ed/ed.sh
@@ -1,9 +1,9 @@
 rname="ed"
-rver="1.19"
+rver="1.20"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="ce2f2e5c424790aa96d09dacb93d9bbfdc0b7eb6249c9cb7538452e8ec77cd48"
+rsha256="c6030defe6ae172f1687906d7354054c75a6a9130af319d4e73c50a91959c5a6"
 rreqs="make lunzip"
 
 . "${cwrecipe}/common.sh"
