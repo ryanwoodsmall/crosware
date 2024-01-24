48941c69cac7abf4281d499c1dec959808262e2a
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-24 07:05:51 +0000

    nextvi: update to 07e9da9acfd5fbe48b2b35c9534032687d45aa67, update and include arrowkeys_insert/arrowkeys_normal/filetype_shebang/stdin_pipe patches

--- recipes/nextvi/nextvi.sh
+++ recipes/nextvi/nextvi.sh
@@ -1,18 +1,14 @@
 rname="nextvi"
-rver="a458e47c0a1293b04da52c75ae7fb3dbbfab1b7c"
+rver="07e9da9acfd5fbe48b2b35c9534032687d45aa67"
 rdir="${rname}-${rver}"
 rfile="${rver}.zip"
 rurl="https://github.com/kyx0r/nextvi/archive/${rfile}"
-rsha256="2cb95b4e49f614a9c1d140d9b3b469ec0a8acee71161f2dbfd184252f1a43f05"
+rsha256="94b741ccb1db9c3cd7a864d80175daeae7c74d94d0c0b3bc53b898ce1843b4b9"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
 
-eval "
-function cwconfigure_${rname}() {
-  true
-}
-"
+cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwmake_${rname}() {
