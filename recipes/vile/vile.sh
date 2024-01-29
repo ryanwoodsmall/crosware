8807b72a5b41bff98713072465efedbd44288dde
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-29 07:47:05 +0000

    vile: limit flags

--- recipes/vile/vile.sh
+++ recipes/vile/vile.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/current/${rfile}"
 rsha256="0b3286c327b70a939f21992d22e42b5c1f8a6e953bd9ab9afa624ea2719272f7"
-rreqs="make flex ncurses"
+rreqs="make flex ncurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
@@ -15,7 +15,10 @@ function cwconfigure_${rname}() {
     --with-screen=ncurses \
     --with-builtin-filters=all \
     --with-loadable-filters=none \
-      LEX=\"${cwsw}/flex/current/bin/flex\"
+      LEX=\"${cwsw}/flex/current/bin/flex\" \
+      CPPFLAGS=\"\$(echo -I${cwsw}/flex/current/include -I${cwsw}/ncurses/current/include{,/ncurses{,w}})\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{flex,ncurses}/current/lib) -static\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"${cwsw}/ncurses/current/lib/pkgconfig\"
   popd >/dev/null 2>&1
 }
 "
