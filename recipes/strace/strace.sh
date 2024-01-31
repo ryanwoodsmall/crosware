356af3f8b45e1d37753777b2eefcb44f1d2c29a9
ryanwoodsmall <rwoodsmall@gmail.com>
2024-01-31 02:27:00 +0000

    strace: update to 6.7

--- recipes/strace/strace.sh
+++ recipes/strace/strace.sh
@@ -1,9 +1,9 @@
 rname="strace"
-rver="6.6"
+rver="6.7"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="421b4186c06b705163e64dc85f271ebdcf67660af8667283147d5e859fc8a96c"
+rsha256="2090201e1a3ff32846f4fe421c1163b15f440bb38e31355d09f82d3949922af7"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
