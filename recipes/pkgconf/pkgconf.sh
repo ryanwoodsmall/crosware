ef70beb1859a5fd3dfd2952386d29bacef85dbc3
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-05 05:58:25 +0000

    pkgconf: update to 2.1.1.

--- recipes/pkgconf/pkgconf.sh
+++ recipes/pkgconf/pkgconf.sh
@@ -2,15 +2,14 @@
 # XXX - opt-in only for now
 # XXX - moving to https://gitea.treehouse.systems/ariadne/pkgconf
 #
-
 rname="pkgconf"
-rver="2.1.0"
+rver="2.1.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 #rurl="https://github.com/${rname}/${rname}/archive/refs/tags/${rfile}"
 #rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rurl="https://distfiles.dereferenced.org/${rname}/${rfile}"
-rsha256="266d5861ee51c52bc710293a1d36622ae16d048d71ec56034a02eb9cf9677761"
+rsha256="3a224f2accf091b77a5781316e27b9ee3ba82c083cc2e539e08940b68a44fec5"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
