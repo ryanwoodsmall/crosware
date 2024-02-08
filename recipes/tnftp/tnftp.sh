729a8d3162fc6f522fd590cbac207f64e7f7c3fc
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-08 06:54:54 +0000

    tnftp: use/require pkgconf and libressl

--- recipes/tnftp/tnftp.sh
+++ recipes/tnftp/tnftp.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.netbsd.org/pub/NetBSD/misc/${rname}/${rfile}"
 rsha256="be0134394bd7d418a3b34892b0709eeb848557e86474e1786f0d1a887d3a6580"
-rreqs="make netbsdcurses libeditnetbsdcurses configgit"
+rreqs="make netbsdcurses libeditnetbsdcurses configgit libressl pkgconf"
 
 . "${cwrecipe}/common.sh"
 
@@ -12,13 +12,16 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} ${cwconfigurelibopts} \
-    --disable-ssl \
     --enable-editcomplete \
+    --enable-ipv6 \
+    --enable-ssl \
+    --with-openssl=\"${cwsw}/libressl/current\" \
     --without-socks \
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include) -I. -I..\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       LIBS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -ledit -lcurses -lterminfo\" \
-      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig| tr ' ' ':')\"
+      PKG_CONFIG=\"${cwsw}/pkgconf/current/bin/pkgconf\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
   popd >/dev/null 2>&1
 }
 "
