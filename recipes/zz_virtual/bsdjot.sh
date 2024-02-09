42532a81448581e723678baf2b6803778f7d2405
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-09 00:55:59 +0000

    bsdprogs: move bsd{jot,rs,{,un}vis} to outils versions

--- recipes/zz_virtual/bsdjot.sh
+++ recipes/zz_virtual/bsdjot.sh
@@ -1,17 +1,2 @@
 rname="bsdjot"
-rver="1.27"
-rdir="${rname}-${rver}"
-rfile="${rdir}.fake"
-rurl="http://cvsweb.netbsd.org/bsdweb.cgi/src/usr.bin/jot/"
-rsha256="628d638d35dfcf8a6fd1b5e0ede650bb6501e02d40113f6e524f9b61f8b92e29"
-rreqs="libbsd pkgconfig"
-
-. "${cwrecipe}/common.sh"
-. "${cwrecipe}/bsdprogs/bsdprogs.sh.common"
-
-eval "
-function cwfetch_${rname}() {
-  cwfetchcheck \"http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/usr.bin/jot/jot.c?rev=${rver}\" \"${cwdl}/${rname}/jot.c\" \"${rsha256}\"
-  cwfetchcheck \"http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/usr.bin/jot/jot.1?rev=1.16\" \"${cwdl}/${rname}/jot.1\" \"39711d1da08196d3f8670949343c177fa0a7d71b5cdb60dcfea917826be8df1b\"
-}
-"
+. "${cwrecipe}/zz_virtual/bsdprogs.sh.common"
--- recipes/zz_virtual/bsdrs.sh
+++ recipes/zz_virtual/bsdrs.sh
@@ -1,17 +1,2 @@
 rname="bsdrs"
-rver="1.16"
-rdir="${rname}-${rver}"
-rfile="${rdir}.fake"
-rurl="http://cvsweb.netbsd.org/bsdweb.cgi/src/usr.bin/rs/"
-rsha256="4ccbd6128302b2eaac39aaf63fb6d292888b4de218d0224074ec4e3cfefad2f8"
-rreqs="libbsd pkgconfig"
-
-. "${cwrecipe}/common.sh"
-. "${cwrecipe}/bsdprogs/bsdprogs.sh.common"
-
-eval "
-function cwfetch_${rname}() {
-  cwfetchcheck \"http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/usr.bin/rs/rs.c?rev=${rver}\" \"${cwdl}/${rname}/rs.c\" \"${rsha256}\"
-  cwfetchcheck \"http://cvsweb.netbsd.org/bsdweb.cgi/~checkout~/src/usr.bin/rs/rs.1?rev=1.11\" \"${cwdl}/${rname}/rs.1\" \"1a5753034daf0bdce754342c34615111a7ba1e1deaf2b6c100dd7c7842725916\"
-}
-"
+. "${cwrecipe}/zz_virtual/bsdprogs.sh.common"
