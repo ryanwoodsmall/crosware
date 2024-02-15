c96a4063637c69bf3bfacae358c96dd402f52148
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-15 03:02:03 +0000

    inih: make sure `meson setup` is run instead of just `meson`; remove unnecessary patch

--- recipes/inih/inih.sh
+++ recipes/inih/inih.sh
@@ -8,20 +8,11 @@ rreqs="meson ninja"
 
 . "${cwrecipe}/common.sh"
 
-# XXX - remove when fixed upstream
-eval "
-function cwpatch_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  sed -i.ORIG s,56,57,g meson.build
-  popd >/dev/null 2>&1
-}
-"
-
 eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/meson/current/bin:${cwsw}/ninja/current/bin:\${PATH}\" \
-    \"${cwsw}/meson/current/bin/meson\" --prefix=\"\$(cwidir_${rname})\" --libdir=\"\$(cwidir_${rname})/lib\" -Ddefault_library=static build
+    \"${cwsw}/meson/current/bin/meson\" setup --prefix=\"\$(cwidir_${rname})\" --libdir=\"\$(cwidir_${rname})/lib\" -Ddefault_library=static build
   popd >/dev/null 2>&1
 }
 "
