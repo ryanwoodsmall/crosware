582b9ed382ba22f18283f44157c26de75cb41e63
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-15 04:33:42 +0000

    scripts/view-cloc-diff.sh: add a modeline for vim, note on `vim -u NONE`

--- scripts/view-cloc-diff.sh
+++ scripts/view-cloc-diff.sh
@@ -3,6 +3,7 @@
 # show the differnces between runs of ${cwtop}/scripts/crosware-cloc-source.sh
 #
 # XXX - you _really_ want diffutils diff here. busybox is slowww...
+# XXX - edit with 'vim -u NONE' or else the modeline injected will be processed and error
 #
 
-vim <(diff -Naur ${cwtop}/tmp/crosware.set.out{.old,} | egrep -v -- '^(-|\+| )(cw.*=\(|(LS_COLORS|PATH|PKG_CONFIG_(LIBDIR|PATH)|(C(|XX|PP)|LD)FLAGS)=)')
+vim <(diff -Naur ${cwtop}/tmp/crosware.set.out{.old,} | egrep -v -- '^(-|\+| )(cw.*=\(|(LS_COLORS|PATH|PKG_CONFIG_(LIBDIR|PATH)|(C(|XX|PP)|LD)FLAGS)=)' ; echo '# vim: ft=diff:' )
