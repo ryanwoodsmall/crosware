25529f4026efdddb6432c3c0d68aafb46722c4cc
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-17 01:57:32 +0000

    libffi: update to 3.4.5, limit flags

--- recipes/libffi/libffi.sh
+++ recipes/libffi/libffi.sh
@@ -1,9 +1,9 @@
 rname="libffi"
-rver="3.4.4"
+rver="3.4.5"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="d66c56ad259a82cf2a9dfc408b32bf5da52371500b84745f7fb8b645712df676"
+rsha256="96fff4e589e3b239d888d9aa44b3ff30693c2ba1617f953925a70ddebcc102b2"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
@@ -11,7 +11,7 @@ rreqs="make configgit"
 eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
   sed -i.ORIG 's/__gnu_linux__/__linux__/g' src/closures.c
   popd >/dev/null 2>&1
 }
