1eb584bc6d1369396873b027c6d8188170514775
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-17 10:16:11 +0000

    scripts/container-packages.sh: add script

---
+++ scripts/container-packages.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# these are the base recipes installed as part of the container build
+#
+set -eu
+set -o pipefail
+
+declare -a container_recipes
+container_recipes+=( '9base' )
+container_recipes+=( 'ag' )
+container_recipes+=( 'alpinemuslutils' )
+container_recipes+=( 'bash' )
+container_recipes+=( 'bearssl' )
+container_recipes+=( 'bim' )
+container_recipes+=( 'bootstrapmake' )
+container_recipes+=( 'busybox' )
+container_recipes+=( 'cacertificates' )
+container_recipes+=( 'cryanc' )
+container_recipes+=( 'curlmbedtls' )
+container_recipes+=( 'dropbear' )
+container_recipes+=( 'elvis' )
+container_recipes+=( 'entr' )
+container_recipes+=( 'htermutils' )
+container_recipes+=( 'jo' )
+container_recipes+=( 'jq' )
+container_recipes+=( 'less' )
+container_recipes+=( 'mbedtls' )
+container_recipes+=( 'mujs' )
+container_recipes+=( 'neatvi' )
+container_recipes+=( 'otools' )
+container_recipes+=( 'outils' )
+container_recipes+=( 'plan9port9p' )
+container_recipes+=( 'pv' )
+container_recipes+=( 'px5g' )
+container_recipes+=( 'rlwrap' )
+container_recipes+=( 'rsyncminimal' )
+container_recipes+=( 'sbase' )
+container_recipes+=( 'shellish' )
+container_recipes+=( 'tini' )
+container_recipes+=( 'tinyscheme' )
+container_recipes+=( 'toybox' )
+container_recipes+=( 'u9fs' )
+container_recipes+=( 'ubase' )
+container_recipes+=( 'x509cert' )
+
+for (( i=0; i<${#container_recipes[@]}; i++ )) ; do
+  echo "${container_recipes[${i}]}"
+done | paste -s -d' ' -
