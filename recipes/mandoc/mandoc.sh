0e1fe57c6405e32b6eaec5e3381a2f5ab714cbcf
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-17 03:44:59 +0000

    mandoc: remove manpages require to avoid coreutils req,, use cw?dir_ funcs, limit flags

--- recipes/mandoc/mandoc.sh
+++ recipes/mandoc/mandoc.sh
@@ -4,23 +4,23 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="http://mandoc.bsd.lv/snapshots/${rfile}"
 rsha256="8bf0d570f01e70a6e124884088870cbed7537f36328d512909eb10cd53179d9c"
-rreqs="make zlib busybox less manpages"
+rreqs="make zlib busybox less"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG '/env -i make/s/env -i/env/g' configure
   {
-    echo 'PREFIX=\"${ridir}\"'
-    echo 'WWWPREFIX=\"${ridir}/www\"'
-    echo 'HTDOCDIR=\"${ridir}/www/htdocs\"'
-    echo 'CGIBINDIR=\"${ridir}/www/htdocs/cgi-bin\"'
-    echo 'BUILD_CGI=1'
-    echo 'INSTALL_LIBMANDOC=1'
-    echo 'UTF8_LOCALE=en_US.UTF-8'
-    echo 'HAVE_WCHAR=1'
+    echo PREFIX=\"\$(cwidir_${rname})\"
+    echo WWWPREFIX=\"\$(cwidir_${rname})/www\"
+    echo HTDOCDIR=\"\$(cwidir_${rname})/www/htdocs\"
+    echo CGIBINDIR=\"\$(cwidir_${rname})/www/htdocs/cgi-bin\"
+    echo BUILD_CGI=1
+    echo INSTALL_LIBMANDOC=1
+    echo UTF8_LOCALE=en_US.UTF-8
+    echo HAVE_WCHAR=1
   } > configure.local
   cat cgi.h.example > cgi.h
   sed -i '/MAN_DIR/s#/man#${cwsw}/manpages/current/share/man#g' cgi.h
@@ -32,28 +32,24 @@ function cwconfigure_${rname}() {
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/less/current/bin:${cwsw}/busybox/current/bin:${cwsw}/make/current/bin\" \
-    make CFLAGS=\"\${CFLAGS} \${CPPFLAGS}\" LDFLAGS=\"\${LDFLAGS}\"
+    make CFLAGS=\"\${CFLAGS} -I${cwsw}/zlib/current/include\" LDFLAGS=\"-L${cwsw}/zlib/current/lib -static\" CPPFLAGS=
   popd >/dev/null 2>&1
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env PATH=\"${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/less/current/bin:${cwsw}/busybox/current/bin:${cwsw}/make/current/bin\" \
-    make install
+    make install CFLAGS=\"\${CFLAGS} -I${cwsw}/zlib/current/include\" LDFLAGS=\"-L${cwsw}/zlib/current/lib -static\" CPPFLAGS=
   local s
   for s in 1 3 5 7 8 ; do
-    cwmkdir \"${ridir}/man/man\${s}\"
-    install -m 0644 *.\${s} \"${ridir}/man/man\${s}/\"
+    cwmkdir \"\$(cwidir_${rname})/man/man\${s}\"
+    install -m 0644 *.\${s} \"\$(cwidir_${rname})/man/man\${s}/\"
   done
   unset s
-  cwmkdir \"${cwsw}/manpages/current/share/man\"
-  #echo \"${cwsw}/manpages/current/share/man\" > \"${cwsw}/manpages/current/share/man/manpath.conf\"
-  echo \"/man\" > \"${cwsw}/manpages/current/share/man/manpath.conf\"
-  ln -sf \"${cwsw}/manpages/current/share/man\" \"${ridir}/www/htdocs/man\"
   popd >/dev/null 2>&1
 }
 "
