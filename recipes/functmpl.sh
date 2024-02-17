e2560c8dffbc0993b81a031701abd2ebdbb26170
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-17 07:58:07 +0000

    recipes/functmpl.sh: make director type configurable; bin/crosware unset rdirtype var

--- recipes/functmpl.sh
+++ recipes/functmpl.sh
@@ -1,13 +1,16 @@
 #
 # XXX - this is an example function template that can be sourced
+# XXX - this is explicitly for recipes - i.e., they need a valid cwbdir_${rname} function by default
+# XXX - could use with cwprependfunc/cwappendfunc to programmatically programmatically
 #
 : ${rname:="fake"}
 : ${rfuncname:="example"}
 : ${rfuncbody:=": noop"}
+: ${rdirtype:="b"}
 
 eval "
 function cw${rfuncname}_${rname}() {
-  pushd \$(cwidir_${rname}) &>/dev/null
+  pushd \$(cw${rdirtype}dir_${rname}) &>/dev/null
   ${rfuncbody}
   popd &>/dev/null
 }
