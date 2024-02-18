18894f7a382e5c46bb6dae5c2b5004a255b2cf6f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-18 02:32:04 +0000

    diffutils: add gawk/remove gettexttiny req, limit flags, add g* symlinks and remove old note

--- recipes/diffutils/diffutils.sh
+++ recipes/diffutils/diffutils.sh
@@ -1,16 +1,10 @@
-#
-# XXX - needs a "--program-prefix=g" and symlinks setup similar to grep/sed
-#     - or symlinks from non-prefix to prefix executables, like make
-#     - gnudiff symlink too
-#
-
 rname="diffutils"
 rver="3.10"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="90e5e93cc724e4ebe12ede80df1634063c7a855692685919bfe60b556c9bd09e"
-rreqs="make sed gettexttiny"
+rreqs="make sed gawk"
 
 . "${cwrecipe}/common.sh"
 
@@ -19,11 +13,31 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   sed -i.ORIG '/gets is a/d' lib/stdio.in.h
-  ./configure ${cwconfigureprefix} --disable-nls
+  ./configure ${cwconfigureprefix} --disable-nls \
+    LDFLAGS=-static \
+    CPPFLAGS= \
+    PKG_CONFIG_{LIBDIR,PATH}=
   cat lib/sigsegv.c > lib/sigsegv.c.ORIG
   echo '#include <config.h>' > lib/sigsegv.c
   echo '#include <sys/reg.h>' >> lib/sigsegv.c
   cat lib/sigsegv.c.ORIG >> lib/sigsegv.c
+  popd >/dev/null 2>&1
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  make install ${rlibtool}
+  (
+    local i
+    cd \$(cwidir_${rname})/bin/
+    for i in \$(find . -type f) ; do
+      i=\"\$(basename \${i})\"
+      test -e g\${i} || ln -s \${i} g\${i}
+    done
+    test -e gnudiff || ln -s diff gnudiff
+  )
   popd >/dev/null 2>&1
 }
 "
