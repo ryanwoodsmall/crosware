572f4d57e295abae1d3d097ced7a45da568a9d40
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-18 02:18:00 +0000

    busybox: require and use bashtiny, toybox and bzip2; clean up a bit

--- recipes/busybox/busybox.sh
+++ recipes/busybox/busybox.sh
@@ -16,50 +16,65 @@ rfile="${rdir}.tar.bz2"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 #rurl="http://${rname}.net/downloads/${rfile}"
 rsha256="b8cc24c9574d809e7279c3be349795c5d5ceb6fdf19ca709f80cde50e47de314"
-rreqs="bootstrapmake"
+rreqs="bootstrapmake toybox bashtiny bzip2"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  chmod -R u+w .
-  csu=\"https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh\"
-  cs=\"\$(basename \${csu})\"
-  cwfetch \"\${csu}\" \"\$(cwbdir_${rname})/\${cs}\"
-  sed -i.ORIG 's/^make/#make/g;s/^test/#test/g' \"\${cs}\"
-  make defconfig HOSTCC=\"\${CC} -static\"
-  bash \"\${cs}\" -m -s
-  make oldconfig HOSTCC=\"\${CC} -static\"
-  cat include/libbb.h > include/libbb.h.ORIG
-  echo '#undef BB_EXTRA_VERSION' >> include/libbb.h
-  echo -n '#define BB_EXTRA_VERSION \" (' >> include/libbb.h
-  echo -n \"\$(cwver_statictoolchain)\" >> include/libbb.h
-  echo -n ')\"' >> include/libbb.h
-  echo >> include/libbb.h
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    export PATH=\"${cwsw}/bashtiny/current/bin:${cwsw}/toybox/current/bin:${cwsw}/bzip2/current/bin:\${PATH}\"
+    chmod -R u+w .
+    csu=\"https://raw.githubusercontent.com/ryanwoodsmall/${rname}-misc/master/scripts/bb_config_script.sh\"
+    cs=\"\$(basename \${csu})\"
+    cwfetch \"\${csu}\" \"\$(cwbdir_${rname})/\${cs}\"
+    sed -i.ORIG 's/^make/#make/g;s/^test/#test/g' \"\${cs}\"
+    make defconfig HOSTCC=\"\${CC} -static\"
+    bash \"\${cs}\" -m -s
+    make oldconfig HOSTCC=\"\${CC} -static\"
+    cat include/libbb.h > include/libbb.h.ORIG
+    echo '#undef BB_EXTRA_VERSION' >> include/libbb.h
+    echo -n '#define BB_EXTRA_VERSION \" (' >> include/libbb.h
+    echo -n \"\$(cwver_statictoolchain)\" >> include/libbb.h
+    echo -n ')\"' >> include/libbb.h
+    echo >> include/libbb.h
+  )
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  make -j${cwmakejobs} CC=\"\${CC}\" HOSTCC=\"\${CC} -static\" CFLAGS=\"\${CFLAGS}\" HOSTCFLAGS=\"\${CFLAGS}\" HOSTLDFLAGS=\"\${LDFLAGS}\"
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    export PATH=\"${cwsw}/bashtiny/current/bin:${cwsw}/toybox/current/bin:${cwsw}/bzip2/current/bin:\${PATH}\"
+    make -j${cwmakejobs} \
+      CC=\"\${CC} -Wl,-static\" \
+      HOSTCC=\"\${CC} -static -Wl,-static\" \
+      CFLAGS=\"\${CFLAGS}\" \
+      HOSTCFLAGS=\"\${CFLAGS}\" \
+      LDFLAGS=-static \
+      HOSTLDFLAGS=-static
+  )
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  cwmkdir \"\$(cwidir_${rname})/bin\"
-  rm -f \"\$(cwidir_${rname})/bin/${rname}\"
-  cp -a \"${rname}\" \"\$(cwidir_${rname})/bin\"
-  local a=''
-  for a in \$(./${rname} --list) ; do
-    ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/\${a}\"
-  done
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    export PATH=\"${cwsw}/bashtiny/current/bin:${cwsw}/toybox/current/bin:${cwsw}/bzip2/current/bin:\${PATH}\"
+    cwmkdir \"\$(cwidir_${rname})/bin\"
+    rm -f \"\$(cwidir_${rname})/bin/${rname}\"
+    cp -a \"${rname}\" \"\$(cwidir_${rname})/bin\"
+    local a=''
+    for a in \$(./${rname} --list) ; do
+      ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/\${a}\"
+    done
+  )
+  popd &>/dev/null
 }
 "
 
