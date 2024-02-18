645e29b52c508abfc5701fa9ad98c37f4787e9d3
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-18 01:46:31 +0000

    bzip2: move to bootstrap make, use cw?dir_ funcs, limit flags, fix some quoting

--- recipes/bzip2/bzip2.sh
+++ recipes/bzip2/bzip2.sh
@@ -4,23 +4,39 @@ rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/ryanwoodsmall/${rname}/archive/${rfile}"
 rsha256="1a8b3a69c92ac300121f37be9d40409c36d783b5914d9b695856e83256cdad0f"
-rreqs="make"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG \"/^PREFIX=/ s#PREFIX=.*#PREFIX=${ridir}#g\" Makefile
-  sed -i \"/^CFLAGS=/ s#CFLAGS=#CFLAGS=-Wl,-static -fPIC #g\" Makefile
-  sed -i \"/^LDFLAGS=/ s#LDFLAGS=#LDFLAGS=-static #g\" Makefile
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  sed -i.ORIG \"/^PREFIX=/ s#PREFIX=.*#PREFIX=\$(cwidir_${rname})#g\" Makefile
+  sed -i '/^CFLAGS=/ s#CFLAGS=#CFLAGS=-Wl,-static -fPIC #g' Makefile
+  sed -i '/^LDFLAGS=/ s#LDFLAGS=#LDFLAGS=-static #g' Makefile
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmake_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  make -j${cwmakejobs} ${rlibtool} LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmakeinstall_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  make install ${rlibtool} LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
 }
