3fa192e4d01d78577b1a17d55b2631e1e31ac35f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-25 06:56:01 +0000

    ksh93: use bashtiny instead of dashtiny

--- recipes/ksh93/ksh93.sh
+++ recipes/ksh93/ksh93.sh
@@ -1,14 +1,10 @@
-#
-# XXX - ash broken on riscv64?
-#
-
 rname="ksh93"
 rver="1.0.8"
 rdir="${rname%93}-${rver}"
 rfile="v${rver}.tar.gz"
-rurl="https://github.com/${rname}/ksh/archive/refs/tags/${rfile}"
+rurl="https://github.com/ksh93/ksh/archive/refs/tags/${rfile}"
 rsha256="b46565045d0eb376d3e6448be6dbc214af454efc405d527f92cb81c244106c8e"
-rreqs="busybox dashtiny"
+rreqs="busybox bashtiny"
 
 . "${cwrecipe}/common.sh"
 
@@ -18,13 +14,13 @@ eval "
 function cwmake_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
   env \
-    PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/dashtiny/current/bin:${cwsw}/busybox/current/bin\" \
+    PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/bashtiny/current/bin:${cwsw}/busybox/current/bin\" \
     TMPDIR=\"${cwtop}/tmp\" \
-      \"${cwsw}/dashtiny/current/bin/dash\" ./bin/package make \
+      \"${cwsw}/bashtiny/current/bin/bash\" ./bin/package make \
         CC=\"\${CC} -static -Wl,-static\" \
         CCFLAGS=\"\${CFLAGS} -Wl,-s -Wl,-static -Os\" \
         LDFLAGS=\"-static -s\" \
-        SHELL=\"${cwsw}/dashtiny/current/bin/dash\"
+        SHELL=\"${cwsw}/bashtiny/current/bin/bash\"
   popd >/dev/null 2>&1
 }
 "
@@ -36,13 +32,13 @@ function cwmakeinstall_${rname}() {
   rm -f \"\$(cwidir_${rname})/bin/${rname}\"
   rm -f \"\$(cwidir_${rname})/bin/${rname%93}\"
   env \
-    PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/dashtiny/current/bin:${cwsw}/busybox/current/bin\" \
+    PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/bashtiny/current/bin:${cwsw}/busybox/current/bin\" \
     TMPDIR=\"${cwtop}/tmp\" \
-      \"${cwsw}/dashtiny/current/bin/dash\" ./bin/package install \"\$(cwidir_${rname})\" \
+      \"${cwsw}/bashtiny/current/bin/bash\" ./bin/package install \"\$(cwidir_${rname})\" \
         CC=\"\${CC} -static -Wl,-static\" \
         CCFLAGS=\"\${CFLAGS} -Wl,-s -Wl,-static -Os\" \
         LDFLAGS=\"-static -s\" \
-        SHELL=\"${cwsw}/dashtiny/current/bin/dash\"
+        SHELL=\"${cwsw}/bashtiny/current/bin/bash\"
   cat \"\$(cwidir_${rname})/bin/${rname%93}\" > \"\$(cwidir_${rname})/bin/${rname}\"
   chmod 755 \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/${rname%93}\"
