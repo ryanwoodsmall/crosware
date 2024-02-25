e4633f6cfb49dbb6ec1d9bf7b89a39aff53d210d
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-25 06:26:54 +0000

    npth: update to 1.7, disable npth_rwlock stuff for now - musl update?, limit flags, use cw?dir_ funcs

--- recipes/gnupg/npth.sh
+++ recipes/gnupg/npth.sh
@@ -1,24 +1,35 @@
+#
+# XXX - 1.7 has r/w locks explicitly disabled; _think_ this should work with musl but need to investigate more
+# XXX - this builds...
+# XXX - error... npth.c:392:21: error: unknown type name 'npth_rwlock_t'; did you mean 'npth_cond_t'?
+#
 rname="npth"
-rver="1.6"
+rver="1.7"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1"
+rsha256="8589f56937b75ce33b28d312fccbf302b3b71ec3f3945fde6aaa74027914ad05"
 rreqs="make slibtool configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --disable-tests \
+    --enable-install-npth-config \
+      LDFLAGS=-static \
+      CPPFLAGS= \
+      PKG_CONFIG_{LIBDIR,PATH}=
+  sed -i.ORIG 's,^#undef _NPTH_NO_RWLOCK,#define _NPTH_NO_RWLOCK 1,' src/npth.h
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
+  echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo 'append_ldflags \"-L${rtdir}/current/lib\"' >> \"${rprof}\"
   echo 'append_cppflags \"-I${rtdir}/current/include\"' >> \"${rprof}\"
 }
