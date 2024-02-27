3a5a1eb6f6189986d52ee280a003100724d428db
ryanwoodsmall <rwoodsmall@gmail.com>
2024-02-27 06:02:30 +0000

    microsocks: update to 1.0.4, use cw?dir_ funcs and &>

--- recipes/microsocks/microsocks.sh
+++ recipes/microsocks/microsocks.sh
@@ -1,26 +1,26 @@
 rname="microsocks"
-rver="1.0.3"
+rver="1.0.4"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/rofl0r/${rname}/archive/refs/tags/${rfile}"
-rsha256="6801559b6f8e17240ed8eef17a36eea8643412b5a7476980fd4e24b02a021b82"
+rsha256="130127a87f55870f18fbe47a64d9b9533020e2900802d36a0f6fd2b074313deb"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  sed -i.ORIG 's,^prefix.*,prefix=${ridir},g' Makefile
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  sed -i.ORIG \"s,^prefix.*,prefix=\$(cwidir_${rname}),g\" Makefile
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make CC=\"\${CC}\" CPPFLAGS= LDFLAGS=-static
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
