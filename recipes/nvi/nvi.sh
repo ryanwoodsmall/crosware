9211d839745cc018989a5cc636b7d694fc9ba3ed
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-01 08:22:59 +0000

    nvi: update to 1.81.6-20, use cw?dir_ funcs, cleanup a little, limit flags, switch to tar.gz

--- recipes/nvi/nvi.sh
+++ recipes/nvi/nvi.sh
@@ -2,20 +2,19 @@
 # XXX - debian gitlab instance, not sure SHA-256 will always be the same for archive...
 # XXX - pull bundle from repo? https://repo.or.cz/nvi.git
 #
-
 rname="nvi"
-rver="1.81.6-16"
+rver="1.81.6-20"
 rdir="${rname}-debian-${rver}"
-rfile="${rdir}.tar.bz2"
+rfile="${rdir}.tar.gz"
 rurl="https://salsa.debian.org/debian/${rname}/-/archive/debian/${rver}/${rfile}"
-rsha256="de5278be023eb932bbfe62b0333764842c65aedb2f44b8cc824761acf1801dd1"
+rsha256="48744c8d9a4004fd8760f2ca842d56e8d96f298da1fb7cd1fab5ce2bc4cb9820"
 rreqs="make netbsdcurses bdb47 configgit slibtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   local p
   for p in \$(cat debian/patches/series) ; do
     patch -p1 < debian/patches/\${p}
@@ -34,28 +33,29 @@ function cwconfigure_${rname}() {
     --without-x \
       CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include -I${cwsw}/bdb47/current/include\" \
       LDFLAGS=\"-static -L${cwsw}/netbsdcurses/current/lib -L${cwsw}/bdb47/current/lib\" \
-      LIBS='-lcurses -lterminfo -ldb -static'
-  popd >/dev/null 2>&1
+      LIBS='-lcurses -lterminfo -ldb -static' \
+      PKG_CONFIG_{LIBDIR,PATH}=
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}/build.unix\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})/build.unix\" &>/dev/null
   make -j${cwmakejobs} ${rlibtool}
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}/build.unix\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/bin\"
-  rm -f ${ridir}/bin/{nex,nvi,nview}
+  pushd \"\$(cwbdir_${rname})/build.unix\" &>/dev/null
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  rm -f \$(cwidir_${rname})/bin/{nex,nvi,nview}
   make install-strip ${rlibtool}
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/nex\"
-  ln -sf \"${rtdir}/current/bin/${rname}\" \"${ridir}/bin/nview\"
-  popd >/dev/null 2>&1
+  ln -sf ${rname} \"\$(cwidir_${rname})/bin/nex\"
+  ln -sf ${rname} \"\$(cwidir_${rname})/bin/nview\"
+  popd &>/dev/null
 }
 "
 eval "
