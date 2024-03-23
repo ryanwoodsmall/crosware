beae870f20c9ab3d44ae2eb284083f5cf66e387f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-23 04:14:00 +0000

    zuluglibc: updates

--- recipes/zulu/zulu11glibc.sh
+++ recipes/zulu/zulu11glibc.sh
@@ -1,16 +1,16 @@
-rver="11.68.17-ca-jdk11.0.21"
+rver="11.70.15-ca-jdk11.0.22"
 mver="${rver%%.*}"
 rname="zulu${mver}glibc"
 rsha256=""
 
 if [[ ${uarch} =~ ^aarch64 ]] ; then
-  rsha256="5638887df0e680c890b4c6f9543c9b61c96c90fb01f877d79ae57566466d3b3d"
+  rsha256="bba5d6317000ad484c31bea3dca14e864231a55611d6860b17459d7bbfed2349"
 elif [[ ${uarch} =~ ^arm ]] ; then
-  rsha256="22b77de4bc46abd9b5463068ccd33e05d9ce6dd6fffb99dfab03e8c6d071b685"
+  rsha256="d354e784a533291bc8deb73613105ccd43295c714981f708315f0a11adc6f4e0"
 elif [[ ${uarch} =~ ^i.86 ]] ; then
-  rsha256="e0a8e9d8a8f5bab7caeab347fa9934761de79cb91967a9f8582f901b6b606eda"
+  rsha256="e48c8060de5d4c4e2c1be476fcc57f22c77df3195ecdeba3049674d9922491a7"
 elif [[ ${uarch} =~ ^x86_64 ]] ; then
-  rsha256="725aba257da4bca14959060fea3faf59005eafdc2d5ccc3cb745403c5b60fb27"
+  rsha256="578d5946b26d9257b78e82a1c28203e5bbd690de48e2016399b1274c3ee7a3c5"
 fi
 
 . "${cwrecipe}/${rname%${mver}glibc}/${rname//${mver}glibc/glibc}.sh.common"
--- recipes/zulu/zulu17glibc.sh
+++ recipes/zulu/zulu17glibc.sh
@@ -1,16 +1,16 @@
-rver="17.46.19-ca-jdk17.0.9"
+rver="17.48.15-ca-jdk17.0.10"
 mver="${rver%%.*}"
 rname="zulu${mver}glibc"
 rsha256=""
 
 if [[ ${uarch} =~ ^aarch64 ]] ; then
-  rsha256="90062201e7911696a449431a61dc0a55cd10cda516a9f2db54c410633a79302a"
+  rsha256="f4e665f2bb9a2ef8dda60e367c2dc8339982ffd966b210a9a4d7d4bb3fd27fc1"
 elif [[ ${uarch} =~ ^arm ]] ; then
-  rsha256="2e0923c1288091df7c63ed1e3cbcdd54cb96259857c89a79021d5f9a0c64427a"
+  rsha256="a97b78e88a6841941d9cb96aaba6da5ebcfdf98f297a628479ee3face71e049e"
 elif [[ ${uarch} =~ ^i.86 ]] ; then
-  rsha256="3ccac20693901dd61b26dd66e0f010c7c27b9e99463d06ee213de7085a1d4b2e"
+  rsha256="7d9e8ad489b8d09c8f58c09e5f42e19f1eb0a0aef55814f2c4bb1deedb34b543"
 elif [[ ${uarch} =~ ^x86_64 ]] ; then
-  rsha256="5317630424ee4e4d2c1024240d2e6f94a7c06d17b01dd36859df4a4d679fc287"
+  rsha256="57284da1bd86d53abf582293d20d18fff7fe243da15a08fa414f6275fbd48fe2"
 fi
 
 . "${cwrecipe}/${rname%${mver}glibc}/${rname//${mver}glibc/glibc}.sh.common"
