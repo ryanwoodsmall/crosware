5713ac6923e68a06e162e193c8dffe93582e584b
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-24 10:09:02 +0000

    pinentry: update to 1.3.0, limit flags, cleanups, note on netbsdcurses

--- recipes/gnupg/pinentry.sh
+++ recipes/gnupg/pinentry.sh
@@ -1,16 +1,19 @@
+#
+# XXX - netbsdcurses?
+#
 rname="pinentry"
-rver="1.2.1"
+rver="1.3.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="457a185e5a85238fb945a955dc6352ab962dc8b48720b62fc9fa48c7540a4067"
+rsha256="9b3cd5226e7597f2fded399a3bc659923351536559e9db0826981bca316494de"
 rreqs="make slibtool libgpgerror libassuan ncurses pkgconfig configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} \
     --enable-pinentry-{curses,tty} \
     --disable-pinentry-{emacs,gtk2,gnome3,qt,qt5,tqt,fltk} \
@@ -18,8 +21,11 @@ function cwconfigure_${rname}() {
     --disable-libsecret \
     --enable-fallback-curses \
     --with-libgpg-error-prefix=\"${cwsw}/libgpgerror/current\" \
-    --with-libassuan-prefix=\"${cwsw}/libassuan/current\"
-  popd >/dev/null 2>&1
+    --with-libassuan-prefix=\"${cwsw}/libassuan/current\" \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
+  popd &>/dev/null
 }
 "
 
