11e7252f4b17a037c643cfeb35dade896018c91b
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-26 07:37:04 +0000

    shellish: update to 6fc7766d7927d738f66a4e3ddc3200122cf1addd, include bin/ymdhms.sh

--- recipes/shellish/shellish.sh
+++ recipes/shellish/shellish.sh
@@ -3,7 +3,7 @@
 #
 
 rname="shellish"
-rver="38dfbc3bd007163aa280297306d4832fcee81a64"
+rver="6fc7766d7927d738f66a4e3ddc3200122cf1addd"
 rdir="${rname//ish/-ish}-${rver}"
 rfile="${rver}.zip"
 #rurl="https://github.com/ryanwoodsmall/${rname//ish/-ish}/archive/refs/heads/${rfile}"
@@ -39,7 +39,7 @@ function cwmakeinstall_${rname}() {
   cwmkdir \"\$(cwidir_${rname})/bin\"
   pushd \"\$(cwidir_${rname})\" >/dev/null 2>&1
   local p
-  for p in box-utils.sh chode coltotal dingafter dingsleep filesizetype.sh ht mixcase.sh nll procdirs.sh revnl revnll trl tru vim9p ; do
+  for p in box-utils.sh chode coltotal dingafter dingsleep filesizetype.sh ht mixcase.sh nll procdirs.sh revnl revnll trl tru vim9p ymdhms.sh ; do
     ln -sf \"${rtdir}/current/\$(cwdir_${rname})/bin/\${p}\" \"\$(cwidir_${rname})/bin/\${p}\"
   done
   unset p
