6d5fa0c0af2e965979194ae5d075ee0ac708628f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-27 06:16:33 +0000

    zstd: update to 1.5.6, cleanups

--- recipes/zstd/zstd.sh
+++ recipes/zstd/zstd.sh
@@ -1,48 +1,48 @@
 rname="zstd"
-rver="1.5.5"
+rver="1.5.6"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/facebook/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="9c4396cc829cfae319a6e2615202e82aad41372073482fce286fac78646d3ee4"
+rsha256="8c29e06cf42aacc1eafc4077ae2ec6c6fcb96a626157e0593d5e82a34fd403c1"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cp lib/Makefile{,.ORIG}
   cp Makefile{,.ORIG}
   sed -i '/^lib/s/libzstd.a libzstd/libzstd.a/g' lib/Makefile
   sed -i '/^install:/s/install-shared//g' lib/Makefile
   sed -i '/^allmost:/s/zlibwrapper//g' Makefile
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make \
     {PREFIX,prefix}=\"\$(cwidir_${rname})\" \
     LDFLAGS='-static' \
     CPPFLAGS=-DXXH_NAMESPACE=ZSTD_ \
     PKG_CONFIG_LIBDIR= \
     PKG_CONFIG_PATH=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install \
     {PREFIX,prefix}=\"\$(cwidir_${rname})\" \
     LDFLAGS='-static' \
     CPPFLAGS=-DXXH_NAMESPACE=ZSTD_ \
     PKG_CONFIG_LIBDIR= \
     PKG_CONFIG_PATH=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
