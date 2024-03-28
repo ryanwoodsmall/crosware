2b85a64c924f903b4b99626885ee5f57f55f72d0
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-28 05:38:48 +0000

    libressl39: add recipe

---
+++ recipes/libressl/libressl39.sh
@@ -0,0 +1,6 @@
+rver="3.9.1"
+rname="libressl${rver%.*}"
+rname="${rname//./}"
+rsha256="6da0b954695f7ee62b03f64200a8a4f02af93717b60cce04ab6c8df262c07a51"
+
+. "${cwrecipe}/libressl/libressl.sh.common"
