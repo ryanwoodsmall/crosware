5289ab8576e27bd9c24a48fde56fe058936633e4
ryanwoodsmall <rwoodsmall@gmail.com>
2024-03-28 05:33:45 +0000

    libressl38: update to 3.8.4

--- recipes/libressl/libressl38.sh
+++ recipes/libressl/libressl38.sh
@@ -1,6 +1,6 @@
-rver="3.8.3"
+rver="3.8.4"
 rname="libressl${rver%.*}"
 rname="${rname//./}"
-rsha256="a65f40e3ef6e3c9451c8318e6f2c454c367e67f09c0cde1849731a4d6ecc7272"
+rsha256="c0cef9cfe174ac366ce482f542fddb07721e7fa0caface34b49a8720fa37fe7d"
 
 . "${cwrecipe}/libressl/libressl.sh.common"
