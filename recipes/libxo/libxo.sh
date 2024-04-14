0ad7c4dbcaad3351298ade4325492d5dcfd92aef
ryanwoodsmall <rwoodsmall@gmail.com>
2024-04-14 03:17:18 +0000

    libxo: update to 1.7.0, restructure recipe, enable filters, cleanups

--- recipes/libxo/libxo.sh
+++ recipes/libxo/libxo.sh
@@ -1,53 +1,91 @@
 #
 # XXX - kind of odd, build a normal static version then a shared version for encoder libenc_*.so files
 #
-
 rname="libxo"
-rver="1.6.0"
+rver="1.7.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://github.com/Juniper/${rname}/releases/download/${rver}/${rfile}"
-rsha256="9f2f276d7a5f25ff6fbfc0f38773d854c9356e7f985501627d0c0ee336c19006"
+rurl="https://github.com/Juniper/libxo/releases/download/${rver}/${rfile}"
+rsha256="9de1e322382ecfdf0310ce7b083ea22e8fdbddca27290652b021edca78fdf201"
 rreqs="make libbsd libmd configgit"
 
 . "${cwrecipe}/common.sh"
 
+cwstubfunc "cwconfigure_${rname}"
+cwstubfunc "cwmake_${rname}"
+
 eval "
-function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+function cwpatch_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwfuncecho 'patching for bsd cdefs/queue/etc.'
   sed -i.ORIG 's,sys/cdefs,bsd/sys/cdefs,g;s,sys/queue,bsd/sys/queue,g;s,sys/syslog,syslog,g;s,sys/sysctl,linux/sysctl,g' \
     libxo/xo_encoder.c \
     libxo/xo_humanize.h \
     libxo/xo_syslog.c \
     xopo/xopo.c
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
-    --disable-gettext \
-    CPPFLAGS=\"-I${cwsw}/libbsd/current/include -I${cwsw}/libmd/current/include\" \
-    LDFLAGS=\"-L${cwsw}/libbsd/current/lib -L${cwsw}/libmd/current/lib -static\" \
-    PKG_CONFIG_LIBDIR= \
-    PKG_CONFIG_PATH=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_static() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    make clean || true
+    make distclean || true
+    ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+      --disable-gettext \
+      --enable-filters \
+        CPPFLAGS=\"-I${cwsw}/libbsd/current/include -I${cwsw}/libmd/current/include\" \
+        LDFLAGS=\"-L${cwsw}/libbsd/current/lib -L${cwsw}/libmd/current/lib -static\" \
+        PKG_CONFIG_{LIBDIR,PATH}=
+    make -j${cwmakejobs} ${rlibtool}
+    make install ${rlibtool}
+  )
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_shared() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    make clean || true
+    make distclean || true
+    ./configure \
+      --prefix=\"\$(cwidir_${rname})/shared\" \
+      --disable-gettext \
+      --disable-static \
+      --enable-static=no \
+      --enable-shared{,=yes} \
+      --enable-filters \
+        CFLAGS=-fPIC \
+        CXXFLAGS=-fPIC \
+        CPPFLAGS=\"-I${cwsw}/libbsd/current/include -I${cwsw}/libmd/current/include\" \
+        LDFLAGS=\"-L${cwsw}/libbsd/current/lib -L${cwsw}/libmd/current/lib\" \
+        PKG_CONFIG_{LIBDIR,PATH}=
+    make -j${cwmakejobs}
+    make install
+    cd encoder
+    make install \
+      CFLAGS=-fPIC \
+      CXXFLAGS=-fPIC \
+      CPPFLAGS=\"-I${cwsw}/libbsd/current/include -I${cwsw}/libmd/current/include -I\$(cwidir_${rname})/shared/include\" \
+      LDFLAGS=\"-L${cwsw}/libbsd/current/lib -L${cwsw}/libmd/current/lib -L\$(cwidir_${rname})/shared/lib\" \
+      PKG_CONFIG_{LIBDIR,PATH}\"=\$(cwidir_${rname})/shared/lib/pkgconfig\"
+    cd -
+    cd \"\$(cwidir_${rname})/lib/libxo/encoder/\"
+    ln -sf ../../../shared/lib/libxo/encoder/*.so* ./
+    cd -
+  )
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  make install ${rlibtool}
-  make clean || true
-  make distclean || true
-  ./configure ${cwconfigureprefix} \
-    --disable-gettext \
-    CFLAGS=-fPIC \
-    CXXFLAGS=-fPIC \
-    CPPFLAGS=\"-I${cwsw}/libbsd/current/include -I${cwsw}/libmd/current/include\" \
-    LDFLAGS=\"-L${cwsw}/libbsd/current/lib -L${cwsw}/libmd/current/lib\" \
-    PKG_CONFIG_LIBDIR= \
-    PKG_CONFIG_PATH=
-  make -j${cwmakejobs} ${rlibtool}
-  cd encoder
-  make install
-  popd >/dev/null 2>&1
+  cwmakeinstall_${rname}_static
+  cwmakeinstall_${rname}_shared
 }
 "
 
