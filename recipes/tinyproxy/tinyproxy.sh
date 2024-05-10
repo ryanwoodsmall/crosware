5c9ec7a66a55fcc1f52e89684437ac22e8ed54b3
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-10 02:29:22 +0000

    tinyproxy: update to 1.11.2

--- recipes/tinyproxy/tinyproxy.sh
+++ recipes/tinyproxy/tinyproxy.sh
@@ -1,27 +1,27 @@
 #
 # XXX - man pages need pod2man/perl
 #
-
 rname="tinyproxy"
-rver="1.11.1"
+rver="1.11.2"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.xz"
+rfile="${rdir}.tar.gz"
 rurl="https://github.com/${rname}/${rname}/releases/download/${rver}/${rfile}"
-rsha256="d66388448215d0aeb90d0afdd58ed00386fb81abc23ebac9d80e194fceb40f7c"
+rsha256="2c8fe5496f2c642bfd189020504ab98d74b9edbafcdb94d9f108e157b5bdf96d"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"${rbdir}\" &>/dev/null
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     --disable-{debug,manpage_support} \
     --enable-{xtinyproxy,filter,upstream,reverse,transparent} \
     --with-stathost=\"${rname}-stats.crosware\" \
       LDFLAGS=-static \
-      CPPFLAGS=
-  popd >/dev/null 2>&1
+      CPPFLAGS= \
+      PKG_CONFIG_{LIBDIR,PATH}=
+  popd &>/dev/null
 }
 "
 
