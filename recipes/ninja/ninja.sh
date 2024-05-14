e607201a475d03a45218de6416c357b5a9575496
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-14 02:23:10 +0000

    ninja: update to 1.12.1, cleanups

--- recipes/ninja/ninja.sh
+++ recipes/ninja/ninja.sh
@@ -1,33 +1,29 @@
 rname="ninja"
-rver="1.12.0"
+rver="1.12.1"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/${rname}-build/${rname}/archive/${rfile}"
-rsha256="8b2c86cd483dc7fcb7975c5ec7329135d210099a89bc7db0590a07b0bbfe49a5"
+rsha256="821bdff48a3f683bc4bb3b6f0b5fe7b2d647cf65d52aeb63328c91a6c6df285a"
 rreqs="make python3"
 
 . "${cwrecipe}/common.sh"
 
-eval "
-function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  \"${cwsw}/python3/current/bin/python3\" ./configure.py --bootstrap
-  popd >/dev/null 2>&1
-}
-"
+cwstubfunc "cwmake_${rname}"
 
 eval "
-function cwmake_${rname}() {
-  true
+function cwconfigure_${rname}() {
+  pushd \"${rbdir}\" &>/dev/null
+  \"${cwsw}/python3/current/bin/python3\" ./configure.py --bootstrap
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/bin\"
-  install -m 755 \"${rname}\" \"${ridir}/bin/\"
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 755 \"${rname}\" \"\$(cwidir_${rname})/bin/\"
+  popd &>/dev/null
 }
 "
 
