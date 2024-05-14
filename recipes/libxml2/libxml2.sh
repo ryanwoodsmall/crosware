216f225874b32d3f8fb52a3ddd2ae5502b1f866f
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-14 21:45:07 +0000

    libxml2: update to 2.11.8

--- recipes/libxml2/libxml2.sh
+++ recipes/libxml2/libxml2.sh
@@ -3,18 +3,18 @@
 # XXX - xmlstarlet needs #inlcude <libxml/xmlsave.h> w/2.12.x
 #
 rname="libxml2"
-rver="2.11.7"
+rver="2.11.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://download.gnome.org/sources/${rname}/${rver%.*}/${rfile}"
-rsha256="fb27720e25eaf457f94fd3d7189bcf2626c6dccf4201553bc8874d50e3560162"
+rsha256="53961af1721b72246180cd844b7ddae36ea8e1e4e27b683567990a1ee78b02c1"
 rreqs="make xz zlib"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --with-ftp \
     --with-legacy \
@@ -22,7 +22,7 @@ function cwconfigure_${rname}() {
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static -s\" \
       PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
