85f68c657d125d3c6cebedd947cea8910a94b59a
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-15 01:31:52 +0000

    reflex: require diffutils

--- recipes/reflex/reflex.sh
+++ recipes/reflex/reflex.sh
@@ -4,13 +4,14 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
 rsha256="312a21529ec28aa1e24583db4d21ac8ad452574e817cc7df3ed3fb22f0f08224"
-rreqs="make byacc"
+rreqs="make byacc diffutils"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwmakeinstall_${rname}() {
   pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  cwmkdir \$(cwidir_${rname})/share/man/man1
   make install
   ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/lex\"
   ln -sf \"${rname}++\" \"\$(cwidir_${rname})/bin/lex++\"
