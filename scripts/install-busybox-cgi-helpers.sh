27cf0d29bce4fc4201574bb9f7821ea7a2a2a22d
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-19 11:50:43 +0000

    scripts/install-busybox-cgi-helpers.sh: move js stuff to application/json, add nashorn and rhino

--- scripts/install-busybox-cgi-helpers.sh
+++ scripts/install-busybox-cgi-helpers.sh
@@ -2,7 +2,6 @@
 #
 # install busybox index.cgi and an env.cgi environment dumper
 #
-
 set -eu
 
 : ${cwtop:="/usr/local/crosware"}
@@ -51,16 +50,14 @@ BEGIN {
 EOF
 chmod 755 env.awk
 
-# XXX - should njs/qjs be application/json?
-
 # env.njs - same but javascript/json (with njs)
 scriptecho "installing ${cgidir}/env.njs"
 cat >env.njs<<EOF
 #!${cwsw}/njs/current/bin/njs
-console.log("Status: 200 OK")
-console.log("Content-type: text/plain")
-console.log("")
-console.log(JSON.stringify(process.env))
+console.log("Status: 200 OK");
+console.log("Content-type: application/json");
+console.log("");
+console.log(JSON.stringify(process.env));
 EOF
 chmod 755 env.njs
 
@@ -68,10 +65,10 @@ chmod 755 env.njs
 scriptecho "installing ${cgidir}/env.qjs"
 cat >env.qjs<<EOF
 #!${cwsw}/quickjs/current/bin/qjs --std
-console.log("Status: 200 OK")
-console.log("Content-type: text/plain")
-console.log("")
-console.log(JSON.stringify(std.getenviron()))
+console.log("Status: 200 OK");
+console.log("Content-type: application/json");
+console.log("");
+console.log(JSON.stringify(std.getenviron()));
 EOF
 chmod 755 env.qjs
 
@@ -81,12 +78,63 @@ cat >env.jojq<<EOF
 #!/usr/bin/env sh
 : \${jo:="/usr/local/crosware/software/jo/current/bin/jo"}
 : \${jq:="/usr/local/crosware/software/jq/current/bin/jq"}
-echo "Content-type: text/plain"
+echo "Content-type: application/json"
 echo
 ( tr '\\0' '\\n' < /proc/\$\$/environ ; echo JO_VER=\$(\${jo} -V) ; echo JQ_VER=\$(\${jq} --version) ) \
   | ( \${jo} -p -- | \${jq} -M -S . ) 2>/dev/null
 EOF
 chmod 755 env.jojq
+
+# XXX - dedupe this and rhino
+# env.nashorn - nashorn javascript on java jvm
+: ${JAVA_HOME:=""}
+if [ ! -z "${JAVA_HOME}" ] ; then
+  scriptecho "installing ${cgidir}/env.nashorn"
+  cat >env.nashorn<<EOF
+#!/usr/bin/env bash
+{
+  echo 'var eka = Object.keys(\$ENV);'
+  echo 'var pka = java.lang.System.getProperties().keySet().toArray();'
+  echo 'var e = {};'
+  echo 'var p = {};'
+  echo 'for (k in eka) { e[eka[k]] = java.lang.System.getenv(eka[k]); }'
+  echo 'for (k in pka) { p[pka[k]] = java.lang.System.getProperty(pka[k]); }'
+  echo 'e["JAVA_PROPERTIES"] = p;'
+  echo 'print("Status: 200 OK");'
+  echo 'print("Content-type: application/json");'
+  echo 'print("");'
+  echo 'print(JSON.stringify(e));'
+} | ${JAVA_HOME}/bin/java -classpath "\$(find ${cwsw}/nashorn/current/lib/ -type f | grep jar\$ | paste -s -d: -)" org.openjdk.nashorn.tools.Shell -scripting - 2>/dev/null
+EOF
+  chmod 755 env.nashorn
+fi
+
+# env.rhino - rhino js on java jvm
+: ${JAVA_HOME:=""}
+if [ ! -z "${JAVA_HOME}" ] ; then
+  scriptecho "installing ${cgidir}/env.rhino"
+  cat >env.rhino<<EOF
+#!/usr/bin/env bash
+${JAVA_HOME}/bin/java -jar "/usr/local/crosware/software/rhino/current/rhino.jar" -e '
+var eka = java.lang.System.getenv().keySet().toArray();
+var pka = java.lang.System.getProperties().keySet().toArray();
+var e = {};
+var p = {};
+for (k in eka) {
+  e[eka[k]] = java.lang.System.getenv(eka[k]);
+}
+for (k in pka) {
+  p[pka[k]] = java.lang.System.getProperty(pka[k]);
+}
+e["JAVA_PROPERTIES"] = p;
+print("Status: 200 OK");
+print("Content-type: application/json");
+print("");
+print(JSON.stringify(e))
+' 2>/dev/null
+EOF
+  chmod 755 env.rhino
+fi
 
 # tar.cgi serves a .tar of common git files, new/updated recipes, etc.
 scriptecho "installing ${cgidir}/tar.cgi"
