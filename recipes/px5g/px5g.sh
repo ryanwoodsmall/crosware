ff7125e4836b57d59490b023d82b4c1eae215b49
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-19 05:32:52 +0000

    px4g: update to 23.05.3, cleanups

--- recipes/px5g/px5g.sh
+++ recipes/px5g/px5g.sh
@@ -2,13 +2,12 @@
 # XXX - selfsigned:
 #   px5g selfsigned -newkey rsa:2048 -keyout blah.key -out blah.crt -subj /CN=host.name
 #
-
 rname="px5g"
-rver="23.05.0"
+rver="23.05.3"
 rdir="${rname}-${rver}"
 rfile="${rname}-mbedtls.c"
 rurl="https://raw.githubusercontent.com/openwrt/openwrt/v${rver}/package/utils/${rname}-mbedtls/${rfile}"
-rsha256="d7fd1842959225ef876a53752d98d1d244a711ad40f72a2ef02e47d064798f73"
+rsha256="493155dfa8e68cb28da67ca22deac8bff136f2cba640180fc8774592002c8e36"
 rreqs="mbedtls"
 
 . "${cwrecipe}/common.sh"
@@ -19,21 +18,21 @@ cwstubfunc "cwconfigure_${rname}"
 eval "
 function cwmake_${rname}() {
   mkdir -p \"\$(cwbdir_${rname})\"
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   \${CC} -I\"${cwsw}/mbedtls/current/include\" \"\$(cwdlfile_${rname})\" -o \"${rname}\" -L\"${cwsw}/mbedtls/current/lib\" -lmbedtls -lmbedx509 -lmbedcrypto -static
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   \$(\${CC} -dumpmachine)-strip --strip-all \"${rname}\"
   cwmkdir \"\$(cwidir_${rname})/bin\"
   install -m 0755 \"${rname}\" \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/${rname}mbedtls\"
   ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/${rname}-mbedtls\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
--- recipes/px5g/px5gwolfssl.sh
+++ recipes/px5g/px5gwolfssl.sh
@@ -1,9 +1,9 @@
 rname="px5gwolfssl"
-rver="23.05.0"
+rver="23.05.3"
 rdir="${rname}-${rver}"
 rfile="${rname%wolfssl}-wolfssl.c"
 rurl="https://raw.githubusercontent.com/openwrt/openwrt/v${rver}/package/utils/${rname%wolfssl}-wolfssl/${rfile}"
-rsha256="818d6557427ecd7202fd2f98a75ecde867b8ce59cf222249d1b9b230dec53053"
+rsha256="29779bd36f64bf4d3c7447ec25c40974963bb3f9dd6421ef2a33327133bf9322"
 rreqs="wolfssl"
 
 . "${cwrecipe}/common.sh"
@@ -14,21 +14,21 @@ cwstubfunc "cwconfigure_${rname}"
 eval "
 function cwmake_${rname}() {
   mkdir -p \"\$(cwbdir_${rname})\"
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   \${CC} -I\"${cwsw}/wolfssl/current/include\" \"\$(cwdlfile_${rname})\" -o \"${rname%wolfssl}\" -L\"${cwsw}/wolfssl/current/lib\" -lwolfssl -static
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   \$(\${CC} -dumpmachine)-strip --strip-all \"${rname%wolfssl}\"
   cwmkdir \"\$(cwidir_${rname})/bin\"
   install -m 0755 \"${rname%wolfssl}\" \"\$(cwidir_${rname})/bin/${rname%wolfssl}\"
   ln -sf \"${rtdir}/current/bin/${rname%wolfssl}\" \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf \"${rtdir}/current/bin/${rname%wolfssl}\" \"\$(cwidir_${rname})/bin/${rname%wolfssl}-${rname#px5g}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
