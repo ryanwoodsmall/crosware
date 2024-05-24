19adcdc1e557c48c30abb67d3aff9e37be21486d
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-24 23:22:09 +0000

    scripts/install-musl-zulu.sh: include aarch64 support, add java 21, use zlib instead of sortix libz, script notifications

--- scripts/install-musl-zulu.sh
+++ scripts/install-musl-zulu.sh
@@ -2,12 +2,9 @@
 #
 # install musl-based zulu jdk "out of tree"
 #
-# XXX - use alpine openjdk .apk packages instead - may work on other arches? ssl/nss/x11?
-# XXX - aarch64 support for zulu17musl+
-#
 
 # only run if we're have valid crosware
-if ! $(hash crosware >/dev/null 2>&1) ; then
+if ! command -v crosware &>/dev/null ; then
   echo 'crosware not found'
   exit 1
 fi
@@ -15,8 +12,8 @@ if [ "${cwtop}x" == "x" ] ; then
  echo '${cwtop} environment variable not set'
  exit 1
 fi
-if ! $(crosware show-uarch | grep -q '^x86_64$') ; then
-  echo 'x86_64 only'
+if ! $(uname -m | grep -qE '^(x86_64|aarch64)$') ; then
+  echo 'x86_64/aarch64 only'
   exit 1
 fi
 
@@ -32,14 +29,15 @@ fi
 
 # version picker
 case "${reqver}" in
-  8|11|17) :
-    zuluver="$(awk -F'"' '/^rver=/{print $2}' ${cwtop}/recipes/zulu/zulu${reqver}musl.sh)"
-    zulusha="$(awk -F'"' '/^rsha256=/{print $2}' ${cwtop}/recipes/zulu/zulu${reqver}musl.sh)"
+  8|11|17|21) :
+    zuluver="$(crosware run-func cwver_zulu${reqver}musl)"
+    zulusha="$(crosware run-func cwsha256_zulu${reqver}musl)"
     ;;
   all) :
     bash "${BASH_SOURCE[0]}" 17
     bash "${BASH_SOURCE[0]}" 8
     bash "${BASH_SOURCE[0]}" 11
+    bash "${BASH_SOURCE[0]}" 21
     exit 0
     ;;
   custom) :
@@ -60,7 +58,7 @@ case "${reqver}" in
     fi
     ;;
   *) :
-    echo "${BASH_SOURCE[0]} [8|11|17|all|custom]"
+    echo "${BASH_SOURCE[0]} [8|11|17|21|all|custom]"
     exit 1
     ;;
 esac
@@ -74,14 +72,21 @@ cwtmp="$(cd ${cwtop}/../tmp && pwd)"
 : ${zulucdn:="https://cdn.azul.com/zulu/bin"}
 : ${zulutop:="/usr/local/zulu"}
 : ${zuluprofd:="${cwetc}/local.d/zulumusl.sh"}
-: ${zuludir:="zulu${zuluver}-linux_musl_x64"}
+if $(uname -m | grep -q x86_64) ; then
+  : ${zuludir:="zulu${zuluver}-linux_musl_x64"}
+elif $(uname -m | grep -q aarch64) ; then
+  : ${zuludir:="zulu${zuluver}-linux_musl_aarch64"}
+else
+  echo "$(uname -m) not supported"
+  exit 1
+fi
 : ${zulufile:="${zuludir}.tar.gz"}
 : ${zuluurl:="${zulucdn}/${zulufile}"}
 : ${zuludldir:="${cwdl}/zulu"}
 : ${zuludlfile:="${zuludldir}/${zulufile}"}
 
 # we need file, libz, and patchelf
-for req in file libz patchelf ; do
+for req in file zlib patchelf ; do
   crosware check-installed ${req} || crosware install ${req}
 done
 . "${cwtop}/etc/profile"
@@ -91,8 +96,8 @@ crosware \
   run-func \
     cwfetchcheck,${zuluurl},${zuludlfile},${zulusha}
 
-# install sortix libz.so in jdk lib dir
-install -m 0755 -D $(realpath ${cwsw}/libz/current/lib/libz.so) ${zulutop}/${zuludir}/lib/libz.so.1
+# install libz.so in jdk lib dir
+install -m 0755 -D $(realpath ${cwsw}/zlib/current/shared/lib/libz.so) ${zulutop}/${zuludir}/lib/libz.so.1
 ln -sf libz.so.1 ${zulutop}/${zuludir}/lib/libz.so
 
 # install zulu jdk
@@ -112,8 +117,12 @@ find ${zulutop}/${zuludir} -type f -exec ${cwsw}/file/current/bin/file {} + \
   done
 
 # write out the profile
+echo "writing ${zuluprofd}"
 cat > ${zuluprofd} << EOF
 export JAVA_HOME="${zulutop}/current"
 export PATH="\${JAVA_HOME}/bin:\${PATH}"
 export _JAVA_OPTIONS="-Djava.io.tmpdir=${cwtmp} -Djava.awt.headless=true -XX:-UsePerfData"
 EOF
+
+echo "done; run the following to pick up the new environment:"
+echo "  . ${cwtop}/scripts/reset-cwenv.sh"
