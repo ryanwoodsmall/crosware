71703503a80d0ade14c41305891215d6211be54a
ryanwoodsmall <rwoodsmall@gmail.com>
2024-05-26 06:49:08 +0000

    bmake: update to 20240520

--- recipes/bmake/bmake.sh
+++ recipes/bmake/bmake.sh
@@ -3,12 +3,12 @@
 # XXX - ugh
 #
 rname="bmake"
-rver="20240508"
+rver="20240520"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="http://www.crufty.net/ftp/pub/sjg/${rfile}"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="847a20f03e6f2ee26b9f7cf8db4a353120540686daa565e2ec6ff800317ddaab"
+rsha256="2210ccd455b008df7951f6dbd347bfcc1837c46473014e4b8dd5ff3091ae2894"
 rreqs=""
 rbdir="${cwbuild}/${rdir}/build"
 rprof="${cwetcprofd}/zz_${rname}.sh"
