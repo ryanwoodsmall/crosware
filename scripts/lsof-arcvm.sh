4b2fff3c1cc9703528dcb32025bab2b7598bdbdd
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-04 21:33:48 +0000

    scripts/lsof-arcvm.sh: goddammit

--- scripts/lsof-arcvm.sh
+++ scripts/lsof-arcvm.sh
@@ -5,7 +5,17 @@
 #     - scripts/start-root-sshd
 #     - scripts/usr-local-bin-sudossh
 # XXX - generalize with option/environment var for termina as well?
+# XXX - pipefail... come on man
+# XXX - okay fuck this it doesn't work
 #
-set -euo pipefail
-pids="$(for p in $(sudossh $(which pgrep) crosvm) ; do tr \\0 \\n < <(sudossh cat /proc/${p}/{cmdline,environ}) | grep -qi arc && echo $p ; done) | sed 's,^.*(,,g;s,)$,,g' | sort -un | paste -s -d, -)"
-sudossh lsof -P -n -l -p $(sudossh $(which pstree) -p "${pids}"
+set -o pipefail
+##set -eu
+#: ${sshkey:="${HOME}/.ssh/id_rsa"}
+#: ${sudocmd:="sudossh -i ${sshkey}"}
+#arcvmpid="$(for p in $(pgrep crosvm) ; do tr '\0' '\n' < <(${sudocmd} cat /proc/${p}/{cmdline,environ} || true) | grep -qi arcvm && echo $p || true ; done | head -1)"
+#echo godd
+##pids="$(${sudocmd} $(which pstree) -p ${arcvmpid} | sed 's,^.*(,,g;s,)$,,g' | sort -un | paste -s -d, -)"
+##${sudocmd} $(which lsof) -P -n -l -p "${pids}"
+#echo what 
+
+sudossh lsof -P -n -l -p $(sudossh $(which pstree) -p $(for p in $(sudossh $(which pgrep) crosvm) ; do tr \\0 \\n < <(sudossh cat /proc/${p}/{cmdline,environ}) | grep -qi arc && echo $p ; done) | sed 's,^.*(,,g;s,)$,,g' | sort -un | paste -s -d, -)
