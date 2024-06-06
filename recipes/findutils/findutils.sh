8844c2f02c146b4934a5cc00dfbd7e093d4a8433
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-06 00:49:34 +0000

    findutils: update to 4.10.0, cleanups

--- recipes/findutils/findutils.sh
+++ recipes/findutils/findutils.sh
@@ -1,19 +1,19 @@
 rname="findutils"
-rver="4.9.0"
+rver="4.10.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
-rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe"
+rurl="https://ftp.gnu.org/gnu/findutils/${rfile}"
+rsha256="1387e0b67ff247d2abde998f90dfbf70c1491391a59ddfecb8ae698789f0a4f5"
 rreqs="make busybox sed configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  ./configure ${cwconfigureprefix} --disable-nls
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  ./configure ${cwconfigureprefix} --disable-nls LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
   echo '#include <sys/sysmacros.h>' >> config.h
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
