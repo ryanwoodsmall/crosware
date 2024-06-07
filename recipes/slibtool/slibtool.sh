a1fb2f624df6eda9b2391037d2ef8fbb6a0d59bc
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-07 07:06:56 +0000

    slibtool: update to 0.6.0

--- recipes/slibtool/slibtool.sh
+++ recipes/slibtool/slibtool.sh
@@ -1,37 +1,37 @@
 rname="slibtool"
-rver="0.5.36"
+rver="0.6.0"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/midipix-project/slibtool/archive/refs/tags/${rfile}"
-rsha256="90334fe8b6271d55953543c7202a05f8d34f6a90a9afcacc0ef2f52a3e23e345"
+rsha256="fd8e80b33f7b105a3847ca9e579f5df9795f69bdd8a7d21c434861889972b117"
 rreqs="make m4"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env \
     CXXFLAGS='-Wl,-static -fPIC' \
     CFLAGS='-Wl,-static -fPIC' \
       ./configure ${cwconfigureprefix} --all-static
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make -j${cwmakejobs} CFLAGS_CMDLINE='-Wl,-static -fPIC'
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install CFLAGS_CMDLINE='-Wl,-static -fPIC'
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
