36382ca73939e0bd60f82502d9a81060ce150290
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-07 06:15:01 +0000

    cmake: update to 3.29.5

--- recipes/cmake/cmake.sh
+++ recipes/cmake/cmake.sh
@@ -3,30 +3,30 @@
 # XXX - CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES - Modules/Platform/UnixPaths.cmake
 # XXX - CMAKE_INSTALL_LIBDIR - https://cmake.org/cmake/help/v3.0/module/GNUInstallDirs.html
 #
+# XXX - need to figure out libs too, looks like /lib64 is default in some places (see libgit2)
+# XXX - defaults to -O3, tweak down/add -Wl,-s -g0 for no debug to speed up compilation?
+#
 # XXX - openssl ugh
 # -- Could NOT find OpenSSL, try to set the path to OpenSSL root folder in the system variable OPENSSL_ROOT_DIR (missing: OPENSSL_CRYPTO_LIBRARY OPENSSL_INCLUDE_DIR)
 # CMake Error at Utilities/cmcurl/CMakeLists.txt:454 (message):
 #  Could not find OpenSSL.  Install an OpenSSL development package or
 #  configure CMake with -DCMAKE_USE_OPENSSL=OFF to build without OpenSSL.
-#
 # XXX - build with libressl+a tiny-curl? probably need a picocurllibressl w/zlib and nothing else... doesn't solve openssl req
-# XXX - need to figure out libs too, looks like /lib64 is default in some places (see libgit2)
-# XXX - defaults to -O3, tweak down/add -Wl,-s -g0 for no debug to speed up compilation?
+# XXX - ccache4, other cmake stuff needs to be able to pull deps, gotta fix this
 #
-
 rname="cmake"
-rver="3.29.4"
+rver="3.29.5"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/Kitware/CMake/releases/download/v${rver}/${rfile}"
-rsha256="b1b48d7100bdff0b46e8c8f6a3c86476dbe872c8df39c42b8d104298b3d56a2c"
+rsha256="dd63da7d763c0db455ca232f2c443f5234fe0b11f8bd6958a81d29cc987dfd6e"
 rreqs="make bash busybox toybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   find . -type f -name '*.cmake' -print0 | xargs -0 sed -i.ORIG 's#/usr/include#/no/usr/include#g;s#/usr/lib#/no/usr/lib#g'
   sed -i.ORIG 's#-DCMAKE_BOOTSTRAP=1#-DCMAKE_BOOTSTRAP=1 -DCMAKE_IGNORE_PATH=/usr/include -DCMAKE_USE_OPENSSL=OFF#g' bootstrap
   sed -i.ORIG 's#\"/lib#\"/no/lib#g;s#\"/usr/lib#\"/no/usr/lib#g' Source/cmExportInstallFileGenerator.cxx CMakeLists.txt
@@ -42,16 +42,16 @@ function cwconfigure_${rname}() {
         --verbose \
           CFLAGS=\"\${CFLAGS} -static\" \
           CXXFLAGS=\"\${CXXFLAGS} -static\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   find \"\$(cwidir_${rname})/bin/\" -type f | xargs file | grep 'ELF.*not stripped' | cut -f1 -d: | xargs strip --strip-all || true
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
