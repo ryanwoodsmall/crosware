0ec2748a348c61a572dc360f054c9bdbef80428b
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-26 03:05:55 +0000

    mujs: update to 1.3.5, cleanups

--- recipes/mujs/mujs.sh
+++ recipes/mujs/mujs.sh
@@ -1,36 +1,36 @@
 rname="mujs"
-rver="1.3.4"
+rver="1.3.5"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/ccxvii/${rname}/archive/${rfile}"
-rsha256="c015475880f6a382e706169c94371a7dd6cc22078832f6e0865af8289c2ef42b"
+rsha256="78a311ae4224400774cb09ef5baa2633c26971513f8b931d3224a0eb85b13e0b"
 rreqs="make netbsdcurses readlinenetbsdcurses"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG 's/-lreadline/-lreadline -lcurses -lterminfo -static/g' Makefile
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make release \
     prefix=\"\$(cwidir_${rname})\" \
     CC=\"\${CC} \${CFLAGS} \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) \$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
     LDFLAGS=-static \
     CPPFLAGS=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install \
     prefix=\"\$(cwidir_${rname})\" \
     CC=\"\${CC} \${CFLAGS} \$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) \$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
@@ -41,7 +41,7 @@ function cwmakeinstall_${rname}() {
   \$(\${CC} -dumpmachine)-strip --strip-all \"\$(cwidir_${rname})/bin/${rname}-pp\"
   cwmkdir \"\$(cwidir_${rname})/docs\"
   ( cd docs/ ; tar -cf - . ) | ( cd \"\$(cwidir_${rname})/docs\" ; tar -xf - )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
