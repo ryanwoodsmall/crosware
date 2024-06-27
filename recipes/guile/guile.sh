a97fb67008b818fa4a636c1b7d20a466f67fec37
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-27 00:56:07 +0000

    guile: update to 3.0.10, cleanups

--- recipes/guile/guile.sh
+++ recipes/guile/guile.sh
@@ -1,9 +1,9 @@
 rname="guile"
-rver="3.0.9"
+rver="3.0.10"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.xz"
-rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="1a2625ac72b2366e95792f3fe758fd2df775b4044a90a4a9787326e66c0d750d"
+rfile="${rdir}.tar.gz"
+rurl="https://ftp.gnu.org/gnu/guile/${rfile}"
+rsha256="2dbdbc97598b2faf31013564efb48e4fed44131d28e996c26abe8a5b23b56c2a"
 rreqs="make sed gawk gmp libtool slibtool pkgconfig libffi gc readline ncurses libunistring"
 
 . "${cwrecipe}/common.sh"
--- recipes/guile/guile2.sh
+++ recipes/guile/guile2.sh
@@ -2,7 +2,7 @@ rname="guile2"
 rver="2.2.7"
 rdir="${rname%2}-${rver}"
 rfile="${rdir}.tar.xz"
-rurl="https://ftp.gnu.org/gnu/${rname%2}/${rfile}"
+rurl="https://ftp.gnu.org/gnu/guile/${rfile}"
 rsha256="cdf776ea5f29430b1258209630555beea6d2be5481f9da4d64986b077ff37504"
 rreqs="make sed gawk gmp libtool slibtool pkgconfig libffi gc readline ncurses libunistring"
 
@@ -11,8 +11,8 @@ rreqs="make sed gawk gmp libtool slibtool pkgconfig libffi gc readline ncurses l
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --program-suffix=${rver%%.*}
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
