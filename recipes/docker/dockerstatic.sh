0406e293d0707649e65f2a56adf710cf72161218
ryanwoodsmall <rwoodsmall@gmail.com>
2024-06-28 02:07:53 +0000

    dockerstatic: update to 27.0.2

--- recipes/docker/dockerstatic.sh
+++ recipes/docker/dockerstatic.sh
@@ -1,5 +1,5 @@
 rname="dockerstatic"
-rver="26.1.4"
+rver="27.0.2"
 rdir="${rname//static/}-${rver}"
 rbdir="${cwbuild}/docker"
 rfile="${rdir}.tgz"
@@ -9,32 +9,26 @@ rsha256=""
 rburl="https://download.docker.com/linux/static/stable"
 if [[ ${karch} =~ ^aarch64 ]] ; then
   rurl="${rburl}/aarch64/${rfile}"
-  rsha256="6f1a5fb161aef875d305ee4f79e65492b3c13e90dbe0a339df2ad6515e4f6849"
+  rsha256="4973e5e5bb2aeded816b9df4d281652535e82d6a8341653ae96ba7b7832dbb0c"
 elif [[ ${karch} =~ ^arm ]] ; then
   rurl="${rburl}/armhf/${rfile}"
-  rsha256="13036528f6008e40ee3c5d7fe9a16cbd393dd6d19ded57b632032647f47e25a2"
+  rsha256="9882f77d73ebcfee8343bb87f71f6c6c5bc5eaec3a67b0962d056ce36707bce2"
 elif [[ ${karch} =~ ^x86_64 ]] ; then
   rurl="${rburl}/x86_64/${rfile}"
-  rsha256="a9cede81aa3337f310132c2c920dba2edc8d29b7d97065b63ba41cf47ae1ca4f"
+  rsha256="cc0dc835067509b7bedb31ed5db5b44f54666f820dd33f5191a9f9f5c68d4fe8"
 fi
 unset rburl
 
 . "${cwrecipe}/common.sh"
 
 eval "
-function cwgenprofd_${rname}() {
-  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
-}
-"
-
-eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
-  mkdir -p \"\$(cwidir_${rname})/bin\"
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwmkdir \"\$(cwidir_${rname})/bin\"
   local p
   find . -mindepth 1 -maxdepth 1 -type f | while read -r p ; do install -m 0755 \"\${p}\" \"\$(cwidir_${rname})/bin/\" ; done
   unset p
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
@@ -54,5 +48,11 @@ function cwinstall_${rname}() {
   cwgenprofd_${rname}
   cwmarkinstall_${rname}
   cwclean_${rname}
+}
+"
+
+eval "
+function cwgenprofd_${rname}() {
+  echo 'append_path \"${rtdir}/current/bin\"' > "${rprof}"
 }
 "
