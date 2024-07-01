6cd7b23057baaf624c964226c671591a7284cfd8
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-01 04:52:48 +0000

    libgcrypt: roll back to 1.10.3, note on conflict with openssl 1.1.x

--- recipes/gnupg/libgcrypt.sh
+++ recipes/gnupg/libgcrypt.sh
@@ -1,9 +1,20 @@
+#
+# XXX - 1.11.0 conflict with openssl 1.1.x (at least - test with 3.x!!! when i move it to default openssl...)
+# XXX - nginx, which brings openssl and libxml2/libxslt, which pull in libgcrypt:
+#    /usr/local/crosware/software/statictoolchain/202109250959-x86_64-linux-musl/bin/../lib/gcc/x86_64-linux-musl/9.4.0/../../../../x86_64-linux-musl/bin/ld: /usr/local/crosware/software/libgcrypt/current/lib/libgcrypt.a(mceliece6688128f.o): in function `gf_mul':
+#    mceliece6688128f.c:(.text+0xa8de): multiple definition of `gf_mul'; /usr/local/crosware/builds/nginx-1.26.1/openssl-1.1.1w/.openssl/lib/libcrypto.a(f_impl.o):f_impl.c:(.text+0x16): first defined here
+#    /usr/local/crosware/software/statictoolchain/202109250959-x86_64-linux-musl/bin/../lib/gcc/x86_64-linux-musl/9.4.0/../../../../x86_64-linux-musl/bin/ld: /usr/local/crosware/software/libgcrypt/current/lib/libgcrypt.a(aria.o): in function `aria_encrypt':
+#    aria.c:(.text+0x7e82): multiple definition of `aria_encrypt'; /usr/local/crosware/builds/nginx-1.26.1/openssl-1.1.1w/.openssl/lib/libcrypto.a(aria.o):aria.c:(.text+0x0): first defined here
+#    collect2: error: ld returned 1 exit status
+#    make[1]: *** [objs/Makefile:426: objs/nginx] Error 1
+#    make[1]: Leaving directory '/usr/local/crosware/builds/nginx-1.26.1'
+#
 rname="libgcrypt"
-rver="1.11.0"
+rver="1.10.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.bz2"
 rurl="https://gnupg.org/ftp/gcrypt/${rname}/${rfile}"
-rsha256="09120c9867ce7f2081d6aaa1775386b98c2f2f246135761aae47d81f58685b9c"
+rsha256="8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa"
 rreqs="make libgpgerror slibtool configgit"
 
 . "${cwrecipe}/common.sh"
