9b4dfe8366ab32d36af4c6e6e27b8ab7b619e9a6
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-01 12:59:09 +0000

    openssh: update to 9.8p1

--- recipes/openssh/openssh.sh
+++ recipes/openssh/openssh.sh
@@ -13,12 +13,12 @@
 #
 
 rname="openssh"
-rver="9.7p1"
+rver="9.8p1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/${rfile}"
 rurl="https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${rfile}"
-rsha256="490426f766d82a2763fcacd8d83ea3d70798750c7bd2aff2e57dc5660f773ffd"
+rsha256="dd8bd002a379b5d499dfb050dd1fa9af8029e80461f4bb6c523c49973f5a39f3"
 
 sslprov="openssl"
 
