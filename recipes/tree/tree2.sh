63f0939e9f87285cdb82af3edc5d214cbdd5c003
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-02 00:28:30 +0000

    tree2: update to 2.1.2, cleanups

--- recipes/tree/tree2.sh
+++ recipes/tree/tree2.sh
@@ -1,30 +1,30 @@
 rname="tree2"
-rver="2.1.1"
+rver="2.1.2"
 rdir="${rname%2}-${rver}"
 rfile="${rver}.tar.gz"
-rurl="https://github.com/Old-Man-Programmer/${rname%2}/archive/refs/tags/${rfile}"
-rsha256="1b70253994dca48a59d6ed99390132f4d55c486bf0658468f8520e7e63666a06"
+rurl="https://github.com/Old-Man-Programmer/tree/archive/refs/tags/${rfile}"
+rsha256="d2a09b7c57473bf0451c37c2dbd5a72406ea2ee0810e97bd01a6a5459c0ea3fb"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG \"/^PREFIX.*=/s#/usr.*#\$(cwidir_${rname})#g\" Makefile
   sed -i \"s#^CC=gcc#CC=\${CC}#g\" Makefile
   sed -i 's/^#LDFLAGS.*=.*\$/LDFLAGS=-static/g' Makefile
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   ln -sf tree \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf tree.1 \"\$(cwidir_${rname})/man/man1/${rname}.1\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
