6e8fbecfa78fd0385fbfcb154275f14ed1caab11
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-02 03:17:14 +0000

    ksh93: update to 1.0.9, disable dynamic lib/bin, cleanups

--- recipes/ksh93/ksh93.sh
+++ recipes/ksh93/ksh93.sh
@@ -1,9 +1,9 @@
 rname="ksh93"
-rver="1.0.8"
+rver="1.0.9"
 rdir="${rname%93}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/ksh93/ksh/archive/refs/tags/${rfile}"
-rsha256="b46565045d0eb376d3e6448be6dbc214af454efc405d527f92cb81c244106c8e"
+rsha256="c58d618b551c594580541a0759e32ea6ddadd7b7a84f9f3ebea2ffb9a5d9d580"
 rreqs="busybox bashtiny"
 
 . "${cwrecipe}/common.sh"
@@ -12,8 +12,9 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env \
+    AST_NO_DYLIB=1 \
     PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/bashtiny/current/bin:${cwsw}/busybox/current/bin\" \
     TMPDIR=\"${cwtop}/tmp\" \
       \"${cwsw}/bashtiny/current/bin/bash\" ./bin/package make \
@@ -21,17 +22,18 @@ function cwmake_${rname}() {
         CCFLAGS=\"\${CFLAGS} -Wl,-s -Wl,-static -Os\" \
         LDFLAGS=\"-static -s\" \
         SHELL=\"${cwsw}/bashtiny/current/bin/bash\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwmkdir \"\$(cwidir_${rname})/bin\"
   rm -f \"\$(cwidir_${rname})/bin/${rname}\"
   rm -f \"\$(cwidir_${rname})/bin/${rname%93}\"
   env \
+    AST_NO_DYLIB=1 \
     PATH=\"${cwsw}/ccache4/current/bin:${cwsw}/ccache/current/bin:${cwsw}/statictoolchain/current/bin:${cwsw}/bashtiny/current/bin:${cwsw}/busybox/current/bin\" \
     TMPDIR=\"${cwtop}/tmp\" \
       \"${cwsw}/bashtiny/current/bin/bash\" ./bin/package install \"\$(cwidir_${rname})\" \
@@ -42,7 +44,7 @@ function cwmakeinstall_${rname}() {
   cat \"\$(cwidir_${rname})/bin/${rname%93}\" > \"\$(cwidir_${rname})/bin/${rname}\"
   chmod 755 \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/${rname%93}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
