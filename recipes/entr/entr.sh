7b7dab8a964b55e0bc87eba2defae536be69a36e
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-02 00:26:17 +0000

    entr: update to 5.6, cleanups

--- recipes/entr/entr.sh
+++ recipes/entr/entr.sh
@@ -1,34 +1,34 @@
 rname="entr"
-rver="5.5"
+rver="5.6"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
-rurl="https://github.com/eradman/${rname}/archive/refs/tags/${rfile}"
-rsha256="128c0ce2efea5ae6bd3fd33c3cd31e161eb0c02609d8717ad37e95b41656e526"
+rurl="https://github.com/eradman/entr/archive/refs/tags/${rfile}"
+rsha256="0222b8df928d3b5a3b5194d63e7de098533e04190d9d9a154b926c6c1f9dd14e"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env CPPFLAGS= LDFLAGS=-static CFLAGS=\"\${CFLAGS} -Wl,-s -static\" PREFIX=\"\$(cwidir_${rname})\" ./configure
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env CPPFLAGS= LDFLAGS=-static CFLAGS=\"\${CFLAGS} -Wl,-s -static\" PREFIX=\"\$(cwidir_${rname})\" make
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env CPPFLAGS= LDFLAGS=-static CFLAGS=\"\${CFLAGS} -Wl,-s -static\" PREFIX=\"\$(cwidir_${rname})\" make install
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
