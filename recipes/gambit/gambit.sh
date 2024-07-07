5e6414f44260a2e948dfeee375c73c978010258d
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-07 08:05:58 +0000

    gambit: update to 4.9.5, cleanups

--- recipes/gambit/gambit.sh
+++ recipes/gambit/gambit.sh
@@ -1,36 +1,35 @@
 #
 # XXX - need a "make modules" in here?
 #
-
 rname="gambit"
-rver="4.9.4"
+rver="4.9.5"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
-rurl="https://github.com/${rname}/${rname}/archive/${rfile}"
-rsha256="19fb44a65b669234f6c0467cdc3dbe2e2c95a442f38e4638e7d89c90e247bd08"
+rurl="https://github.com/gambit/${rname}/archive/${rfile}"
+rsha256="758da7b4afe6411e9c4fed14b0cc5ada39b5f1393c1edd4d3dd9c9a06127c310"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} \
     CFLAGS=-fPIC \
     CXXFLAGS=-fPIC \
     LDFLAGS= \
     CPPFLAGS= \
     PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   ln -sf \"${rtdir}/current/bin/gsi\" \"${ridir}/bin/${rname}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
