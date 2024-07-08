97e3ceaee5b930c07f3b76e3225d0c3603fc0cb8
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-08 04:38:14 +0000

    zlibng: update to 2.2.1, cleanups

--- recipes/zlibng/zlibng.sh
+++ recipes/zlibng/zlibng.sh
@@ -2,13 +2,12 @@
 # XXX - explicitly opt-in
 # XXX - 2.1.x broke something with shared, might be gcc version. turn it off for now
 #
-
 rname="zlibng"
-rver="2.1.7"
+rver="2.2.1"
 rdir="zlib-ng-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/zlib-ng/zlib-ng/archive/refs/tags/${rfile}"
-rsha256="59e68f67cbb16999842daeb517cdd86fc25b177b4affd335cd72b76ddc2a46d8"
+rsha256="ec6a76169d4214e2e8b737e0850ba4acb806c69eeace6240ed4481b9f5c57cdf"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -20,11 +19,11 @@ eval "
 function cwmakeinstall_${rname}_zlibng_static() {
   cwclean_${rname}
   cwextract_${rname}
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env LDFLAGS=-static CPPFLAGS= ./configure ${cwconfigureprefix} --static --without-{unaligned,optimizations,new-strategies,neon,acle}
   make -j${cwmakejobs} ${rlibtool} LDFLAGS=-static CPPFLAGS=
   make install ${rlibtool} LDFLAGS=-static CPPFLAGS=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
   cwclean_${rname}
 }
 "
@@ -33,11 +32,11 @@ function cwmakeinstall_${rname}_zlibng_static() {
 #function cwmakeinstall_${rname}_zlibng_shared() {
 #  cwclean_${rname}
 #  cwextract_${rname}
-#  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+#  pushd \"\$(cwbdir_${rname})\" &>/dev/null
 #  env CFLAGS=-fPIC LDFLAGS= CPPFLAGS= ./configure --prefix=\"\$(cwidir_${rname})/shared\" --without-{unaligned,optimizations,new-strategies,neon,acle}
 #  make -j${cwmakejobs} ${rlibtool} CFLAGS=-fPIC LDFLAGS= CPPFLAGS=
 #  make install ${rlibtool} CFLAGS=-fPIC LDFLAGS= CPPFLAGS=
-#  popd >/dev/null 2>&1
+#  popd &>/dev/null
 #  cwclean_${rname}
 #}
 #"
@@ -46,11 +45,11 @@ eval "
 function cwmakeinstall_${rname}_zlib_static() {
   cwclean_${rname}
   cwextract_${rname}
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env LDFLAGS=-static CPPFLAGS= ./configure ${cwconfigureprefix} --static --zlib-compat --without-{unaligned,optimizations,new-strategies,neon,acle}
   make -j${cwmakejobs} ${rlibtool} LDFLAGS=-static CPPFLAGS=
   make install ${rlibtool} LDFLAGS=-static CPPFLAGS=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
   cwclean_${rname}
 }
 "
@@ -59,11 +58,11 @@ function cwmakeinstall_${rname}_zlib_static() {
 #function cwmakeinstall_${rname}_zlib_shared() {
 #  cwclean_${rname}
 #  cwextract_${rname}
-#  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+#  pushd \"\$(cwbdir_${rname})\" &>/dev/null
 #  env CFLAGS=-fPIC LDFLAGS= CPPFLAGS= CC=\"\${CC} -I\$(cwidir_${rname})/shared/include -L\$(cwidir_${rname})/shared/lib -lz-ng -Wl,-rpath=${rtdir}/current/shared/lib\" ./configure --prefix=\"\$(cwidir_${rname})/shared\" --zlib-compat --without-{unaligned,optimizations,new-strategies,neon,acle}
 #  make -j${cwmakejobs} ${rlibtool} CFLAGS=-fPIC LDFLAGS= CPPFLAGS= CC=\"\${CC} -I\$(cwidir_${rname})/shared/include -L\$(cwidir_${rname})/shared/lib -lz-ng -Wl,-rpath=${rtdir}/current/shared/lib\"
 #  make install ${rlibtool} CFLAGS=-fPIC LDFLAGS= CPPFLAGS= CC=\"\${CC} -I\$(cwidir_${rname})/shared/include -L\$(cwidir_${rname})/shared/lib -lz-ng -Wl,-rpath=${rtdir}/current/shared/lib\"
-#  popd >/dev/null 2>&1
+#  popd &>/dev/null
 #  cwclean_${rname}
 #  cwcreatesharedlib \"\$(cwidir_${rname})/lib/libz.a\" \"\$(cwidir_${rname})/shared/lib/libz.so.1.2.11.standalone\"
 #  ln -sf libz.so.1.2.11.standalone \"\$(cwidir_${rname})/shared/lib/libz.so.standalone\"
