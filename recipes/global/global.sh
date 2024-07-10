8e8bcacefda8d546c4920d3bc37ee7b0c8428a21
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-10 04:43:10 +0000

    global: update to 6.6.13, cleanups, disable "-shared" in plugins (no)

--- recipes/global/global.sh
+++ recipes/global/global.sh
@@ -1,19 +1,20 @@
 rname="global"
-rver="6.6.12"
+rver="6.6.13"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="542a5b06840e14eca548b4bb60b44c0adcf01024e68eb362f8bf716007885901"
+rsha256="945f349730da01f77854d9811ca8f801669c9461395a29966d8d88cb6703347b"
 rreqs="make ncurses sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG 's/-lcurses/-lncurses/g;s/makeinfo/true/g' configure
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
-  popd >/dev/null 2>&1
+  sed -i.ORIG s,-shared,,g plugin-factory/Makefile
+  popd &>/dev/null
 }
 "
 
