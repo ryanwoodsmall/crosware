abfb8ecb3a8b94055954fb48dd11ff8e016cb2ac
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-21 18:34:03 +0000

    sysklogd: update to 2.6.0, cleanups

--- recipes/sysklogd/sysklogd.sh
+++ recipes/sysklogd/sysklogd.sh
@@ -1,16 +1,16 @@
 rname="sysklogd"
-rver="2.5.2"
+rver="2.6.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/troglobit/sysklogd/releases/download/v${rver}/${rfile}"
-rsha256="9da4444285772ce2bfc9c687be8f978b7a9fe7d3937347cd4938eda0c4545e05"
+rsha256="c5ae067b479b54ef90fa9352c277d44ff7b4314023fc6ee0a37e81b99a78a917"
 rreqs="bootstrapmake slibtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --sysconfdir=\"${cwtop}/etc/${rname}\" \
     --localstatedir=\"${cwtop}/var\" \
@@ -19,18 +19,18 @@ function cwconfigure_${rname}() {
       LDFLAGS='-s -static' \
       CPPFLAGS= \
       PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   cwmkdir \"${cwtop}/etc/${rname}\"
   cwmkdir \"${cwtop}/var/log\"
   install -m 644 syslog.conf \"${cwtop}/etc/${rname}/syslog.conf.example\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
