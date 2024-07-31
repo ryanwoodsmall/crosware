ae5fb861e945708d0508ecda85051d2c19a2d089
ryanwoodsmall <rwoodsmall@gmail.com>
2024-07-31 03:35:45 +0000

    mawk: update to 1.3.4-20240622, cleanups

--- recipes/mawk/mawk.sh
+++ recipes/mawk/mawk.sh
@@ -1,32 +1,32 @@
 rname="mawk"
-rver="1.3.4-20240123"
+rver="1.3.4-20240622"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
-rsha256="a8e319a83744b1f1fb6988dfa189d61887f866e9140cc9a49eb003b2b0655e88"
+rsha256="4e917e87a7a9fbaf76995784a4b0b5dc0dd954b977d0983030f78f6a07b1a765"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     CFLAGS=\"\${CFLAGS} -static\" \
     LDFLAGS=\"-static\" \
     CPPFLAGS= \
     PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install
   rm -f \"\$(cwidir_${rname})/bin/awk\"
   ln -sf \"${rtdir}/current/bin/${rname}\" \"\$(cwidir_${rname})/bin/awk\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
