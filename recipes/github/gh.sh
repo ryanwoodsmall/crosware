46925970b66266d607e1d0dd511fc3590ea1d00e
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-02 19:52:27 +0000

    gh: update to 2.54.0, cleanups

--- recipes/github/gh.sh
+++ recipes/github/gh.sh
@@ -1,9 +1,9 @@
 rname="gh"
-rver="2.52.0"
+rver="2.54.0"
 rdir="cli-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/cli/cli/archive/refs/tags/${rfile}"
-rsha256="41de39d0f1bcacb454d9b8a46e5b97ff8b8e803cd26d284e553e45bf025325d9"
+rsha256="eedac958431876cebe243925dc354b0c21915d1bc84c5678a8073f0ec91d6a4c"
 rreqs="go bootstrapmake"
 
 if ! command -v git &>/dev/null ; then
@@ -16,26 +16,26 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   if [ -e \"\$(cwbdir_${rname})\" ] ; then
      chmod -R u+rw \"\$(cwbdir_${rname})\" || true
   fi
   rm -rf \"\$(cwbdir_${rname})\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwpatch_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG \"s,/usr/local,\$(cwidir_${rname}),g\" Makefile
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
@@ -49,13 +49,13 @@ function cwmake_${rname}() {
         make
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
@@ -67,7 +67,7 @@ function cwmakeinstall_${rname}() {
         make install prefix=\"\$(cwidir_${rname})\"
     chmod -R u+rw .
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
