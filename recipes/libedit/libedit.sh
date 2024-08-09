d9abf3fe268ff6b7cb819351e6f09ade04c94333
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-09 06:31:11 +0000

    libedit: update to 20240808-3.1

--- recipes/libedit/libedit.sh
+++ recipes/libedit/libedit.sh
@@ -1,10 +1,10 @@
 rname="libedit"
-rver="20240517-3.1"
+rver="20240808-3.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://www.thrysoee.dk/editline/${rfile}"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="3a489097bb4115495f3bd85ae782852b7097c556d9500088d74b6fa38dbd12ff"
+rsha256="5f0573349d77c4a48967191cdd6634dd7aa5f6398c6a57fe037cc02696d6099f"
 rreqs="make ncurses"
 
 . "${cwrecipe}/common.sh"
