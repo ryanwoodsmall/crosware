62f9fbf007756ad6bd9918ad4f14db72253fb72d
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-10 01:55:05 +0000

    pkgconf: update to 2.3.0, enable 2038+ timestamps, cleanups

--- recipes/pkgconf/pkgconf.sh
+++ recipes/pkgconf/pkgconf.sh
@@ -3,26 +3,27 @@
 # XXX - moving to https://gitea.treehouse.systems/ariadne/pkgconf
 #
 rname="pkgconf"
-rver="2.2.0"
+rver="2.3.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://github.com/${rname}/${rname}/archive/refs/tags/${rfile}"
 #rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rurl="https://distfiles.dereferenced.org/${rname}/${rfile}"
-rsha256="28f8dfc279a10ef66148befa3f6eb266e5f3570316600208ed50e9781c7269d8"
+rsha256="a2df680578e85f609f2fa67bd3d0fc0dc71b4bf084fc49119de84cd6ed28e723"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
-    CC=\"\${CC} -Os -Wl,-s\" \
-    CFLAGS=\"\${CFLAGS} -Os -Wl,-s\" \
-    CXXFLAGS=\"\${CXXFLAGS} -Os -Wl,-s\" \
-    LDFLAGS=\"\${LDFLAGS} -static -s\" \
-    PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+    --enable-year2038 \
+      CC=\"\${CC} -Os -Wl,-s\" \
+      CFLAGS=\"\${CFLAGS} -Os -Wl,-s\" \
+      CXXFLAGS=\"\${CXXFLAGS} -Os -Wl,-s\" \
+      LDFLAGS=\"\${LDFLAGS} -static -s\" \
+      PKG_CONFIG_{LIBDIR,PATH}=
+  popd &>/dev/null
 }
 "
