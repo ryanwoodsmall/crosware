84ab85872aaf403887328fb2c930532569cdcd0c
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-10 02:08:15 +0000

    nftables: update to 1.1.0, cleanups

--- recipes/netfilter/nftables.sh
+++ recipes/netfilter/nftables.sh
@@ -4,28 +4,28 @@
 # XXX - editline needs editline/history.h, netbsdcurses can be coaxed:
 #   eval "
 #   function cwpatch_${rname}() {
-#     pushd \"${rbdir}\" >/dev/null 2>&1
+#     pushd \"${rbdir}\" &>/dev/null
 #     mkdir -p include/editline
 #     ln -sf \"${cwsw}/netbsdcurses/current/include/editline/readline.h\" include/editline/
 #     ln -sf readline.h include/editline/history.h
-#     popd >/dev/null 2>&1
+#     popd &>/dev/null
 #   }
 #   "
 #
 
 rname="nftables"
-rver="1.0.9"
+rver="1.1.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
-rsha256="a3c304cd9ba061239ee0474f9afb938a9bb99d89b960246f66f0c3a0a85e14cd"
+rsha256="ef3373294886c5b607ee7be82c56a25bc04e75f802f8e8adcd55aac91eb0aa24"
 rreqs="bootstrapmake pkgconfig byacc netbsdcurses readlinenetbsdcurses libpcap libnl jansson libmnl libnetfilterconntrack libnfnetlink libnftnl iptables slibtool"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --with-json --with-mini-gmp --with-xtables --disable-man-doc --disable-python --without-python-bin --with-cli=readline \
       CPPFLAGS=\"-I\$(cwbdir_${rname})/include \$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
@@ -34,7 +34,7 @@ function cwconfigure_${rname}() {
       PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       LIBS='-lreadline -lcurses -lterminfo -static' \
       YACC=\"${cwsw}/byacc/current/bin/byacc\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
