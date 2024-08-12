9de5f447bd29055cb0e2a3e2961bd5ab3b70f5b8
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-12 02:42:42 +0000

    meson: update to 1.5.1, cleanups

--- recipes/meson/meson.sh
+++ recipes/meson/meson.sh
@@ -2,11 +2,11 @@
 # XXX - ugh
 #
 rname="meson"
-rver="1.5.0"
+rver="1.5.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/mesonbuild/meson/releases/download/${rver}/${rfile}"
-rsha256="45d7b8653c1e5139df35b33be2dd5b2d040c5b2c6129f9a7c890d507e33312b8"
+rsha256="567e533adf255de73a2de35049b99923caf872a455af9ce03e01077e0d384bed"
 rreqs="python3 ninja"
 
 . "${cwrecipe}/common.sh"
@@ -15,15 +15,15 @@ cwstubfunc "cwmake_${rname}"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   find . -type f -exec touch '{}' +
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   local p3d=\"${cwsw}/python3/current\"
   local p3v=\"\$(env PATH=\"\${p3d}/bin:\${PATH}\" python3 -V 2>&1 | cut -f2 -d' ')\"
   local p3p=\"\$(cwidir_${rname})/lib/python\${p3v%.*}/site-packages\"
@@ -32,7 +32,7 @@ function cwmakeinstall_${rname}() {
   env PYTHONPATH=\"\${p3p}\" \
     \"\${p3d}/bin/python3\" setup.py install --force --prefix=\"\${p3d}\"
   unset p3d p3v p3p
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
