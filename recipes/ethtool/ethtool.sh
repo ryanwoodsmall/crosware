b4c4b27c4aa66a857b65c7d0dd62d0250d1cb2f7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-12 02:30:42 +0000

    ethtool: update to 6.10, cleanups

--- recipes/ethtool/ethtool.sh
+++ recipes/ethtool/ethtool.sh
@@ -3,18 +3,18 @@
 # XXX - mailing list message: https://www.spinics.net/lists/netdev/msg658962.html
 #
 rname="ethtool"
-rver="6.9"
+rver="6.10"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
-rurl="https://mirrors.edge.kernel.org/pub/software/network/${rname}/${rfile}"
-rsha256="a71b0354010661c5cf178bc606ed50fcb91805cf1897ad0eb2818387a5fd0cd9"
+rurl="https://mirrors.edge.kernel.org/pub/software/network/ethtool/${rfile}"
+rsha256="cc613fe8a2bcddee17a1e6e0d763c0f3ea33c7e930658d2d7f601aa65e426a1f"
 rreqs="make libmnl"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     CC=\"\${CC} -D_GNU_SOURCE\" \
     CXX=\"\${CXX} -D_GNU_SOURCE\" \
@@ -27,7 +27,7 @@ function cwconfigure_${rname}() {
   cat uapi/linux/ethtool.h > uapi/linux/ethtool.h.ORIG
   echo '#include <linux/kernel.h>' > uapi/linux/ethtool.h
   cat uapi/linux/ethtool.h.ORIG >> uapi/linux/ethtool.h
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
