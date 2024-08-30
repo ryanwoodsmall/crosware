18ac69dbdbab79dccea5c195b8a48508f9c392a7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-08-30 02:52:17 +0000

    screen: update to 5.0.0, cleanups

--- recipes/screen/screen.sh
+++ recipes/screen/screen.sh
@@ -1,16 +1,16 @@
 rname="screen"
-rver="4.9.1"
+rver="5.0.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="26cef3e3c42571c0d484ad6faf110c5c15091fbf872b06fa7aa4766c7405ac69"
+rsha256="f04a39d00a0e5c7c86a55338808903082ad5df4d73df1a2fd3425976aed94971"
 rreqs="make netbsdcurses sed"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} \
     --disable-pam \
     --disable-socket-dir \
@@ -19,13 +19,13 @@ function cwconfigure_${rname}() {
       CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\" \
       LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -static\" \
       LIBS=\"-lcurses -lterminfo\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install
   echo 'hardstatus alwayslastline \"%Lw\"' >> etc/screenrc
   cwmkdir \"\$(cwidir_${rname})/etc\"
@@ -33,7 +33,7 @@ function cwmakeinstall_${rname}() {
   install -m 0644 etc/etcscreenrc \"\$(cwidir_${rname})/etc/etcscreenrc\"
   cwmkdir \"\$(cwidir_${rname})/share/terminfo\"
   install -m 0644 terminfo/screeninfo.src \"\$(cwidir_${rname})/share/terminfo/screeninfo.src\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
