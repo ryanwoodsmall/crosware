74053026530044ed87d08c675aaa1bd70a739a45
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-04 02:07:12 +0000

    k0s: update to v1.30.4+k0s.0

--- recipes/k0s/k0s.sh
+++ recipes/k0s/k0s.sh
@@ -3,7 +3,7 @@
 # XXX - package for airgap images?
 #
 rname="k0s"
-rver="1.30.3_${rname}.0"
+rver="1.30.4_${rname}.0"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
@@ -12,13 +12,13 @@ rbfile="${rname}-v${rver//_/+}"
 rburl="https://github.com/${rname}project/${rname}/releases/download/v${rver//_/%2B}"
 if [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rbfile}-amd64"
-  rsha256="623102445b7b7e9e8e25c1a36bcf2f424bcaf7f66643e4842390b2121cb3fcfd"
+  rsha256="70e3d6ba40043a2485b66650ec92e01e37eaf5d918c8228cb5dbd770128d29f9"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rbfile}-arm"
-  rsha256="f985fc1665c33653be8ec11c7f7b3c4859d2e64d5fb5ea13eb9fe7d8be9e9d19"
+  rsha256="7bd629a8b515c659da6dd8dd680e2af7adcb8b405444243f5dade8c537a1ec1a"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rbfile}-arm64"
-  rsha256="c7875b761108ca0ca019b6011a5520d8bad377267a5269fcf179de046acf5f95"
+  rsha256="3757619d0920c947330ced1146f108a116c55cee20a54bafb8b6b28dbe9e4dd0"
 fi
 rurl="${rburl}/${rfile//+/%2B}"
 unset rbfile
