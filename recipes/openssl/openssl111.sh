9b5992029c7c2e413005d8d95b32d13ddc4f2ab2
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-04 01:39:35 +0000

    openssl30: update to 3.0.15, move downloads to github, cleanups

--- recipes/openssl/openssl111.sh
+++ recipes/openssl/openssl111.sh
@@ -2,7 +2,7 @@ rname="openssl111"
 rver="1.1.1w"
 rdir="${rname%111}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://www.openssl.org/source/old/1.1.1/${rfile}"
+rurl="https://github.com/openssl/openssl/releases/download/OpenSSL_${rver//./_}/${rfile}"
 rsha256="cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8"
 rreqs="make perl cacertificates"
 
@@ -10,7 +10,7 @@ rreqs="make perl cacertificates"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG '/if.*eq.*-static/s/-static/-blahblahblah/g' Configure
   env \
     PATH=\"${cwsw}/perl/current/bin:\${PATH}\" \
@@ -18,16 +18,16 @@ function cwconfigure_${rname}() {
     CPPFLAGS= \
     PKG_CONFIG_{LIBDIR,PATH}= \
       ./config --prefix=\"\$(cwidir_${rname})\" --openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic \${CFLAGS} -static -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install_sw
   sed -i '/^Requires/s/\\.private:/:/g' \$(cwidir_${rname})/lib/pkgconfig/*.pc
   sed -i s,\$(cwidir_${rname}),${rtdir}/current,g \$(cwidir_${rname})/lib/pkgconfig/*.pc
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
--- recipes/openssl/openssl30.sh
+++ recipes/openssl/openssl30.sh
@@ -1,16 +1,16 @@
 rname="openssl30"
-rver="3.0.14"
+rver="3.0.15"
 rdir="${rname%30}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://www.openssl.org/source/${rfile}"
-rsha256="eeca035d4dd4e84fc25846d952da6297484afa0650a6f84c682e39df3a4123ca"
+rurl="https://github.com/openssl/openssl/releases/download/openssl-${rver}/${rfile}"
+rsha256="23c666d0edf20f14249b3d8f0368acaee9ab585b09e1de82107c66e1f3ec9533"
 rreqs="make perl cacertificates"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG '/if.*eq.*-static/s/-static/-blahblahblah/g' Configure
   env \
     PATH=\"${cwsw}/perl/current/bin:\${PATH}\" \
@@ -19,16 +19,16 @@ function cwconfigure_${rname}() {
     PKG_CONFIG_{LIBDIR,PATH}= \
       ./config --prefix=\"\$(cwidir_${rname})\" --openssldir=${cwetc}/ssl no-asm no-shared no-zlib no-zlib-dynamic \${CFLAGS} -static -fPIC -DOPENSSL_PIC -DOPENSSL_THREADS
   sed -i.ORIG s,LIBDIR=lib64,LIBDIR=lib,g Makefile || true
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install_sw
   sed -i '/^Requires/s/\\.private:/:/g' \$(cwidir_${rname})/lib/pkgconfig/*.pc
   sed -i s,\$(cwidir_${rname}),${rtdir}/current,g \$(cwidir_${rname})/lib/pkgconfig/*.pc
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
