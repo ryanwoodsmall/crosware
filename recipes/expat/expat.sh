318d5307a16fd4a1420ee95aaf65225a309ecfe2
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-05 02:59:08 +0000

    expat: update to 2.6.3, cleanups

--- recipes/expat/expat.sh
+++ recipes/expat/expat.sh
@@ -1,27 +1,27 @@
 rname="expat"
-rver="2.6.2"
+rver="2.6.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/libexpat/libexpat/releases/download/R_${rver//./_}/${rfile}"
-rsha256="d4cf38d26e21a56654ffe4acd9cd5481164619626802328506a2869afab29ab3"
+rsha256="17aa6cfc5c4c219c09287abfc10bc13f0c06f30bb654b28bfe6f567ca646eb79"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --without-docbook LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   sed -i \"s#\$(cwidir_${rname})#${rtdir}/current#g\" \"\$(cwidir_${rname})/lib/lib${rname}.la\" \"\$(cwidir_${rname})/lib/pkgconfig/${rname}.pc\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
