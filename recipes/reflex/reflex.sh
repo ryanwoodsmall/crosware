e0fdbac2c885d0b393e40c4e877a6cd678439370
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-08 07:42:05 +0000

    reflex: update to 20240906, cleanup

--- recipes/reflex/reflex.sh
+++ recipes/reflex/reflex.sh
@@ -1,21 +1,21 @@
 rname="reflex"
-rver="20230523"
+rver="20240906"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/${rfile}"
-rsha256="312a21529ec28aa1e24583db4d21ac8ad452574e817cc7df3ed3fb22f0f08224"
+rsha256="4e69f0e358da742e72de3996bbfc0f3ed2d438a5c16ee52e24667b07b566222e"
 rreqs="make byacc diffutils"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwmkdir \$(cwidir_${rname})/share/man/man1
   make install
   ln -sf \"${rname}\" \"\$(cwidir_${rname})/bin/lex\"
   ln -sf \"${rname}++\" \"\$(cwidir_${rname})/bin/lex++\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
