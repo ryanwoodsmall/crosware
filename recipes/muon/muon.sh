db8d3c2716efbc695d3a1056867dc615c1a6adcd
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-25 03:54:54 +0000

    muon: update 0.3.0. bestline option change, cleanups

--- recipes/muon/muon.sh
+++ recipes/muon/muon.sh
@@ -2,31 +2,31 @@
 # XXX - libcurl, libarchive, etc.
 #
 rname="muon"
-rver="0.2.0"
+rver="0.3.0"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/annacrombie/muon/archive/refs/tags/${rfile}"
-rsha256="d73db1be5388821179a25a15ba76fd59a8bf7c8709347a4ec2cb91755203f36c"
+rsha256="e1c2741e7cbcdcb6152ad4251f032aa9ea3b77e96ce84f760a3265dc7c56ae5c"
 rreqs="samurai pkgconf"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env \
     PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/bin | tr ' ' ':'):\${PATH}\" \
     PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
     CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
     LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       ./bootstrap.sh build
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   env \
     PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/bin | tr ' ' ':'):\${PATH}\" \
     PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
@@ -35,20 +35,21 @@ function cwmake_${rname}() {
       \"\$(cwbdir_${rname})/build/muon\" setup \
         -Dprefix=\"\$(cwidir_${rname})\" \
         -Dstatic=true \
-        -D{bestline,libpkgconf}=enabled \
+        -Dlibpkgconf=enabled \
+        -Dreadline=bestline \
         -D{docs,libarchive,libcurl,tracy}=disabled \
         -Dwebsite=false \
          build
   \"${cwsw}/samurai/current/bin/samu\" -C build
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   \"\$(cwbdir_${rname})/build/muon\" -C build install
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
