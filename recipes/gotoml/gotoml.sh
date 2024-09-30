b734000aacb569c12c681d72971c5564e6c1ba1b
ryanwoodsmall <rwoodsmall@gmail.com>
2024-09-30 23:37:49 +0000

    gotoml: update to 2.2.3, cleanups

--- recipes/gotoml/gotoml.sh
+++ recipes/gotoml/gotoml.sh
@@ -1,9 +1,9 @@
 rname="gotoml"
-rver="2.2.2"
+rver="2.2.3"
 rdir="${rname%toml}-${rname#go}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/pelletier/go-toml/archive/refs/tags/${rfile}"
-rsha256="8c5c1107f764d5abc50cfa5721a1c5fd43a30ad09f84105c5c5bcbbd1290fb25"
+rsha256="3a5dfdc1e543efd6032813cb27a5b06d66bbefbbcc88bb664f69d605725c42af"
 rreqs="go"
 
 . "${cwrecipe}/common.sh"
@@ -12,16 +12,16 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   chmod -R u+rw \"\$(cwdir_${rname})\" &>/dev/null || true
   rm -rf \"${rbdir}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
@@ -36,18 +36,18 @@ function cwmake_${rname}() {
     done
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwmkdir \"\$(cwidir_${rname})/bin\"
   for p in jsontoml tomljson tomll ; do
     install -m 755 \"\${p}\" \"\$(cwidir_${rname})/bin/\${p}\"
   done
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
