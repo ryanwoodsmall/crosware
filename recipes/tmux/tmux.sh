858fbf9d60debe48558d02ee895a988aab6deff7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-05 22:38:19 +0000

    tmux: update to 3.5a

--- recipes/tmux/tmux.sh
+++ recipes/tmux/tmux.sh
@@ -3,11 +3,11 @@
 # XXX - explicitly disable jemalloc
 #
 rname="tmux"
-rver="3.5"
+rver="3.5a"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/tmux/tmux/releases/download/${rver}/${rfile}"
-rsha256="2fe01942e7e7d93f524a22f2c883822c06bc258a4d61dba4b407353d7081950f"
+rsha256="16216bd0877170dfcc64157085ba9013610b12b082548c7c9542cc0103198951"
 rreqs="make libevent netbsdcurses pkgconfig byacc"
 
 . "${cwrecipe}/common.sh"
