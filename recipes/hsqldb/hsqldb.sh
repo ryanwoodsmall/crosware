1f3f9fef744a482ea5d1e45d8b15cc9d83b1acb4
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-16 20:57:06 +0000

    hsqldb: update to 2.7.3, cleanups

--- recipes/hsqldb/hsqldb.sh
+++ recipes/hsqldb/hsqldb.sh
@@ -1,12 +1,12 @@
 rname="hsqldb"
-rver="2.7.2"
+rver="2.7.3"
 rvermaj="${rver%%.*}"
 rvermin="${rver#*.}"
 rvermin="${rvermin%.*}"
 rdir="${rname}-${rver}"
 rfile="${rdir}.zip"
-rurl="https://sourceforge.net/projects/${rname}/files/${rname}/${rname}_${rvermaj}_${rvermin}/${rfile}/download"
-rsha256="ffb359cbe66ba3179317b6afdb62cd274765333bd30167ce332a4b2d85975812"
+rurl="https://sourceforge.net/projects/hsqldb/files/hsqldb/${rname}_${rvermaj}_${rvermin}/${rfile}/download"
+rsha256="d3edee859f7fc58237e108864a71733eff5613d92dc65508dfa335bc0c5b1e94"
 rreqs=""
 
 unset rvermaj rvermin
@@ -18,46 +18,47 @@ cwstubfunc "cwmake_${rname}"
 eval "
 function cwextract_${rname}() {
   cwscriptecho \"extracting ${rdlfile}\"
-  cwextract \"${rdlfile}\" \"${cwbuild}\" >/dev/null 2>&1
+  cwextract \"${rdlfile}\" \"${cwbuild}\" &>/dev/null
 }
 "
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cd \"${rname}\"
   chmod 644 bin/*.bat
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
-
-
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cd \"${rname}\"
   local hsqldbserver=\"\$(cwidir_${rname})/bin/${rname}-server.sh\"
   local hsqldbwebserver=\"\$(cwidir_${rname})/bin/${rname}-webserver.sh\"
   local hsqldbshell=\"\$(cwidir_${rname})/bin/${rname}-sqltool.sh\"
-  cd \"${rname}\"
   cwmkdir \"\$(cwidir_${rname})\"
   tar -cf - . | ( cd \"\$(cwidir_${rname})\" ; tar -xf - )
   rm -rf \"\$(cwidir_${rname})/jar\"
   cwmkdir \"\$(cwidir_${rname})/jar\"
   ln -sf \"${rtdir}/current/lib/sqltool.jar\" \"\$(cwidir_${rname})/jar/\"
   ln -sf \"${rtdir}/current/lib/${rname}.jar\" \"\$(cwidir_${rname})/jar/\"
-  echo -n | tee \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo '#!/usr/bin/env bash' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo ': \${CLASSPATH:=\"\"}' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo 'export CLASSPATH=\"${rtdir}/current/jar/${rname}.jar:\${CLASSPATH}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo 'mkdir -p \"${cwtmp}/${rname}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo 'cd \"${cwtmp}/${rname}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbwebserver}\" >/dev/null 2>&1
-  echo 'java org.${rname}.server.Server \"\${@}\"' >> \"\${hsqldbserver}\"
-  echo 'java org.${rname}.server.WebServer \"\${@}\"' >> \"\${hsqldbwebserver}\"
-  echo 'java -jar \"${rtdir}/current/jar/sqltool.jar\" \"\${@}\"' >> \"\${hsqldbshell}\"
+  {
+    echo -n | tee \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
+    echo '#!/usr/bin/env bash' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
+    echo ': \${CLASSPATH:=\"\"}' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
+    echo 'export CLASSPATH=\"${rtdir}/current/jar/${rname}.jar:\${CLASSPATH}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
+    echo 'mkdir -p \"${cwtmp}/${rname}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbwebserver}\"
+    echo 'cd \"${cwtmp}/${rname}\"' | tee -a \"\${hsqldbserver}\" \"\${hsqldbwebserver}\"
+    echo 'echo \"# pwd: \${PWD}\" 1>&2' | tee -a \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
+    echo 'java org.${rname}.server.Server \"\${@}\"' >> \"\${hsqldbserver}\"
+    echo 'java org.${rname}.server.WebServer \"\${@}\"' >> \"\${hsqldbwebserver}\"
+    echo 'java -jar \"${rtdir}/current/jar/sqltool.jar\" \"\${@}\"' >> \"\${hsqldbshell}\"
+  } &>/dev/null
   chmod 755 \"\${hsqldbserver}\" \"\${hsqldbshell}\" \"\${hsqldbwebserver}\"
   unset hsqldbserver hsqldbshell
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
