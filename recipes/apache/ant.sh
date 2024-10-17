484a60f3066bdd852c91fd249e7b8b9486f8a2e7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-17 00:12:29 +0000

    ant: fix double / in url

--- recipes/apache/ant.sh
+++ recipes/apache/ant.sh
@@ -2,7 +2,7 @@ rname="ant"
 rver="1.10.15"
 rdir="apache-${rname}-${rver}"
 rfile="${rdir}-bin.tar.gz"
-rurl="https://dlcdn.apache.org//ant/binaries/${rfile}"
+rurl="https://dlcdn.apache.org/ant/binaries/${rfile}"
 rsha256="71334d7e5d98cfe53d6c429a648a5021137a967378667306c5f613dff5180506"
 rreqs=""
 
