e619c4c540faa4038e495ddef4fc2153b1dec106
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-17 15:53:16 +0000

    w{gctrl,ireguard}go: require go 1.22.x, cleanups

--- recipes/wireguard/wgctrlgo.sh
+++ recipes/wireguard/wgctrlgo.sh
@@ -1,10 +1,14 @@
+#
+# XXX - remove specific go version req below
+#
 rname="wgctrlgo"
 rver="97bc4ad4a1cb712d87f5ccf4d4a62b1cd42574ab"
 rdir="wgctrl-go-${rver}"
 rfile="${rver}.zip"
 rurl="https://github.com/WireGuard/wgctrl-go/archive/${rfile}"
 rsha256="e3dbe21f04a91cb1d402d2ccb82b27c6d0786a6dc9cd095ea12caa4b38bee2d0"
-rreqs="go wireguardtools wireguardgo"
+rgover="122"
+rreqs="go${rgover} wireguardtools wireguardgo"
 
 . "${cwrecipe}/common.sh"
 
@@ -12,41 +16,41 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   test -e \"${rbdir}\" && chmod -R u+rw \"${rbdir}\" || true
   rm -rf \"${rbdir}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
     env \
-      PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
+      PATH=\"${cwsw}/go${rgover}/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       GOPATH=\"\$(cwbdir_${rname})/gopath\" \
-      GOROOT=\"${cwsw}/go/current\" \
+      GOROOT=\"${cwsw}/go${rgover}/current\" \
       CGO_ENABLED=0 \
         go build -o wgctrl-go cmd/wgctrl/main.go
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwmkdir \"\$(cwidir_${rname})/bin\"
   install -m 755 wgctrl-go \"\$(cwidir_${rname})/bin/\"
   ln -sf wgctrl-go \"\$(cwidir_${rname})/bin/${rname}\"
   ln -sf wgctrl-go \"\$(cwidir_${rname})/bin/${rname%go}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
@@ -55,3 +59,5 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
+
+unset rgover
--- recipes/wireguard/wireguardgo.sh
+++ recipes/wireguard/wireguardgo.sh
@@ -1,10 +1,14 @@
+#
+# XXX - remove specific go version req below
+#
 rname="wireguardgo"
 rver="0.0.20230223"
 rdir="wireguard-go-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/WireGuard/wireguard-go/archive/refs/tags/${rfile}"
 rsha256="c679fff905cbd097836d280c99c1b86454264921951491716b235e162178263a"
-rreqs="bootstrapmake go wireguardtools"
+rgover="122"
+rreqs="bootstrapmake go${rgover} wireguardtools"
 
 . "${cwrecipe}/common.sh"
 
@@ -12,50 +16,50 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   test -e \"${rbdir}\" && chmod -R u+rw \"${rbdir}\" || true
   rm -rf \"${rbdir}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
     env \
-      PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
+      PATH=\"${cwsw}/go${rgover}/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       GOPATH=\"\$(cwbdir_${rname})/gopath\" \
-      GOROOT=\"${cwsw}/go/current\" \
+      GOROOT=\"${cwsw}/go${rgover}/current\" \
       CGO_ENABLED=0 \
         make PREFIX=\"\$(cwidir_${rname})\"
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
     env \
-      PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
+      PATH=\"${cwsw}/go${rgover}/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       GOPATH=\"\$(cwbdir_${rname})/gopath\" \
-      GOROOT=\"${cwsw}/go/current\" \
+      GOROOT=\"${cwsw}/go${rgover}/current\" \
       CGO_ENABLED=0 \
         make install PREFIX=\"\$(cwidir_${rname})\"
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
@@ -64,3 +68,5 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
+
+unset rgover
