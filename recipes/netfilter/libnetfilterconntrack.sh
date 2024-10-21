43ba9e0979b17f8280b61c2148edc35961c50bc7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-21 15:30:33 +0000

    libnetfilterconntrack: update to 1.1.0, move to .tar.xz, cleanups

--- recipes/netfilter/libnetfilterconntrack.sh
+++ recipes/netfilter/libnetfilterconntrack.sh
@@ -1,21 +1,20 @@
 rname="libnetfilterconntrack"
-rver="1.0.9"
+rver="1.1.0"
 rdir="${rname//rc/r_c}-${rver}"
-rfile="${rdir}.tar.bz2"
+rfile="${rdir}.tar.xz"
 rurl="https://www.netfilter.org/pub/${rname//rc/r_c}/${rfile}"
-rsha256="67bd9df49fe34e8b82144f6dfb93b320f384a8ea59727e92ff8d18b5f4b579a8"
+rsha256="67edcb4eb826c2f8dc98af08dabff68f3b3d0fe6fb7d9d0ac1ee7ecce0fe694e"
 rreqs="bootstrapmake pkgconfig libmnl libnfnetlink"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
     LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
-    PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
-    PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
-  popd >/dev/null 2>&1
+    PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
+  popd &>/dev/null
 }
 "
