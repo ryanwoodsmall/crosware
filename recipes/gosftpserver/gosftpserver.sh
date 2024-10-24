2c1bb924b813173f175da2d438fd0a0efbfa7883
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-24 03:53:41 +0000

    gosftpserver: update to 1.13.7, cleanups

--- recipes/gosftpserver/gosftpserver.sh
+++ recipes/gosftpserver/gosftpserver.sh
@@ -1,9 +1,9 @@
 rname="gosftpserver"
-rver="1.13.6"
+rver="1.13.7"
 rdir="sftp-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/pkg/sftp/archive/refs/tags/${rfile}"
-rsha256="e7deacdc4b11286c0f9269338e156da33a774d1e5b4995d1904d69ac68bac551"
+rsha256="e48cee5bbf57777264c30d3aea0063d978c8321a7a261cba05f04a6c339dd9c8"
 rreqs="go"
 
 . "${cwrecipe}/common.sh"
@@ -12,16 +12,16 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   chmod -R u+rw \"\$(cwdir_${rname})\" &>/dev/null || true
   rm -rf \"${rbdir}\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   (
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
@@ -33,17 +33,17 @@ function cwmake_${rname}() {
         go build -ldflags '-s -w -extldflags \"-s -static\"' -o ${rname} \"\$(cwbdir_${rname})/server_standalone/\"
     chmod -R u+rw . || true
   )
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwmkdir \"\$(cwidir_${rname})/sbin\"
   install -m 755 ${rname} \"\$(cwidir_${rname})/sbin/${rname}\"
   ln -sf ${rname} \"\$(cwidir_${rname})/sbin/go-sftp-server\"
   ln -sf ${rname} \"\$(cwidir_${rname})/sbin/sftp-server\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
