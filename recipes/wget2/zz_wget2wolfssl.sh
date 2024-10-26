97f8c9f25f9be0d26871abd190ba74b8039596fe
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-26 08:29:28 +0000

    wget2wolfssl: change in XFREE definition in wolfssl 5.7.4 breaks direct usage here; might need to fix in upstream wolfssl type.sh?

--- recipes/wget2/zz_wget2wolfssl.sh
+++ recipes/wget2/zz_wget2wolfssl.sh
@@ -6,3 +6,13 @@ rurl="$(cwurl_wget2)"
 rsha256="$(cwsha256_wget2)"
 
 . "${cwrecipe}/${rname%wolfssl}/${rname%wolfssl}.sh.common"
+
+eval "
+function cwpatch_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwpatch_wget2
+  sed -i '/XFREE.*subject/s,XFREE.*,if(subject) free(subject);,' libwget/ssl_wolfssl.c
+  sed -i '/XFREE.*issuer/s,XFREE.*,if(issuer) free(issuer);,' libwget/ssl_wolfssl.c
+  popd &>/dev/null
+}
+"
