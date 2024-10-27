94ad91e300ff59a7e0b6e5be2418f1297192e060
ryanwoodsmall <rwoodsmall@gmail.com>
2024-10-27 07:07:06 +0000

    njsquickjs: some fiddling after updating quickjs

--- recipes/nginx/zz_njsquickjs.sh
+++ recipes/nginx/zz_njsquickjs.sh
@@ -20,9 +20,27 @@ function cwpatch_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
   cwpatch_${rname%%quickjs}
   cat auto/quickjs > auto/quickjs.ORIG
+  echo -n > auto/quickjs
+  : echo 'set -x' > auto/quickjs
+  cat auto/quickjs.ORIG >> auto/quickjs
   sed -i s,/usr/include/quickjs,${cwsw}/quickjs/current/include/quickjs,g auto/quickjs
   sed -i s,/usr/lib/quickjs,${cwsw}/quickjs/current/lib/quickjs,g auto/quickjs
   sed -i s,-lquijs,-lquickjs,g auto/quickjs
+  : sed -i 's,\\.lto,,g' auto/quickjs
+  : sed -i 's,-I${cwsw}/quickjs/current/include/quickjs/,-I${cwsw}/quickjs/current/include/quickjs/ -I${cwsw}/quickjs/current/include/,g' auto/quickjs
+  : sed -i 's,-L${cwsw}/quickjs/current/lib/quickjs/,-L${cwsw}/quickjs/current/lib/quickjs/ -L${cwsw}/quickjs/current/lib/,g' auto/quickjs
+  : echo 'set +x' >> auto/quickjs
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  ./configure \
+    --cc-opt=\"\${CFLAGS} \$(echo -I${cwsw}/{${rreqs// /,}}/current/include -I${cwsw}/quickjs/current/include/quickjs)\" \
+    --ld-opt=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib -L${cwsw}/quickjs/current/lib/quickjs) -static\" \
+    --with-quickjs
   popd &>/dev/null
 }
 "
