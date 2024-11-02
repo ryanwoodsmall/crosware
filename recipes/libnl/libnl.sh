fab816c5ccb54b3d86ce93b671c80055ca860dbf
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-02 01:55:54 +0000

    libnl: update to 3.11.0, limit flags, enable cli

--- recipes/libnl/libnl.sh
+++ recipes/libnl/libnl.sh
@@ -1,9 +1,9 @@
 rname="libnl"
-rver="3.10.0"
+rver="3.11.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/thom311/libnl/releases/download/${rname}${rver//./_}/${rfile}"
-rsha256="49b3e2235fdb58f5910bbb3ed0de8143b71ffc220571540502eb6c2471f204f5"
+rsha256="2a56e1edefa3e68a7c00879496736fdbf62fc94ed3232c0baba127ecfa76874d"
 rreqs="make bison"
 
 . "${cwrecipe}/common.sh"
@@ -12,7 +12,13 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG 's#sys/poll.h#poll.h#g' include/netlink/netlink.h
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+    --disable-silent-rules \
+    --enable-cli=yes \
+    --with-pic \
+      LDFLAGS=-static \
+      CPPFLAGS= \
+      PKG_CONFIG_{LIBDIR,PATH}=
   popd &>/dev/null
 }
 "
