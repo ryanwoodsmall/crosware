b90bec49421f65695bb3267ff5e70e8a0e06c299
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-14 07:53:32 +0000

    wget: update to 1.25.0, cleanups

--- recipes/wget/wget.sh
+++ recipes/wget/wget.sh
@@ -6,13 +6,14 @@
 # XXX - disabled metalink+expat 20220226
 # XXX - metalink has/needs(?) gpgme support, probably not worth it for now
 # XXX - --without-included-unistring for full-fat gnutls
+# XXX - make virtual
 #
 rname="wget"
-rver="1.24.5"
+rver="1.25.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="fa2dc35bab5184ecbc46a9ef83def2aaaa3f4c9f3c97d4bd19dcb07d4da637de"
+rurl="https://ftp.gnu.org/gnu/wget/${rfile}"
+rsha256="766e48423e79359ea31e41db9e5c289675947a7fcf2efdcedb726ac9d0da3784"
 
 . "${cwrecipe}/${rname}/${rname}.sh.common"
 
