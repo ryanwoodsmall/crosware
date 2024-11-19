5c9aa9eb65ddd72e49f41de279ad5e41a6477144
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-19 00:20:14 +0000

    habitat: update to 1.6.1215.20241115182310, cleanups

--- recipes/habitat/habitat.sh
+++ recipes/habitat/habitat.sh
@@ -4,11 +4,11 @@
 #
 
 rname="habitat"
-rver="1.6.1041.20240411141804"
+rver="1.6.1215.20241115182310"
 rdir="${rname}-${rver}"
 rfile="core-hab-${rver%.*}-${rver##*.}-x86_64-linux.hart"
 rurl="https://bldr.habitat.sh/v1/depot/pkgs/core/hab/${rver%.*}/${rver##*.}/download?target=x86_64-linux"
-rsha256="8643e13d08693a4b6355fa85710acdb77816ea50bb8a79779152ff545461313e"
+rsha256="97f876425290f6724538a6040ff3ab7183cf2020fdde0501e6b8c5053f0b73a9"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
@@ -26,17 +26,17 @@ cwstubfunc "cwconfigure_${rname}"
 
 eval "
 function cwclean_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   rm -rf \"${rname}\" hab
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwextract_${rname}() {
-  pushd \"${cwbuild}\" >/dev/null 2>&1
+  pushd \"${cwbuild}\" &>/dev/null
   tail -n +6 \"\$(cwdlfile_${rname})\" | xzcat -dc | tar -xf -
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
@@ -53,6 +53,6 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
   echo ': \${HAB_LICENSE:=\"accept\"}' >> \"${rprof}\"
   echo ': \${HAB_BINLINK_DIR:=\"${rtdir}/current/bin\"}' >> \"${rprof}\"
-  echo 'export HAB_LICENSE HAB_BINLINK_DIR' >> \"${rprof}\"
+  echo ': export HAB_LICENSE HAB_BINLINK_DIR' >> \"${rprof}\"
 }
 "
