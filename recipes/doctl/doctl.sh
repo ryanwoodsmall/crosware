aa4512b04bfd06c44713935c255de0d7dbed4103
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-20 05:23:51 +0000

    doctl: update to 1.118.0

--- recipes/doctl/doctl.sh
+++ recipes/doctl/doctl.sh
@@ -1,9 +1,9 @@
 rname="doctl"
-rver="1.117.0"
+rver="1.118.0"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/digitalocean/doctl/archive/refs/tags/${rfile}"
-rsha256="2ff734465d45a95a1e31032ea607d05c2582ef28406d7f64142705339a58f3da"
+rsha256="fe80387fbcc07ac5487cf136e998b11c5cd2e4f240fde87d5813661abeac7aae"
 
 rreqs="go"
 
