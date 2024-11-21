a11a08787f258b89693397f98885d3b1f4730cc8
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-21 01:44:54 +0000

    kubernetes: update to 1.31.3

--- recipes/kubernetes/kubernetes.sh
+++ recipes/kubernetes/kubernetes.sh
@@ -9,7 +9,7 @@
 #   https://github.com/kubernetes/community/blob/master/contributors/devel/development.md
 #
 rname="kubernetes"
-rver="1.31.2"
+rver="1.31.3"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
