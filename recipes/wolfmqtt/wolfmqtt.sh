74da325e218bfe17acfc9f5e1f791ef8fc867730
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-21 01:04:19 +0000

    wolfmqtt: update to 1.19.1

--- recipes/wolfmqtt/wolfmqtt.sh
+++ recipes/wolfmqtt/wolfmqtt.sh
@@ -1,29 +1,29 @@
 rname="wolfmqtt"
-rver="1.19.0"
+rver="1.19.1"
 rdir="${rname//mqtt/MQTT}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="8ee9c9323ddafb6deb7c2ed73417ee20addc500b380cc8355ac35268833bb750"
+rsha256="cca85bcc681839d065206d1518ede4457a6d4a22c656bb3b9bf88a17895f82b8"
 rreqs="make wolfssl configgit toybox"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --enable-all \
     --with-libwolfssl-prefix=\"${cwsw}/wolfssl/current\" \
       PKG_CONFIG_{LIBDIR,PATH}= \
       CPPFLAGS=\"-I${cwsw}/wolfssl/current/include\" \
       LDFLAGS=\"-L${cwsw}/wolfssl/current/lib -static -s\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   mkdir -p \"\$(cwidir_${rname})/bin\"
   for i in \$(find examples/ -type f -exec \"${cwsw}/toybox/current/bin/toybox\" file {} + | awk -F: '/ELF.*executable/{print \$1}') ; do
@@ -33,7 +33,7 @@ function cwmakeinstall_${rname}() {
   unset i n
   sed -i \"s,\$(cwidir_${rname}),${rtdir}/current,g\" \"\$(cwidir_${rname})/bin/${rname}-config\"
   sed -i 's,-lwolfssl,-L${cwsw}/wolfssl/current/lib -lwolfssl,g' \"\$(cwidir_${rname})/bin/${rname}-config\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
