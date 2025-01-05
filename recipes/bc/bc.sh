9bc195999db6b6e7dd191719856c30df3aa60a10
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-05 22:41:44 +0000

    bc: update to 1.08.0, use netbsdcurses and texinfo, include docs since bison brings in perl anyway, cleanups

--- recipes/bc/bc.sh
+++ recipes/bc/bc.sh
@@ -1,21 +1,24 @@
 rname="bc"
-rver="1.07.1"
+rver="1.08.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="62adfca89b0a1c0164c2cdca59ca210c1d44c3ffc46daf9931cf4942664cb02a"
-rreqs="bison ed flex make ncurses readline sed"
-
-# XXX - docs need makeinfo/texinfo
+rurl="https://ftp.gnu.org/gnu/bc/${rfile}"
+rsha256="7db49996cbe16d7602936fef586e69e492c3df65765c0a891841025a1ad741ef"
+rreqs="bison ed flex make readlinenetbsdcurses netbsdcurses sed texinfo"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd "${rbdir}" >/dev/null 2>&1
-  sed -i.ORIG '/^SUBDIRS/ s/doc//g' Makefile.{am,in}
-  ./configure ${cwconfigureprefix} --with-readline LIBS='-lreadline -lncurses -static'
-  popd >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  : sed -i.ORIG '/^SUBDIRS/ s/doc//g' Makefile.{am,in}
+  ./configure ${cwconfigureprefix} \
+    --with-readline \
+      CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include) -static -s\" \
+      LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static -s\" \
+      LIBS='-lreadline -lcurses -lterminfo -static'
+  sed -i.ORIG '/:.*LDADD/s,LDADD,LIBBC,g' ./dc/Makefile
+  pushd &>/dev/null
 }
 "
 
