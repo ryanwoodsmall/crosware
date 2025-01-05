d1c140dc0f89f936d7a34a19b19f4a69068353b6
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-05 21:49:34 +0000

    texinfo: update to 7.2

--- recipes/texinfo/texinfo.sh
+++ recipes/texinfo/texinfo.sh
@@ -1,9 +1,9 @@
 rname="texinfo"
-rver="7.1.1"
+rver="7.2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="a46b46b54fd79641a8af5be4ad525788956ccf9798d3113396abeafa9020ef63"
+rsha256="e86de7dfef6b352aa1bf647de3a6213d1567c70129eccbf8977706d9c91919c8"
 rreqs="make sed perl ncurses"
 
 . "${cwrecipe}/common.sh"
