9706b46ee159b16958d636079ad55522b6233225
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-05 22:54:41 +0000

    ddrescue: update to 1.29

--- recipes/ddrescue/ddrescue.sh
+++ recipes/ddrescue/ddrescue.sh
@@ -1,24 +1,23 @@
 rname="ddrescue"
-rver="1.28"
+rver="1.29"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
-#rurl="http://download.savannah.gnu.org/releases/${rname}/${rfile}"
 rurl="http://ftp.gnu.org/pub/gnu/ddrescue/${rfile}"
-rsha256="6626c07a7ca1cc1d03cad0958522c5279b156222d32c342e81117cfefaeb10c1"
+rsha256="01a414327853b39fba2fd0ece30f7bee2e9d8c8e8eb314318524adf5a60039a3"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} \
     CC=\"\${CC}\" \
     CXX=\"\${CXX}\" \
     CFLAGS=\"\${CFLAGS} -g0 -Wl,-s -Os\" \
     CXXFLAGS=\"\${CXXFLAGS} -g0 -Wl,-s -Os\" \
     LDFLAGS='-static -s'
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
