d7db5213e0c5a670f83b60ce1dd7f242ffdf5879
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-06 01:58:36 +0000

    iperf3: update to 3.18

--- recipes/iperf3/iperf3.sh
+++ recipes/iperf3/iperf3.sh
@@ -1,9 +1,9 @@
 rname="iperf3"
-rver="3.17.1"
+rver="3.18"
 rdir="iperf-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/esnet/iperf/releases/download/${rver}/${rfile}"
-rsha256="84404ca8431b595e86c473d8f23d8bb102810001f15feaf610effd3b318788aa"
+rsha256="c0618175514331e766522500e20c94bfb293b4424eb27d7207fb427b88d20bab"
 rreqs="make openssl configgit zlib"
 
 . "${cwrecipe}/common.sh"
