b9f74f2b12ed26bb379500f44624621011e73772
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-07 02:33:16 +0000

    sysklogd: update to 2.7.0

--- recipes/sysklogd/sysklogd.sh
+++ recipes/sysklogd/sysklogd.sh
@@ -1,9 +1,9 @@
 rname="sysklogd"
-rver="2.6.2"
+rver="2.7.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/troglobit/sysklogd/releases/download/v${rver}/${rfile}"
-rsha256="40a3bb593d7507e49a4379f48ae8a9bc4b68dcc583efcdbf3b9056128442c92a"
+rsha256="6ab74ab5001121bb32697fd2f7ab3cc4b4452c3f721677e06e5b60982a04d0cc"
 rreqs="bootstrapmake slibtool"
 
 . "${cwrecipe}/common.sh"
