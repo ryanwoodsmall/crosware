e5efe7549c7546be186e472002c605baf81375aa
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-23 01:49:05 +0000

    k0sctl: update to 0.22.0

--- recipes/k0s/k0sctl.sh
+++ recipes/k0s/k0sctl.sh
@@ -1,18 +1,18 @@
 rname="k0sctl"
-rver="0.21.0"
+rver="0.22.0"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
 rsha256=""
 if [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rname}-linux-amd64"
-  rsha256="1a623fc840b0c6b7570a138b6df14d3e22ea55a63a0e12d2028084bd5496654c"
+  rsha256="e7e12dc60d1496f140404055973a2e38bf58974b357f84699244496546136cb9"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rname}-linux-arm64"
-  rsha256="5e43ea7de1f304b0b534593a53541771640740a4150e128e9b18d84a96408317"
+  rsha256="c277bcde641ecf3f5bc12ed2f60b9c41842e7f6c48b15394d0ce479cbe332446"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rname}-linux-arm"
-  rsha256="964a235f3e072ec0b29a61d216c2f354a03933b4afbe67431539bc2649d5a9ee"
+  rsha256="15864cecb3857150ff6b3e7f32421e3ff69961867f886582474527ce9314490e"
 fi
 rurl="https://github.com/k0sproject/${rname}/releases/download/v${rver}/${rfile}"
 
