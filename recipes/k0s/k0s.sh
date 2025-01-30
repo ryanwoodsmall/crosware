30f574ff4518e325106cf01b9b7d4be991f979d5
ryanwoodsmall <rwoodsmall@gmail.com>
2025-01-30 02:59:08 +0000

    k0s: update to v1.32.1+k0s.0, use sha256sum.txt file for easier upgrade

--- recipes/k0s/k0s.sh
+++ recipes/k0s/k0s.sh
@@ -1,9 +1,10 @@
 #
 # XXX - k3s notes apply here too
 # XXX - package for airgap images?
+# XXX - online install only ala kubernetes, k3s, ...
 #
 rname="k0s"
-rver="1.31.5_${rname}.0"
+rver="1.32.1_${rname}.0"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
@@ -12,13 +13,10 @@ rbfile="${rname}-v${rver//_/+}"
 rburl="https://github.com/${rname}project/${rname}/releases/download/v${rver//_/%2B}"
 if [[ ${karch} =~ ^x86_64 ]] ; then
   rfile="${rbfile}-amd64"
-  rsha256="4766c130c4802e856a2d19ea4eb89dda723e3c0cc1af246152765117ae87f92c"
 elif [[ ${karch} =~ ^arm ]] ; then
   rfile="${rbfile}-arm"
-  rsha256="3d92b4f3e3d2bbac29d9a5a352e8a1d2d8ce6381aa9e45091d498572056a3737"
 elif [[ ${karch} =~ ^aarch64 ]] ; then
   rfile="${rbfile}-arm64"
-  rsha256="de37465e2646ea6fa486fe2e79dcc11dd113c82e37f5e9a0d264afb26b590dc8"
 fi
 rurl="${rburl}/${rfile//+/%2B}"
 unset rbfile
@@ -37,6 +35,17 @@ fi
 cwstubfunc "cwextract_${rname}"
 cwstubfunc "cwconfigure_${rname}"
 cwstubfunc "cwmake_${rname}"
+
+eval "
+function cwfetch_${rname}() {
+  local s=\"\$(dirname \$(cwdlfile_${rname}))/sha256sums.txt\"
+  local u=\"\$(cwurl_${rname})\"
+  u=\"\${u%/*}\"
+  u=\"\${u}/sha256sums.txt\"
+  cwfetch \"\${u}\" \"\${s}\"
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(grep \\*\$(cwfile_${rname})\$ \${s} | awk '{print \$1}')\"
+}
+"
 
 eval "
 function cwmakeinstall_${rname}() {
