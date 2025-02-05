41cb2cc15c8e641178fe60f758dc18b867e0b32f
ryanwoodsmall <rwoodsmall@gmail.com>
2025-02-05 02:59:37 +0000

    yash: update to 2.58.1

--- recipes/yash/yash.sh
+++ recipes/yash/yash.sh
@@ -2,11 +2,11 @@
 # XXX - minimal termcap variant? doesn't seem to work, looking for tinfo/terminfo/curses/ncurses{,w}
 #
 rname="yash"
-rver="2.58"
+rver="2.58.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/magicant/${rname}/releases/download/${rver}/${rfile}"
-rsha256="4c7367998ad8fea0ea9dc787985925c3fff11853196bfbe931627ad5ee265cc5"
+rsha256="31bf974ca817624446fde8a5edd876f083f187ad2b16a66edbac2f1463aea134"
 rreqs="make netbsdcurses libeditnetbsdcurses"
 
 . "${cwrecipe}/common.sh"
