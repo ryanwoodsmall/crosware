5cba25292da7945f147bcece53f4e440146ce663
ryanwoodsmall <rwoodsmall@gmail.com>
2025-02-14 20:13:42 +0000

    sqlite: update to 3490000, split out cwmakeinstall_, manually include linenoise

--- recipes/sqlite/sqlite.sh
+++ recipes/sqlite/sqlite.sh
@@ -2,29 +2,86 @@
 # XXX - look at alpine: https://git.alpinelinux.org/aports/tree/main/sqlite/APKBUILD
 # XXX - and at ALL the options... https://sqlite.org/compile.html
 # XXX - editline? probably not, libeditminimal though?
-# XXX - unofficial looking linenoise support?
-# XXX - custom make install builds w/readline and bare; configure/build twice is slow/wasteful
 # XXX - minimal/tiny variant?
+# XXX - separate out linenoise variant, make that minimal?
+# XXX - clean this up!
 #
 rname="sqlite"
-rver="3470200"
+rver="3490000"
 rdir="${rname}-autoconf-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://www.sqlite.org/2024/${rfile}"
-rsha256="f1b2ee412c28d7472bc95ba996368d6f0cdcf00362affdadb27ed286c179540b"
+rurl="https://www.sqlite.org/2025/${rfile}"
+rsha256="4d8bfa0b55e36951f6e5a9fb8c99f3b58990ab785c57b4f84f37d163a0672759"
 rreqs="make netbsdcurses readlinenetbsdcurses zlib"
 
 . "${cwrecipe}/common.sh"
 
+cwstubfunc "cwconfigure_${rname}"
+cwstubfunc "cwmake_${rname}"
+
 eval "
-function cwconfigure_${rname}() {
+function cwfetch_${rname}() {
+  cwfetchcheck \"\$(cwurl_${rname})\" \"\$(cwdlfile_${rname})\" \"\$(cwsha256_${rname})\"
+  cwfetch_linenoise
+}
+"
+
+eval "
+function cwextract_${rname}() {
+  cwextract \"\$(cwdlfile_${rname})\" \"${cwbuild}\"
+  cwextract \"\$(cwdlfile_linenoise)\" \"\$(cwbdir_${rname})\"
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_readline() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
-  sed -i.ORIG s/termcap/terminfo/g configure
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --enable-readline \
-    CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
-    LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
-    LIBS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -lreadline -lcurses -lterminfo -lz -static\" \
-    PKG_CONFIG_{LIBDIR,PATH}=
+  (
+    export CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\"
+    export LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\"
+    export LIBS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -lreadline -lcurses -lterminfo -lz -static\"
+    export PKG_CONFIG_{LIBDIR,PATH}=
+    ./configure ${cwconfigureprefix} --disable-shared --with-readline-ldflags=\"\${LDFLAGS} \${LIBS}\" --with-readline-cflags=\"\${CPPFLAGS}\"
+    make -j${cwmakejobs} ${rlibtool}
+  )
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 755 sqlite3 \"\$(cwidir_${rname})/bin/sqlite3-readline\"
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_linenoise() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  make clean || true
+  (
+    export CPPFLAGS=\"-I${cwsw}/zlib/current/include\"
+    export LDFLAGS=\"-L${cwsw}/zlib/current/lib -static\"
+    export LIBS=\"-L${cwsw}/zlib/current/lib -lz -static\"
+    export PKG_CONFIG_{LIBDIR,PATH}=
+    ./configure ${cwconfigureprefix} --disable-shared --disable-readline --disable-editline --with-linenoise=\"\$(cwbdir_${rname})/\$(cwdir_linenoise)\"
+    make -j${cwmakejobs} ${rlibtool}
+  )
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 755 sqlite3 \"\$(cwidir_${rname})/bin/sqlite3-linenoise\"
+  popd &>/dev/null
+}
+"
+
+eval "
+function cwmakeinstall_${rname}_minimal() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  make clean || true
+  (
+    export CPPFLAGS=\"-I${cwsw}/zlib/current/include\"
+    export LDFLAGS=\"-L${cwsw}/zlib/current/lib -static\"
+    export LIBS=\"-L${cwsw}/zlib/current/lib -lz -static\"
+    export PKG_CONFIG_{LIBDIR,PATH}=
+    ./configure ${cwconfigureprefix} --disable-shared --disable-readline --disable-editline
+    make -j${cwmakejobs} ${rlibtool}
+  )
+  make install ${rlibtool}
+  install -m 755 sqlite3 \"\$(cwidir_${rname})/bin/sqlite3-minimal\"
   popd &>/dev/null
 }
 "
@@ -32,19 +89,9 @@ function cwconfigure_${rname}() {
 eval "
 function cwmakeinstall_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
-  make install ${rlibtool}
-  install -m 755 \"\$(cwidir_${rname})/bin/sqlite3\" \"\$(cwidir_${rname})/bin/sqlite3-readline\"
-  make clean || true
-  make distclean || true
-  rm -f \"\$(cwidir_${rname})/bin/sqlite3\"
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} --disable-readline --disable-editline \
-    CPPFLAGS=\"-I${cwsw}/zlib/current/include\" \
-    LDFLAGS=\"-L${cwsw}/zlib/current/lib -static\" \
-    LIBS=\"-L${cwsw}/zlib/current/lib -lz -static\" \
-    PKG_CONFIG_{LIBDIR,PATH}=
-  make -j${cwmakejobs} ${rlibtool}
-  make install ${rlibtool}
-  install -m 755 \"\$(cwidir_${rname})/bin/sqlite3\" \"\$(cwidir_${rname})/bin/sqlite3-minimal\"
+  cwmakeinstall_${rname}_readline
+  cwmakeinstall_${rname}_linenoise
+  cwmakeinstall_${rname}_minimal
   ln -sf sqlite3-readline \"\$(cwidir_${rname})/bin/sqlite3\"
   popd &>/dev/null
 }
