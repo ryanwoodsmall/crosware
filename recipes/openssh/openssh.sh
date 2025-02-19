a1f0890cdd5793a799a295a3ce99938827e77a12
ryanwoodsmall <rwoodsmall@gmail.com>
2025-02-19 02:42:17 +0000

    openssh: update to 9.9p2

--- recipes/openssh/openssh.sh
+++ recipes/openssh/openssh.sh
@@ -13,12 +13,12 @@
 #
 
 rname="openssh"
-rver="9.9p1"
+rver="9.9p2"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/${rfile}"
 rurl="https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${rfile}"
-rsha256="b343fbcdbff87f15b1986e6e15d6d4fc9a7d36066be6b7fb507087ba8f966c02"
+rsha256="91aadb603e08cc285eddf965e1199d02585fa94d994d6cae5b41e1721e215673"
 
 sslprov="openssl"
 
