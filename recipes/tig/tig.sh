9f1fa6adf4c31a77fa010c01b2bef21236e3bea6
ryanwoodsmall <rwoodsmall@gmail.com>
2025-02-19 07:08:29 +0000

    tig: update to 2.5.12

--- recipes/tig/tig.sh
+++ recipes/tig/tig.sh
@@ -1,9 +1,9 @@
 rname="tig"
-rver="2.5.11"
+rver="2.5.12"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/jonas/tig/releases/download/${rdir}/${rfile}"
-rsha256="b36ef52b1a19d87257adfa9047cc3e21d6bd528bbd28458fe49003eb3e374aa7"
+rsha256="5dda8a098810bb499096e17fc9f69c0a5915a23f46be27209fc8195d7a792108"
 rreqs="make ncurses readline git pkgconfig pcre2"
 
 . "${cwrecipe}/common.sh"
