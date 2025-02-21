ece10fd27bdf9d9e305e320b0fc05d9043390acf
ryanwoodsmall <rwoodsmall@gmail.com>
2025-02-21 05:19:22 +0000

    vile: update to 9.8z, cleanups

--- recipes/vile/vile.sh
+++ recipes/vile/vile.sh
@@ -1,16 +1,16 @@
 rname="vile"
-rver="9.8z"
+rver="9.8za"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tgz"
 rurl="https://invisible-mirror.net/archives/${rname}/current/${rfile}"
-rsha256="0b3286c327b70a939f21992d22e42b5c1f8a6e953bd9ab9afa624ea2719272f7"
+rsha256="65ba15ec145dfc5506217162228c7d88f01c0490a0dccde7a8a19f1c7c1b93b2"
 rreqs="make flex ncurses pkgconfig"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} \
     --with-screen=ncurses \
     --with-builtin-filters=all \
@@ -19,15 +19,15 @@ function cwconfigure_${rname}() {
       CPPFLAGS=\"\$(echo -I${cwsw}/flex/current/include -I${cwsw}/ncurses/current/include{,/ncurses{,w}})\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{flex,ncurses}/current/lib) -static\" \
       PKG_CONFIG_{LIBDIR,PATH}=\"${cwsw}/ncurses/current/lib/pkgconfig\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make LEX=\"${cwsw}/flex/current/bin/flex\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
