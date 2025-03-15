a314c7ca0e2ba509fe955bde86667fd40e66b924
ryanwoodsmall <rwoodsmall@gmail.com>
2025-03-15 03:58:45 +0000

    libidn2: update to 2.3.8

--- recipes/libidn/libidn2.sh
+++ recipes/libidn/libidn2.sh
@@ -1,32 +1,32 @@
 rname="libidn2"
-rver="2.3.7"
+rver="2.3.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/libidn/${rfile}"
-rsha256="4c21a791b610b9519b9d0e12b8097bf2f359b12f8dd92647611a929e6bfd7d64"
+rsha256="f557911bf6171621e1f72ff35f5b1825bb35b52ed45325dcdee931e5d3c0787a"
 rreqs="make libunistring"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
     --disable-doc \
     --disable-nls \
       CPPFLAGS=\"\$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       PKG_CONFIG_{LIBDIR,PATH}=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   make install ${rlibtool}
   sed -i 's/-lidn2/-lidn2 -lunistring/g' \"\$(cwidir_${rname})/lib/pkgconfig/${rname}.pc\"
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
