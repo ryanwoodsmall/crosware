3b72726937fd44225f692a455d00916ba706efee
ryanwoodsmall <rwoodsmall@gmail.com>
2025-03-25 15:42:28 +0000

    jansson: update to 2.14.1

--- recipes/jansson/jansson.sh
+++ recipes/jansson/jansson.sh
@@ -1,18 +1,18 @@
 rname="jansson"
-rver="2.14"
+rver="2.14.1"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.bz2"
-rurl="https://github.com/akheron/${rname}/releases/download/v${rver}/${rfile}"
-rsha256="fba956f27c6ae56ce6dfd52fbf9d20254aad42821f74fa52f83957625294afb9"
+rfile="${rdir}.tar.gz"
+rurl="https://github.com/akheron/jansson/releases/download/v${rver}/${rfile}"
+rsha256="2521cd51a9641d7a4e457f7215a4cd5bb176f690bc11715ddeec483e85d9e2b3"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"${rbdir}\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     LDFLAGS=-static CPPFLAGS= PKG_CONFIG_{LIBDIR,PATH}=
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
