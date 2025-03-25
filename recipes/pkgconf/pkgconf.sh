28e05f8feca5b343e6e72afcfa0f41ccaa80f6fb
ryanwoodsmall <rwoodsmall@gmail.com>
2025-03-25 15:59:37 +0000

    pkgconf: update to 2.4.3

--- recipes/pkgconf/pkgconf.sh
+++ recipes/pkgconf/pkgconf.sh
@@ -3,13 +3,13 @@
 # XXX - moving to https://gitea.treehouse.systems/ariadne/pkgconf
 #
 rname="pkgconf"
-rver="2.3.0"
+rver="2.4.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 #rurl="https://github.com/${rname}/${rname}/archive/refs/tags/${rfile}"
 #rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
 rurl="https://distfiles.dereferenced.org/${rname}/${rfile}"
-rsha256="a2df680578e85f609f2fa67bd3d0fc0dc71b4bf084fc49119de84cd6ed28e723"
+rsha256="cf6be37c79265802f2cb1dfc412e18de23a35b5204fc5868bc09fcfd092ac225"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
@@ -19,6 +19,7 @@ function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
     --enable-year2038 \
+    --with-pic \
       CC=\"\${CC} -Os -Wl,-s\" \
       CFLAGS=\"\${CFLAGS} -Os -Wl,-s\" \
       CXXFLAGS=\"\${CXXFLAGS} -Os -Wl,-s\" \
