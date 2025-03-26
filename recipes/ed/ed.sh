950a9846d25442b1536b40a6ca4160aa33cc7800
ryanwoodsmall <rwoodsmall@gmail.com>
2025-03-26 14:42:25 +0000

    ed: update to 1.21.1

--- recipes/ed/ed.sh
+++ recipes/ed/ed.sh
@@ -1,9 +1,9 @@
 rname="ed"
-rver="1.21"
+rver="1.21.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="60e24998727d453a5cf02c54664b97536de46a2b34cd1f4f67c1c1a61bbbad75"
+rsha256="d6d0c7192b02b0519c902a93719053e865ade5a784a3b327d93d888457b23c4b"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
