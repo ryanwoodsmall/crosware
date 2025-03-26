ccc3aa3d79c4b834265c32750e46cc36967366d3
ryanwoodsmall <rwoodsmall@gmail.com>
2025-03-26 14:35:24 +0000

    ddrescue: update to 1.29.1

--- recipes/ddrescue/ddrescue.sh
+++ recipes/ddrescue/ddrescue.sh
@@ -1,9 +1,9 @@
 rname="ddrescue"
-rver="1.29"
+rver="1.29.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.lz"
 rurl="http://ftp.gnu.org/pub/gnu/ddrescue/${rfile}"
-rsha256="01a414327853b39fba2fd0ece30f7bee2e9d8c8e8eb314318524adf5a60039a3"
+rsha256="ddd7d45df026807835a2ec6ab9c365df2ef19e8de1a50ffe6886cd391e04dd75"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
