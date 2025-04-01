e33ebc4f3090393717b31670a86b953b9c2e4c3a
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-01 19:45:45 +0000

    colorizedlogs: update to 2.7

--- recipes/colorizedlogs/colorizedlogs.sh
+++ recipes/colorizedlogs/colorizedlogs.sh
@@ -1,24 +1,24 @@
 rname="colorizedlogs"
-rver="2.6"
+rver="2.7"
 rdir="colorized-logs-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/kilobyte/colorized-logs/archive/refs/tags/${rfile}"
-rsha256="d209d9fb45f5332ba4792624746f2bbbbcf425e6539fa69657e9602e2bee6570"
+rsha256="1fb97c9c90efd739f03dabd8cf5825c2afc95d1f1e0d6cacf62d53a8c540b3df"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
 
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   echo > config.h
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   for a in ansi2html ansi2txt ttyrec2ansi ; do
     echo \${a}
     \${CC} \${a}.c -o \${a} -static -Os -Wl,-s -Wl,-static
@@ -27,20 +27,20 @@ function cwmake_${rname}() {
   echo '#define HAVE_PTY_H 1' >> config.h
   echo '#define HAVE_FUNC_OPENPTY 1' >> config.h
   \${CC} pipetty.c signals.c -o pipetty -static -Os -Wl,-s -Wl,-static
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" >/dev/null 2>&1
-  cwmkdir \"${ridir}/bin\"
-  cwmkdir \"${ridir}/share/man/man1\"
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  cwmkdir \"\$(cwidir_${rname})/share/man/man1\"
   for a in ansi2html ansi2txt ttyrec2ansi pipetty ; do
-    install -m 755 \${a} \"${ridir}/bin/\${a}\"
-    install -m 644 \${a}.1 \"${ridir}/share/man/man1/\${a}.1\"
+    install -m 755 \${a} \"\$(cwidir_${rname})/bin/\${a}\"
+    install -m 644 \${a}.1 \"\$(cwidir_${rname})/share/man/man1/\${a}.1\"
   done
-  popd >/dev/null 2>&1
+  popd &>/dev/null
 }
 "
 
