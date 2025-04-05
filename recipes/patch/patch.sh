ea807d20878a92789084f885668a31ae0f95a4f1
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-05 20:38:00 +0000

    patch: update to 2.8, cleanups, move to .tar.gz

--- recipes/patch/patch.sh
+++ recipes/patch/patch.sh
@@ -1,12 +1,24 @@
 rname="patch"
-rver="2.7.6"
+rver="2.8"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.xz"
+rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd"
+rsha256="308a4983ff324521b9b21310bfc2398ca861798f02307c79eb99bb0e0d2bf980"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
+
+eval "
+function cwconfigure_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  (
+    unset CPPFLAGS PKG_CONFIG_{LIBDIR,PATH}
+    export LDFLAGS=-static
+    ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts}
+  )
+  popd &>/dev/null
+}
+"
 
 eval "
 function cwgenprofd_${rname}() {
