170be00729b9a8eb445cc9e9d4892360d0a2bbd7
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-06 00:35:37 +0000

    jgitsh7: note

--- recipes/jgitsh/jgitsh7.sh
+++ recipes/jgitsh/jgitsh7.sh
@@ -1,3 +1,7 @@
+#
+# XXX - 7.2.0.202503040940-r fails with a java.lang.classNotFoundException
+# XXX - https://github.com/eclipse-jgit/jgit/issues/157
+#
 rver="7.1.0.202411261347-r"
 rname="jgitsh${rver%%.*}"
 rsha256="9d93f77cb73842f4502b55493f9da2d98f83a19dd496059e6c7a520dc35a716e"
