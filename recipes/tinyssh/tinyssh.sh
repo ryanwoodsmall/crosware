b3301428b04d4c7fd70930775908bcaba3c16213
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-12 22:42:53 +0000

    tinyssh: update to 20250411

--- recipes/tinyssh/tinyssh.sh
+++ recipes/tinyssh/tinyssh.sh
@@ -5,11 +5,11 @@
 #   env -i ${cwsw}/busybox/current/bin/busybox tcpsvd -vE 0.0.0.0 22222 ${cwsw}/tinyssh/current/sbin/tinysshd -v -x sftp=${cwsw}/lshsftpserver/current/sbin/sftp-server ${cwtop}/etc/tinyssh/
 #
 rname="tinyssh"
-rver="20250201"
+rver="20250411"
 rdir="${rname}-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/janmojzis/${rname}/archive/refs/tags/${rfile}"
-rsha256="7eddb7ef4b4853cbba63d0834b2b347f01a2fb980fc50aa6c13a35c387930bfc"
+rsha256="38bb076eba10f24b2c1dac531faff2a3645acd9899532dd1d57833f40aa85cd9"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
