3e6da03e1039f80119a2dc90ad0d1e658c23077b
ryanwoodsmall <rwoodsmall@gmail.com>
2025-05-17 21:42:38 +0000

    nftables: update to 1.1.3, fix recipe

--- recipes/netfilter/nftables.sh
+++ recipes/netfilter/nftables.sh
@@ -13,11 +13,11 @@
 #   "
 #
 rname="nftables"
-rver="1.1.1"
+rver="1.1.3"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.xz"
 rurl="https://www.netfilter.org/pub/${rname}/${rfile}"
-rsha256="6358830f3a64f31e39b0ad421d7dadcd240b72343ded48d8ef13b8faf204865a"
+rsha256="9c8a64b59c90b0825e540a9b8fcb9d2d942c636f81ba50199f068fde44f34ed8"
 rreqs="bootstrapmake pkgconfig byacc netbsdcurses readlinenetbsdcurses libpcap libnl jansson libmnl libnetfilterconntrack libnfnetlink libnftnl iptables slibtool"
 
 . "${cwrecipe}/common.sh"
@@ -26,13 +26,15 @@ eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
   ./configure ${cwconfigureprefix} ${cwconfigurelibopts} ${rconfigureopts} ${rcommonopts} \
-    --with-json --with-mini-gmp --with-xtables --disable-man-doc --disable-python --without-python-bin --with-cli=readline \
+    --with-json --with-mini-gmp --with-xtables --disable-man-doc --with-cli=readline \
       CPPFLAGS=\"-I\$(cwbdir_${rname})/include \$(echo -I${cwsw}/{${rreqs// /,}}/current/include)\" \
       LDFLAGS=\"\$(echo -L${cwsw}/{${rreqs// /,}}/current/lib) -static\" \
       PKG_CONFIG_LIBDIR=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       PKG_CONFIG_PATH=\"\$(echo ${cwsw}/{${rreqs// /,}}/current/lib/pkgconfig | tr ' ' ':')\" \
       LIBS='-lreadline -lcurses -lterminfo -static' \
       YACC=\"${cwsw}/byacc/current/bin/byacc\"
+  echo '#include <sys/types.h>' >> config.h
+  echo '#include <netinet/if_ether.h>' >> config.h
   popd &>/dev/null
 }
 "
