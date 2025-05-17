42fbca69890a995746be4d36e5d44e23e436c11b
ryanwoodsmall <rwoodsmall@gmail.com>
2025-05-17 00:06:44 +0000

    zulu21musl: update to 21.42.19-ca-jdk21.0.7

--- recipes/zulu/zulu21musl.sh
+++ recipes/zulu/zulu21musl.sh
@@ -1,10 +1,10 @@
-rver="21.40.17-ca-jdk21.0.6"
+rver="21.42.19-ca-jdk21.0.7"
 mver="${rver%%.*}"
 rname="zulu${mver}musl"
-rsha256="f609630f9e0aacecf8027da8b93b3e9a12104874695497f5c01435c3d15a62c2"
+rsha256="02dbdaf6e31c1dd767953615e2928cd33f2935715f715fa181283f373c3c47ad"
 if [[ ${karch} =~ aarch64 ]] ; then
   rdir="zulu${rver}-linux_musl_aarch64"
-  rsha256="ad6a3fc6be7ba4eec0afb5c9221082db6294722700782c13021e6c99ccefb146"
+  rsha256="e1fdd8ceb0297d1480581296bcf88088377fe49a9b49c847eb6e5ab759940da0"
 fi
 
 . "${cwrecipe}/${rname%${mver}musl}/${rname//${mver}musl/musl}.sh.common"
