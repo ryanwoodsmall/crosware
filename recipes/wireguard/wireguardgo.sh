337edbc164d42e35b0824c93740a964a502afd62
ryanwoodsmall <rwoodsmall@gmail.com>
2025-05-17 19:34:33 +0000

    wireguardgo: update to 0.0.20250515, remove go <1.23 workaround

--- recipes/wireguard/wireguardgo.sh
+++ recipes/wireguard/wireguardgo.sh
@@ -1,14 +1,10 @@
-#
-# XXX - remove specific go version req below
-#
 rname="wireguardgo"
-rver="0.0.20230223"
+rver="0.0.20250515"
 rdir="wireguard-go-${rver}"
 rfile="${rver}.tar.gz"
 rurl="https://github.com/WireGuard/wireguard-go/archive/refs/tags/${rfile}"
-rsha256="c679fff905cbd097836d280c99c1b86454264921951491716b235e162178263a"
-rgover="122"
-rreqs="bootstrapmake go${rgover} wireguardtools"
+rsha256="764b0f80b62c1399267d347ddfdbdd9ec37525193104d87987d4f7ca21c841f0"
+rreqs="bootstrapmake go wireguardtools"
 
 . "${cwrecipe}/common.sh"
 
@@ -30,11 +26,11 @@ function cwmake_${rname}() {
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
     env \
-      PATH=\"${cwsw}/go${rgover}/current/bin:\${PATH}\" \
+      PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       GOPATH=\"\$(cwbdir_${rname})/gopath\" \
-      GOROOT=\"${cwsw}/go${rgover}/current\" \
+      GOROOT=\"${cwsw}/go/current\" \
       CGO_ENABLED=0 \
         make PREFIX=\"\$(cwidir_${rname})\"
     chmod -R u+rw . || true
@@ -50,11 +46,11 @@ function cwmakeinstall_${rname}() {
     : \${GOCACHE=\"\$(cwbdir_${rname})/gocache\"}
     : \${GOMODCACHE=\"\$(cwbdir_${rname})/gomodcache\"}
     env \
-      PATH=\"${cwsw}/go${rgover}/current/bin:\${PATH}\" \
+      PATH=\"${cwsw}/go/current/bin:\${PATH}\" \
       GOCACHE=\"\${GOCACHE}\" \
       GOMODCACHE=\"\${GOMODCACHE}\" \
       GOPATH=\"\$(cwbdir_${rname})/gopath\" \
-      GOROOT=\"${cwsw}/go${rgover}/current\" \
+      GOROOT=\"${cwsw}/go/current\" \
       CGO_ENABLED=0 \
         make install PREFIX=\"\$(cwidir_${rname})\"
     chmod -R u+rw . || true
@@ -68,5 +64,3 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/bin\"' > \"${rprof}\"
 }
 "
-
-unset rgover
