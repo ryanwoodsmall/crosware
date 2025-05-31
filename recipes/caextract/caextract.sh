bb05ada67b04c20ddf9f6c1e85b19bcca5f9c9d5
ryanwoodsmall <rwoodsmall@gmail.com>
2025-05-31 07:03:04 +0000

    caextract: update to 2025-05-20

--- recipes/caextract/caextract.sh
+++ recipes/caextract/caextract.sh
@@ -3,11 +3,11 @@
 # XXX - if this doesn't match what we have stored, should indicate an update
 #
 rname="caextract"
-rver="2025-02-25"
+rver="2025-05-20"
 rdir="${rname}-${rver}"
 rfile="cacert-${rver}.pem"
 rurl="https://curl.se/ca/${rfile}"
-rsha256="50a6277ec69113f00c5fd45f09e8b97a4b3e32daa35d3a95ab30137a55386cef"
+rsha256="ab3ee3651977a4178a702b0b828a4ee7b2bbb9127235b0ab740e2e15974bf5db"
 rreqs=""
 
 . "${cwrecipe}/common.sh"
