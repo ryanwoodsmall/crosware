a6ed1a28e32f211521d6eaceb0736c8fe3df914a
ryanwoodsmall <rwoodsmall@gmail.com>
2025-05-31 03:53:17 +0000

    outils: update to 0.14

--- recipes/outils/outils.sh
+++ recipes/outils/outils.sh
@@ -1,9 +1,9 @@
 rname="outils"
-rver="0.13"
+rver="0.14"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
-rurl="https://github.com/leahneukirchen/${rname}/archive/${rfile}"
-rsha256="49d46211fe84a5b96cf55d689696d190b7aba7d3e043c8c8dc9f5ff9af8f927a"
+rurl="https://github.com/leahneukirchen/outils/archive/${rfile}"
+rsha256="e4dcbd92b25bbb371216b0fad5aa80cdff19f466f7ec8b5e145111fb348c91eb"
 rreqs="bootstrapmake"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 
