6d78645c2b18614a03871c94c25b690900596816
ryanwoodsmall <rwoodsmall@gmail.com>
2025-06-15 08:41:31 +0000

    subproxy: notes

--- recipes/acmelabs/subproxy.sh
+++ recipes/acmelabs/subproxy.sh
@@ -1,3 +1,9 @@
+#
+# XXX - fork to only CONNECT, build in automatic http:// -> https:// protocol upgrade
+# XXX - cryanc 'carl -p' proxy mode can do this too, i think? only 'GET http://...' style though
+# XXX - combine this (or micro_proxy) with microsocks and front with sslh. ssl with socat? or nc?
+# XXX - C -> header file conversion for html templates w/(f|)printf
+#
 rname="subproxy"
 rsha256="ec2243629892d8901160ea33f40719153c29e6afff967f17b0303cf54114ab76"
 . "${cwrecipe}/acmelabs/acmelabs.sh.common"
