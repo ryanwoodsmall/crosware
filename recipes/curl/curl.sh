2d01e3ad5eb280b2dd356145a88d192992363a1b
ryanwoodsmall <rwoodsmall@gmail.com>
2025-09-14 06:58:03 +0000

    curl: notes

--- recipes/curl/curl.sh
+++ recipes/curl/curl.sh
@@ -23,6 +23,8 @@
 # XXX - libpsl support is enabled by default now; it requires libidn2, libunistring and a python, explicitly disable...
 # XXX - mbedtls is messy at/after fba9afebba22d577f122239b184edc90c18fd81b (bisected) - need to figure out why
 # XXX - 8.13.0 is broken with everything i support except openssl/libressl
+# XXX - 8.15.0 removed bearssl support
+# XXX - 8.16.0 requires mbedtls 3.2+
 #
 rname="curl"
 rver="8.15.0"
