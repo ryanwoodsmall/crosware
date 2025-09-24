8a46bcd3c71a23a095915f74a51a5a7d05b0180c
ryanwoodsmall <rwoodsmall@gmail.com>
2025-09-24 14:54:34 +0000

    coreutils: note on 9.8

--- recipes/coreutils/coreutils.sh
+++ recipes/coreutils/coreutils.sh
@@ -1,14 +1,17 @@
 #
 # XXX - aarch64 only has SYS_getdents64, not SYS_getdents - introduced in 05a99f7d7f8e0999994b760bb6337ca10ea0a14b
 # XXX - 9.4 includes openssl/configuration.h, which... ehh, just turn it off
+# XXX - 9.8 needs gmp > 6.2
 #
 
 rname="coreutils"
 rver="9.7"
+#rver="9.8"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/coreutils/${rfile}"
 rsha256="0898a90191c828e337d5e4e4feb71f8ebb75aacac32c434daf5424cda16acb42"
+#rsha256="1be88d53f694671cf7fb85e7723dbd1de9131d031880161b452a0685b986296e"
 rreqs="make gettexttiny sed attr acl perl gmp utillinux libcap"
 
 . "${cwrecipe}/common.sh"
