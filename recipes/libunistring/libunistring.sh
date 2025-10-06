88d859d7f608d8f6b9eb8b04cf98f224df712acb
ryanwoodsmall <rwoodsmall@gmail.com>
2025-10-06 17:29:35 +0000

    libunistring: update to 1.4.1

--- recipes/libunistring/libunistring.sh
+++ recipes/libunistring/libunistring.sh
@@ -1,9 +1,9 @@
 rname="libunistring"
-rver="1.3"
+rver="1.4.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
-rsha256="8ea8ccf86c09dd801c8cac19878e804e54f707cf69884371130d20bde68386b7"
+rsha256="12542ad7619470efd95a623174dcd4b364f2483caf708c6bee837cb53a54cb9d"
 rreqs="make sed slibtool"
 
 . "${cwrecipe}/common.sh"
