caa44b3b956612abfffb6511cada78789a7fc4e5
ryanwoodsmall <rwoodsmall@gmail.com>
2025-10-15 02:27:40 +0000

    libressl42: add recipe

---
+++ recipes/libressl/libressl42.sh
@@ -0,0 +1,6 @@
+rver="4.2.0"
+rname="libressl${rver%.*}"
+rname="${rname//./}"
+rsha256="0f7dba44d7cb8df8d53f2cfbf1955254bc128e0089595f1aba2facfaee8408b2"
+
+. "${cwrecipe}/libressl/libressl.sh.common"
