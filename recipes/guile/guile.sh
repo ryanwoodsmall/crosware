ab8718496264c3bceda40e42169981130acd6574
ryanwoodsmall <rwoodsmall@gmail.com>
2025-12-17 19:59:37 +0000

    guile: update to 3.0.11

--- recipes/guile/guile.sh
+++ recipes/guile/guile.sh
@@ -1,9 +1,9 @@
 rname="guile"
-rver="3.0.10"
+rver="3.0.11"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/guile/${rfile}"
-rsha256="2dbdbc97598b2faf31013564efb48e4fed44131d28e996c26abe8a5b23b56c2a"
+rsha256="3c9c16972a73bb792752f2e4f1cce7212d7638d5494b5f7e8e19f3819dbf3a19"
 rreqs="make sed gawk gmp libtool slibtool pkgconfig libffi gc readline ncurses libunistring"
 
 . "${cwrecipe}/common.sh"
