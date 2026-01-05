58e0d2b871c17d02666dac24a29b3a7eb896dfd5
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-05 18:20:34 +0000

    wget2: update to 2.2.1

--- recipes/wget2/wget2.sh
+++ recipes/wget2/wget2.sh
@@ -5,10 +5,10 @@
 # XXX - libressl (at least 3.7.x, haven't tested with 3.8.x) breaks with wget2: https://github.com/rockdaboot/wget2/issues/271
 #
 rname="wget2"
-rver="2.2.0"
+rver="2.2.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname%2}/${rfile}"
-rsha256="2b3b9c85b7fb26d33ca5f41f1f8daca71838d869a19b406063aa5c655294d357"
+rsha256="d7544b13e37f18e601244fce5f5f40688ac1d6ab9541e0fbb01a32ee1fb447b4"
 
 . "${cwrecipe}/${rname}/${rname}.sh.common"
