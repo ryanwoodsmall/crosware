6544871b016e0f99d2c722e722a4feaf69cbf9fb
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-05 18:42:30 +0000

    rhino: update to 1.9.0

--- recipes/rhino/rhino.sh
+++ recipes/rhino/rhino.sh
@@ -1,9 +1,9 @@
 rname="rhino"
-rver="1.8.1"
+rver="1.9.0"
 rdir="${rname}-all-${rver}"
 rfile="${rdir}.jar"
 rurl="https://repo.maven.apache.org/maven2/org/mozilla/rhino-all/${rver}/${rfile}"
-rsha256="8bb0b8b4f6a86a584d04d704c7109bf9bc2817be85679a15259d1ea51b1f6cd7"
+rsha256="caafa887e3b32efa74f1aac9984f0f54e733ff5a0ea62b057861c053a6619328"
 rreqs="rlwrap"
 
 . "${cwrecipe}/common.sh"
