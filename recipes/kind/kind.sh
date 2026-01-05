f2f4e857b5381f4015a9ce3a9ad3e6d27abcd318
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-05 17:28:49 +0000

    kind: update to 0.31.0

--- recipes/kind/kind.sh
+++ recipes/kind/kind.sh
@@ -1,9 +1,9 @@
 rname="kind"
-rver="0.30.0"
+rver="0.31.0"
 rdir="${rname}-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/kubernetes-sigs/${rname}/archive/refs/tags/${rfile}"
-rsha256="bfaa702786beae1a9958d4d9cdddbdcc94c8ca5f7a5dca3a4a5a748c51477693"
+rsha256="f4aaa1f572f9965eea3f7513d166f545f41b61ab5efeed953048bdcb13c51032"
 rreqs="bootstrapmake go"
 
 . "${cwrecipe}/common.sh"
