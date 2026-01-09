0ce66db70c6f65d1c7fcbd0e03e4ee850a3c39d3
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-09 17:45:56 +0000

    gettexttiny: update to 0.3.3

--- recipes/gettexttiny/gettexttiny.sh
+++ recipes/gettexttiny/gettexttiny.sh
@@ -1,9 +1,9 @@
 rname="gettexttiny"
-rver="0.3.2"
+rver="0.3.3"
 rdir="gettext-tiny-${rver}"
 rfile="v${rver}.tar.gz"
 rurl="https://github.com/sabotage-linux/gettext-tiny/archive/${rfile}"
-rsha256="29cc165e27e83d2bb3760118c2368eadab550830d962d758e51bd36eb860f383"
+rsha256="daff8b4a7863745710ec0b02657fd20bda2d90398f54945751259bb94d6ebffd"
 rreqs="make"
 
 . "${cwrecipe}/common.sh"
