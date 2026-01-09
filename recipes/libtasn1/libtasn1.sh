e842bc8a602ca8a5e85b365debfa2b2b34f2f8b2
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-09 19:22:48 +0000

    libtasn1: update to 4.21.0

--- recipes/libtasn1/libtasn1.sh
+++ recipes/libtasn1/libtasn1.sh
@@ -1,9 +1,9 @@
 rname="libtasn1"
-rver="4.20.0"
+rver="4.21.0"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/libtasn1/${rfile}"
-rsha256="92e0e3bd4c02d4aeee76036b2ddd83f0c732ba4cda5cb71d583272b23587a76c"
+rsha256="1d8a444a223cc5464240777346e125de51d8e6abf0b8bac742ac84609167dc87"
 rreqs="make sed slibtool gettexttiny byacc pkgconfig"
 
 . "${cwrecipe}/common.sh"
