fb6dc0981394bc0737afb7127721231de456678b
ryanwoodsmall <rwoodsmall@gmail.com>
2026-01-22 16:22:10 +0000

    units: update to 2.25

--- recipes/units/units.sh
+++ recipes/units/units.sh
@@ -2,11 +2,11 @@
 # XXX - readline support worth it?
 #
 rname="units"
-rver="2.24"
+rver="2.25"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/units/${rfile}"
-rsha256="1e502c4edfacf20b29284716c72e5ddb51a495a2365d7b03e7960494c4a0c902"
+rsha256="36edf43ac00b4d6304baea91387e65ab05118bf65c921f73d3b08828e5a6ec0b"
 rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
