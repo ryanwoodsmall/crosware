568662074e3bb078002e02759cff2ab3cf4396ed
ryanwoodsmall <rwoodsmall@gmail.com>
2026-02-05 16:00:02 +0000

    k3s: update to v1.35.0+k3s3

--- recipes/k3s/k3s.sh
+++ recipes/k3s/k3s.sh
@@ -8,7 +8,7 @@
 # XXX - without airgap, that makes sense, but i'm not a huge fan of this approach (no caching, no offline)
 #
 rname="k3s"
-rver="1.35.0_${rname}1"
+rver="1.35.0_${rname}3"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
