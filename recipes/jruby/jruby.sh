581b3c8e1595b838a55ad991b14693eff6f8ff29
ryanwoodsmall <rwoodsmall@gmail.com>
2026-02-05 16:02:25 +0000

    jruby: update to 10.0.3.0

--- recipes/jruby/jruby.sh
+++ recipes/jruby/jruby.sh
@@ -1,9 +1,9 @@
 rname="jruby"
-rver="10.0.2.0"
+rver="10.0.3.0"
 rdir="${rname}-${rver}"
 rfile="${rname}-dist-${rver}-bin.tar.gz"
 rurl="https://repo1.maven.org/maven2/org/jruby/jruby-dist/${rver}/${rfile}"
-rsha256="b8a026f38aa98461a04ed0aa0b20891ce257ecbe53e124719ce9ee5b804525f1"
+rsha256="0edb5b02c3f482205d1cf8358f38e31d9e4c6d93a210039224750c72501e4717"
 rprof="${cwetcprofd}/zz_${rname}.sh"
 rreqs=""
 
