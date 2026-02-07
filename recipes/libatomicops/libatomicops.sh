3ecce510d56688e184f8a80931039674ac3ed386
ryanwoodsmall <rwoodsmall@gmail.com>
2026-02-07 05:55:37 +0000

    libatomicops: update to 7.10.0, cleanup env

--- recipes/libatomicops/libatomicops.sh
+++ recipes/libatomicops/libatomicops.sh
@@ -1,9 +1,9 @@
 rname="libatomicops"
-rver="7.8.4"
+rver="7.10.0"
 rdir="libatomic_ops-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/ivmai/libatomic_ops/releases/download/v${rver}/${rfile}"
-rsha256="2356e002e80ef695875e971d6a4fd8c61ca5c6fa4fd1bf31cce54a269c8bfcd5"
+rsha256="0db3ebff755db170f65e74a64ec4511812e9ee3185c232eeffeacd274190dfb0"
 rreqs="make configgit"
 
 . "${cwrecipe}/common.sh"
@@ -11,7 +11,10 @@ rreqs="make configgit"
 eval "
 function cwconfigure_${rname}() {
   pushd \"\$(cwbdir_${rname})\" &>/dev/null
-  ./configure ${cwconfigureprefix} ${cwconfigurelibopts}
+  ./configure ${cwconfigureprefix} ${cwconfigurelibopts} \
+     --enable-pic \
+       CPPFLAGS= \
+       LDFLAGS=-static
   popd &>/dev/null
 }
 "
