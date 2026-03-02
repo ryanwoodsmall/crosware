08e36ca387318c6943abb4aa275a0d182e6d573e
ryanwoodsmall <rwoodsmall@gmail.com>
2026-03-02 18:19:07 +0000

    glab: update to 1.87.0

--- recipes/gitlab/glab.sh
+++ recipes/gitlab/glab.sh
@@ -1,9 +1,9 @@
 rname="glab"
-rver="1.86.0"
+rver="1.87.0"
 rdir="cli-v${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://gitlab.com/gitlab-org/cli/-/archive/v${rver}/${rfile}"
-rsha256="b9f42a53a26cf24eaee66b7079cc7642facd47411090cde45c974c24960e910a"
+rsha256="891be43034048622b9a63018f96b5bb561342f47491a064266c27bc6571558e5"
 rreqs="go"
 
 . "${cwrecipe}/common.sh"
