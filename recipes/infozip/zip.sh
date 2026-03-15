64c91d81eba9330dac1aa693b25291fd50b5d64a
ryanwoodsmall <rwoodsmall@gmail.com>
2026-03-15 06:58:18 +0000

    infozip: use bootstrapmake, works with only bash+busybox+curl+statictoolchain(+ccache)

--- recipes/infozip/zip.sh
+++ recipes/infozip/zip.sh
@@ -4,7 +4,7 @@ rdir="${rname}${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://sourceforge.net/projects/infozip/files/Zip%203.x%20%28latest%29/3.0/${rfile}/download"
 rsha256="f0e8bb1f9b7eb0b01285495a2699df3a4b766784c1765a8f1aeedf63c0806369"
-rreqs="make"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 . "${cwrecipe}/info${rname##un}/info${rname##un}.sh.common"
