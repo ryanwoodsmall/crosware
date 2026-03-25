66e5e022870b255bcbc98540937c380619bb3040
ryanwoodsmall <rwoodsmall@gmail.com>
2026-03-25 05:22:07 +0000

    patch: require toybox and use its sed

--- recipes/patch/patch.sh
+++ recipes/patch/patch.sh
@@ -4,7 +4,7 @@ rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://ftp.gnu.org/gnu/${rname}/${rfile}"
 rsha256="308a4983ff324521b9b21310bfc2398ca861798f02307c79eb99bb0e0d2bf980"
-rreqs="bootstrapmake"
+rreqs="bootstrapmake toybox"
 
 . "${cwrecipe}/common.sh"
 
@@ -14,7 +14,7 @@ function cwconfigure_${rname}() {
   (
     unset CPPFLAGS PKG_CONFIG_{LIBDIR,PATH}
     export LDFLAGS=-static
-    ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts}
+    ./configure ${cwconfigureprefix} ${rconfigureopts} ${rcommonopts} SED=\"${cwsw}/toybox/current/bin/sed\"
   )
   popd &>/dev/null
 }
