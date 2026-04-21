11d09dc585bb19a4e6911beb560193c3ae62b69a
ryanwoodsmall <rwoodsmall@gmail.com>
2026-04-21 17:20:16 +0000

    kubernetes: update to 1.35.4

--- recipes/kubernetes/kubernetes.sh
+++ recipes/kubernetes/kubernetes.sh
@@ -9,7 +9,7 @@
 #   https://github.com/kubernetes/community/blob/master/contributors/devel/development.md
 #
 rname="kubernetes"
-rver="1.35.3"
+rver="1.35.4"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
