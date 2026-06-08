4129c0bfcc480ad6d2dc024ab674ab7e9636ba87
ryanwoodsmall <rwoodsmall@gmail.com>
2026-06-08 14:59:48 +0000

    rsync: update to 3.4.4

--- recipes/rsync/rsync.sh
+++ recipes/rsync/rsync.sh
@@ -5,28 +5,28 @@
 # XXX - 3.4.3 introduced a reliance on linux/openat2.h, needs linux 5.6+
 #
 rname="rsync"
-rver="3.4.3"
+rver="3.4.4"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://download.samba.org/pub/rsync/src/${rfile}"
-rsha256="c72e63ca3021cbc80ba86ec30102773f4c5631fbc492b52e773b3958f82a53d3"
+rsha256="bd88cf82fa653da32314fb229136407c5c90f80d1758d8f4b091767877d8fa96"
 rreqs="make lz4 xxhash zstd attr acl"
 
 . "${cwrecipe}/common.sh"
 
-# XXX - UGH
-eval "
-function cwpatch_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" &>/dev/null
-  cat rsync.h > rsync.h.ORIG
-  echo '#include <sys/syscall.h>' > rsync.h
-  cat rsync.h.ORIG >> rsync.h
-  cat syscall.c > syscall.c.ORIG
-  echo '#undef __linux__' > syscall.c
-  cat syscall.c.ORIG >> syscall.c
-  popd &>/dev/null
-}
-"
+## XXX - UGH
+#eval "
+#function cwpatch_${rname}() {
+#  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+#  cat rsync.h > rsync.h.ORIG
+#  echo '#include <sys/syscall.h>' > rsync.h
+#  cat rsync.h.ORIG >> rsync.h
+#  cat syscall.c > syscall.c.ORIG
+#  echo '#undef __linux__' > syscall.c
+#  cat syscall.c.ORIG >> syscall.c
+#  popd &>/dev/null
+#}
+#"
 
 eval "
 function cwconfigure_${rname}() {
