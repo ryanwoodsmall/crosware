6000a779f760bdcda098aded105dc45bfec38f47
ryanwoodsmall <rwoodsmall@gmail.com>
2026-06-08 15:02:36 +0000

    rsync: remove commented stuff

--- recipes/rsync/rsync.sh
+++ recipes/rsync/rsync.sh
@@ -2,7 +2,6 @@
 # XXX - for rsync 3.2.x...
 #     - openssl
 # XXX - workaround ipv6 thing, via: https://git.alpinelinux.org/aports/tree/main/rsync/APKBUILD
-# XXX - 3.4.3 introduced a reliance on linux/openat2.h, needs linux 5.6+
 #
 rname="rsync"
 rver="3.4.4"
@@ -13,20 +12,6 @@ rsha256="bd88cf82fa653da32314fb229136407c5c90f80d1758d8f4b091767877d8fa96"
 rreqs="make lz4 xxhash zstd attr acl"
 
 . "${cwrecipe}/common.sh"
-
-## XXX - UGH
-#eval "
-#function cwpatch_${rname}() {
-#  pushd \"\$(cwbdir_${rname})\" &>/dev/null
-#  cat rsync.h > rsync.h.ORIG
-#  echo '#include <sys/syscall.h>' > rsync.h
-#  cat rsync.h.ORIG >> rsync.h
-#  cat syscall.c > syscall.c.ORIG
-#  echo '#undef __linux__' > syscall.c
-#  cat syscall.c.ORIG >> syscall.c
-#  popd &>/dev/null
-#}
-#"
 
 eval "
 function cwconfigure_${rname}() {
