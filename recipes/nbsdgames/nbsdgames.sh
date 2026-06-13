f24f85c8b88a93a317f2b6b0af53c160f51f52b8
rwoodsmall <rwoodsmall@gmail.com>
2026-06-13 03:41:02 +0000

    nbsdgames: fix something i broke, normalize a bit more, make sure static/stripped

--- recipes/nbsdgames/nbsdgames.sh
+++ recipes/nbsdgames/nbsdgames.sh
@@ -1,3 +1,6 @@
+#
+# XXX - move scores to shared etc/var
+#
 rname="nbsdgames"
 rver="6.0.2"
 rdir="${rname}-${rver}"
@@ -17,21 +20,21 @@ function cwpatch_${rname}() {
   sed -i \"/^GAMES_DIR/s,^.*,GAMES_DIR=\$(cwidir_${rname})/bin,g\" src/Makefile
   sed -i \"/^SCORES_DIR/s,^.*,SCORES_DIR=\$(cwidir_${rname})/scores,g\" src/Makefile
   sed -i \"/^MAN_DIR/s,^.*,MAN_DIR=\$(cwidir_${rname})/man/man6,g\" src/Makefile
-  sed -i \"/^LDFLAGS/s,^.*,LDFLAGS=-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static,g\" src/Makefile
-  sed -i \"/^CFLAGS/s|= |= -I${cwsw}/netbsdcurses/current/include -Wl,-L${cwsw}/netbsdcurses/current/lib |g\" src/Makefile
-  sed -i \"/^LIBS_PKG_CONFIG/s,^.*,LIBS_PKG_CONFIG=-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static,g\" src/Makefile
-  grep -rl -- '-lcurses -lterminfo' . | grep -v ORIG$ | xargs sed -i 's,-lcurses -lterminfo,-lcurses -lterminfo,g'
+  sed -i \"/^LDFLAGS/s,^.*,LDFLAGS=-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static -s,g\" src/Makefile
+  sed -i \"/^CFLAGS/s|= |= -Os -I${cwsw}/netbsdcurses/current/include -Wl,-L${cwsw}/netbsdcurses/current/lib -Wl,-static -Wl,-s |g\" src/Makefile
+  sed -i \"/^LIBS_PKG_CONFIG/s,^.*,LIBS_PKG_CONFIG=-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static -s,g\" src/Makefile
+  grep -rl -- -lncurses \"\$(cwbdir_${rname})/\" | grep -v ORIG\$ | xargs sed -i.NCURSES 's,-lncurses,-lcurses -lterminfo,g'
   popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  pushd \"\$(cwbdir_${rname})/src\" &>/dev/null
   (
     unset PKG_CONFIG_{LIBDIR,PATH}
     export CPPFLAGS=\"-I${cwsw}/netbsdcurses/current/include\"
-    export LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static\"
+    export LDFLAGS=\"-L${cwsw}/netbsdcurses/current/lib -lcurses -lterminfo -static -s\"
     make -j${cwmakejobs} ${rlibtool}
   )
   popd &>/dev/null
@@ -40,7 +43,7 @@ function cwmake_${rname}() {
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  pushd \"\$(cwbdir_${rname})/src\" &>/dev/null
   cwmkdir \"\$(cwidir_${rname})/bin\"
   cwmkdir \"\$(cwidir_${rname})/scores\"
   cwmkdir \"\$(cwidir_${rname})/man/man6\"
