7cb35dd83deced3e6e7b35d3cadf76f5c788d4c7
rwoodsmall <rwoodsmall@gmail.com>
2026-06-20 06:22:27 +0000

    9pro: move to https://git.sr.ht/~ft/9pro , update to c66e91c68b28138da3ea4b761d63271591078414

--- recipes/9pro/9pro.sh
+++ recipes/9pro/9pro.sh
@@ -1,34 +1,41 @@
 rname="9pro"
-rver="d1a7ba61b9c5cf0a7f8246a52653b5980c20ac34"
+rver="c66e91c68b28138da3ea4b761d63271591078414"
 rdir="${rname}-${rver}"
-rfile="${rdir}.tar.gz"
-rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rsha256="0a1e7aa97a2f6e95f423efb86a4e7ffbc31841f90286afaa6937b3b96945254d"
-rreqs=""
+rfile="${rver}.tar.gz"
+#rfile="${rdir}.tar.gz"
+#rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
+rurl="https://git.sr.ht/~ft/9pro/archive/${rfile}"
+rsha256="08dd402e9c00314455e10d31d83ecbce2086478a6900756d859c7985135d035a"
+rreqs="bootstrapmake"
 
 . "${cwrecipe}/common.sh"
 
+cwstubfunc "cwconfigure_${rname}"
+
 eval "
-function cwconfigure_${rname}() {
-  true
+function cwpatch_${rname}() {
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cat Makefile > Makefile.ORIG
+  sed -i '/CFLAGS.*=/s/$/ -Wl,-static -Wl,-s -g0/g' Makefile
+  sed -i '/{CC} -o .*{COMMON_O}/s/$/ -g0 -static -s -Wl,-static -Wl,-s/g' Makefile
+  popd &>/dev/null
 }
 "
 
 eval "
 function cwmake_${rname}() {
-  pushd \"${rbdir}\" &>/dev/null
-  env CC=\"\${CC}\" CFLAGS='-static -Wl,-static' ./build.sh
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  env CC=\"\${CC}\" make 9pex 9gc
   popd &>/dev/null
 }
 "
 
 eval "
 function cwmakeinstall_${rname}() {
-  pushd \"${rbdir}\" &>/dev/null
-  cwmkdir \"${ridir}/bin\"
-  \$(\${CC} -dumpmachine)-strip --strip-all 9pex 9gc
-  install -m 0755 9pex \"${ridir}/bin/\"
-  install -m 0755 9gc \"${ridir}/bin/\"
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
+  cwmkdir \"\$(cwidir_${rname})/bin\"
+  install -m 0755 9pex \"\$(cwidir_${rname})/bin/\"
+  install -m 0755 9gc \"\$(cwidir_${rname})/bin/\"
   popd &>/dev/null
 }
 "
