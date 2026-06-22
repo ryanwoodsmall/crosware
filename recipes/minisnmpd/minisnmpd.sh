ed2fa6e314d8dd8a204a5a9c1798b261879c45dc
rwoodsmall <rwoodsmall@gmail.com>
2026-06-22 15:52:48 +0000

    minisnmpd: update to 1.7, move to pkgconf, enable/require ethtool, workaround missing ethtool-conf.h file, limit build environment

--- recipes/minisnmpd/minisnmpd.sh
+++ recipes/minisnmpd/minisnmpd.sh
@@ -1,18 +1,34 @@
+#
+# XXX - ethtool support? no idea if it works
+#
 rname="minisnmpd"
-rver="1.6"
+rver="1.7"
 rdir="${rname//is/i-s}-${rver}"
 rfile="${rdir}.tar.gz"
 rurl="https://github.com/troglobit/${rdir%-*}/releases/download/v${rver}/${rfile}"
-rsha256="77bc704a4ed4fdc386e2ba2e972d9457564c84abef7e9af5de5a2a231e5a9efe"
-rreqs="make pkgconfig libconfuse"
+rsha256="bf119818276cd63e37d29d4c5e88f8cdf2975113bc9a2a39ee2b3a91f66de20a"
+rreqs="make pkgconf libconfuse ethtool"
 
 . "${cwrecipe}/common.sh"
 
+cwappendfunc \
+  "cwfetch_${rname}" \
+  "cwfetchcheck https://github.com/troglobit/mini-snmpd/raw/refs/tags/v\$(cwver_${rname})/ethtool-conf.h ${cwdl}/${rname}/ethtool-conf.h 3b3c0cf9fc7df375dbd0c949b40ed644413c7c448cb37dde206f2c77f0a20629"
+
+cwappendfunc "cwextract_${rname}" "install -m 0644 ${cwdl}/${rname}/ethtool-conf.h \$(cwbdir_${rname})/ethtool-conf.h"
+
 eval "
 function cwconfigure_${rname}() {
-  pushd \"${rbdir}\" &>/dev/null
+  pushd \"\$(cwbdir_${rname})\" &>/dev/null
   sed -i.ORIG s,sys/sysinfo,linux/sysinfo,g linux.c
-  ./configure ${cwconfigureprefix} --with-config --without-systemd
+  ./configure ${cwconfigureprefix} \
+    --enable-ethtool \
+    --with-config \
+    --without-systemd \
+      CPPFLAGS=\"-I${cwsw}/libconfuse/current/include\" \
+      LDFLAGS=\"-L${cwsw}/libconfuse/current/lib -static\" \
+      PKG_CONFIG=\"${cwsw}/pkgconf/current/bin/pkgconf\" \
+      PKG_CONFIG_{LIBDIR,PATH}=\"${cwsw}/libconfuse/current/lib/pkgconfig\"
   popd &>/dev/null
 }
 "
@@ -22,3 +38,5 @@ function cwgenprofd_${rname}() {
   echo 'append_path \"${rtdir}/current/sbin\"' > \"${rprof}\"
 }
 "
+
+# vim: set ft=bash:
