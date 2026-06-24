3863d4cb0cc1c5b30eb79496ce7185c8cc3e9446
rwoodsmall <rwoodsmall@gmail.com>
2026-06-24 04:11:51 +0000

    scripts/view-cloc-diff.sh: fix modeline issue, grep out CW_OLD_PATH and OLDPATH

--- scripts/view-cloc-diff.sh
+++ scripts/view-cloc-diff.sh
@@ -3,7 +3,9 @@
 # show the differnces between runs of ${cwtop}/scripts/crosware-cloc-source.sh
 #
 # XXX - you _really_ want diffutils diff here. busybox is slowww...
-# XXX - edit with 'vim -u NONE' or else the modeline injected will be processed and error
 #
+modeline="vim: ft=diff:"
 
-vim <(diff -Naur ${cwtop}/tmp/crosware.set.out{.old,} | egrep -v -- '^(-|\+| )(cw.*=\(|(LS_COLORS|PATH|PKG_CONFIG_(LIBDIR|PATH)|(C(|XX|PP)|LD)FLAGS)=)' ; echo '# vim: ft=diff:' )
+vim <(diff -Naur ${cwtop}/tmp/crosware.set.out{.old,} | egrep -v -- '^(-|\+| )(cw.*=\(|(LS_COLORS|(|CW_OLD_|OLD)PATH|PKG_CONFIG_(LIBDIR|PATH)|(C(|XX|PP)|LD)FLAGS)=)' ; echo "# ${modeline}" )
+
+# vim: set ft=bash:
