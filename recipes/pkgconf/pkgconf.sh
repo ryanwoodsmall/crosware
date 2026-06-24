c089f8581a90b27be8fa6d860ac4ed95d311613a
rwoodsmall <rwoodsmall@gmail.com>
2026-06-24 03:53:41 +0000

    pkgconfig: note on 2.9.90 breaking with muon, cleanups

--- recipes/pkgconf/pkgconf.sh
+++ recipes/pkgconf/pkgconf.sh
@@ -1,14 +1,22 @@
 #
 # XXX - opt-in only for now
-# XXX - moving to https://gitea.treehouse.systems/ariadne/pkgconf
+#
+# XXX - 2.9.90 breaks with muon
+#     rver="2.9.90"
+#     rurl="https://github.com/pkgconf/pkgconf/releases/download/${rdir}/${rfile}"
+#     rsha256="e87eef36b5b016f84f802b468435d2c14dd29c6bcba57c8668f2d68382c43480"
+#
+# XXX - add to configure
+#     --with-personality-dir
+#     --with-pkg-config-dir
+#     --with-system-includedir
+#     --with-system-libdir
 #
 rname="pkgconf"
 rver="2.5.1"
 rdir="${rname}-${rver}"
 rfile="${rdir}.tar.gz"
-#rurl="https://github.com/${rname}/${rname}/archive/refs/tags/${rfile}"
-#rurl="https://github.com/ryanwoodsmall/crosware-source-mirror/raw/master/${rname}/${rfile}"
-rurl="https://distfiles.dereferenced.org/${rname}/${rfile}"
+rurl="https://distfiles.dereferenced.org/pkgconf/${rfile}"
 rsha256="ab89d59810d9cad5dfcd508f25efab8ea0b1c8e7bad91c2b6351f13e6a5940d8"
 rreqs="bootstrapmake"
 
