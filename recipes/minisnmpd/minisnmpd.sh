89ef91e9f3c48e73f3283b9c6bc664f133512b09
rwoodsmall <rwoodsmall@gmail.com>
2026-07-07 00:47:24 +0000

    minisnmpd: update to 2.0

--- recipes/minisnmpd/minisnmpd.sh
+++ recipes/minisnmpd/minisnmpd.sh
@@ -2,20 +2,14 @@
 # XXX - ethtool support? no idea if it works
 #
 rname="minisnmpd"
-rver="1.7"
+rver="2.0"
 rdir="${rname//is/i-s}-${rver}"
 rfile="${rdir}.tar.gz"
-rurl="https://github.com/troglobit/${rdir%-*}/releases/download/v${rver}/${rfile}"
-rsha256="bf119818276cd63e37d29d4c5e88f8cdf2975113bc9a2a39ee2b3a91f66de20a"
+rurl="https://github.com/troglobit/mini-snmpd/releases/download/v${rver}/${rfile}"
+rsha256="851acf49a1a36356664af0a7a040fa31f75403eb26e03627eba188ee15d4854c"
 rreqs="make pkgconf libconfuse ethtool"
 
 . "${cwrecipe}/common.sh"
-
-cwappendfunc \
-  "cwfetch_${rname}" \
-  "cwfetchcheck https://github.com/troglobit/mini-snmpd/raw/refs/tags/v\$(cwver_${rname})/ethtool-conf.h ${cwdl}/${rname}/ethtool-conf.h 3b3c0cf9fc7df375dbd0c949b40ed644413c7c448cb37dde206f2c77f0a20629"
-
-cwappendfunc "cwextract_${rname}" "install -m 0644 ${cwdl}/${rname}/ethtool-conf.h \$(cwbdir_${rname})/ethtool-conf.h"
 
 eval "
 function cwconfigure_${rname}() {
