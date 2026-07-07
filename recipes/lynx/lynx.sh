9937345e8ac288dc300e7dced613d06db16f9acb
rwoodsmall <rwoodsmall@gmail.com>
2026-07-07 00:16:47 +0000

    lynx: update to 2.9.3

--- recipes/lynx/lynx.sh
+++ recipes/lynx/lynx.sh
@@ -3,11 +3,11 @@
 # XXX - netbsdcurses (non-slang) HAVE_DELSCREEN/delete_screen/stop_curses breakage
 #
 rname="lynx"
-rver="2.9.2"
+rver="2.9.3"
 rdir="${rname}${rver}"
 rfile="${rname}${rver}.tar.gz"
 rurl="https://invisible-mirror.net/archives/${rname}/tarballs/${rfile}"
-rsha256="99f8f28f860094c533100d1cedf058c27fb242ce25e991e2d5f30ece4457a3bf"
+rsha256="6e99e46980974a6d89eceefbb26ca8c7aa7702b78ecb5bad383b859af225d052"
 rreqs="ncurses openssl"
 
 . "${cwrecipe}/lynx/lynx.sh.common"
