6bd996b08fe6839e8a4eb93dae2005e7d6a2dd4a
ryanwoodsmall <rwoodsmall@gmail.com>
2026-08-25 21:34:27 +0000

    jgitsh7: update to 7.7.1.202607240634-r

--- recipes/jgitsh/jgitsh7.sh
+++ recipes/jgitsh/jgitsh7.sh
@@ -1,4 +1,4 @@
-rver="7.7.0.202606012155-r"
+rver="7.7.1.202607240634-r"
 rname="jgitsh${rver%%.*}"
-rsha256="3b04fec2ca98f90a2d6020c5eef2bf789baca37018a4a57e973dbc86c5472c6f"
+rsha256="e68a5a4783d50a2cb879dc48aae54f68184b44fed8335f5c40af4bc081b880df"
 . "${cwrecipe}/jgitsh/jgitsh.sh.common"
