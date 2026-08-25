f69aeeda20a28b197e04083281d3fb2dbff682e5
rwoodsmall <rwoodsmall@gmail.com>
2026-08-25 19:31:05 +0000

    lua55: update to 5.5.1

--- recipes/lua/lua55.sh
+++ recipes/lua/lua55.sh
@@ -1,7 +1,7 @@
-rver="5.5.0"
+rver="5.5.1"
 sv="${rver%.*}"
 sv="${sv//./}"
 rname="lua${sv}"
-rsha256="57ccc32bbbd005cab75bcc52444052535af691789dba2b9016d5c50640d68b3d"
+rsha256="1c4b4068d67061f2a2231ad2b5422e77acea1487ea9890f6320af614f4373dce"
 . "${cwrecipe}/${rname%${sv}}/${rname%${sv}}.sh.common"
 unset sv
