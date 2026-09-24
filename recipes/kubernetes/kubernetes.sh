d444bfcc047c13e297c5987befb8f9ef538ea81b
rwoodsmall <rwoodsmall@gmail.com>
2026-09-24 14:56:53 +0000

    kubernetes: update to 1.37.1

--- recipes/kubernetes/kubernetes.sh
+++ recipes/kubernetes/kubernetes.sh
@@ -9,7 +9,7 @@
 #   https://github.com/kubernetes/community/blob/master/contributors/devel/development.md
 #
 rname="kubernetes"
-rver="1.36.5"
+rver="1.37.1"
 rdir="${rname}-${rver}"
 rfile=""
 rreqs=""
